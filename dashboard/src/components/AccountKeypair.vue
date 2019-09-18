6a08535c9f12822143fcca06eeb1491cb2796a06
Matej Nemček <ybdaba@gmail.com>
2019-09-18 12:57:48 +0200

    selecting between polkadot,substrate,beachball

---
+++ dashboard/src/components/AccountKeypair.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="AccountKeypair">
+      <div>
+        <Identicon
+          :value="address"
+          :theme="theme"
+          :size="size"
+        />
+      </div>
+      <div>
+        {{meta}}
+      </div>
+      <div>
+        {{address}}
+      </div>
+      <div>
+        {{publicKey}}
+      </div>
+      <div>
+        {{type}}
+      </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Identicon from '@vue-polkadot/vue-identicon';
+
+@Component({
+  components: {
+    Identicon,
+  },
+})
+export default class Keypair extends Vue {
+  @Prop(String) public address!: string;
+  @Prop() public publicKey!: any;
+  @Prop(String) public type!: string;
+  @Prop({ default: 'no-meta'}) public meta!: string;
+  @Prop({ default: 'polkadot'}) public theme!: string;
+  @Prop({ default: 64 }) public size!: number;
+}
+</script>
--- dashboard/src/components/Accounts.vue
+++ dashboard/src/components/Accounts.vue
@@ -2,29 +2,35 @@
   <div class="Accounts">
     <button @click="loadKeyring()">Load testing accounts</button>
     <button @click="mapAccounts()">Map Accounts</button>
-    <AccountKeypair
-      :address="tmpAcc.alice"
-      meta="aliceX"
-      theme="polkadot"
-    />
-    <AccountKeypair
-      :address="tmpAcc.bob"
-      theme="substrate"
-    />
-    <AccountKeypair
-      :address="tmpAcc.charlie"
-      theme="beachball"
-    />
+    <select v-model="theme">
+      <option selected>polkadot</option>
+      <option>substrate</option>
+      <option>beachball</option>
+    </select>
+    <ul>
+      <li 
+        v-for="acc in keyringAccounts"
+        v-bind:key="acc.address"
+      >
+        <AccountKeypair
+          :address="acc.address"
+          :theme="theme"
+          :meta="acc.meta"
+          :publicKey="vueU8aToHex(acc.publicKey)"
+          :type="acc.type"
+        />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import Identicon from '@vue-polkadot/vue-identicon';
+import keyring from '@vue-polkadot/vue-keyring';
 import AccountKeypair from './AccountKeypair.vue';
 import { waitReady } from '@polkadot/wasm-crypto';
-
-import keyring from '@vue-polkadot/vue-keyring';
+import { u8aToHex } from '@polkadot/util';
 
 @Component({
   components: {
@@ -33,17 +39,21 @@ import keyring from '@vue-polkadot/vue-keyring';
   },
 })
 export default class Accounts extends Vue {
+  public keys: any = '';
   public tmpAcc: object = {
     alice: 'SXYSytZ7wxpQHbRo5FzUFA9wjTfWvTQgYzhVEybWRQvBrvMS',
     bob: 'SVyHhpNypEpW8awHeL2XVpEydcedTv1M4AZABcTqMRzx61Ja',
     charlie: 'SW1wmzX29Ei8mCLxB7crVUhGTkL1TDtCUwpVickwdqBUNunM',
    };
-
-  public keyringAccounts: object = [
-    { address: '', meta: { name: 'x_x'}, publicKey: '', type: 'x_x' },
+  public theme: string = 'polkadot';
+  public keyringAccounts: any = [
+    { address: '', meta: { name: ''}, publicKey: '', type: '' },
   ];
 
-  public keys: any = '';
+  public vueU8aToHex(publicKey: Uint8Array): string {
+    return u8aToHex(publicKey);
+  }
+
   public loadKeyring(): void {
     keyring.loadAll({
       ss58Format: 42, type: 'sr25519',
@@ -53,10 +63,12 @@ export default class Accounts extends Vue {
   }
   public mapAccounts(): void {
     this.keyringAccounts = keyring.getPairs();
+    // this.keyringAccounts = Object.keys(importAccounts).forEach(a.publicKey => u8aToHex(a.publicKey));
   }
 
   public async mountWasmCrypto(): Promise<void> {
     await waitReady();
+    console.log('loaded');
     // console.log(keyring.encodeAddress('0x6674a2958bf589aca9056d57b26f758c50d5aa95aa36dcfbb8659a8bdf7eef6d'));
   }
 
