f563fff659156aad973fd947d94ce34ff5eb2ee0
Matej Nemček <ybdaba@gmail.com>
2019-09-28 21:56:19 +0200

    working nodeInfo in explorer

--- dashboard/src/components/explorer/Explorer.vue
+++ dashboard/src/components/explorer/Explorer.vue
@@ -7,14 +7,21 @@
       <b-tab-item label="Block Details">
       </b-tab-item>
       <b-tab-item label="Node Info">
-        <!-- <b-button @click="NodeInfoData">NodeInfoData
-        </b-button> -->
-        Blocknumber {{nodeInfo.blockNumber}}
+        
+        
+        Total Peers {{nodeInfo.health.peers}}<br>
+        Syncing {{nodeInfo.health.isSyncing}}<br>
+        Our best 
+        <p>{{nodeInfo.blockNumber}}</p>
+        Peer best
+        <p v-if="bestPeer"
+        > {{bestPeer.bestNumber}}</p>
+        <!-- should be separate component -->
+        <!-- <p>Peers {{nodeInfo.peers}}</p> -->
       </b-tab-item>
     </b-tabs>
-    
-    <p>Recent Block {{conn.header.number}}</p>
-    <p>Genesis Hash {{api.genesisHash}}</p>
+    <!-- <p>Recent Block {{conn.header.number}}</p> -->
+    <!-- <p>Genesis Hash {{api.genesisHash}}</p> -->
     <!-- <p>RuntimeVersion {{api.runtimeVersion}}</p>
     <p>Library Info {{api.libraryInfo}}</p> -->
 
@@ -32,42 +39,75 @@ export default class Explorer extends Vue {
     {name: 'alex.unfrastructure', value: 'wss://alex.unfrastructure.io/public/ws'},
     {name: 'localhost:9444', value: 'ws://127.0.0.1:9944'}];
   public conn: any = { chain: '', nodeName: '', nodeVersion: '', header: {}};
-
-  public api: any = {
-    genesisHash: '',
-  };
+  public bestPeer: any = null;
+  public bestPeerBlock: any = null;
+  public api: any = null;
   public nodeInfo: any = {
     blockNumber: '', health: '', peers: '', extrinsics: '',
   };
 
-  // public async NodeInfoData() {
+  // public async NodeInfoData(api) {
   //   try {
   //     [this.nodeInfo.blockNumber, this.nodeInfo.health,
   //     this.nodeInfo.peers, this.nodeInfo.extrinsics] = await Promise.all([
-  //       this.api.derive.chain.bestNumber(),
-  //       this.api.rpc.system.health(),
-  //       this.api.rpc.system.peers(),
-  //       this.api.rpc.author.pendingExtrinsics(),
+  //       api.derive.chain.bestNumber(),
+  //       api.rpc.system.health(),
+  //       api.rpc.system.peers(),
+  //       api.rpc.author.pendingExtrinsics(),
   //     ]);
+
+  //     return {};
   //   } catch (error) {
   //     console.error(error);
   //     return;
   //   }
   // }
 
+  // public timerNodeInfo() {
+  //   const timer = window.setInterval(() => {
+  //     this.NodeInfoData();
+  //     }, 9000);
+  // }
+  @Watch('nodeInfo')
+  public sortBestPeerBlock() {
+    // // console.log(peers)
+    // if (this.nodeInfo.peers) {
+      this.bestPeer = this.nodeInfo.peers.sort((a, b): number => b.bestNumber.cmp(a.bestNumber))[0];
+      // this.bestPeerBlock = this.bestPeer.bestNumber
+    // }
+    // return;
+  }
+
+  // need make it in vue-api wrapper or vuex
   public async apiInit(): Promise<void> {
     const provider = new WsProvider('wss://poc3-rpc.polkadot.io/');
     // this line causes error Default via asOptional
-    // this.api = await ApiPromise.create({provider});
-    const api = await ApiRx.create({provider}).toPromise();
-    this.api.genesisHash = api.genesisHash;
-    api.rpc.chain.subscribeNewHeads().subscribe((header: any) => {
-      this.conn.header = header;
-    });
+    const api = await ApiPromise.create({provider});
+    // const api = await ApiRx.create({provider}).toPromise();
+
+    // this.api.genesisHash = api.genesisHash;
+    // api.rpc.chain.subscribeNewHeads().subscribe((header: any) => {
+    //   this.conn.header = header;
+    // });
+    try {
+      [this.nodeInfo.blockNumber, this.nodeInfo.health,
+      this.nodeInfo.peers, this.nodeInfo.extrinsics] = await Promise.all([
+        api.derive.chain.bestNumber(),
+        api.rpc.system.health(),
+        api.rpc.system.peers(),
+        api.rpc.author.pendingExtrinsics(),
+      ]);
+      this.sortBestPeerBlock();
+      // return {};
+    } catch (error) {
+      console.error(error);
+      return;
+    }
   }
 
   public mounted(): void {
     this.apiInit();
+    // this.timerNodeInfo();
   }
 }
 </script>
