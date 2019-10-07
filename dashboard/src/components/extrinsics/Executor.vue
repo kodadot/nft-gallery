3567e6fbeab2613a877be0d832fe1edb14eccc10
Viktor Valastin <viktorko99@gmail.com>
2019-10-08 00:02:26 +0200

    added method selector

--- dashboard/src/components/extrinsics/Executor.vue
+++ dashboard/src/components/extrinsics/Executor.vue
@@ -1,6 +1,18 @@
 <template>
-<div>
-  Executor Works 
+<div class="executor-select">
+  <b-field :label="label">
+    <b-select
+            v-model="selected"
+            :placeholder="label"
+            expanded
+    >
+      <option
+              v-for="method in methods"
+              v-bind:key="method"
+              :value="method"
+      >{{ method }} </option>
+    </b-select>
+  </b-field>
 </div>
 </template>
 
@@ -8,5 +20,18 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
-export default class Executor extends Vue { }
+export default class Executor extends Vue {
+  private label = 'submit the following extrinsic';
+  private selectedMethod = null;
+  @Prop() methods!: string[];
+
+  get selected() {
+    return this.selectedMethod;
+  }
+
+  set selected(value) {
+    this.$emit('selected', value);
+  }
+
+}
 </script>
--- dashboard/src/views/Extrinsics.vue
+++ dashboard/src/views/Extrinsics.vue
@@ -4,7 +4,10 @@
       <b-tab-item label="Extrinsic submission"></b-tab-item>
     </b-tabs>
     <Selection />
-    <Executor />
+    <div class="executor-wrapper">
+    <Executor :methods="sections" @selected="handleSectionSelection" />
+    <Executor :methods="methods" @selected="handleMethodSelection" />
+    </div>
   </div>
 </template>
 
@@ -12,7 +15,22 @@
 import Selection from '../components/extrinsics/Selection.vue';
 import Executor from '../components/extrinsics/Executor.vue';
 import { Prop, Vue, Component } from 'vue-property-decorator';
-import { VueConstructor } from 'vue';
+
+// import Connector from '@vue-polkadot/vue-api'
+//
+// const api = Connector.getInstance().api;
+//
+// if (api) {
+//   const defaultSection = Object.keys(api.tx)[0];
+// const defaultMethod = Object.keys(api.tx[defaultSection])[0];
+// const apiDefaultTx = api.tx[defaultSection][defaultMethod];
+// const apiDefaultTxSudo =
+//         (api.tx.system && api.tx.system.setCode) || // 2.x
+//         (api.tx.consensus && api.tx.consensus.setCode) || // 1.x
+//         apiDefaultTx; // other
+// }
+
+
 
 @Component({
   components: {
@@ -25,9 +43,27 @@ export default class Extrinsics extends Vue {
   private isValidUnsigned: boolean = false;
   private method = null;
   private extrinsic = null;
+  // private apiDefaultTxSudo = apiDefaultTxSudo;
+  // private methods =
+  private fnSection = '';
+  private fnMethod = '';
 
-  set extrinsicMethod(value: any) {
-    this.extrinsic = value;
+  get sections() {
+    return Object.keys((this as any).$http.api.tx);
+  }
+
+  get methods() {
+    return this.fnSection ? Object.keys((this as any).$http.api.tx[this.fnSection]) : [];
+  }
+
+  handleSectionSelection(value: string) {
+
+    this.fnSection = value
+  }
+
+  handleMethodSelection(value: string) {
+    console.log((this as any).$http.api.tx[this.fnSection][value].meta['args']);
+    this.fnMethod = value;
   }
 
   private getExtrinsic() {
@@ -35,9 +71,22 @@ export default class Extrinsics extends Vue {
     const { method } = this;
 
     if (method) {
+      // @ts-ignore: Method has always value
       const fn = api.findCall(method.callIndex);
-      this.extrinsicMethod(api.tx[fn.section][fn.method](...method.args));
+      // @ts-ignore: Method has always value
+      this.extrinsic = (api.tx[fn.section][fn.method](...method.args));
     }
   }
 }
 </script>
+
+<style>
+  .executor-wrapper {
+    display: flex;
+  }
+
+  .executor-wrapper > * {
+    flex: 1;
+  }
+
+</style>
