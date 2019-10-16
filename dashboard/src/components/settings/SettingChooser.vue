1b484ca6f10829e5c5fbceb8d6f6892033e3a3c2
Viktor Valastin <viktorko99@gmail.com>
2019-10-16 13:04:57 +0200

    added option to add setting

--- dashboard/src/components/settings/SettingChooser.vue
+++ dashboard/src/components/settings/SettingChooser.vue
@@ -1,31 +1,37 @@
 <template>
-  <b-field :label="label">
-    <b-select
-    :placeholder="label"
-    v-model="selected"
-    expanded
-    >
-      <option
-        v-for="option in options"
-        :value="option.value"
-        :key="option.value"
-      >{{ option.text }}</option>
-    </b-select>
-  </b-field>
+  <div class="setting-chooser-wrapper">
+    <b-field :label="label">
+      <b-select :placeholder="label" v-model="selected" expanded>
+        <option
+          v-for="option in options"
+          :value="option.value"
+          :key="option.value"
+        >{{ option.text }}</option>
+      </b-select>
+    </b-field>
+    <AddOption v-if="hasAdd" @add="handleAddedOption" :label="label" />
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import AddOption from './AddOption.vue';
 
-@Component
+@Component({
+  components: {
+    AddOption,
+  },
+})
 export default class SettingChooser extends Vue {
   @Prop() public label!: string;
   @Prop() public selector!: string;
   @Prop() public setter!: string;
   @Prop() public defaultValue!: string;
+  @Prop() public addOption!: string;
+  @Prop() public addMethod!: string;
 
   get options() {
-      return this.$store.getters[this.selector];
+    return this.$store.getters[this.selector];
   }
 
   get selected() {
@@ -36,5 +42,17 @@ export default class SettingChooser extends Vue {
     this.$store.dispatch(this.setter, value);
   }
 
+  get hasAdd() {
+    return this.addOption;
+  }
+
+  public handleAddedOption(nodeOption: any) {
+    this.$store.dispatch(this.addMethod, nodeOption);
+  }
 }
-</script>
+</script> 
+<style scoped>
+.setting-chooser-wrapper {
+  margin-top: 1em;
+}
+</style>
