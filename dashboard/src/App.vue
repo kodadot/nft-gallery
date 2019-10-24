99848d7a155e56bdede400c388f6f3937d5869da
Viktor Valastin <viktorko99@gmail.com>
2019-10-24 21:41:29 +0200

    thin sidebar

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,38 +1,39 @@
 <template>
-  <div id="dashboard">
-    <div class="friendly-view" >
-      <SidebarMenu class="happy-menu" />
-      <router-view id='routerview'/>
+  <div id="dashboard" class="columns">
+    <SidebarMenu />
+    <div class="column">
+      <router-view id="routerview" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import { waitReady } from '@polkadot/wasm-crypto';
-import keyring from '@vue-polkadot/vue-keyring';
-import SidebarMenu from './components/SidebarMenu.vue';
+import { Component, Vue } from "vue-property-decorator";
+import { waitReady } from "@polkadot/wasm-crypto";
+import keyring from "@vue-polkadot/vue-keyring";
+import SidebarMenu from "./components/SidebarMenu.vue";
 
 @Component({
   components: {
-    SidebarMenu,
-  },
+    SidebarMenu
+  }
 })
 export default class Dashboard extends Vue {
   public loadKeyring(): void {
     keyring.loadAll({
       ss58Format: 42,
-      type: 'sr25519',
-      isDevelopment: false });
+      type: "sr25519",
+      isDevelopment: false
+    });
   }
 
   public async mountWasmCrypto(): Promise<void> {
     await waitReady();
-    console.log('wasmCrypto loaded');
+    console.log("wasmCrypto loaded");
     this.loadKeyring();
-    console.log('keyring init');
-    this.$store.commit('keyringLoaded');
-    console.log('keyring loaded');
+    console.log("keyring init");
+    this.$store.commit("keyringLoaded");
+    console.log("keyring loaded");
   }
 
   public mounted(): void {
@@ -42,24 +43,24 @@ export default class Dashboard extends Vue {
 </script>
 
 <style>
-  #routerview {
-    padding: 1em;
-  }
+#routerview {
+  padding: 1em;
+}
 
-  .friendly-view {
-    display: flex;
-    flex-direction: row;
-  }
+.friendly-view {
+  display: flex;
+  flex-direction: row;
+}
 
-  .friendly-view > .happy-menu {
-    flex: 2
-  }
+.friendly-view > .happy-menu {
+  flex: 2;
+}
 
-  .friendly-view #routerview {
-    flex: 10;
-  }
+.friendly-view #routerview {
+  flex: 10;
+}
 
-  .friendly-view {
-    height: 100vh;
-  }
+.friendly-view {
+  height: 100vh;
+}
 </style>
