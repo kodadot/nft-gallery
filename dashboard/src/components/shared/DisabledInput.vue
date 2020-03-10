426d2335490de89d6d5e0a05af7a41f5fc82651d
Matej Nemček <ybdaba@gmail.com>
2020-03-10 11:03:15 +0100

    disabled inputs in summary, waiting for api fix

---
+++ dashboard/src/components/shared/DisabledInput.vue
@@ -0,0 +1,14 @@
+<template>
+    <b-field :label="label">
+      <b-input :value="value" disabled></b-input>
+    </b-field>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+
+@Component({})
+export default class DisabledInput extends Vue {
+  @Prop(String) public label!: any;
+  @Prop(String) public value!: any;
+}
+</script>
