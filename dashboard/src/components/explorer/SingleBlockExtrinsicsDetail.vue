c42a7e5509d600cf1f5b51c372caf51f95269985
Matej Nemček <ybdaba@gmail.com>
2020-04-17 20:04:54 +0200

    wip explorer: logs

--- dashboard/src/components/explorer/SingleBlockExtrinsicsDetail.vue
+++ dashboard/src/components/explorer/SingleBlockExtrinsicsDetail.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <!-- {{block}} -->
     <div v-if="block && block.block && block.block.extrinsics">
-      <!-- _
-      {{ decodedData }}
-      _ -->
       <div v-if="decodedData">
         <div v-for="e in decodedData">
           <CardExtrinsic
@@ -17,21 +13,13 @@
             item3header="lifetime"
             item3="immortal" />
         </div>
+        <br>
+        <!-- <div v-for="(v, n, i) in block.block.header.digest.logs">
+          <CardExtrinsic
+            :header="`${v}`"
+            :open="true" />
+        </div> -->
       </div>
-      <!-- <div v-for="ex in block.block.extrinsics"> -->
-      <!-- {{ex}} -->
-      <!-- <CardExtrinsic 
-        :header="findMetaCall(ex.method.callIndex)"
-        :content="ex.method.args"
-        
-        :open="true" /> -->
-      <!-- </div> -->
-      <!-- {{block.block.header.digest.logs}} -->
-      <!-- <div v-for="(l, i) in block.block.header.digest.logs" :key="l">
-        <CardEvents
-          :header="l"
-          content="aaa" />
-      </div> -->
     </div>
   </div>
 </template>
@@ -40,6 +28,7 @@ import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 import CardExtrinsic from '@/components/shared/CardExtrinsic.vue';
 import CardEvents from '@/components/shared/CardEvents.vue';
 import Connector from '@vue-polkadot/vue-api';
+import { Struct, Tuple, Raw, Vec, getTypeDef } from '@polkadot/types';
 
 @Component({
   components: {
@@ -50,6 +39,7 @@ import Connector from '@vue-polkadot/vue-api';
 export default class SingleBlockExtrinsicsDetail extends Vue {
   private block: any = '';
   private decodedData: any[] = [];
+  private decodedLogs: any[] = [];
   private subs: any[] = [];
   @Prop() public hash!: any;
   @Prop({default: false}) public open!: boolean;
@@ -62,11 +52,26 @@ export default class SingleBlockExtrinsicsDetail extends Vue {
 
   @Watch('block')
   private async decode(): Promise<void> {
-    const { api } = Connector.getInstance()
-    this.decodedData = [];
-    for (const e of this.block.block.extrinsics) {
-      const { meta, method, section } = await api.registry.findMetaCall(e.method.callIndex)
-      this.decodedData.push({e, meta, method, section})
+    if (this.block && this.block.block && this.block.block.extrinsics) {
+      const { api } = Connector.getInstance()
+      this.decodedData = [];
+      for (const e of this.block.block.extrinsics) {
+        const { meta, method, section } = await api.registry.findMetaCall(e.method.callIndex)
+        this.decodedData.push({e, meta, method, section})
+      }
+    }
+  }
+
+  @Watch('block')
+  private async decodeLogs(): Promise<void> {
+    if(this.block && this.block.block 
+    && this.block.block.header 
+    && this.block.block.header.digest.logs) {
+      const logs = this.block.block.header.digest.logs
+      console.log('SingleBlockExtrinsicsDetail -> logs', logs);
+      
+      // const decode = Object.keys(this.block.block.header.digest.logs)
+      // console.log('SingleBlockExtrinsicsDetail -> decode', decode);
     }
   }
 
