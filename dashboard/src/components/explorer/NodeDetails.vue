383969b3b4c51f2c9f80470bc36c1f5d04eb4482
Matej Nemček <ybdaba@gmail.com>
2020-04-28 14:41:57 +0200

    wip nodedetails,sortable,testable

--- dashboard/src/components/explorer/NodeDetails.vue
+++ dashboard/src/components/explorer/NodeDetails.vue
@@ -9,8 +9,6 @@
 		<b-field label="Our Best Block">
 			<b-input :value="ourBestBlock.toString()" disabled></b-input>
 		</b-field>
-    <b-button @click="refreshPeers" icon-left="sync">Refresh</b-button>
-    <!-- {{ peersAdjusted }} -->
     <b-field grouped group-multiline>
       <div v-for="(column, index) in columns" 
         :key="index"
@@ -23,19 +21,16 @@
     <b-table 
       :data="peers"
       :striped="true"
-      default-sort="peers.roles">
+      default-sort-direction="desc"
+      default-sort="bestNumber">
       <template slot-scope="props">
-        <b-table-column field="peerId" label="peerID" sortable :visible="columns.peerId.visible">
-          {{ props.row.peerId }}
-        </b-table-column>
-        <b-table-column field="roles" label="roles" sortable :visible="columns.roles.visible">
-          {{ props.row.roles }}
-        </b-table-column>
-        <b-table-column field="bestNumber" label="b #" sortable :visible="columns.bestBlock.visible">
-          {{ props.row.bestNumber }}
-        </b-table-column>
-        <b-table-column field="bestHash" label="best hash" sortable :visible="columns.beshHash.visible">
-          {{ props.row.bestHash }}
+        <b-table-column v-for="(col, index) in columns"
+          :key="index"
+          :field="col.field"
+          :label="col.label"
+          :visible="col.visible"
+          sortable>
+          {{ props.row[col.field]}}
         </b-table-column>
       </template>
     </b-table>
@@ -46,12 +41,10 @@ import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 import Connector from '@vue-polkadot/vue-api';
 import Router from 'vue-router';
 import Card from '../shared/Card.vue';
-import Table from '@/components/shared/Table.vue';
 
 @Component({
   components: {
     Card,
-    Table,
   },
 })
 export default class NodeDetails extends Vue {  
@@ -60,14 +53,33 @@ export default class NodeDetails extends Vue {
   private peersAdjusted: any[] = [];
   private subs: any[] = [];
   private ourBestBlock: any = '';
-  private columns: any = {
-    peerId: { label: 'peerId', visible: true},
-    roles: { label: 'roles', visible: true},
-    bestBlock: { label: 'best block', visible: true},
-    beshHash: { label: 'best hash', visible: false},
-  }
-  
-  
+  private interval: any = '';
+  private columns: any = [
+    {
+      field: 'peerId',
+      label: 'peerId',
+      visible: true,
+      width: '10',
+    },
+    {
+      field: 'roles',
+      label: 'role',
+      width: '10',
+      visible: true
+    },
+    {
+      field: 'bestNumber',
+      label: 'best block',
+      visible: true,
+    },
+    {
+      field: 'bestHash',
+      label: 'best hash',
+      width: '10',
+      visible: false
+    }
+  ]
+    
   public async loadExternalInfo() {
     const { api } = Connector.getInstance();
     this.peers = await api.rpc.system.peers();
@@ -78,29 +90,30 @@ export default class NodeDetails extends Vue {
   public async refreshPeers() {
     const { api } = Connector.getInstance();
     this.peers = await api.rpc.system.peers();
-    const arr = Object.entries(this.peers)
+    console.log('refreshing peers')
+    // const arr = Object.entries(this.peers)
 
-    const a: any[] = []
-    for (const [key, value] of arr) {    
-      a.push([key, (value as any)])
-    }
+    // const a: any[] = []
+    // for (const [key, value] of arr) {    
+    //   a.push([key, (value as any)])
+    // }
 
-    const m = new Map(a)
+    // const m = new Map(a)
     
-    for (const o of m) {
-      this.peersAdjusted.push(o[1])
-    }
+    // for (const o of m) {
+    //   this.peersAdjusted.push(o[1])
+    // }
     // console.log('NodeDetails -> refreshPeers -> a', a);
-
-    
   }
 
   public async mounted(): Promise<void> {
     this.loadExternalInfo();
+    this.interval = setInterval(this.refreshPeers, 10000)
   }
 
   // Unsubscribe before destroying component
   public beforeDestroy() {
+    clearInterval(this.interval)
     this.subs.forEach((sub) => sub());
   }
 }
