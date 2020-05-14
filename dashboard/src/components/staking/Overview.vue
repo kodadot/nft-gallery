701ef1694a64a81fb05d395d594363960b608d06
Matej Nemček <ybdaba@gmail.com>
2020-05-14 17:48:22 +0200

    wip staking added debugging and debunking components for staker query

--- dashboard/src/components/staking/Overview.vue
+++ dashboard/src/components/staking/Overview.vue
@@ -32,13 +32,15 @@ import { Component, Prop, Vue, Watch, PropSync } from 'vue-property-decorator';
 import Connector from '@vue-polkadot/vue-api';
 import SummarySession from '@/components/explorer/SummarySession.vue'
 import DisabledInput from '@/components/shared/DisabledInput.vue';
-
 @Component({
   components: {
     SummarySession,
     DisabledInput,
   }
 })
+
+// await api.derive.accounts.info('G7eJUS1A7CdcRb2Y3zEDvfAJrM1QtacgG6mPD1RsPTJXxPQ')
+// await api.derive.staking.query('EtgM5E6pecndYpKCLkfYeRfKSmVxT1mVvaxceJFnhuKqTpG')
 export default class Overview extends Vue {
   private SummarySessionLoaded: boolean = false;
   private currentBlock: any = {};
---
+++ dashboard/src/components/staking/StakerInfo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>EtgM5E6pecndYpKCLkfYeRfKSmVxT1mVvaxceJFnhuKqTpG</p>
+    <b-input placeholder="address of staker" v-model="address"></b-input>
+    <b-button @click="fetch()">Fetch</b-button>
+    <br>
+    {{result}}
+  </div>
+</template>
+<script lang="ts" >
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+import Connector from '@vue-polkadot/vue-api';
+
+@Component({})
+export default class StakerInfo extends Vue {
+  private address: any= '';
+  private result: any = '';
+  @Prop() public value!: any;
+
+  private async fetch() {
+    const { api } = Connector.getInstance();
+    this.result = await api.derive.staking.query(this.address)
+  }
+}
+</script>
