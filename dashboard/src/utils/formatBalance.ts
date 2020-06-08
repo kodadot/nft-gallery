f643815cd46f076d4d8792c3ff51eca86ed9f8c9
Ryan Gordon <ryangordon210@gmail.com>
2020-06-08 23:08:06 +0100

    Change formatBalance impl
    Fixes cases like:
    9,280,531.601T Unit
    Becomes 9.280M
    
    Using substring to cut excess off and get the right  power
    
    Also fixed 000.000 now shows as 0.000

--- dashboard/src/utils/formatBalance.ts
+++ dashboard/src/utils/formatBalance.ts
@@ -2,17 +2,17 @@ import { formatBalance } from '@polkadot/util';
 
 const format = (value: string, currency: string, withSi?: boolean): string => {
     const M_LENGTH = 6 + 1;
-    const K_LENGTH = 3 + 1;
-
+    const K_LENGTH = 3 + 1; 
     const [prefix, postfix] = formatBalance(value, { forceUnit: '-', withSi: false }).split('.');
-
+    console.log(`${prefix}.${`000${postfix || ''}`.slice(-3)} ${currency}`);
+    
     const isShort = withSi && prefix.length >= K_LENGTH;
-
+  
     if (prefix.length > M_LENGTH) {
-      return `${formatBalance(value, { forceUnit: 'T', withSi })} ${currency}`;
+      return `${formatBalance(value.substring(0, value.length-12)).replace('Unit','')} ${currency}`;
     }
   
-  return `${value}${prefix}${!isShort && value}.000${''.slice(-3) } ${currency}`
+  return `${!isShort && value ? prefix : '0'}.000${''.slice(-3) } ${currency}`
 
   }
 
