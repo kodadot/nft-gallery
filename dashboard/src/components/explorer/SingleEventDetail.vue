52a85a9d29cf3647a1ff756bfeed7f70bb8b99e5
Matej Nemček <ybdaba@gmail.com>
2020-07-16 16:28:16 +0200

    wip recent events,showing identicon

--- dashboard/src/components/explorer/SingleEventDetail.vue
+++ dashboard/src/components/explorer/SingleEventDetail.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <CardRecentEvent 
-      :header="`🎟 ${record.blockNumber}-${record.index} - ${record.record.event.method}.${record.record.event.section}`"
+      :header="`🎟 ${record.blockNumber}-${record.index} - ${record.record.event.section}.${record.record.event.method}`"
       :content="`${record.record.event.meta.documentation[0]}`"
       :event="processedData"
     />
@@ -25,24 +25,21 @@ export default class SingleEventDetail extends Vue {
 
   private async mounted(): Promise<void> {
     this.sanitizedEvent = this.record && this.record
-    this.processRecord(this.record)
+    // this.processRecord(this.record)
   }
 
+  @Watch('record')
   private async processRecord(record: any) {
     // const { api } = Connector.getInstance();    
-    console.log('SingleEventDetail -> processRecord -> record', record.record);
-    console.log('SingleEventDetail -> processRecord -> X', record.record.event.typeDef);
+    // console.log('SingleEventDetail -> processRecord -> record', record.record);
+    // console.log('SingleEventDetail -> processRecord -> X', record.record.event.typeDef);
     const types = record.record.event.typeDef
     record.record.event.data.forEach((data: any, index: any) => {
-      
       // console.log('SingleEventDetail -> processRecord -> types[index].type', types[index].type);
       // this.processedData.push(types[index].type, data.toString())
       // const d = {'k': types[index].type, 'i': data.toString()}
       this.processedData.push([types[index].type, data])
-      
       // console.log('SingleEventDetail -> processRecord -> data.toString()', data.toString());
-      
-      console.log('SingleEventDetail -> processRecord -> typeof data', typeof this.processedData);
     });
     // console.log('SingleEventDetail -> processRecord -> record.record.data.toString()', record.record.data);
   }
