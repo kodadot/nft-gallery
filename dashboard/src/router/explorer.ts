347bed0f26b0ecce1d1ea395b3e71b9de1809e16
Matej Nemček <ybdaba@gmail.com>
2020-08-07 19:23:30 +0200

    rendering tab while click

--- dashboard/src/router/explorer.ts
+++ dashboard/src/router/explorer.ts
@@ -1,6 +1,7 @@
 import { apiEnabled } from '@/routeGuard';
 
 const Explorer = () => import(/* webpackChunkName:'explorer' */ '@/components/explorer/Explorer.vue');
+const ExplorerDetails = () => import(/* webpackChunkName:'explorer' */ '@/components/explorer/NodeDetails.vue');
 
 export default [
     {
@@ -20,5 +21,11 @@ export default [
         name: 'explorerByTabHash',
         component: Explorer,
         beforeEnter: apiEnabled,
+    },
+    {
+      path: '/explorer/peers',
+      name: 'explorerPeers',
+      component: ExplorerDetails,
+      beforeEnter: apiEnabled,
     },
 ]
