6630c79611489785b0a3145e1db5b64b2b5de626
Viktor Valastin <viktorko99@gmail.com>
2020-10-26 16:11:28 +0100

    formating

--- dashboard/src/components/toolbox/ConvertAddress.vue
+++ dashboard/src/components/toolbox/ConvertAddress.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
     <b-field label="Convert following address">
-      <b-input
-        v-model="address"
-      />
+      <b-input v-model="address" />
     </b-field>
     <DisabledInput label="hex input data" :value="isHexAddress" />
     <b-field label="ETH address in Substrate" grouped>
-      <b-input class="toolbox-covert__eth-wrapper" placeholder="Substrate address" :value="convertedAddress" disabled />
+      <b-input
+        class="toolbox-covert__eth-wrapper"
+        placeholder="Substrate address"
+        :value="convertedAddress"
+        disabled
+      />
       <p class="control">
         <b-button
           icon-left="copy"
@@ -23,9 +26,9 @@
 <script lang="ts" >
 import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 import DisabledInput from '@/components/shared/DisabledInput.vue';
-import { blake2AsU8a } from "@polkadot/util-crypto/blake2";
-import { encodeAddress } from "@polkadot/keyring";
-import { stringToU8a, u8aConcat, hexToU8a, isHex } from "@polkadot/util";
+import { blake2AsU8a } from '@polkadot/util-crypto/blake2';
+import { encodeAddress } from '@polkadot/keyring';
+import { stringToU8a, u8aConcat, hexToU8a, isHex } from '@polkadot/util';
 
 const components = {
   DisabledInput
@@ -38,27 +41,25 @@ export default class extends Vue {
 
   private mounted() {
     if (this.$route.params.data) {
-      this.address = this.$route.params.data
-      this.handleAddressWatch(this.$route.params.data)
+      this.address = this.$route.params.data;
+      this.handleAddressWatch(this.$route.params.data);
     }
   }
 
   @Watch('address')
   private handleAddressWatch(value: string) {
-    this.convertedAddress = isHex(value) ? this.convertAddress(value) : '';    
+    this.convertedAddress = isHex(value) ? this.convertAddress(value) : '';
   }
-  
+
   private convertAddress(input: string): string {
     const addr = hexToU8a(input);
-    const data = stringToU8a("evm:");
+    const data = stringToU8a('evm:');
     const res = blake2AsU8a(u8aConcat(data, addr));
     return encodeAddress(res);
   }
 
   get isHexAddress() {
-   return isHex(this.address)
-      ? 'Yes'
-      : 'No';
+    return isHex(this.address) ? 'Yes' : 'No';
   }
 }
 </script>
