2e37ff86260797d5adff9f17d1e3330ecbe4a907
Matej Nemček <ybdaba@gmail.com>
2021-01-13 12:15:19 +0100

    goddamnit this frontends :D

--- dashboard/vue.config.js
+++ dashboard/vue.config.js
@@ -8,5 +8,22 @@ module.exports = {
     port: 9090,
     hot: true,
     disableHostCheck: true,
-  }, 
+  },
+  
+  chainWebpack: config => {
+    // ...other chains
+    config.module // fixes https://github.com/graphql/graphql-js/issues/1272
+      .rule('mjs$')
+      .test(/\.mjs$/)
+      .include
+      .add(/node_modules/)
+      .end()
+      .type('javascript/auto');
+  },
+  configureWebpack: {
+    resolve: {
+      // .mjs needed for https://github.com/graphql/graphql-js/issues/1272
+      extensions: ['*', '.mjs', '.js', '.vue', '.json']
+    }
+  }
 }
