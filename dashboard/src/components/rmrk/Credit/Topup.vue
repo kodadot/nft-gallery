c2a3a5c102da1a5e4082460c2cfa66b8bd5cebe1
Matej Nemček <ybdaba@gmail.com>
2021-02-06 15:15:19 +0100

    added faq basic q

---
+++ dashboard/src/components/rmrk/Credit/Topup.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <b-input v-model="userAddress"></b-input>
+
+
+  </div>
+</template>
+
+<script lang="ts" >
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+
+@Component({})
+export default class Credit extends Vue {
+
+  public userAddress: string = '';
+}
+</script>
---
+++ dashboard/src/components/rmrk/Faq.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    
+    <div class="box">
+      <p class="title">
+        Frequently Asked Question
+      </p>
+      <div v-for="qa in faqQuestionsAnswers"
+        v-bind:key="qa[0]">
+        <p class="title">
+          {{qa[0]}}
+        </p>
+        <p class="subtitle">
+          {{qa[1]}}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" >
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+
+@Component({})
+export default class  extends Vue {
+
+  public faqQuestionsAnswers: any = [
+    ['What coin is used for NFTs?', 'Kusama, you can get it through our Credit feature, using fiat on-ramp.'],
+    ['What wallet do I need to mint, buy and collect?', 'Polkadot extension for now, you can find it in Chrome Store'],
+    ['How to mint my work?','We will have video demo for that'],
+    ['How I can trade KSM for my artwork?','Its supported widely on popular exhchanges, liqudity on exchanges is around $200M, plenty']
+    ];
+  @Prop() public value!: any;
+}
+</script>
