68f8dc7abe3aedc3a0f83e4bf059fa47c6c720f1
Matej Nemček <ybdaba@gmail.com>
2021-02-19 11:49:12 +0100

    i18n fixed for navbar, now it translates items

--- dashboard/src/i18n.ts
+++ dashboard/src/i18n.ts
@@ -17,7 +17,7 @@ function loadLocaleMessages (): LocaleMessages {
 }
 
 export default new VueI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || 'en',
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
+  locale: process.env.VUE_APP_I18N_LOCALE || 'cz',
+  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'cz',
   messages: loadLocaleMessages()
 })
