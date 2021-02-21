ca95626fde41b09433f988e1f9ef9ae2079a8871
Matej Nemček <ybdaba@gmail.com>
2021-02-21 15:15:36 +0100

    shaved gallery item into childs

--- dashboard/src/components/rmrk/Gallery/GalleryItem.vue
+++ dashboard/src/components/rmrk/Gallery/GalleryItem.vue
@@ -37,45 +37,7 @@
       </div>
 
       <div class="tile is-vertical is-parent">
-        <b-collapse class="card" animation="slide" 
-          aria-id="contentIdForA11y3" :open="false">
-          <template #trigger="props">
-            <div
-              class="card-header"
-              role="button"
-              aria-controls="contentIdForA11y3">
-              <p class="card-header-title is-size-1">
-                <span v-if="!isLoading">
-                  {{ nft.name }}
-                </span>
-                <b-skeleton height="100px" size="is-large" :active="isLoading"></b-skeleton>
-              </p>
-              <a class="card-header-icon">
-                <b-icon
-                  :icon="props.open ? 'chevron-up' : 'chevron-down'">
-                </b-icon>
-              </a>
-            </div>
-          </template>
-          <div class="card-content">
-            <div class="content">
-              <p class="title is-size-4">
-                {{ $t('collection') }}
-              </p>
-              <p class="subtitle is-size-6"> 
-                {{ nft.collection }}
-              </p>
-              <p class="title is-size-4">
-                {{ $t('owner') }}
-              </p>
-              <p class="subtitle is-size-7">
-                <a :href="`https://kusama.subscan.io/account/${nft.currentOwner}`" target="_blank">{{ nft.currentOwner }}</a>
-              </p>
-            </div>
-          </div>
-        </b-collapse>
-        <br>
-        
+        <Name :nft="nft" />
         <b-collapse class="card" animation="slide" 
           aria-id="contentIdForA11y3" :open="false">
           <template #trigger="props">
@@ -102,94 +64,8 @@
             </div>
           </div>
         </b-collapse>
-
-        <div class="card">
-          <footer class="card-footer">
-            <p class="card-footer-item">
-              <span>
-                <a :href="twitterUri"
-                    target="_blank">
-                  <b-icon 
-                    size="is-large"
-                    pack="fab" 
-                    icon="twitter">
-                  </b-icon>
-                </a>
-              </span>
-            </p>
-            <p class="card-footer-item">
-              <span>
-                <a :href="telegramUri"
-                    target="_blank">
-                  <b-icon 
-                    size="is-large"
-                    pack="fab" 
-                    icon="telegram">
-                  </b-icon>
-                </a>
-              </span>
-            </p>
-            <p class="card-footer-item">
-              <span>
-                <a :href="linemeUri"
-                    target="_blank">
-                  <b-icon 
-                    size="is-large"
-                    pack="fab" 
-                    icon="line">
-                  </b-icon>
-                </a>
-              </span>
-            </p>
-            <p class="card-footer-item">
-              <span>
-                <a 
-                  v-clipboard:copy="realworldFullPathShare"
-                  @click="toast('URL copied to clipboard')">
-                 <b-icon   
-                    size="is-medium"
-                    pack="fas" 
-                    icon="link">
-                  </b-icon>
-                </a>
-              </span>
-            </p>
-          </footer>
-        </div>
-      <br>
-
-        <b-collapse class="card" animation="slide" 
-          aria-id="contentIdForA11y3" :open="false">
-          <template #trigger="props">
-            <div
-              class="card-header"
-              role="button"
-              aria-controls="contentIdForA11y3">
-              <p class="card-header-title">
-                {{ $t('facts') }}
-              </p>
-              <a class="card-header-icon">
-                <b-icon
-                  :icon="props.open ? 'chevron-up' : 'chevron-down'">
-                </b-icon>
-              </a>
-            </div>
-          </template>
-
-          <div class="card-content">
-            <div class="content">
-              <p class="subtitle is-size-6">
-                <b>{{ $t('collection') }}:</b>{{ nft.collection }}
-              </p>
-              <p class="subtitle is-size-6">
-                <b>SN:</b>{{ nft.sn }}
-              </p>
-              <p class="subtitle is-size-6">
-                <b>{{ $t('instance') }}:</b>{{ nft.sn }}
-              </p>
-            </div>
-          </div>
-        </b-collapse>
+        <Sharing />
+        <Facts :nft="nft" :isLoading="isLoading" />
       </div>
     </div>
   </div>
@@ -205,6 +81,9 @@ import AccountSelect from '@/components/shared/AccountSelect.vue';
 import AvailableActions from './AvailableActions.vue';
 import { notificationTypes, showNotification } from '@/utils/notification';
 import Money from '@/components/shared/format/Money.vue';
+import Sharing from '@/components/rmrk/Gallery/Item/Sharing.vue';
+import Facts from '@/components/rmrk/Gallery/Item/Facts.vue';
+import Name from '@/components/rmrk/Gallery/Item/Name.vue';
 import Appreciation from './Appreciation.vue';
 // import MediaResolver from '../Media/MediaResolver.vue'
 import api from '@/fetch';
@@ -239,6 +118,9 @@ type NFTType =  NFTWithMeta;
     AccountSelect,
     AvailableActions,
     Money,
+    Sharing,
+    Facts,
+    Name,
     Appreciation: () => import('./Appreciation.vue'),
     MediaResolver: () => import('../Media/MediaResolver.vue'),
     PackSaver: () => import('../Pack/PackSaver.vue')
@@ -299,30 +181,6 @@ export default class GalleryItem extends Vue {
 
   public toast(message: string): void {
     this.$buefy.toast.open(message);
-  }
-
-  get helloText() {
-    return 'Check this cool NFT on %23KusamaNetwork %23kodadot';
-  }
-
-  get realworldFullPath() {
-    return `${window.location.origin}${this.$route.fullPath}`;
-  }
-
-  get realworldFullPathShare() {
-    return `${window.location.origin}${this.$route.fullPath}`;
-  }
-
-  get telegramUri() {
-    return `tg://msg_url?url=${this.realworldFullPath}&text=${this.helloText}`;
-  }
-
-  get twitterUri() {
-    return `https://twitter.com/intent/tweet?text=${this.helloText}&via=KodaDot&url=${this.realworldFullPath}`;
-  }
-
-  get linemeUri() {
-    return `https://lineit.line.me/share/ui?url=${this.realworldFullPath}&text=${this.helloText}`;
   }
 
   get nftId() {
---
+++ dashboard/src/components/rmrk/Gallery/Item/Facts.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <b-collapse class="card" animation="slide" 
+      aria-id="contentIdForA11y3" :open="false">
+      <template #trigger="props">
+        <div
+          class="card-header"
+          role="button"
+          aria-controls="contentIdForA11y3">
+          <p class="card-header-title">
+            {{ $t('facts') }}
+          </p>
+          <a class="card-header-icon">
+            <b-icon
+              :icon="props.open ? 'chevron-up' : 'chevron-down'">
+            </b-icon>
+          </a>
+        </div>
+      </template>
+
+      <div class="card-content">
+        <div class="content">
+          <p class="subtitle is-size-6">
+            <b>{{ $t('collection') }}:</b>{{ nft.collection }}
+          </p>
+          <p class="subtitle is-size-6">
+            <b>SN:</b>{{ nft.sn }}
+          </p>
+          <p class="subtitle is-size-6">
+            <b>{{ $t('instance') }}:</b>{{ nft.sn }}
+          </p>
+        </div>
+      </div>
+    </b-collapse>
+
+  </div>
+</template>
+
+<script lang="ts" >
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+
+@Component({})
+export default class Facts extends Vue {
+  @Prop() public nft!: any;
+}
+</script>
