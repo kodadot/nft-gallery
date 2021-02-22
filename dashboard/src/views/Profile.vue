cc19633922ee5d2cd196fd18285a082962fa7704
Viki Val <viktorko99@gmail.com>
2021-02-22 14:38:25 +0100

    Can fetch nfts for profile

--- dashboard/src/views/Profile.vue
+++ dashboard/src/views/Profile.vue
@@ -1,45 +1,62 @@
 <template>
-  <div>
-    Profile works!
+  <div class="profile-wrapper">
+    <div class="tile is-ancestor">
+      <div class="tile is-parent">
+        <div class="tile is-child box">
+          <p class="title">Profile {{ id }}</p>
+        </div>
+      </div>
+    </div>
+    <!-- <GalleryCardList :items="nfts" /> -->
+    <GalleryCardList :items="nfts" />
   </div>
 </template>
 
-<script lang="ts" >
+<script lang="ts">
 import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 import { getInstance } from '@/components/rmrk/service/RmrkService';
 import { notificationTypes, showNotification } from '@/utils/notification';
-import { Collection, CollectionWithMeta, NFTWithMeta, Pack } from '@/components/rmrk/service/scheme';
+import { fetchNFTMetadata, sanitizeIpfsUrl, defaultSortBy, fetchCollectionMetadata } from '@/components/rmrk/utils';
+import {
+  Collection,
+  CollectionWithMeta,
+  NFTWithMeta,
+  Pack
+} from '@/components/rmrk/service/scheme';
+import GalleryCardList from '@/components/rmrk/Gallery/GalleryCardList.vue'
 
-@Component({})
+const components = { GalleryCardList };
+
+@Component({ components })
 export default class Profile extends Vue {
   private id: string = '';
   private isLoading: boolean = false;
   private collections: CollectionWithMeta[] = [];
   private nfts: NFTWithMeta[] = [];
   private packs: Pack[] = [];
-  
+
   public async mounted() {
     this.checkId();
     const rmrkService = getInstance();
-     if (!rmrkService || !this.id) {
+    if (!rmrkService || !this.id) {
       return;
     }
 
     this.isLoading = true;
 
     try {
-      const nfts = await rmrkService.getNFTsForAccount(this.id);
-      const collections = await rmrkService.getCollectionListForAccount(this.id);
-      const packs = await rmrkService.getPackListForAccount(this.id);
+      // this.nfts = await rmrkService.getNFTsForAccount(this.id).then(defaultSortBy);
+      // this.collectionMeta();
+      // const collections = await rmrkService.getCollectionListForAccount(
+      //   this.id
+      // ).then(defaultSortBy);
+      const packs = await rmrkService.getPackListForAccount(this.id).then(defaultSortBy);
     } catch (e) {
       showNotification(`${e}`, notificationTypes.danger);
       console.warn(e);
     }
 
     this.isLoading = false;
-
-
-
   }
 
   public checkId() {
@@ -48,5 +65,15 @@ export default class Profile extends Vue {
     }
   }
 
+  collectionMeta() {
+    this.nfts.map(fetchNFTMetadata).forEach(async (call, index) => {
+      const res = await call;
+      Vue.set(this.nfts, index, {
+        ...this.nfts[index],
+        ...res,
+        image: sanitizeIpfsUrl(res.image || '')
+      });
+    });
+  }
 }
 </script>
