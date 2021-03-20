6bf5acae6ffc443fc07893ba07c39c549363569d
Jarek <jarek.krupinski@snow.dog>
2021-03-20 11:47:21 +0100

    Remove KusamaPrice component

--- dashboard/src/components/Navbar.vue
+++ dashboard/src/components/Navbar.vue
@@ -64,7 +64,6 @@
       </b-navbar-dropdown> -->
     </template>
     <template #end>
-      <kusama-price />
       <b-navbar-item>
         <div class="buttons">
           <b-field grouped>
@@ -92,14 +91,12 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import LocaleChanger from '@/components/shared/SwitchLocale.vue';
-import KusamaPrice from '@/components/KusamaPrice.vue';
 import { getCurrentColor } from '@/colors'
 import i18n from '@/i18n.ts';
 
 @Component({
   components: {
-    LocaleChanger,
-    KusamaPrice
+    LocaleChanger
   }
 })
 export default class NavbarMenu extends Vue {
