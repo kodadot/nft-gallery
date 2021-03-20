40fad821ca2a4aca1313588696118c13c8d93364
Jarek <jarek.krupinski@snow.dog>
2021-03-20 11:48:47 +0100

    Extend Money component, get fiat value

--- dashboard/src/components/shared/format/Money.vue
+++ dashboard/src/components/shared/format/Money.vue
@@ -1,20 +1,30 @@
 <template>
-  <div v-if="!inline">{{value | formatBalance(decimals, unit)}}</div>
-  <span v-else>{{value | formatBalance(decimals, unit)}}</span>
+  <div :class="['money', { 'money--inline': inline }]">
+    <span>
+      {{ value | formatBalance(decimals, unit) }}
+    </span>
+    <span v-if="fiatValue">
+      / {{ fiatValue | formatBalance(decimals, showFiatValue.toUpperCase()) }}
+    </span>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
-import { mapGetters } from 'vuex'
-
+import axios from 'axios'
 
 @Component
 export default class Money extends Vue {
-  @Prop({default: 0}) readonly value: number | string | undefined;
+  @Prop({default: 0}) readonly value: number | string | undefined
   @Prop(Boolean) readonly inline!: boolean
+  @Prop({default: ''}) readonly showFiatValue!: string
+
+  private readonly apiUrl: string = 'https://api.coingecko.com/api/v3'
+  private readonly coinId: string = 'kusama'
+  private fiatValue: number = 0
 
   get chainProperties() {
-    return this.$store.getters.getChainProperties;
+    return this.$store.getters.getChainProperties
   }
 
   get decimals(): number {
@@ -25,6 +35,33 @@ export default class Money extends Vue {
     return this.chainProperties.tokenSymbol
   }
 
-}
+  public created() {
+    if (this.showFiatValue) {
+      this.getFiatValue()
+    }
+  }
 
+  private async getFiatValue() {
+    try {
+      const { data } = await axios.get(`${this.apiUrl}/simple/price`, {
+        params: {
+          ids: this.coinId,
+          vs_currencies: this.showFiatValue
+        }
+      })
+
+      this.fiatValue = data[this.coinId][this.showFiatValue] * Number(this.value)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
 </script>
+
+<style lang="scss">
+  .money {
+    &--inline {
+      display: inline-block;
+    }
+  }
+</style>
