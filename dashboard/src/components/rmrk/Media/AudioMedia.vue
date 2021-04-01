1585efe752e991323f77848c9e66738c15b1a9bb
Viki Val <viktorko99@gmail.com>
2021-04-01 17:15:52 +0200

    Added waveform component to audio media

--- dashboard/src/components/rmrk/Media/AudioMedia.vue
+++ dashboard/src/components/rmrk/Media/AudioMedia.vue
@@ -1,10 +1,19 @@
 
 <template>
   <div>
-    <audio controls class="media-audio__player">
+    <av-waveform
+    class="media-audio__player"
+      :audio-src="src"
+      audio-class="media-audio__audio"
+      canv-class="media-audio__canvas"
+      played-line-color="rgba(0,0,0,0.74)"
+      noplayed-line-color="#d32e79"
+      playtime-font-color="rgba(0,0,0,0.74)"
+    ></av-waveform>
+    <!-- <audio controls class="media-audio__player">
       <source :src="src" :type="mimeType" />
       Unable to show audio
-    </audio>
+    </audio> -->
   </div>
 </template>
 
@@ -17,8 +26,10 @@ export default class AppAudio extends Vue {
 }
 </script>
 
-<style scoped>
-.media-audio__player {
-  width: 100%;
+<style>
+div.media-audio__player div > canvas.media-audio__canvas,
+div.media-audio__player > div > audio.media-audio__audio {
+  width: 100% !important;
 }
+
 </style>
