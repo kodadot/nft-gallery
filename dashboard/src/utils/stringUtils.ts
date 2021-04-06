54ff677ddd4e0fb223f48f675eb187a45f88a7ce
Rémi Ferrand <remi.ferrand@cc.in2p3.fr>
2021-04-06 10:18:34 +0200

    Fixes bad condition for string truncation

--- dashboard/src/utils/stringUtils.ts
+++ dashboard/src/utils/stringUtils.ts
@@ -1,6 +1,6 @@
 
 export const truncateStr = (s: string, maxLen : number = 20): string => {
-    if (s.length < maxLen) {
+    if (s.length <= maxLen) {
       return s;
     }
     return s.substring(0, maxLen) + "...";
