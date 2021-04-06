3948c43d776942d386f57a6d83624f0f8265ddd4
Rémi Ferrand <remi.ferrand@cc.in2p3.fr>
2021-04-06 17:13:15 +0200

    WIP

--- dashboard/src/components/rmrk/Faq.vue
+++ dashboard/src/components/rmrk/Faq.vue
@@ -20,34 +20,23 @@
 
 <script lang="ts" >
 import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
-import i18n from '@/i18n.ts';
+import i18n from '@/i18n';
+
+function i18nLoadQandA(): Array<any> {
+  let i: number;
+  let qa: Array<any> = [];
+  for (i = 1; i < 24; i++) {
+    qa.push([
+      i18n.t(`faq.q.${i}`),
+      i18n.t(`faq.a.${i}.m`),
+    ]);
+  }
+
+  return qa;
+}
 
 @Component({})
 export default class Faq extends Vue {
-  public faqQuestionsAnswers: any = [
-    [i18n.t('faq.q.1'), i18n.t('faq.a.1.m')],
-    [i18n.t('faq.q.2'), i18n.t('faq.a.2.m')],
-    [i18n.t('FAQQ3'), i18n.t('FAQA3')],
-    [i18n.t('FAQQ4'), i18n.t('FAQA4')],
-    [i18n.t('FAQQ5'), i18n.t('FAQA5')],
-    [i18n.t('FAQQ6'), i18n.t('FAQA6')],
-    [i18n.t('FAQQ7'), i18n.t('FAQA7')],
-    [i18n.t('FAQQ8'), i18n.t('FAQA8')],
-    [i18n.t('FAQQ9'), i18n.t('FAQA9')],
-    [i18n.t('FAQQ10'), i18n.t('FAQA10')],
-    [i18n.t('FAQQ11'), i18n.t('FAQA11')],
-    [i18n.t('FAQQ12'), i18n.t('FAQA12')],
-    [i18n.t('FAQQ13'), i18n.t('FAQA13')],
-    [i18n.t('FAQQ14'), i18n.t('FAQA14')],
-    [i18n.t('FAQQ15'), i18n.t('FAQA15')],
-    [i18n.t('FAQQ16'), i18n.t('FAQA16')],
-    [i18n.t('FAQQ17'), i18n.t('FAQA17')],
-    [i18n.t('FAQQ18'), i18n.t('FAQA18')],
-    [i18n.t('FAQQ19'), i18n.t('FAQA19')],
-    [i18n.t('FAQQ20'), i18n.t('FAQA20')],
-    [i18n.t('FAQQ21'), i18n.t('FAQA21')],
-    [i18n.t('FAQQ22'), i18n.t('FAQA22')],
-    [i18n.t('FAQQ23'), i18n.t('FAQA23')],
-  ];
+  public faqQuestionsAnswers: any = i18nLoadQandA();
 }
 </script>
--- dashboard/src/i18n.ts
+++ dashboard/src/i18n.ts
@@ -3,12 +3,25 @@ import VueI18n, { LocaleMessages } from 'vue-i18n'
 import store from '@/store'
 import MarkdownIt from 'markdown-it';
 
+// File containing data common to ALL
+// languages
+const allLangDataFile = "all_lang.json";
+
+// Data common to ALL languages
+const commonData = require('./locales/' + allLangDataFile);
+
 Vue.use(VueI18n)
-const md = new MarkdownIt('commonmark');
+const md = require('markdown-it')({
+  breaks: false,
+});
+
 function loadLocaleMessages (): LocaleMessages {
   const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
   const messages: LocaleMessages = {}
   locales.keys().forEach(key => {
+    if (key === allLangDataFile) {
+      return;
+    }
     const matched = key.match(/([A-Za-z0-9-_]+)\./i)
     if (matched && matched.length > 1) {
       const locale = matched[1]
@@ -21,10 +34,11 @@ function loadLocaleMessages (): LocaleMessages {
 // toto moze brat vue s vuexu teoretisch
 // const locale = store.getters.getUserLang;
 export default new VueI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || 'en',
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
+  locale: process.env.VUE_APP_I18N_LOCALE || 'nl',
+  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'nl',
   modifiers: {
-    md: (str) => md.render(str)
+    md: (str) => md.renderInline(str),
+    common: (str) => str.split('.').reduce((o,i)=>o[i], commonData),
   },
   messages: loadLocaleMessages()
 })
