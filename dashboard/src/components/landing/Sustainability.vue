b56d2d5ec912a5129c5531e962aa8a9b73161eab
Matej Nemček <ybdaba@gmail.com>
2021-04-07 10:58:05 +0200

    sustainability typos2

--- dashboard/src/components/landing/Sustainability.vue
+++ dashboard/src/components/landing/Sustainability.vue
@@ -14,15 +14,15 @@
               </h1>
               <p>
                 As part of our micro-dao-bedrock-boostrap experiment,
-                we would like to keep our efforts sustainable in the long-run
-                and create trust from creators culture and economy for KodaDot
+                we would like to keep our efforts sustainable <b>in the long-run
+                and create trust from creators culture</b> and economy for KodaDot
                 that we will won't dissapear tomorrow and keep running as long
                 we can cover our efforts to keep it online and working.
               </p><br>
               <p>
                 We will be slowly elaborating what is best possible way to embrace
-                Creators needs and help them to make sustainable living out of
-                their work and we are here to support it. On other part,
+                Creators economy needs and help them to make sustainable living out of
+                their work. <b>We are here to support the case</b>. On other part,
                 we need to mind our long-term sustainability.
                 We are in position being universal viewer and
                 not being landlord in form of platform as other galleries do.
@@ -46,22 +46,24 @@
                 Textile.io (we expecting this costs would go linear, later exponential).
                 Maintance, R&D costs need to be paid as
                 <a href="https://github.com/kodadot/nft-gallery/issues">our backlog of
-                new features is popping up</a> and we are trying invite more developers
-                to our circles by putting bounties on issues and anyone can help with our efforts.
+                new features is popping up</a> and we are <b>trying invite more developers
+                to our circles by putting bounties on issues</b>.
+                Anyone can join and help with our efforts.
               </p></br>
               <p>
                 As we would like to establish our accrual vector for sustainibility fund
-                and introduce small fee through minting through KodaDot,
+                and introduce <b>small fee through minting</b> through KodaDot,
                 we will be adding button where you still can voluntary opt-out
                 and mint without supporting us at all.
               </p><br>
               <p>
-                As start and giving community value back, we are trying to establish
-                <a href="https://hackmd.io/cy3jpAhvR-S32SUiMqEabg?edit">artists-support-program</a>
+                To <b>give community value back</b>, we are about to establish
+                <b><a href="https://hackmd.io/cy3jpAhvR-S32SUiMqEabg?edit">artists-support-program</a></b>
                 which will flow back to artists. We are trying to create self-reinforcment
                 accrual cycle, where you as creator supporting KodaDot gallery will flow back
                 and support new artists, thus creating network effects for early adopters and
-                next wave of adopters and so which will later could be self-propelled at some point.
+                next wave of adopters and so on, which will later could be self-propelled at some point
+                if we manage to crack on the right tokenomics.
                 We just need play bit more with right flow curves. It's experiment.
               </p><br>
               <p>
