26a0dafda63938a0999c290fd1890261c2b89304
Viki Val <viktorko99@gmail.com>
2021-04-09 12:01:50 +0200

    rest is now hidden in button

--- dashboard/src/components/rmrk/Gallery/Search/SearchBar.vue
+++ dashboard/src/components/rmrk/Gallery/Search/SearchBar.vue
@@ -2,20 +2,35 @@
   <div class="card mb-3 mt-5">
     <div class="card-content">
       <b-field >
-        <b-input expanded placeholder="Search..." type="search" v-model="searchQuery" icon="search">
+        <b-input
+          placeholder="Search..."
+          type="search"
+          v-model="searchQuery"
+          icon="search"
+        >
         </b-input>
       </b-field>
-      <Sort @input="updateSortBy" />
-      <TypeTagInput v-model="typeQuery" />
+      <b-collapse :open="false" aria-id="contentIdForA11y1">
+        <template #trigger>
+          <b-button
+            label="Sort & Filter"
+            aria-controls="contentIdForA11y1"
+            icon-right="caret-down"
+          />
+        </template>
+        <div class="mt-3">
+        <Sort @input="updateSortBy" />
+        <TypeTagInput v-model="typeQuery" />
+        </div>
+      </b-collapse>
     </div>
   </div>
 </template>
 
 <script lang="ts" >
-import { Component, Prop, Vue, Emit,  } from 'vue-property-decorator';
+import { Component, Prop, Vue, Emit } from 'vue-property-decorator';
 import { Debounce } from 'vue-debounce-decorator';
-import { SearchQuery, SortBy } from './types'
-
+import { SearchQuery, SortBy } from './types';
 
 @Component({
   components: {
@@ -29,58 +44,56 @@ export default class SearchBar extends Vue {
   @Prop() public sortBy!: SortBy;
 
   public mounted() {
-    if (this.$route.query.search && typeof this.$route.query.search === 'string') {
-      this.updateSearch(this.$route.query.search)
+    if (
+      this.$route.query.search &&
+      typeof this.$route.query.search === 'string'
+    ) {
+      this.updateSearch(this.$route.query.search);
     }
   }
 
-
   get searchQuery() {
-    return this.search
+    return this.search;
   }
 
   set searchQuery(value: string) {
     // this.replaceUrl(value)
-    this.updateSearch(value)
+    this.updateSearch(value);
   }
 
   get typeQuery() {
-    return this.type
+    return this.type;
   }
 
   set typeQuery(value: string) {
-    this.updateType(value)
+    this.updateType(value);
   }
-
 
   @Emit('update:type')
   @Debounce(400)
   updateType(value: string) {
-    return value
+    return value;
   }
-
 
   @Emit('update:sortBy')
   @Debounce(400)
   updateSortBy(value: SortBy) {
-    console.log('Debounced', value)
-    return value
+    console.log('Debounced', value);
+    return value;
   }
-
 
   @Emit('update:search')
   @Debounce(400)
   updateSearch(value: string) {
-    console.log('Debounced', value)
-    return value
+    console.log('Debounced', value);
+    return value;
   }
 
   @Debounce(400)
   replaceUrl(value: string) {
     if (this.search !== value) {
-      this.$router.replace({ name: 'nft', query: {search: value} })
+      this.$router.replace({ name: 'nft', query: { search: value } });
     }
   }
-
 }
 </script>
