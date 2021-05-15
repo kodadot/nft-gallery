ac4bdd7911ba751fd81c3eb1b04e0a7c39972dbc
Matej Nemček <ybdaba@gmail.com>
2021-05-15 11:53:33 +0200

    ah, mayb bc it isnt deployed on production branch, now it make sense

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -28,13 +28,13 @@ import coingecko from '@/coingecko'
       { property: 'og:url', content: 'https://nft.kodadot.xyz'},
       { property: 'og:description', content: ' KodaDot | Kusama NFT Market Explorer' },
       { property: 'og:site_name', content: ' KodaDot | Kusama NFT Market Explorer'},
-      { property: 'og:image', content: 'https://kodadot.xyz/kodadot_community.jpeg'},
+      { property: 'og:image', content: '//kodadot_community.jpeg'},
       { property: 'og:locale', content: 'en_US'},
       { property: 'twitter:card', content: 'summary_large_image' },
       { property: 'twitter:site', content: '@KodaDot' },
       { property: 'twitter:title', content: 'KodaDot | Kusama NFT Market Explorer' },
       { property: 'twitter:description', content: 'KodaDot | Kusama NFT Market Explorer' },
-      // { property: 'twitter:image', content: (this.nft.image as string) },
+      { property: 'twitter:image', content: '//kodadot_community.jpeg'},
       ]
     }
   },
