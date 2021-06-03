8f80f971601966b7d401bfbbd76323ecf2ab1042
Viki Val <viktorko99@gmail.com>
2021-06-03 18:29:28 +0200

    hard code nft

--- dashboard/src/subquery.ts
+++ dashboard/src/subquery.ts
@@ -3,8 +3,8 @@ import ApolloClient from 'apollo-boost'
 const apolloClient = new ApolloClient({
   // You should use an absolute URL here
   // uri: 'http://localhost:3000'
-  // uri: 'https://api.subquery.network/sq/vikiival/magick'
-  uri: process.env.SUBQUERY_URL
+  uri: 'https://api.subquery.network/sq/vikiival/magick'
+  // uri: process.env.SUBQUERY_URL
 })
 
 export default apolloClient
