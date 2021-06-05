46494d2ace8b35da06ebb1ba82e357b8cdb9233e
Jarek <jarek.krupinski@snow.dog>
2021-06-05 14:04:39 +0200

    Add leaderboard base view

--- dashboard/src/components/landing/Leaderboard.vue
+++ dashboard/src/components/landing/Leaderboard.vue
@@ -5,7 +5,13 @@
         <div class="container">
           <div class="columns">
             <div class="column is-12">
-              Leaderboard
+              <li
+                v-for="c in creators"
+                :key="c">
+                  <router-link :to="{ name: 'profile', params: { id: c } }">
+                    <Identity :address="c" :inline="true" />
+                  </router-link>
+              </li>
             </div>
           </div>
         </div>
@@ -18,24 +24,21 @@ import { Component, Vue } from 'vue-property-decorator';
 
 
 @Component<Leaderboard>({
-  metaInfo() {
-    return {
-      title: 'KodaDOT cares about environmental impact',
-      titleTemplate: '%s | Low Carbon NFTs',
-      meta: [
-        { name: 'description', content: 'Creating Carbonless NFTs on Kusama' },
-        { property: 'og:title', content: 'KodaDOT cares about environmental impact'},
-        { property: 'og:image', content: 'https://nft.kodadot.xyz/kodadot_carbonless.jpg'},
-        { property: 'og:description', content: 'Creating Carbonless NFTs on Kusama'},
-        { property: 'twitter:title', content: 'KodaDOT cares about environmental impact'},
-        { property: 'twitter:description', content: 'Creating Carbonless NFTs on Kusama'},
-        { property: 'twitter:image', content: 'https://nft.kodadot.xyz/kodadot_carbonless.jpg'},
-      ]
-    }
-  },
-  components: {}
+  components: {
+    Identity: () => import('@/components/shared/format/Identity.vue')
+  }
 })
 export default class Leaderboard extends Vue {
-
+  public creators: any = [
+    'Dx6nVUy6f2znn4ZwNZ3TGbEyUz3FLbCRGQGKAut4LxjCVRs',
+    'J6mwrrDE5ZywDe8T4mWgsdgHCcrDcrVtoGh37i7PSaZARRs',
+    'FqCJeGcPidYSsvvmT17fHVaYdE2nXMYgPsBn3CP9gugvZR5',
+    'DmUVjSi8id22vcH26btyVsVq39p8EVPiepdBEYhzoLL8Qby',
+    'CxQJNQeowRUJBgUHQpspudWy4BU6Rk2u93yDZEKe6GzcLDe',
+    'GHuTwhh9txr69tHBM2f1mPE6tWn9XCYKMi3nRNFrYvGv9Yy',
+    'CuHWHNcBt3ASMVSJmcJyiBWGxxiWLyjYoYbGjfhL4ovoeSd',
+    // 'HtRTwHSP6fYC5PtCsJ7pG4H1hwyPhzXbtVTTVRJ6kvfPFe1', did not set identity
+    // 'Cu7QaEnRGPE91WvLduzUii2ZNa3jhMWtmB8SYwumycNRmoN' did not set identity
+  ]
 }
 </script>
