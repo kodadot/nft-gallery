76e73da688783d8107ff2fce25e0571145e9a8eb
Jarek <jarek.krupinski@snow.dog>
2021-06-05 14:40:20 +0200

    Faq styles

--- dashboard/src/components/rmrk/Faq.vue
+++ dashboard/src/components/rmrk/Faq.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="box container">
-    <p class="title">
-      {{ $t('Frequently Asked Question') }}
+    <h2 class="heading text-bold uppercase mb-6">
+      <span class="text__stroked heading heading-is-6">
+        FAQ
+      </span>
+    </h2>
+    <p class="faq__box heading heading-is-2 text-bold inline-flex mb-6">
+      No time for reading? <br>
+      No problem. Check out <br>
+      <router-link :to="{name: 'tutorials'}">
+        Our tutorials.
+      </router-link>
     </p>
     <div
       v-for="n in 23"
       :key="n">
-      <p class="title">
+      <h3 class="title heading heading-is-2 max-w-600">
         <span v-html="$t(`FAQQ${n}`)"></span>
-      </p>
+      </h3>
       <p class="subtitle">
         <span v-html="$t(`FAQA${n}`)"></span>
       </p>
@@ -23,3 +32,14 @@ import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 @Component({})
 export default class Faq extends Vue {}
 </script>
+
+
+<style lang="scss">
+  @import '@/styles/variables';
+  .faq {
+    &__box {
+      padding: 16px 32px;
+      border: 2px solid $primary;
+    }
+  }
+</style>
