5f91544699b468f70cce5d3144f4c50c078d8638
Viki Val <viktorko99@gmail.com>
2021-06-11 14:25:44 +0200

    199 post should work

--- dashboard/src/components/subsocial/CreatePost.vue
+++ dashboard/src/components/subsocial/CreatePost.vue
@@ -1,15 +1,25 @@
 <template>
-  <PostButton @click="handlePost" />
+  <div>
+    <Loader v-model="isLoading" :status="status" />
+    <PostButton @click="handlePost" />
+  </div>
 </template>
 
 <script lang="ts" >
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue, Mixins } from 'vue-property-decorator';
 import { pinSubSocialPost } from '@/proxy';
 import { emptyObject } from '@/utils/empty';
 import { NFT, NFTMetadata } from '../rmrk/service/scheme';
 import { extractCid } from '@/utils/ipfs';
+import { SUBSOCIAL_KODA_SPACE, subsocialAddress } from './utils';
+import TransactionMixin from '@/utils/mixins/txMixin';
+import AuthMixin from '@/utils/mixins/authMixin';
+import { resolveSubsocialApi } from './api';
+import exec, { txCb, execResultValue } from '@/utils/transactionExecutor';
+import { showNotification, notificationTypes } from '@/utils/notification';
 
 const components = {
+  Loader: () => import('@/components/shared/Loader.vue'),
   PostButton: () => import('./PostButton.vue')
 };
 
@@ -19,22 +29,22 @@ type Post = {
   image: string;
   tags: string[];
   canonical: string;
-}
+};
 
 @Component({
   name: 'CreatePost',
   components
 })
-export default class CreatePost extends Vue {
+export default class CreatePost extends Mixins(TransactionMixin, AuthMixin) {
   @Prop({ default: () => emptyObject<NFT>() }) public nft!: NFT;
-  @Prop({ default: () => emptyObject<NFTMetadata>() }) public meta!: NFTMetadata;
-  @Prop({ default: 'https://nft.kodadot.xyz/rmrk/detail/' }) public url!: string;
+  @Prop({ default: () => emptyObject<NFTMetadata>() })
+  public meta!: NFTMetadata;
+  @Prop({ default: 'https://nft.kodadot.xyz/rmrk/detail/' })
+  public url!: string;
 
-
-  protected async handlePost() {
-    const { name, id } = this.nft
-    const { image, attributes } = this.meta
-
+  protected async buildParams() {
+    const { name, id } = this.nft;
+    const { image, attributes } = this.meta;
 
     const post: Post = {
       title: name,
@@ -42,25 +52,59 @@ export default class CreatePost extends Vue {
       body: this.forceHackNftUrl(),
       tags: attributes.map(({ value }) => String(value)),
       canonical: `${this.url}${id}`
-    }
+    };
 
     const newExtension = { RegularPost: null };
     const cid = await pinSubSocialPost(post);
 
+    const args = [SUBSOCIAL_KODA_SPACE, newExtension, { IPFS: cid }];
 
-    const args =  [
-      3417,
-      newExtension,
-      { IPFS: cid }
-    ];
+    return args;
+  }
 
-    console.log(args)
+  protected async handlePost() {
+    try {
+      const ss = await resolveSubsocialApi();
+      const args = await this.buildParams();
+      const api = await ss.substrate.api;
+      const address = subsocialAddress(this.accountId)
 
+      const isFollower = await ss.substrate.isSpaceFollower(address, SUBSOCIAL_KODA_SPACE as any)
+
+      const cb = isFollower ? api.tx.posts.createPost : api.tx.utility.batch;
+      const finalArgs = isFollower ? args : [[api.tx.spaceFollows.followSpace(SUBSOCIAL_KODA_SPACE), api.tx.posts.createPost(...args)]]
+      const tx = await exec(
+        subsocialAddress(this.accountId),
+        '',
+        cb as any,
+        finalArgs,
+        txCb(
+          async blockHash => {
+            execResultValue(tx);
+            showNotification(blockHash.toString(), notificationTypes.info);
+
+            showNotification(
+              `[SUBSOCIAL] ${this.nft.name}`,
+              notificationTypes.success
+            );
+            this.isLoading = false;
+          },
+          err => {
+            execResultValue(tx);
+            showNotification(`[ERR] ${err.hash}`, notificationTypes.danger);
+            this.isLoading = false;
+          },
+          res => this.resolveStatus(res.status)
+          )
+      );
+    } catch (e) {
+      showNotification(`[POST] ${e.message}`, notificationTypes.warn);
+    }
   }
 
   // Subsocial does not show source so far
   protected forceHackNftUrl() {
-    return `${this.meta.description}\n\n[View this NFT on KodaDot](${this.url})`
+    return `${this.meta.description}\n\n[View this NFT on KodaDot](${this.url})`;
   }
 }
 </script>
