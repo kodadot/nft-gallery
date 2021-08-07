49bd5ef87c6822fff1c59a8c236d8520f90cb25f
Christopher Saburo <christopher.saburo@outlook.com>
2021-08-07 22:35:40 +0900

    added Loader / Debounce component inside rmrk/search route page

--- src/components/rmrk/Search/Search.vue
+++ src/components/rmrk/Search/Search.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+		<Loader :value="isLoading" />
     <FilterBar @selectCategory="selectCategory" />
     <ResultBar :category="category" />
     <QueryBar @selectQuery="selectQuery" />
@@ -8,17 +9,27 @@
 
 <script lang="ts">
 import { Component, Vue, Prop, Watch, Emit } from "vue-property-decorator";
+import { Debounce } from "vue-debounce-decorator";
 
 const components = {
 	FilterBar: () => import("@/components/rmrk/Search/FilterBar.vue"),
 	ResultBar: () => import("@/components/rmrk/Search/ResultBar.vue"),
 	QueryBar: () => import("@/components/rmrk/Search/QueryBar.vue"),
+	Loader: () => import("@/components/shared/Loader.vue"),
 };
 
 @Component({ components })
 export default class Search extends Vue {
 	private category: string = "";
 	private query: string = "";
+
+	get isLoading() {
+		return false;
+	}
+
+	get results() {
+		return [];
+	}
 
 	@Emit("selectCategory")
 	private selectCategory(category: string) {
