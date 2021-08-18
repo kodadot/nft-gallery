24bf1cc7a9cb8ab29173226a3b3e21980a7ba716
Christopher Saburo <christopher.saburo@outlook.com>
2021-08-18 18:47:20 +0900

    revert

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -63,11 +63,6 @@
             {{ $t('Settings') }}
           </b-navbar-item>
       </b-navbar-dropdown>
-      <b-navbar-item
-        tag='router-link'
-        :to="{ name: 'tutorials'}">
-        {{ $t('Tutorial') }}
-      </b-navbar-item>
     </template>
     <template v-slot:end>
       <LocaleChanger />
