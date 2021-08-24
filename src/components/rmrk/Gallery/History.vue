ef81790777b3f0e949b8a2757b284df9f374f377
roiLeo <medina.leo42@gmail.com>
2021-08-24 12:00:12 +0200

    fix(gallery-item): table spacing

--- src/components/rmrk/Gallery/History.vue
+++ src/components/rmrk/Gallery/History.vue
@@ -3,7 +3,7 @@
     <p class="label">
       {{ $t('History') }}
     </p>
-    <b-table :data="data" hoverable>
+    <b-table :data="data" class="mb-4" hoverable>
       <b-table-column
         cell-class="short-identity__table"
         field="Type"
