144a5e652abb9e7d01845ade89e977853ef3db70
roiLeo <medina.leo42@gmail.com>
2021-09-14 16:53:20 +0200

    feat(collection): fix deepscan issue

--- src/components/rmrk/Gallery/CollectionActivity.vue
+++ src/components/rmrk/Gallery/CollectionActivity.vue
@@ -42,7 +42,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue, Watch } from 'vue-property-decorator'
+import { Component, Prop, Vue } from 'vue-property-decorator'
 import { NFT } from '@/components/rmrk/service/scheme'
 
 const components = {
