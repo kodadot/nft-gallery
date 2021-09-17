a36283484470f1cea1b444ca09eb8ac34709ee62
Viki Val <viktorko99@gmail.com>
2021-09-17 15:22:13 +0200

    Closing #575

---
+++ src/components/rmrk/Create/AttributeInput.vue
@@ -0,0 +1,45 @@
+<template>
+  <b-field grouped>
+    <p class="control">
+      <b-button @click="remove" icon-left="times" />
+    </p>
+    <b-field label="Type" expanded>
+      <b-input
+        placeholder="Background"
+        v-model="vKey"
+        expanded
+        spellcheck="true"
+      ></b-input>
+    </b-field>
+    <b-field label="Name" expanded>
+      <b-input
+        placeholder="Dark"
+        v-model="vValue"
+        expanded
+        spellcheck="true"
+      ></b-input>
+    </b-field>
+  </b-field>
+</template>
+
+<script lang="ts" >
+import {
+  Component,
+  Vue,
+  PropSync,
+  Prop,
+  Emit
+} from 'vue-property-decorator';
+
+@Component
+export default class AttributeInput extends Vue {
+  @Prop(Number) index!: number;
+  @PropSync('trait_type', { type: String }) vKey!: string;
+  @PropSync('value', { type: String }) vValue!: string;
+
+  @Emit('remove')
+  protected remove() {
+    return this.index;
+  }
+}
+</script>
