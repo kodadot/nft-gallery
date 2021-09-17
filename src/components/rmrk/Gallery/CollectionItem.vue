e295774245f6ce217e55d0d1ee25cfd09de69275
roiLeo <medina.leo42@gmail.com>
2021-09-17 13:49:43 +0200

    feature(collection): ui changes

--- src/components/rmrk/Gallery/CollectionItem.vue
+++ src/components/rmrk/Gallery/CollectionItem.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="pack-item-wrapper container">
-    <div class="columns">
-      <div class="column">
-        <h1 class="title">
-          Collection {{ name }}
+    <div class="columns is-centered">
+      <div class="column is-half has-text-centered">
+        <figure class="image container is-128x128">
+          <img class="is-rounded" :src="meta.image">
+        </figure>
+        <h1 class="title is-2 mt-2">
+          {{ name }}
         </h1>
       </div>
+    </div>
+
+    <div class="columns">
       <div class="column">
         <p class="subtitle">
           Creator <ProfileLink :address="issuer" :inline="true" :showTwitter="true"/>
@@ -22,6 +28,14 @@
     </div>
 
     <CollectionActivity :nfts="collection.nfts" />
+
+    <div class="columns is-centered">
+      <div class="column is-8 has-text-centered">
+        <p class="content">
+          {{meta.description}}
+        </p>
+      </div>
+    </div>
 
     <GalleryCardList :items="collection.nfts" />
 
