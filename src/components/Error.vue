5668fab62f17a36ec307e99d4a6ec468ba102ba2
Viki Val <viktorko99@gmail.com>
2021-09-22 00:40:04 +0200

    Just a cat with cables

---
+++ src/components/Error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="box container has-text-centered" id="Eror">
+    <p class="title">Indexer Error</p>
+    <p class="subtitle">Indexer is not working properly
+    <a target="_blank" rel="noopener noreferrer"  href="https://explorer.subquery.network/subquery/vikiival/magick">and you can check the status here</a></p>
+    <img  src="https://http.cat/500" alt="Internal error cat" />
+    <p class="subtitle">If you think this should't happen, report us by
+      <a target="_blank" rel="noopener noreferrer"  href="https://github.com/kodadot/nft-gallery/issues/new?assignees=&labels=bug&template=bug_report.md&title=">creating bug issue with steps to reproduce and screenshot.</a></p>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class FourZeroFour extends Vue {}
+</script>
