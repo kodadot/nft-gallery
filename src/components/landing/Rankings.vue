9e3e724304f0e39f7ea3f99bdb3309ed9bb1dc14
roiLeo <medina.leo42@gmail.com>
2021-09-26 11:31:19 +0200

    feat(rankings): refact rankings page

--- src/components/landing/Rankings.vue
+++ src/components/landing/Rankings.vue
@@ -13,7 +13,7 @@
 <script lang="ts" >
 import { Component, Vue } from 'vue-property-decorator';
 
-@Component<Leaderboard>({
+@Component<Rankings>({
   metaInfo() {
     return {
       meta: [
@@ -24,7 +24,7 @@ import { Component, Vue } from 'vue-property-decorator';
         { property: 'og:url', content: 'https://nft.kodadot.xyz' },
         {
           property: 'og:description',
-          content: 'Discover new artists based on the ranking'
+          content: 'Discover new artists based on rank'
         },
         {
           property: 'og:site_name',
@@ -36,11 +36,11 @@ import { Component, Vue } from 'vue-property-decorator';
         },
         {
           property: 'twitter:title',
-          content: 'NFT artist ranking'
+          content: 'NFT artist rank'
         },
         {
           property: 'twitter:description',
-          content: 'Discover new artists based on the ranking'
+          content: 'Discover new artists based on rank'
         },
         {
           property: 'twitter:image',
@@ -54,30 +54,5 @@ import { Component, Vue } from 'vue-property-decorator';
     RankingsTable: () => import('@/components/Rankings/RankingsTable.vue')
   }
 })
-export default class Leaderboard extends Vue {
-  public creators: any = [
-    'Dx6nVUy6f2znn4ZwNZ3TGbEyUz3FLbCRGQGKAut4LxjCVRs',
-    'J6mwrrDE5ZywDe8T4mWgsdgHCcrDcrVtoGh37i7PSaZARRs',
-    'FqCJeGcPidYSsvvmT17fHVaYdE2nXMYgPsBn3CP9gugvZR5',
-    'DmUVjSi8id22vcH26btyVsVq39p8EVPiepdBEYhzoLL8Qby',
-    'CxQJNQeowRUJBgUHQpspudWy4BU6Rk2u93yDZEKe6GzcLDe',
-    'GHuTwhh9txr69tHBM2f1mPE6tWn9XCYKMi3nRNFrYvGv9Yy',
-    'CuHWHNcBt3ASMVSJmcJyiBWGxxiWLyjYoYbGjfhL4ovoeSd',
-    'ESRGCgBvTnuWkjeQs9G5W5hhXa4XuquXHJMtkDVQFvNUAb5',
-    'H1bSKJxoxzxYRCdGQutVqFGeW7xU3AcN6vyEdZBU7Qb1rsZ',
-    'Ea34UVcfamiLCFSRXD1bc4HRbZzsoo8RsTfKj2cDtf8GUdZ',
-    'H4XB5xxo1YhDmBf3fVnobqDJPnEuF1m2GTctdkiXSPNchy5',
-    'HnccMvwD4zsrG2mdyyiZvNCvDgMPZKrMA4BGHkT17c3KfkP',
-    'EB1oqZ5MEnEwxhJ5DySCH3pyY55a2CUDfAbYKmLz2QcqWgx',
-    'ERyyXEbUDa5AEMqY8dM7Nrtt6WY4BEAb65y1nbCmgE3rtQi',
-    'FLPSoZhUwJwZvGwihQbrnF7aNYP62GaNKmDKDoiVZFe3TaQ',
-    'HRL2QctmfjgFPLCSaiDJThYShQy7V4KvJqLWCY6pga6AEnE',
-    'Gskn3eRyr3tDiKxhzU1MPG4ha8RFTBG9ft3C6Rrn3kGJMXn',
-    'EB1oqZ5MEnEwxhJ5DySCH3pyY55a2CUDfAbYKmLz2QcqWgx',
-    'Eki6rrJtqfF5RA7gshbe9twhL3umQRebqdWxRkUoLRtofHv',
-    'DFxLeonhy3wNSDPVh5uqyai31vsMp2rY7SSJiM7dK6WQSgn'
-    // 'HtRTwHSP6fYC5PtCsJ7pG4H1hwyPhzXbtVTTVRJ6kvfPFe1', did not set identity
-    // 'Cu7QaEnRGPE91WvLduzUii2ZNa3jhMWtmB8SYwumycNRmoN' did not set identity
-  ];
-}
+export default class Rankings extends Vue {}
 </script>
