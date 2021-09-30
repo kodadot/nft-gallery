296638cbb4788a5925c3da7cbdccc5ac02568273
Isa Noguchi <isanoguchi91@gmail.com>
2021-09-30 09:29:56 +0900

    Nuxt-migration: create initial nuxt project

---
+++ nuxt/layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="
+      is-flex
+      is-flex-direction-column
+      min-h-full
+    "
+  >
+    <Navbar v-if="isNavbarVisible" />
+    <main class="is-flex-grow-1 mt-6">
+      <router-view />
+    </main>
+    <!-- <Footer /> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' }
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ nuxt/pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="section">
+    <div class="columns is-mobile">
+      <card
+        title="Free"
+        icon="github"
+      >
+        Open source on <a href="https://github.com/buefy/buefy">
+          GitHub
+        </a>
+      </card>
+
+      <card
+        title="Responsive"
+        icon="cellphone-link"
+      >
+        <b class="has-text-grey">
+          Every
+        </b> component is responsive
+      </card>
+
+      <card
+        title="Modern"
+        icon="alert-decagram"
+      >
+        Built with <a href="https://vuejs.org/">
+          Vue.js
+        </a> and <a href="http://bulma.io/">
+          Bulma
+        </a>
+      </card>
+
+      <card
+        title="Lightweight"
+        icon="arrange-bring-to-front"
+      >
+        No other internal dependency
+      </card>
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    Card
+  }
+}
+</script>
