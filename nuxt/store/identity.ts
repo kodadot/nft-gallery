2633c62058e8eca3f99bbe2c30d02fd9dea611ba
Isa Noguchi <isanoguchi91@gmail.com>
2021-10-02 02:19:16 +0900

    Nuxt-migration: Store basic configuration

---
+++ nuxt/store/identity.ts
@@ -0,0 +1,80 @@
+import { emptyObject } from '@/utils/empty';
+import { Registration } from '@polkadot/types/interfaces/identity/types';
+import Connector from '@vue-polkadot/vue-api';
+import Vue from 'vue'
+
+
+export interface IdentityMap {
+  [address: string]: Registration
+}
+
+export interface Auth {
+  address: string;
+  source: 'keyring' | 'extension' | 'ledger'
+}
+
+export interface IdentityStruct {
+  identities: IdentityMap;
+  auth: Auth
+}
+
+export interface IdenityRequest {
+  address: string;
+  identity: Registration
+}
+
+const defaultState: IdentityStruct = {
+  identities: {},
+  auth: emptyObject<Auth>()
+};
+
+export const state = () => defaultState
+
+export const mutations = {
+  addIdentity(state: IdentityStruct, identityRequest: IdenityRequest) {
+    const { address, identity  } = identityRequest;
+    if (!state.identities[address]) {
+      Vue.set(state.identities, address, identity)
+    }
+  },
+  addAuth(state: IdentityStruct, authRequest: Auth) {
+    state.auth = {...authRequest}
+  }
+}
+
+export const actions = {
+  setIdentity({commit}: any, identityRequest: IdenityRequest) {
+    commit('addIdentity', identityRequest);
+  },
+  async fetchIdentity({dispatch}: any, address: string) {
+    const { api } = Connector.getInstance()
+    try {
+      const optionIdentity = await api?.isReady
+      .then((a) => a?.query.identity?.identityOf(address))
+      const identity = optionIdentity?.unwrapOr(null)
+      if (identity) {
+        dispatch('setIdentity', { address, identity })
+      }
+    } catch(e) {
+      console.error('[FETCH IDENTITY] Unable to get identity', e)
+    }
+  },
+  setAuth({commit}: any, authRequest: Auth) {
+    commit('addAuth', authRequest)
+  }
+}
+
+export const getters = {
+  availableIdentities(state: IdentityStruct): IdentityMap {
+    return state.identities
+  },
+  getIdentityFor(state: IdentityStruct): (address: string) => Registration | undefined {
+   return (address: string) => state.identities[address];
+ },
+ getAuth(state: IdentityStruct): Auth {
+   return state.auth
+ },
+ getAuthAddress(state: IdentityStruct): string {
+   return state.auth.address
+ }
+}
---
+++ nuxt/store/setting.ts
@@ -0,0 +1,9 @@
+import setting from '@vue-polkadot/vue-settings';
+
+export const state = () => setting.state
+
+export const actions = setting.actions
+
+export const mutations = setting.mutations
+
+export const getters = setting.getters
