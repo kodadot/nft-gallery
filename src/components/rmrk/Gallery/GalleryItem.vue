317a7db8d1cad5e7ffab56cc7bf03503cbd7b676
Akshaj <akshajb@gmail.com>
2021-10-11 19:19:00 +0530

    Deduplicate horizontal line at actions component #812

--- src/components/rmrk/Gallery/GalleryItem.vue
+++ src/components/rmrk/Gallery/GalleryItem.vue
@@ -66,7 +66,7 @@
         <div class="column is-3 is-offset-3" v-if="detailVisible">
 
           <b-skeleton :count="2" size="is-large" :active="isLoading"></b-skeleton>
-          <div class="price-block" v-if="hasPrice">
+          <div class="price-block mb-4" v-if="hasPrice">
             <div class="label">{{ $t('price') }}</div>
             <div class="price-block__container">
               <div class="price-block__original">{{ nft.price | formatBalance(12, 'KSM') }}</div>
@@ -100,8 +100,8 @@
               </div>
           </template>
 
-          <Sharing />
-          <br>
+          <Sharing class="mb-4" />
+
           <template v-if="detailVisible">
             <Facts :nft="nft" :meta="meta"  />
           </template>
