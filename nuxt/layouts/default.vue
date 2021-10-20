211c018c0dc3509dc00744c66b3879f45c743a71
Isa Noguchi <isanoguchi91@gmail.com>
2021-10-20 10:06:48 +0900

    Nuxt-migration: Move OnChainProperty from view to components

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -15,13 +15,13 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator';
-import { cryptoWaitReady } from '@polkadot/util-crypto';
-import keyring from '@polkadot/ui-keyring';
-import isShareMode from '@/utils/isShareMode';
-import correctFormat from '@/utils/ss58Format';
-import checkIndexer from '@/queries/checkIndexer.graphql';
-import coingecko from '@/coingecko';
+import { Component, Vue } from 'nuxt-property-decorator'
+import { cryptoWaitReady } from '@polkadot/util-crypto'
+import keyring from '@polkadot/ui-keyring'
+import isShareMode from '@/utils/isShareMode'
+import correctFormat from '@/utils/ss58Format'
+import checkIndexer from '@/queries/checkIndexer.graphql'
+import coingecko from '@/coingecko'
 
 @Component<Dashboard>({
   metaInfo() {
@@ -35,64 +35,64 @@ import coingecko from '@/coingecko';
         { property: 'twitter:card', content: 'summary_large_image' },
         { property: 'twitter:site', content: '@KodaDot' }
       ]
-    };
+    }
   },
 })
 export default class Dashboard extends Vue {
   get chainProperties() {
-    return this.$store.getters.getChainProperties;
+    return this.$store.getters.getChainProperties
   }
 
   get ss58Format(): number {
-    return this.chainProperties?.ss58Format;
+    return this.chainProperties?.ss58Format
   }
 
   public async loadKeyring(): Promise<void> {
-    const isDevelopment = process.env.VUE_APP_KEYRING === 'true';
+    const isDevelopment = process.env.VUE_APP_KEYRING === 'true'
     keyring.loadAll({
       ss58Format: correctFormat(this.ss58Format),
       type: 'sr25519',
       isDevelopment
-    });
+    })
   }
 
   public async mountWasmCrypto(): Promise<void> {
-    await cryptoWaitReady();
-    console.log('wasmCrypto loaded');
-    this.loadKeyring();
-    this.$store.commit('keyringLoaded');
+    await cryptoWaitReady()
+    console.log('wasmCrypto loaded')
+    this.loadKeyring()
+    this.$store.commit('keyringLoaded')
   }
 
   public async getKsmPrice(): Promise<void> {
     try {
-      const { data } = await coingecko.get(`/simple/price`, {
+      const { data } = await coingecko.get('/simple/price', {
         params: {
           ids: 'kusama',
           vs_currencies: 'usd'
         }
-      });
+      })
 
-      this.$store.dispatch('setFiatPrice', data);
+      this.$store.dispatch('setFiatPrice', data)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   }
 
   public mounted(): void {
-    this.mountWasmCrypto();
-    this.fetchIndexer();
-    this.getKsmPrice();
+    this.mountWasmCrypto()
+    this.fetchIndexer()
+    this.getKsmPrice()
   }
 
   private async fetchIndexer() {
     try {
-      const indexer = this.$nuxt.context.app.apolloProvider.defaultClient.query({
+      const indexer = this.$apollo.query({
         query: checkIndexer
-      });
+      })
 
       const {
         data: { _meta: data }
-      } = await indexer;
+      } = await indexer
 
       console.log(
         `
@@ -101,8 +101,8 @@ export default class Dashboard extends Vue {
     Last: ${new Date(Number(data?.lastProcessedTimestamp))}
     `,
         'background: #222; color: #bada55; padding: 0.3em'
-      );
-      this.$store.dispatch('upateIndexerStatus', data);
+      )
+      this.$store.dispatch('upateIndexerStatus', data)
     } catch (error) {
       const type = {
         indefinite: true,
@@ -116,15 +116,15 @@ export default class Dashboard extends Vue {
         `,
         type: 'is-danger',
         hasIcon: true
-      };
-      this.$buefy.snackbar.open(type as any);
+      }
+      this.$buefy.snackbar.open(type as any)
       // this.$router.push({ name: 'error' });
-      console.warn('Do something', error);
+      console.warn('Do something', error)
     }
   }
 
   get isNavbarVisible() {
-    return !isShareMode;
+    return !isShareMode
   }
 }
 </script>
