f6ad61eea8bb2595c43006d890ed508bd11fee07
Isa Noguchi <isanoguchi91@gmail.com>
2021-10-22 05:52:24 +0900

    Nuxt-migration: Fix duplicated bulma css import in NavbarProfileDropdown

--- nuxt/components/rmrk/Profile/NavbarProfileDropdown.vue
+++ nuxt/components/rmrk/Profile/NavbarProfileDropdown.vue
@@ -1,55 +1,117 @@
 <template>
-  <b-dropdown position="is-bottom-left" aria-role="menu">
+  <b-dropdown
+    position="is-bottom-left"
+    aria-role="menu"
+  >
     <template #trigger>
-      <span v-if="account" class="is-mobile is-vcentered navbar__avatar">
+      <span
+        v-if="account"
+        class="is-mobile is-vcentered navbar__avatar"
+      >
         <!-- <Avatar class="navbar__avatar-icon" :value="account" :size="34" /> -->
-        <b-button type="is-primary" class="navbar__button">
-          <Identity :address="account" :inline="true" class="navbar__address" />
+        <b-button
+          type="is-primary"
+          class="navbar__button"
+        >
+          <Identity
+            :address="account"
+            :inline="true"
+            class="navbar__address"
+          />
         </b-button>
 
         <!-- <span class="navbar__identity subtitle is-4 is-align-self-center"><Identity :address="account" :inline="true"/></span> -->
       </span>
       <template v-else>
-        <b-button type="is-primary" class="navbar__button ml-3 my-3" @click="checkExtension()">Log in</b-button>
+        <b-button
+          type="is-primary"
+          class="navbar__button ml-3 my-3"
+          @click="checkExtension()"
+        >
+          Log in
+        </b-button>
       </template>
     </template>
 
     <template v-if="account">
-      <b-dropdown-item has-link aria-role="menuitem">
+      <b-dropdown-item
+        has-link
+        aria-role="menuitem"
+      >
         <router-link :to="{ name: 'profile', params: { id: account } }">
           Profile
         </router-link>
       </b-dropdown-item>
-      <b-dropdown-item has-link aria-role="menuitem">
+      <b-dropdown-item
+        has-link
+        aria-role="menuitem"
+      >
         <router-link :to="{ name: 'rmrkCredit' }">
           {{ $t("Credit") }}
         </router-link>
       </b-dropdown-item>
-      <b-dropdown-item has-link aria-role="menuitem">
-        <router-link :to="{ name: 'rmrkFaq' }" >
+      <b-dropdown-item
+        has-link
+        aria-role="menuitem"
+      >
+        <router-link :to="{ name: 'rmrkFaq' }">
           F.A.Q.
         </router-link>
       </b-dropdown-item>
-      <hr class="dropdown-divider" aria-role="menuitem" />
-      <b-dropdown-item has-link aria-role="menuitem">
-        <a href="https://twitter.com/kodadot" target="_blank" rel="noopener noreferrer" class="is-flex is-align-items-center pl-3">
-          <b-icon pack="fab" icon="twitter" class="mr-1"> </b-icon>
+      <hr
+        class="dropdown-divider"
+        aria-role="menuitem"
+      >
+      <b-dropdown-item
+        has-link
+        aria-role="menuitem"
+      >
+        <a
+          href="https://twitter.com/kodadot"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="is-flex is-align-items-center pl-3"
+        >
+          <b-icon
+            pack="fab"
+            icon="twitter"
+            class="mr-1"
+          />
           <strong>KodaDot</strong>
         </a>
       </b-dropdown-item>
-      <b-dropdown-item has-link aria-role="menuitem">
-        <a href="https://discord.gg/u6ymnbz4PR" target="_blank" rel="noopener noreferrer" class="is-flex is-align-items-center pl-3">
-          <b-icon pack="fab" icon="discord" class="mr-1"> </b-icon>
+      <b-dropdown-item
+        has-link
+        aria-role="menuitem"
+      >
+        <a
+          href="https://discord.gg/u6ymnbz4PR"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="is-flex is-align-items-center pl-3"
+        >
+          <b-icon
+            pack="fab"
+            icon="discord"
+            class="mr-1"
+          />
           <strong>Discord</strong>
         </a>
       </b-dropdown-item>
-      <hr class="dropdown-divider" aria-role="menuitem" />
+      <hr
+        class="dropdown-divider"
+        aria-role="menuitem"
+      >
     </template>
-    <b-dropdown-item v-if="account" custom aria-role="menuitem">
+    <b-dropdown-item
+      v-if="account"
+      custom
+      aria-role="menuitem"
+    >
       <b-button
         type="is-primary"
-        @click="changeAccount = !changeAccount"
         expanded
+        @click="changeAccount = !changeAccount"
       >
         Change account
       </b-button>
@@ -60,24 +122,48 @@
       aria-role="menuitem"
     >
       <AccountSelect
-        :label="$i18n.t('Account')"
         v-model="account"
-        :tooltipVisible="false"
+        :label="$i18n.t('Account')"
+        :tooltip-visible="false"
       />
     </b-dropdown-item>
     <template v-if="isExtension && (changeAccount || !account)">
-      <b-dropdown-item has-link aria-role="menuitem">
-        <a href="https://polkadot.js.org/extension/" rel="noopener noreferrer" target="_blank" class="is-flex is-align-items-center pl-3">
+      <b-dropdown-item
+        has-link
+        aria-role="menuitem"
+      >
+        <a
+          href="https://polkadot.js.org/extension/"
+          rel="noopener noreferrer"
+          target="_blank"
+          class="is-flex is-align-items-center pl-3"
+        >
           Install Desktop Wallet Extension
         </a>
       </b-dropdown-item>
-      <b-dropdown-item has-link aria-role="menuitem">
-        <a href="https://chrome.google.com/webstore/detail/polkadot%7Bjs%7D-extension/mopnmbcafieddcagagdcbnhejhlodfdd" rel="noopener noreferrer" target="_blank" class="is-flex is-align-items-center pl-3">
+      <b-dropdown-item
+        has-link
+        aria-role="menuitem"
+      >
+        <a
+          href="https://chrome.google.com/webstore/detail/polkadot%7Bjs%7D-extension/mopnmbcafieddcagagdcbnhejhlodfdd"
+          rel="noopener noreferrer"
+          target="_blank"
+          class="is-flex is-align-items-center pl-3"
+        >
           Install Chrome Wallet Extension
         </a>
       </b-dropdown-item>
-      <b-dropdown-item has-link aria-role="menuitem">
-        <a href="https://addons.mozilla.org/en-US/firefox/addon/polkadot-js-extension/" rel="noopener noreferrer" target="_blank" class="is-flex is-align-items-center pl-3">
+      <b-dropdown-item
+        has-link
+        aria-role="menuitem"
+      >
+        <a
+          href="https://addons.mozilla.org/en-US/firefox/addon/polkadot-js-extension/"
+          rel="noopener noreferrer"
+          target="_blank"
+          class="is-flex is-align-items-center pl-3"
+        >
           Install Firefox Wallet Extension
         </a>
       </b-dropdown-item>
@@ -123,7 +209,7 @@ export default class NavbarProfileDropdown extends Vue {
 </script>
 
 <style lang="scss">
-@import "bulma/bulma.sass";
+@import "bulma/sass/utilities/mixins.sass";
 
 .navbar {
   &__identity {
