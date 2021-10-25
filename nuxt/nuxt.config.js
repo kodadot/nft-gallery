355cc94ce64d7ffdcc1552223c5bb83fc97d30d8
Isa Noguchi <isanoguchi91@gmail.com>
2021-10-26 00:29:40 +0900

    Nuxt-migration: Switch to SSR

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
