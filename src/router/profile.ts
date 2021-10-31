38885577e3c9cebf9de4d0d712ef65d14581f53e
Viki Val <viktorko99@gmail.com>
2021-10-31 11:09:00 +0100

    965 identity form

--- src/router/profile.ts
+++ src/router/profile.ts
@@ -1,9 +1,15 @@
 const Profile = () => import('@/views/Profile.vue')
+const Identity = () => import('@/components/rmrk/Profile/Identity.vue')
 
 export default [
   {
     path: '/rmrk/u/:id',
     name: 'profile',
     component: Profile,
-  }
+  },
+  {
+    path: '/identity',
+    name: 'identity',
+    component: Identity,
+  },
 ]
