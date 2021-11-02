4d99bf2ae3b7d6f99bd776f9b08f42f268228de2
TK-Development <timkern.lancer@gmail.com>
2021-11-02 09:30:18 +0900

    Mobile menu not opening/closing well #932

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
     fixed-top
     spaced
     wrapper-class="container"
-    :close-on-click="false"
+    :close-on-click="true"
     >
     <template #brand>
       <b-navbar-item tag="router-link" :to="{ path: '/' }" class="logo">
