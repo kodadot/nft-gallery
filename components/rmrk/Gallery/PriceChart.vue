637008586233eef44d9b0dfd55ec369a0f425da5
Rhaidzsal Ali <rhaidzsalali@gmail.com>
2021-11-12 16:41:48 +0800

    1072: chartjs integration

--- components/rmrk/Gallery/PriceChart.vue
+++ components/rmrk/Gallery/PriceChart.vue
@@ -3,153 +3,120 @@
     <p class="label">
       {{ $t('Price Chart') }}
     </p>
-    <div
-      id="chart"
-      ref="chart"
-      class="echart"
-    />
+    <div class="pt-2">
+      <canvas id="priceChart2" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop, Watch } from 'nuxt-property-decorator'
+import { Component, Vue, Prop, Watch } from 'nuxt-property-decorator';
 
-import * as ECharts from 'echarts/core'
-import { GridComponent } from 'echarts/components'
-import { LineChart } from 'echarts/charts'
-import { UniversalTransition } from 'echarts/features'
-import { CanvasRenderer } from 'echarts/renderers'
-import { TooltipComponent } from 'echarts/components'
-import { DataZoomComponent } from 'echarts/components'
+import Chart from 'chart.js/auto';
+import 'chartjs-adapter-luxon';
 
-ECharts.use([GridComponent, LineChart, CanvasRenderer, UniversalTransition, TooltipComponent, DataZoomComponent])
-
-// type EChartsOption = ECharts.ComposeOption<
-//   GridComponentOption | LineSeriesOption
-// >;
-
-
-const components = {
-  // chart: () => ECharts,
-}
+const components = {};
 
 @Component({ components })
 export default class PriceChart extends Vue {
   @Prop() public priceData!: any[];
-  // @Prop() public eventData!: Date[];
 
   protected chartOptionsLine: any = {};
-  protected Chart!: ECharts.ECharts;
-  // protected date: any = [];
-  // protected UTCDate: any = {};
+  protected Chart!: Chart;
 
   protected onWindowResize() {
     if (this.Chart) {
-      this.Chart.resize({ width: 'auto', height: 400 })
+      this.Chart.resize();
     }
   }
 
   public async created() {
-    window.addEventListener('resize', this.onWindowResize)
+    window.addEventListener('resize', this.onWindowResize);
   }
 
   public async mounted() {
-    // console.log(this.priceData)
-    this.priceChart()
+    this.priceChart();
   }
 
   protected priceChart() {
-    // this.createDate();
-    // console.log(document.documentElement.clientWidth);
-    this.Chart = ECharts.init(this.$refs.chart as HTMLElement)
-    this.Chart.setOption({
-      tooltip: {
-        trigger: 'item',
-        formatter: (params: { data: string[] }) => {
-          const date = this.parseDate(
-            params.data[0] as unknown as Date
-          )
-          const price = params.data[1] + ' KSM'
-          return '<center>' + date + '<br>' + price + '</center>'
+    if (this.priceData.length) {
+      const ctx = (
+        document?.getElementById('priceChart2') as HTMLCanvasElement
+      )?.getContext('2d')!;
+      const chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          datasets: [
+            {
+              label: 'Price',
+              data: this.priceData.map((data) => ({ x: data[0], y: data[1] })),
+              borderColor: '#d32e79',
+              tension: 0.3,
+              pointBackgroundColor: 'white',
+              pointBorderColor: 'blue',
+            },
+          ],
         },
-        backgroundColor: '#363636',
-        textStyle: {
-          color: '#fff',
-          fontFamily: 'Fira Code',
-        },
-      },
-      xAxis: {
-        type: 'time',
-        boundaryGap: false,
-        axisLabel: {
-          fontFamily: 'Fira Code',
-          color: '#fff',
-          hideOverlap: true,
-        },
-      },
-      yAxis: {
-        type: 'value',
-        axisLabel: {
-          formatter: '{value} KSM',
-          fontFamily: 'Fira Code',
-          color: '#fff',
-        },
-        splitLine: {
-          lineStyle: {
-            color: '#f8f8f8',
-            opacity: '.2'
-          }
-        },
-        nameTextStyle: {
-          color: '#fff',
-          fontFamily: 'Fira Code',
-        },
-      },
-      dataZoom: {
-        type: 'slider',
-        bottom: '2%',
-      },
-      series: [
-        {
-          name: 'priceHistory',
-          type: 'line',
-          smooth: 'true',
-          lineStyle: {
-            color: '#d32e79',
+        options: {
+          plugins: {
+            legend: {
+              labels: {
+                filter: function (item, chart) {
+                  return !item.text.includes('Price');
+                },
+              },
+            },
           },
-          data: this.priceData,
+          scales: {
+            x: {
+              type: 'time',
+              grid: {
+                drawOnChartArea: false,
+                drawTicks: false,
+                drawBorder: false,
+              },
+              ticks: {
+                callback: (value) => {
+                  return value;
+                },
+                major: {
+                  enabled: true,
+                },
+                maxRotation: 0,
+                minRotation: 0,
+                color: '#fff',
+              },
+            },
+            y: {
+              ticks: {
+                callback: (value) => {
+                  return `${Number(value).toFixed(2)} KSM`;
+                },
+                maxTicksLimit: 7,
+
+                color: '#fff',
+              },
+              grid: {
+                color: '#3a3a3a',
+              },
+            },
+          },
         },
-      ],
-    })
+      });
 
-    this.Chart.resize({ width: 'auto', height: 400 })
-  }
-
-  protected parseDate(date: Date) {
-    const utcDate: string = date.toUTCString()
-    return utcDate.substring(4)
-  }
-
-  protected formatDate(date: Date) {
-    const yyyy = date.getUTCFullYear()
-    const mm = this.padDigits(date.getUTCMonth() + 1)
-    const dd = this.padDigits(date.getUTCDate())
-    const hrs = this.padDigits(date.getUTCHours())
-    const mins = this.padDigits(date.getUTCMinutes())
-    const secs = this.padDigits(date.getUTCSeconds())
-    const YYYY_MM_DD_HRS_MINS_SECS =
-      yyyy + '/' + mm + '/' + dd + '\n' + hrs + ':' + mins + ':' + secs
-    return YYYY_MM_DD_HRS_MINS_SECS
-  }
-
-  protected padDigits(time: number) {
-    return time.toString().padStart(2, '0')
+      this.Chart = chart;
+    }
   }
 
   @Watch('priceData')
   async watchData(newPriceData: string[], oldPriceData: string[]) {
-    // console.log(this.priceData)
-    this.priceChart()
+    this.priceChart();
   }
 }
 </script>
+
+<style scoped>
+.pt-2 {
+  padding-top: 2rem;
+}
+</style>
