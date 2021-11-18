230abaf30666e462cf92d207372195d13fcd3278
Viki Val <viktorko99@gmail.com>
2021-11-18 13:32:03 +0100

    :zap: using prod version instead of test

--- src/subquery.ts
+++ src/subquery.ts
@@ -4,8 +4,7 @@ const apolloClient = new ApolloClient({
   // You should use an absolute URL here
   name: 'subquery',
   cache: new InMemoryCache(),
-  // uri: process.env.VUE_APP_SUBQUERY_URL || 'https://api.subquery.network/sq/vikiival/magick'
-  uri: 'https://api.subquery.network/sq/vikiival/magick-west'
+  uri: process.env.VUE_APP_SUBQUERY_URL || 'https://api.subquery.network/sq/vikiival/magick'
 })
 
 export default apolloClient
