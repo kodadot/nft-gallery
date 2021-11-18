d5c79a1fd3777416cdfcbef9f20f987cd11e8380
Viki Val <viktorko99@gmail.com>
2021-11-18 13:01:15 +0100

    :zap: added Inmemorry cache

--- src/subquery.ts
+++ src/subquery.ts
@@ -1,9 +1,11 @@
-import ApolloClient from 'apollo-boost'
+import ApolloClient, { InMemoryCache } from 'apollo-boost'
 
 const apolloClient = new ApolloClient({
   // You should use an absolute URL here
-  uri: process.env.VUE_APP_SUBQUERY_URL || 'https://api.subquery.network/sq/vikiival/magick'
-  // uri: 'https://api.subquery.network/sq/vikiival/magick'
+  name: 'subquery',
+  cache: new InMemoryCache(),
+  // uri: process.env.VUE_APP_SUBQUERY_URL || 'https://api.subquery.network/sq/vikiival/magick'
+  uri: 'https://api.subquery.network/sq/vikiival/magick-west'
 })
 
 export default apolloClient
