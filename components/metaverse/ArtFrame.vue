0bc9c298502719ce043c615c0665d6b82895d9ab
Viki Val <viktorko99@gmail.com>
2021-11-24 11:40:59 +0100

    :construction: Dynamic components in the progress

---
+++ components/metaverse/ArtFrame.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+
+  </div>
+</template>
+
+<script lang="ts" >
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class ArtFrame extends Vue {
+  @Prop() public position!: any;
+  @Prop() public rotation!: any;
+  @Prop() public src!: string;
+}
+</script>
