9fbd7e437ac7eab8399f80745742e5b2fe68de02
Isa Noguchi <isanoguchi91@gmail.com>
2021-11-26 05:18:53 +0900

    Nuxt-migration: Configuration for pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,6 +61,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
+    '@nuxtjs/pwa'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
