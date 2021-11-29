7b85e337daa68f4c949e112151792f2303a3a221
Viki Val <viktorko99@gmail.com>
2021-11-29 22:09:17 +0100

    :top: copying routes for unique

---
+++ pages/unique/collection/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <CollectionItem />
+</template>
+
+<script lang="ts" >
+import { Component, Vue } from 'nuxt-property-decorator'
+import CollectionItem from '@/components/rmrk/Gallery/CollectionItem.vue'
+
+@Component({
+  components: {
+    CollectionItem
+  }
+})
+
+export default class CollectionItemPage extends Vue {
+}
+</script>
---
+++ pages/unique/gallery/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <Gallery />
+</template>
+
+<script lang="ts" >
+import { Vue } from 'nuxt-property-decorator'
+
+export default class GalleryPage extends Vue {
+}
+</script>
+
