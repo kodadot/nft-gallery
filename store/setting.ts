ac08fb47fa7b0a2207e47508daad2e5df6fdfb15
Viki Val <viktorko99@gmail.com>
2021-11-30 11:47:36 +0100

    :zap: app can correctly process .env variables

--- store/setting.ts
+++ store/setting.ts
@@ -1,5 +1,7 @@
 import { OverrideSettingModule } from '@vue-polkadot/vue-settings'
 
+console.log(process.env.VUE_APP_WS_URL, process.env.NUXT_ENV_WS_URL)
+
 const defaultApiUrl = process.env.VUE_APP_WS_URL ? { apiUrl: process.env.VUE_APP_WS_URL } : {}
 const SettingModule = OverrideSettingModule(defaultApiUrl)
 
