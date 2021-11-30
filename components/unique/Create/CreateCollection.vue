edc033f1f34952246fd50b211f1364889eea1d15
Viki Val <viktorko99@gmail.com>
2021-11-30 10:33:12 +0100

    :contruction: WIP Create Pages for unique

---
+++ components/unique/Create/CreateCollection.vue
@@ -0,0 +1,264 @@
+<template>
+  <div>
+    <Loader v-model="isLoading" :status="status" />
+    <div class="box">
+      <p class="title is-size-3">
+        {{ $t("context") }}
+      </p>
+      <p class="subtitle is-size-7">{{ $t("using") }} {{ version }}</p>
+      <b-field>
+        <div>
+          {{ $t("computed id") }}: <b>{{ rmrkId }}</b>
+        </div>
+      </b-field>
+      <b-field>
+        <Auth />
+      </b-field>
+
+      <MetadataUpload
+        v-model="image"
+        label="Drop collection logo here or click to upload or simple paste image from clipboard. We support various media types (PNG, JPEG, GIF, SVG)"
+        expanded
+        preview
+        accept="image/png, image/jpeg, image/gif, image/svg+xml, image/svg"
+      />
+      <BasicInput
+        v-model="rmrkMint.name"
+        :label="$t('mint.collection.name.label')"
+        :message="$t('mint.collection.name.message')"
+        :placeholder="$t('mint.collection.name.placeholder')"
+        expanded
+        spellcheck="true"
+      />
+
+      <b-field>
+        <b-switch v-model="unlimited" :rounded="false">
+          {{ $t("mint.unlimited") }}
+        </b-switch>
+      </b-field>
+      <b-field v-if="!unlimited" class="mt-1" :label="$i18n.t('Maximum NFTs in collection')">
+        <b-numberinput
+          v-model="rmrkMint.max"
+          placeholder="1 is minumum"
+          :min="1"
+        ></b-numberinput>
+      </b-field>
+
+      <BasicInput
+        v-model="rmrkMint.symbol"
+        :label="$t('mint.collection.symbol.label')"
+        :message="$t('mint.collection.symbol.message')"
+        :placeholder="$t('mint.collection.symbol.placeholder')"
+        @keydown.native.space.prevent
+        maxlength="10"
+        expanded
+      />
+      <BasicInput
+        v-model="meta.description"
+        maxlength="500"
+        type="textarea"
+        spellcheck="true"
+        class="mb-0 mt-5"
+        :label="$t('mint.collection.description.label')"
+        :message="$t('mint.collection.description.message')"
+        :placeholder="$t('mint.collection.description.placeholder')"
+      />
+      <b-field>
+        <PasswordInput v-model="password" :account="accountId" />
+      </b-field>
+      <b-field>
+        <b-button
+          type="is-primary"
+          icon-left="paper-plane"
+          @click="submit"
+          :disabled="disabled"
+          :loading="isLoading"
+          outlined
+        >
+          {{ $t("create collection") }}
+        </b-button>
+      </b-field>
+      <b-field>
+        <Support v-model="hasSupport" :price="filePrice" />
+      </b-field>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" >
+import { Component, mixins } from 'nuxt-property-decorator'
+import { emptyObject } from '@/utils/empty'
+
+import Connector from '@vue-polkadot/vue-api'
+import exec, { execResultValue, txCb } from '@/utils/transactionExecutor'
+import { notificationTypes, showNotification } from '@/utils/notification'
+import SubscribeMixin from '@/utils/mixins/subscribeMixin'
+import RmrkVersionMixin from '@/utils/mixins/rmrkVersionMixin'
+import { Collection, CollectionMetadata } from '@/components/rmrk/service/scheme'
+import { unSanitizeIpfsUrl } from '@/utils/ipfs'
+import { pinJson, pinFileDirect } from '@/proxy'
+import { decodeAddress } from '@polkadot/keyring'
+import { u8aToHex } from '@polkadot/util'
+import { generateId } from '@/components/rmrk/service/Consolidator'
+import { supportTx, calculateCost } from '@/utils/support'
+import NFTUtils from '../NftUtils'
+import TransactionMixin from '@/utils/mixins/txMixin'
+
+const components = {
+  Auth: () => import('@/components/shared/Auth.vue'),
+  MetadataUpload: () => import('@/components/rmrk/Create/DropUpload.vue'),
+  PasswordInput: () => import('@/components/shared/PasswordInput.vue'),
+  Tooltip: () => import('@/components/shared/Tooltip.vue'),
+  Support: () => import('@/components/shared/Support.vue'),
+  Loader: () => import('@/components/shared/Loader.vue'),
+  BasicInput: () => import('@/components/shared/form/BasicInput.vue'),
+}
+
+@Component({ components })
+export default class CreateCollection extends mixins(
+  SubscribeMixin,
+  RmrkVersionMixin,
+  TransactionMixin
+) {
+  private rmrkMint: Collection = emptyObject<Collection>();
+  private meta: CollectionMetadata = emptyObject<CollectionMetadata>();
+  // private accountId: string = '';
+  private uploadMode = true;
+  private image: Blob | null = null;
+  private password = '';
+  private hasSupport = true;
+  protected unlimited = true;
+
+  get accountId() {
+    return this.$store.getters.getAuthAddress
+  }
+
+  get rmrkId(): string {
+    return generateId(this.accountId, this.rmrkMint?.symbol || '')
+  }
+
+  get accountIdToPubKey() {
+    return (this.accountId && u8aToHex(decodeAddress(this.accountId))) || ''
+  }
+
+  get disabled(): boolean {
+    const { name, symbol, max } = this.rmrkMint
+    return !(name && symbol && (this.unlimited || max) && this.accountId && this.image)
+  }
+
+  public constructRmrkMint(metadata: string): Collection {
+    const { symbol, name, max } = this.rmrkMint
+    const count = this.unlimited ? 0 : max
+    return NFTUtils.createCollection(
+      this.accountId,
+      symbol,
+      name,
+      metadata,
+      count
+    )
+  }
+
+  get filePrice() {
+    return calculateCost(this.image)
+  }
+
+  public async constructMeta() {
+    if (!this.image) {
+      throw new ReferenceError('No file found!')
+    }
+
+    this.meta = {
+      ...this.meta,
+      attributes: [],
+      external_url: 'https://nft.kodadot.xyz'
+    }
+
+    // TODO: upload image to IPFS
+    const imageHash = await pinFileDirect(this.image)
+    this.meta.image = unSanitizeIpfsUrl(imageHash)
+    // TODO: upload meta to IPFS
+    const metaHash = await pinJson(this.meta)
+
+    return unSanitizeIpfsUrl(metaHash)
+  }
+
+  protected async canSupport() {
+    if (this.hasSupport && this.image) {
+      return [await supportTx(this.image)]
+    }
+
+    return []
+  }
+
+  private toRemark(remark: string) {
+    const { api } = Connector.getInstance()
+    return api.tx.system.remark(remark)
+  }
+
+  private async submit() {
+    this.isLoading = true
+    this.status = 'loader.ipfs'
+
+    try {
+      const metadata = await this.constructMeta()
+      const mint = this.constructRmrkMint(metadata)
+      const mintString = NFTUtils.encodeCollection(mint, this.version)
+
+      const { api } = Connector.getInstance()
+      showNotification(mintString)
+      const cb = !this.hasSupport
+        ? api.tx.system.remark
+        : api.tx.utility.batchAll
+      const args = !this.hasSupport
+        ? mintString
+        : [this.toRemark(mintString), ...(await this.canSupport())]
+
+      const tx = await exec(
+        this.accountId,
+        '',
+        cb,
+        [args],
+        txCb(
+          async blockHash => {
+            execResultValue(tx)
+            const header = await api.rpc.chain.getHeader(blockHash)
+            const blockNumber = header.number.toString()
+
+            showNotification(
+              `[Collection] Saved ${this.rmrkMint.name} in block ${blockNumber}`,
+              notificationTypes.success
+            )
+
+            this.isLoading = false
+          },
+          dispatchError => {
+            execResultValue(tx)
+            if (dispatchError.isModule) {
+              const decoded = api.registry.findMetaError(
+                dispatchError.asModule
+              )
+              const { docs, name, section } = decoded
+              showNotification(
+                `[ERR] ${section}.${name}: ${docs.join(' ')}`,
+                notificationTypes.danger
+              )
+            } else {
+              showNotification(
+                `[ERR] ${dispatchError.toString()}`,
+                notificationTypes.danger
+              )
+            }
+
+            this.isLoading = false
+          },
+          res => this.resolveStatus(res.status)
+        )
+      )
+    } catch (e: any) {
+      showNotification(`[ERR] ${e}`, notificationTypes.danger)
+      console.error(e)
+      this.isLoading = false
+    }
+  }
+}
+</script>
---
+++ components/unique/Create/CreateItem.vue
@@ -0,0 +1,155 @@
+<template>
+  <div>
+    <MetadataUpload
+      v-model="vFile"
+      label="Drop your NFT here or click to upload or simply paste image from clipboard. We support various media types (BMP, GIF, JPEG, PNG, SVG, TIFF, WEBP, MP4, OGV, QUICKTIME, WEBM, GLB, FLAC, MP3, JSON)"
+      expanded
+      preview
+    />
+
+    <b-field
+      grouped
+      :label="$i18n.t('Name')"
+    >
+      <b-input
+        v-model="vName"
+        placeholder="Name your NFT"
+        expanded
+        class="mr-0"
+        spellcheck="true"
+      />
+      <Tooltip
+        iconsize="is-medium"
+        :label="$i18n.t('tooltip.name')"
+      />
+    </b-field>
+    <b-field
+      :label="$i18n.t('nft.description')"
+      class="mb-0"
+    >
+      <b-input
+        v-model="vDescription"
+        maxlength="500"
+        type="textarea"
+        placeholder="Describe your NFT"
+        spellcheck="true"
+      />
+    </b-field>
+    <b-field
+      grouped
+      :label="$i18n.t('Edition')"
+    >
+      <b-numberinput
+        v-model="vEdition"
+        placeholder="1 is minumum"
+        expanded
+        :min="1"
+        :max="clickableMax"
+      />
+      <Tooltip
+        iconsize="is-medium"
+        :label="$i18n.t('tooltip.edition')"
+      />
+    </b-field>
+    <MetadataUpload
+      v-if="secondaryFileVisible"
+      v-model="vSecondFile"
+      label="Your NFT requires a poster/cover to be seen in gallery. Please upload image (jpg/ png/ gif)"
+      icon="file-image"
+      preview
+      accept="image/png, image/jpeg, image/gif"
+      expanded
+    />
+    <AttributeTagInput
+      v-model="vTags"
+      placeholder="Get discovered easier through tags"
+    />
+
+    <b-field>
+      <b-switch
+        v-model="vNsfw"
+        :rounded="false"
+      >
+        {{ vNsfw ? "NSFW" : "SFW" }}
+      </b-switch>
+    </b-field>
+
+    <BalanceInput
+      label="Price"
+      @input="updateMeta"
+    />
+    <b-message
+      v-if="hasPrice"
+      icon="exclamation-triangle"
+      class="mt-3 has-text-primary"
+      title="Additional transaction"
+      type="is-primary"
+      has-icon
+      aria-close-label="Close message"
+    >
+      <span
+        class="has-text-primary"
+      >Setting the price now requires making an additional transaction.</span>
+    </b-message>
+  </div>
+</template>
+
+<script lang="ts" >
+import { Component, Prop, Vue, PropSync } from 'nuxt-property-decorator'
+import { Attribute } from '@/components/rmrk/service/scheme'
+import { resolveMedia } from '@/components/rmrk/utils'
+import { MediaType } from '@/components/rmrk/types'
+
+@Component({
+  components: {
+    AttributeTagInput: () => import('@/components/rmrk/Create/AttributeTagInput.vue'),
+    BalanceInput: () => import('@/components/shared/BalanceInput.vue'),
+    MetadataUpload: () => import('@/components/rmrk/Create/DropUpload.vue'),
+    Tooltip: () => import('@/components/shared/Tooltip.vue'),
+  }
+})
+export default class CreateItem extends Vue {
+  @PropSync('name', { type: String }) vName!: string
+  @PropSync('description', { type: String }) vDescription!: string
+  @PropSync('edition', { type: Number }) vEdition!: number;
+  @PropSync('nsfw', { type: Boolean }) vNsfw!: boolean;
+  @PropSync('price', { type: [Number, String] }) vPrice!: string | number;
+  @PropSync('tags', { type: Array }) vTags!: Attribute[];
+  @PropSync('file', { type: Blob }) vFile!: Blob | null;
+  @PropSync('secondFile', { type: Blob }) vSecondFile!: Blob | null;
+
+
+  @Prop(Number) public max!: number;
+  @Prop(Number) public alreadyMinted!: number;
+
+  protected updateMeta(value: number) {
+    console.log(typeof value, value)
+    this.vPrice = value
+  }
+
+  get fileType() {
+    return resolveMedia(this.vFile?.type)
+  }
+
+  get secondaryFileVisible() {
+    const fileType = this.fileType
+    return ![MediaType.UNKNOWN, MediaType.IMAGE].some(t => t === fileType)
+  }
+
+  get hasPrice() {
+    return Number(this.vPrice)
+  }
+
+  get clickableMax() {
+    return (this.max || Infinity) - this.alreadyMinted
+  }
+
+
+}
+</script>
+
+<style scoped>
+.create-token-card {
+  margin-bottom: 1em;
+}
+</style>
