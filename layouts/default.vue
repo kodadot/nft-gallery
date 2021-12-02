b64f2e54edf32d4439c45ff78d627c4b0230c5a0
Viki Val <viktorko99@gmail.com>
2021-12-02 11:10:31 +0100

    :spiderweb: we love changing types in graphql

--- layouts/default.vue
+++ layouts/default.vue
@@ -76,7 +76,7 @@ export default class Dashboard extends Vue {
       })
 
       const {
-        data: { _meta: data }
+        data: { _metadata: data }
       } = await indexer
 
       console.log(
