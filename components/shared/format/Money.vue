a745f88859aad82d4a88568fb43d90314b09fd42
roiLeo <medina.leo42@gmail.com>
2021-12-02 17:21:14 +0100

    👔 remove unused prop

--- components/shared/format/Money.vue
+++ components/shared/format/Money.vue
@@ -3,21 +3,16 @@
     <span>
       {{ value | formatBalance(decimals, unit) }}
     </span>
-    <span v-if="fiatValue">
-      / {{ fiatValue | formatBalance(decimals, showFiatValue.toUpperCase()) }}
-    </span>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'nuxt-property-decorator'
-import coingecko from '@/coingecko'
 
 @Component
 export default class Money extends Vue {
   @Prop({default: 0}) readonly value: number | string | undefined
   @Prop(Boolean) readonly inline!: boolean
-  @Prop({default: ''}) readonly showFiatValue!: string
 
   private readonly coinId: string = 'kusama'
   private fiatValue = 0
@@ -32,36 +27,6 @@ export default class Money extends Vue {
 
   get unit(): string {
     return this.chainProperties.tokenSymbol
-  }
-
-  public mounted() {
-    if (this.showFiatValue) {
-      this.getFiatValue()
-    }
-  }
-
-  private async getFiatValue() {
-    // TODO: unused, should fetch store fiat/
-    // try {
-    //   const price = this.$store.state.fiatPrice[this.coinId][this.showFiatValue]
-
-    //   if (price) {
-    //     this.fiatValue = price * Number(this.value)
-    //   } else {
-    //     const { data } = await coingecko.get('/simple/price', {
-    //       params: {
-    //         ids: this.coinId,
-    //         vs_currencies: this.showFiatValue
-    //       }
-    //     })
-
-    //     // 420 * 10 ** 12
-    //     this.fiatValue = data[this.coinId][this.showFiatValue] * Number(this.value)
-    //     this.$store.dispatch('fiat/fetchFiatPrice', data)
-    //   }
-    // } catch (error) {
-    //   console.log(error)
-    // }
   }
 }
 </script>
