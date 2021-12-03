fb53bb46128f6c26398edba7e541d85f5a710bf7
Viki Val <viktorko99@gmail.com>
2021-12-03 15:36:05 +0100

    :zap: added impl for CollapseWrapper

--- components/shared/collapse/CollapseWrapper.vue
+++ components/shared/collapse/CollapseWrapper.vue
@@ -1,26 +1,31 @@
 <template>
   <div>
-    <b-collapse
-      :open="false"
-      aria-id="contentIdForA11y1"
-    >
-      <button
-        slot="trigger"
-        class="button is-primary"
-        aria-controls="contentIdForA11y1"
-      >
-        Click me!
-      </button>
-      <slot />
+    <b-collapse :open="false" :position="position" aria-id="contentIdForA11y1" class="is-unselectable">
+      <template #trigger="props">
+        <a aria-controls="contentIdForA11y1" class="mb-2">
+          <b-icon
+            class="collapse-icon"
+            :icon="!props.open ? 'eye' : 'eye-slash'"
+          ></b-icon>
+          <span class="pl-1">{{  $t(!props.open ? visible : hidden) }}</span>
+        </a>
+      </template>
+      <slot class="mt-2"></slot>
     </b-collapse>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
+import { Component, Vue, Prop } from 'vue-property-decorator'
 
 @Component
 export default class CollapseWrapper extends Vue {
-}
+  @Prop({ type: String, default: 'Show' }) visible!: string;
+  @Prop({ type: String, default: 'Hide' }) hidden!: string;
+  @Prop(Boolean) bottom!: boolean;
 
+  get position(): string {
+    return this.bottom ? 'is-bottom' : 'is-top'
+  }
+}
 </script>
