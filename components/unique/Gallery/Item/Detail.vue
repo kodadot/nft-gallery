26c1e463dee27da338cc1905d89accba2c88cf5a
Viki Val <viktorko99@gmail.com>
2021-12-03 11:20:45 +0100

    :zap: added delegate to show in the statemine

--- components/unique/Gallery/Item/Detail.vue
+++ components/unique/Gallery/Item/Detail.vue
@@ -4,7 +4,9 @@
       {{ $t('collection') }}
     </p>
     <p class="subtitle is-size-6">
-      <router-link :to="{ name: 'collectionDetail', params: { id: nft.collectionId } }">
+      <router-link
+        :to="{ name: 'collectionDetail', params: { id: nft.collectionId } }"
+      >
         {{ nft.collectionId }}
       </router-link>
       <b-skeleton :count="1" size="is-large" :active="isLoading"></b-skeleton>
@@ -13,7 +15,7 @@
       {{ $t('creator') }}
     </p>
     <p class="subtitle is-size-6">
-      <ProfileLink :address="nft.issuer" :showTwitter="true"/>
+      <ProfileLink :address="nft.issuer" :showTwitter="true" />
       <b-skeleton :count="1" size="is-large" :active="isLoading"></b-skeleton>
       <!-- <a :href="`https://kusama.subscan.io/account/${nft.currentOwner}`" target="_blank"><Identity :address="nft.currentOwner" /></a> -->
     </p>
@@ -22,15 +24,27 @@
         {{ $t('owner') }}
       </p>
       <p class="subtitle is-size-6">
-        <ProfileLink :address="nft.currentOwner" :showTwitter="true"/>
+        <ProfileLink :address="nft.currentOwner" :showTwitter="true" />
         <b-skeleton :count="1" size="is-large" :active="isLoading"></b-skeleton>
         <!-- <a :href="`https://kusama.subscan.io/account/${nft.currentOwner}`" target="_blank"><Identity :address="nft.currentOwner" /></a> -->
+      </p>
+    </template>
+    <template v-if="nft.delegate">
+      <p class="label">
+        {{ $t('delegate') }}
+        <span>
+          <a class="has-text-danger"><b-icon icon="times"> </b-icon></a>
+        </span>
+      </p>
+      <p class="subtitle is-size-6">
+        <ProfileLink :address="nft.delegate" :showTwitter="true" />
+        <b-skeleton :count="1" size="is-large" :active="isLoading"></b-skeleton>
       </p>
     </template>
   </div>
 </template>
 
-<script lang="ts" >
+<script lang="ts">
 import { Component, Prop, Vue } from 'nuxt-property-decorator'
 import isShareMode from '@/utils/isShareMode'
 import { NFTWithMeta } from '@/components/rmrk/service/scheme'
@@ -38,20 +52,22 @@ import { emptyObject } from '@/utils/empty'
 // import Identity from '@/components/shared/format/Identity.vue'
 
 const components = {
-  ProfileLink: () => import('@/components/rmrk/Profile/ProfileLink.vue')
+  ProfileLink: () => import('@/components/rmrk/Profile/ProfileLink.vue'),
 }
 
 @Component({ components })
 export default class Name extends Vue {
-  @Prop({ default: () => emptyObject<NFTWithMeta>() }) public nft!: NFTWithMeta;
-  @Prop() public isLoading!: boolean;
+  @Prop({ default: () => emptyObject<NFTWithMeta>() }) public nft!: NFTWithMeta
+  @Prop() public isLoading!: boolean
 
   get detailVisible() {
     return !isShareMode
   }
 
   get carbonlessBadge() {
-    return this.nft.attributes?.some(({trait_type, value}) => trait_type === 'carbonless' && value)
+    return this.nft.attributes?.some(
+      ({ trait_type, value }) => trait_type === 'carbonless' && value
+    )
   }
 }
 </script>
