c8b5d8f60e97e82bf246e5e4c7ce174f12885eb2
Rhaidzsal Ali <rhaidzsalali@gmail.com>
2021-12-14 22:59:10 +0800

    1478: fix .vue property

--- nuxt.config.js
+++ nuxt.config.js
@@ -162,7 +162,7 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     extend(config, { isDev, isClient }) {
-      config.resolve.alias['vue'] = 'vue/dist/vue.common'; //https://github.com/nuxt/nuxt.js/issues/1142#issuecomment-317272538
+      config.resolve.alias.vue = 'vue/dist/vue.common'; //https://github.com/nuxt/nuxt.js/issues/1142#issuecomment-317272538
       config.node = {
         fs: 'empty',
       };
