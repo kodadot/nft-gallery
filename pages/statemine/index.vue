75da415e9d83be644fcd2c0804e17c95453e94ab
Viki Val <viktorko99@gmail.com>
2021-12-15 14:07:29 +0100

    :zap: statemine has it's own landing

---
+++ pages/statemine/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <Landing prefix="statemine" />
+</template>
+
+<script lang="ts" >
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({
+  components: {
+    Landing: () => import('@/components/landing/Landing.vue')
+  }
+})
+
+export default class LandingPage extends Vue {
+}
+</script>
