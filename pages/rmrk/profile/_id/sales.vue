94b5e375e11f3c9e7a1910e1c06ffcdfc03588cb
roiLeo <medina.leo42@gmail.com>
2021-12-15 14:44:44 +0100

    ✨ init sales table component

--- pages/rmrk/profile/_id/sales.vue
+++ pages/rmrk/profile/_id/sales.vue
@@ -1,27 +1,53 @@
 <template>
   <div>
     <!-- Sales stats goes brrrrrr -->
-    <!-- {{ nftSales }} -->
+    <div class="card-content">
+      <div class="content">
+        <b-table :data="nfts" :columns="columns" hoverable></b-table>
+      </div>
+    </div>
+    {{ nfts }}
   </div>
 </template>
 
 <script lang="ts" >
 import { Component, Vue, Watch } from 'nuxt-property-decorator'
 import { fetchNFTMetadata, getSanitizer } from '@/components/rmrk/utils'
+import { NFT, NFTMetadata, MintNFT, getNftId } from '@/components/rmrk/service/scheme'
 import nftListSoldByIssuer from '@/queries/nftListSoldByIssuer.graphql'
+
 
 @Component({})
 export default class  extends Vue {
   protected first = 50
   protected currentValue = 1
-  // private nfts: NFT[] = []
+  protected nfts: NFT[] = []
+  protected columns = [
+    {
+      field: 'name',
+      label: 'Name',
+    },
+    {
+      field: 'currentOwner',
+      label: 'Owner',
+    },
+    {
+      field: 'price',
+      label: 'Price',
+      numeric: true
+    },
+    {
+      field: 'gender',
+      label: 'Gender',
+    }
+  ]
 
   get offset() {
     return this.currentValue * this.first - this.first
   }
 
   async asyncData({ app, params }) {
-    const nfts = await app.apolloProvider.defaultClient.query({
+    const res = await app.apolloProvider.defaultClient.query({
       query: nftListSoldByIssuer,
       variables: {
         account: params.id,
@@ -30,19 +56,24 @@ export default class  extends Vue {
       },
     })
 
+    const total = res.data.nFTEntities.totalCount
+    const nfts = res.data.nFTEntities.nodes
+
     if (nfts?.length) {
       const meta = await fetchNFTMetadata(nfts)
+      console.log('meta:')
       console.log(meta)
       // this.firstNFTData = {
       //   ...meta
       // }
     }
-    return { nfts }
+    return { total, nfts }
   }
 
-  public created() {
+  public async created() {
     console.log('yeet')
     console.log(this.$route.params.id)
+    // if (nfts)
   }
 
 }
