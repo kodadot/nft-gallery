d491e181a7cc71ded583e9e4d5142fc7a128a5cf
Viki Val <viktorko99@gmail.com>
2021-12-16 16:12:28 +0100

    Added Westmint to the statemine integration

---
+++ pages/westmint/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <Landing prefix="statemine" buildOn="Statemine Unique Pallet" />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({
+  components: {
+    Landing: () => import('@/components/landing/Landing.vue'),
+  },
+})
+export default class LandingPage extends Vue {
+  middleware({ store, route }) {
+    const prefix = store.getters.currentUrlPrefix
+    if (prefix !== route.name) {
+      console.log('[EXPECT]', route.name, 'got', prefix)
+      store.dispatch('setUrlPrefix', route.name)
+    }
+  }
+}
+</script>
