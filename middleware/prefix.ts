92bdea833af64d03a11b7e349fe423e478402bf3
Viki Val <viktorko99@gmail.com>
2021-12-17 11:21:23 +0100

    :bug: series-insight was handled as prefix

--- middleware/prefix.ts
+++ middleware/prefix.ts
@@ -2,9 +2,8 @@ const withPrefix = /-/
 
 export default function ({ store, route }) {
   const prefix = store.getters.currentUrlPrefix
-
   if (withPrefix.test(route.name)) {
-    if (!RegExp(prefix).test(route.name)) {
+    if (!RegExp(prefix).test(route.name) && !RegExp(route.name).test(route.path)) {
       console.log('[PREFIX] from', prefix, 'to', route.name)
       store.dispatch('setUrlPrefix', route.name.split(withPrefix)[0])
     }
