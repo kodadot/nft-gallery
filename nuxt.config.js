e09a58f3188e5695a96cd099c6169caddd335adf
Rhaidzsal Ali <rhaidzsalali@gmail.com>
2021-12-18 18:56:37 +0800

    1478: folding cube loader

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,11 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
-  loadingIndicator: '~/components/loading.html',
+  loadingIndicator: {
+    name: 'folding-cube',
+    color: '#fc007b',
+    background: 'black',
+  },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['styles/index.scss'],
