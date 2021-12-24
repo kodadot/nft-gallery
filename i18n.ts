ca82f45d8b43ad7088a7a96df0d70a76ece2baa3
Rhaidzsal Ali <rhaidzsalali@gmail.com>
2021-12-24 08:56:02 +0800

    1542: use regex quantifier

--- i18n.ts
+++ i18n.ts
@@ -12,13 +12,13 @@ const md = MarkdownIt({
 function loadLocaleMessages(): LocaleMessages {
   // File containing data common to ALL languages
   const allLangDataFile = 'all_lang.json'
-  const locales = require.context('./langDir', true, /[A-Za-z0-9-_,\s]+?\.json$/i)
+  const locales = require.context('./langDir', true, /[A-Za-z0-9-_,\s]{0,8}\.json$/i)
   const messages: LocaleMessages = {}
   locales.keys().forEach(key => {
     if (key === allLangDataFile) {
       return
     }
-    const matched = key.match(/([A-Za-z0-9-_]+?)\./i)
+    const matched = key.match(/([A-Za-z0-9-_]{0,8})\./i)
     if (matched && matched.length > 1) {
       const locale = matched[1]
       messages[locale] = locales(key)
