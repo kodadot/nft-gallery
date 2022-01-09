9b57ce1c6bafbd616a79540cf8355b7311cd0ea6
Darshana Prachi <39299315+prachi00@users.noreply.github.com>
2022-01-09 19:11:40 +0530

    Update components/unique/Gallery/Item/Detail.vue
    Co-authored-by: roiLeo <medina.leo42@gmail.com>

--- components/unique/Gallery/Item/Detail.vue
+++ components/unique/Gallery/Item/Detail.vue
@@ -22,7 +22,7 @@
         {{ $t('owner') }}
       </p>
       <p class="subtitle is-size-6">
-        <ProfileLink :address="nft.currentOwner" :showTwitter="true" showDiscord />
+        <ProfileLink :address="nft.currentOwner" showTwitter showDiscord />
         <b-skeleton :count="1" size="is-large" :active="isLoading"></b-skeleton>
         <!-- <a :href="`https://kusama.subscan.io/account/${nft.currentOwner}`" target="_blank"><Identity :address="nft.currentOwner" /></a> -->
       </p>
