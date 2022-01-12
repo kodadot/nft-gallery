8f5f06f30f713ee7f506ae7125ab3615c64bf128
roiLeo <medina.leo42@gmail.com>
2022-01-12 15:02:00 +0100

    📊 vueGtag plugin config

--- plugins/vueGtag.ts
+++ plugins/vueGtag.ts
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import VueGtag from 'vue-gtag'
 
-Vue.use(VueGtag, {
-  config: { id: process.env.GOOGLE_ANALYTICS_ID },
-  enable: false,
-})
+export default ({ app }) => {
+  Vue.use(VueGtag, {
+    config: { id: app.$config.googleAnalyticsId },
+    enable: false,
+  })
+}
