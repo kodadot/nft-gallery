e35cc1f1bfb4e780b0cdbe5d0345e6e604120777
Viki Val <viktorko99@gmail.com>
2022-01-12 20:59:53 +0100

    :construction: maaaybe?

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,12 @@ module.exports = {
     '^~/(.*)$': '<rootDir>/$1',
     '^vue$': 'vue/dist/vue.common.js'
   },
+  transformIgnorePatterns: [
+    '/node_modules/(?!@vue-polkadot|@polkadot|@babel/runtime/helpers/esm/)'
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx|mjs)$': 'babel-jest',
+  },
   moduleFileExtensions: [
     'ts', // if using TypeScript
     'js',
