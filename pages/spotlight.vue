6fc80898bd7a182108da2493ff380d2941f2ca44
kkukelka <17953978+kkukelka@users.noreply.github.com>
2022-01-12 18:11:53 +0100

    feat: adding rmrk logo

--- pages/spotlight.vue
+++ pages/spotlight.vue
@@ -1,5 +1,19 @@
 <template>
   <section>
+    <div class="columns">
+      <div class="column is-four-fifths">
+        <h1 class="title is-2">{{ $t('spotlight.title') }}</h1>
+        <p class="subtitle is-size-5">{{ $t('spotlight.subtitle') }}</p>
+      </div>
+      <div class="column">
+        <img
+            src="~/assets/rmrk-logo-pink-faded.png"
+            alt="RMRK"
+            class="rmrk-logo is-hidden-mobile"
+          >
+      </div>
+    </div>
+
     <SpotlightTable />
   </section>
 </template>
@@ -28,3 +42,9 @@ import { Component, Vue } from 'nuxt-property-decorator'
 export default class Leaderboard extends Vue {
 }
 </script>
+
+<style scoped>
+.rmrk-logo {
+  aspect-ratio: 127 / 42
+}
+</style>
