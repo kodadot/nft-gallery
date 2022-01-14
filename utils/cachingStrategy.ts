d96802fe71eaaf02b55f4551237723fd19b43743
Viki Val <viktorko99@gmail.com>
2022-01-14 15:33:27 +0100

    :zap: working caching strategy

--- utils/cachingStrategy.ts
+++ utils/cachingStrategy.ts
@@ -1,10 +1,11 @@
 // TODO: hacky, but works for now
-import { getMany, setMany } from 'idb-keyval'
+import { getMany, setMany, update } from 'idb-keyval'
 
 import { queryBatch } from '@/utils/cloudflare'
-import { zip } from '~/components/rmrk/utils'
+import { fetchMetadata, fetchNFTMetadata, zip } from '~/components/rmrk/utils'
 import { imageStore } from './idbStore'
 import { fastExtract } from './ipfs'
+import { emptyObject } from './empty'
 
 type Option<T> = T | undefined
 type MayString = Option<string>
@@ -24,6 +25,7 @@ export const getCloudflareImageLinks = async (keys: string[]): P<KeyValue>  => {
   const fromCache = await getMany<string>(values, imageStore)
   const zipped = zip<string, MayString, ZipResult>(values, fromCache)
   const uncached = zipped.filter(([, value]) => !value).map(([key]) => key)
+  console.log(uncached)
   const cached = zipped.filter(withValue) as ZipResult[]
   const deliveryLinks: KeyValue = await queryBatch(uncached).then(values => Object.entries(values).map(withUrlOf)).then(Object.fromEntries).catch(() => ({}))
   setMany(Object.entries(deliveryLinks), imageStore).catch(console.warn)
@@ -34,4 +36,17 @@ export const getCloudflareImageLinks = async (keys: string[]): P<KeyValue>  => {
   }
 }
 
+export const cacheOrFetchMetadata = async<T>(fromCache: T | undefined, metadata: string): P<T> => {
+  if (fromCache) {
+    return fromCache
+  }
 
+  try {
+    const meta = await fetchMetadata<T>({metadata})
+    update(metadata, () => meta)
+    return meta
+  } catch (e) {
+    console.warn('[ERR] unable to get metadata', e)
+    return emptyObject<T>()
+  }
+}
