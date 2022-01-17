a17549875d6df213ca1a75261935badc38c94b35
roiLeo <medina.leo42@gmail.com>
2022-01-17 16:52:55 +0100

    ✨ user deny list

--- components/series/SeriesTable.vue
+++ components/series/SeriesTable.vue
@@ -298,6 +298,7 @@ export default class SeriesTable extends mixins(PrefixMixin) {
       query: collectionSeriesList,
       client: 'subsquid',
       variables: {
+        denyList,
         limit,
         offset: 0,
         orderBy: sort.field,
