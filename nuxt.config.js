7f47424972b0ffc40b830926e9aba1f42ea54e8b
Viki Val <viktorko99@gmail.com>
2022-01-19 21:19:16 +0100

    :truck: moved plugins/api to plugins/polkadot

--- nuxt.config.js
+++ nuxt.config.js
@@ -88,7 +88,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/api', mode: 'client' },
+    { src: '~/plugins/polkadot', mode: 'client' },
     { src: '~/plugins/seoMetaGenerator', mode: 'client' },
     '~/plugins/filters',
     '~/plugins/globalVariables',
--- plugins/api.ts
+++
@@ -1,1 +0,0 @@
-import '@polkadot/api-augment'
