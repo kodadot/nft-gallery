dec8a830a61cfd0930acf21ab2d95d7abadc007a
Viki Val <viktorko99@gmail.com>
2022-01-31 11:23:06 +0100

    :parachute: correct ss58 format is finally correct

--- utils/api/Query.ts
+++ utils/api/Query.ts
@@ -1,5 +1,12 @@
 import type { ApiPromise } from '@polkadot/api'
 import BN from 'bn.js'
+import correctFormat from '../ss58Format'
+
+type ChainProperties = {
+  ss58Format: number
+  tokenDecimals: number
+  tokenSymbol: string
+}
 
 class Query {
   static async getNonce(api: ApiPromise, address: string): Promise<BN> {
@@ -16,6 +23,15 @@ class Query {
     const balance = await cb(arg)
     const accountData = (balance as any).data.free.toString()
     return accountData
+  }
+
+  static getChainProperties(api: ApiPromise): ChainProperties {
+    const { chainSS58, chainDecimals, chainTokens } = api.registry
+    return {
+      ss58Format: correctFormat(chainSS58),
+      tokenDecimals: chainDecimals[0] || 12,
+      tokenSymbol: chainTokens[0] || 'Unit',
+    }
   }
 }
 
