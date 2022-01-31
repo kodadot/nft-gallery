62b1318c8839a3c2bf274f1679d033946b2e5655
roiLeo <medina.leo42@gmail.com>
2022-01-31 13:46:49 +0100

    ⚒️  Model viewer container

--- components/rmrk/Media/ModelMedia.vue
+++ components/rmrk/Media/ModelMedia.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="view-model__wrapper">
+    <div class="view-model__overflow"></div>
     <model-viewer
-      id="reveal"
       class="view-model__component"
       :src="src"
       :poster="poster"
@@ -10,7 +10,6 @@
       auto-rotate
       :camera-controls="isDetail"
       :ar="isDetail"
-      ar-modes="webxr scene-viewer quick-look"
       shadow-intensity="1"
       autoplay>
       <button id="ar-button" slot="ar-button">Activate AR</button>
@@ -36,20 +35,35 @@ export default class ViewModel extends Vue {
 }
 </script>
 
-<style scoped>
-.view-model__wrapper {
-  height: 100%;
-  width: 100%;
-}
+<style lang="scss" scoped>
+.view-model {
+  &__wrapper {
+    z-index: 1;
+    min-width: 0;
+    position: relative;
+    box-sizing: border-box;
+  }
 
-.view-model__component {
-  height: 100%;
-  width: 100%;
-  min-width: 300px;
-  min-height: 300px;
-}
+  &__overflow {
+    height: 0;
+    width: 100%;
+    padding-bottom: 100%;
+  }
 
-model-viewer#reveal {
-  --poster-color: transparent;
+  &__component {
+    inset: 0;
+    min-width: 0;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    position: absolute;
+    box-sizing: border-box;
+    -moz-box-align: center;
+    -moz-box-pack: center;
+    justify-content: center;
+    --poster-color: transparent;
+    --progress-bar-color: lightpink;
+  }
 }
 </style>
