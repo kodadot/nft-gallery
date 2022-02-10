5e84a647d38c843ae9fc2bc9f3220447c1b80d85
kkukelka <17953978+kkukelka@users.noreply.github.com>
2022-02-10 10:49:23 +0100

    chore: add function return types

--- plugins/keyboardEvents.ts
+++ plugins/keyboardEvents.ts
@@ -78,6 +78,6 @@ const listenGlobalKeyboardEvents = (app, store) => {
   })
 }
 
-export default ({ app, store }) => {
+export default ({ app, store }): void => {
   listenGlobalKeyboardEvents(app, store)
 }
