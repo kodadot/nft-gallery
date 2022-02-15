619837b244dc7f46a2cc12fb18a7561936617101
roiLeo <medina.leo42@gmail.com>
2022-02-15 09:15:32 +0100

    wip

--- pages/rmrk/profile/_id/sales.vue
+++ pages/rmrk/profile/_id/sales.vue
@@ -10,17 +10,19 @@
   </div>
 </template>
 
-<script lang="ts" >
-import { Component, Vue } from 'nuxt-property-decorator'
+<script lang="ts">
+import { Component, mixins } from 'nuxt-property-decorator'
 import { NFT } from '@/components/rmrk/service/scheme'
 import nftListSoldByIssuer from '@/queries/nftListSoldByIssuer.graphql'
+import lastNftListByEvent from '@/queries/rmrk/subsquid/lastNftListByEvent.graphql'
+import PrefixMixin from '~/utils/mixins/prefixMixin'
 
 @Component({
   components: {
     History: () => import('@/components/rmrk/Gallery/History.vue'),
-  }
+  },
 })
-export default class  extends Vue {
+export default class extends mixins(PrefixMixin) {
   protected first = 50
   protected currentValue = 1
   protected nfts: NFT[] = []
@@ -29,21 +31,24 @@ export default class  extends Vue {
     return this.currentValue * this.first - this.first
   }
 
-  async asyncData({ app, params }) {
-    const res = await app.apolloProvider.defaultClient.query({
-      query: nftListSoldByIssuer,
-      variables: {
-        account: params.id,
-        first: 50,
-        offset: 1,
-      },
-    })
+  async asyncData({ app, context, params }) {
+    console.log(app)
+    console.log(context)
+    // const res = await app.$apollo.query({
+    //   query: nftListSoldByIssuer,
+    //   client: this.urlPrefix,
+    //   variables: {
+    //     account: params.id,
+    //     first: 50,
+    //     offset: 1,
+    //   },
+    // })
 
-    const total = res.data.nFTEntities.totalCount
-    const nfts = res.data.nFTEntities.nodes
-    const events = res.data.nFTEntities.nodes.flatMap((nft: { events: any }) => nft.events)
+    // const total = res.data.nFTEntities.totalCount
+    // const nfts = res.data.nFTEntities.nodes
+    // const events = res.data.nFTEntities.nodes.flatMap((nft: { events: any }) => nft.events)
 
-    return { total, nfts, events }
+    // return { total, nfts, events }
   }
 
   public async created() {
