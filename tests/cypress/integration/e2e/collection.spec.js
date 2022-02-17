377a66996a0647498b775d272c14838ba3db3106
rchovatiya88 <rchovatiya88@gmail.com>
2022-02-17 10:09:19 -0500

    collectionfilter

--- tests/cypress/integration/e2e/collection.spec.js
+++ tests/cypress/integration/e2e/collection.spec.js
@@ -6,6 +6,22 @@ describe('Collection Page should display the various collections and their detai
   it('Navigate to the Collection Page', () => {
     cy.visit(baseUrl + '/rmrk/collections')
   })
+
+  context('Filter by New First', () => {
+    it('Should display the various collections and their details.', () => {
+      cy.get('.is-grouped > :nth-child(1) > .control > .select > select')
+        .then(($select) => {
+          const opt = $select.find('option[value="BLOCK_NUMBER_DESC"]')
+          $select.val(opt.attr('value'))
+          return $select
+        })
+        .trigger('change')
+    })
+    it('Validate Page loaded and view Newest first', () => {
+      cy.get('.columns').should('be.visible')
+      cy.wait(2000)
+    })
+  })
   context('Filter by Old First', () => {
     it('Should display the various collections and their details.', () => {
       cy.get('.is-grouped > :nth-child(1) > .control > .select > select')
@@ -18,15 +34,53 @@ describe('Collection Page should display the various collections and their detai
     })
     it('Validate Page loaded and view oldest first', () => {
       cy.get('.columns').should('be.visible')
+      cy.wait(2000)
     })
   })
 
-  // it('Search for term', () => {
-  //     cy.get('.input').type('generative', { force: true })
-  // })
-
-  // it('View Search Results', () => {
-  //     cy.get('.input').type('{enter}')
-  //     cy.get('.section').click({ force: true })
-  // })
+  context('Filter by Last Interacted', () => {
+    it('Should display the various collections and their details.', () => {
+      cy.get('.is-grouped > :nth-child(1) > .control > .select > select')
+        .then(($select) => {
+          const opt = $select.find('option[value="UPDATED_AT_DESC"]')
+          $select.val(opt.attr('value'))
+          return $select
+        })
+        .trigger('change')
+    })
+    it('Validate Page loaded and view Last Interacted', () => {
+      cy.get('.columns').should('be.visible')
+      cy.wait(2000)
+    })
+  })
+  context('Filter by Most Expensive', () => {
+    it('Should display the various collections and their details.', () => {
+      cy.get('.is-grouped > :nth-child(1) > .control > .select > select')
+        .then(($select) => {
+          const opt = $select.find('option[value="PRICE_DESC"]')
+          $select.val(opt.attr('value'))
+          return $select
+        })
+        .trigger('change')
+    })
+    it('Validate Page loaded and view Last Interacted', () => {
+      cy.get('.columns').should('be.visible')
+      cy.wait(2000)
+    })
+  })
+  context('Filter by from cheaper', () => {
+    it('Should display the various collections and their details.', () => {
+      cy.get('.is-grouped > :nth-child(1) > .control > .select > select')
+        .then(($select) => {
+          const opt = $select.find('option[value="PRICE_ASC"]')
+          $select.val(opt.attr('value'))
+          return $select
+        })
+        .trigger('change')
+    })
+    it('Validate Page loaded and view Last Interacted', () => {
+      cy.get('.columns').should('be.visible')
+      cy.wait(2000)
+    })
+  })
 })
