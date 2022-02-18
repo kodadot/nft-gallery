a97b8584bf0474065c7f6db5121e23bcd50fcfde
rchovatiya88 <rchovatiya88@gmail.com>
2022-02-18 11:10:31 -0500

    no var

--- tests/cypress/integration/e2e/collection.spec.js
+++ tests/cypress/integration/e2e/collection.spec.js
@@ -3,7 +3,21 @@
 /* eslint-disable no-undef */
 const baseUrl = 'http://localhost:9090'
 describe('Collection Page should display the various collections and their details.', () => {
+  beforeEach(() => {
+    cy.viewport(1280, 800)
+    Cypress.Cookies.defaults({
+      preserve: 'session_id',
+    })
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      // returning false here prevents Cypress from
+      // failing the test
+      return false
+    })
+    // cy.wait(2000)
+  })
+
   it('Navigate to the Collection Page', () => {
+    cy.viewport(1280, 800)
     cy.visit(baseUrl + '/rmrk/collections')
   })
 
@@ -65,7 +79,7 @@ describe('Collection Page should display the various collections and their detai
     })
     it('Validate Page loaded and view Last Interacted', () => {
       cy.get('.columns').should('be.visible')
-      cy.wait(2000)
+      // cy.wait(2000)
     })
   })
   context('Filter by from cheaper', () => {
@@ -80,7 +94,45 @@ describe('Collection Page should display the various collections and their detai
     })
     it('Validate Page loaded and view Last Interacted', () => {
       cy.get('.columns').should('be.visible')
+      // cy.wait(2000)
+    })
+  })
+  describe('Collection Search Functionality.', () => {
+    it('Serach for a collection', () => {
+      cy.get('.is-grouped > :nth-child(1) > .control > .select > select')
+        .then(($select) => {
+          const opt = $select.find('option[value="BLOCK_NUMBER_DESC"]')
+          $select.val(opt.attr('value'))
+          return $select
+        })
+        .trigger('change')
+      cy.get('.field.is-expanded > .control > .input').type('japandas')
       cy.wait(2000)
+      cy.get(':nth-child(3) > .columns').should('be.visible')
+      cy.get('.field.is-expanded > .control > .input').clear()
+    })
+  })
+  describe('Collection Buy Now.', () => {
+    it('Click on Buy Now button', () => {
+      cy.get('.mb-5 > .switch > .check').click()
+    })
+  })
+
+  describe('Navaigate to Next Page  ', () => {
+    let i = 0
+    // eslint-disable-next-line no-const-assign
+    for (i; i < 5; i++) {
+      it('Go to Next Page' + i, () => {
+        cy.get(
+          'div:nth-child(1) > nav > a.pagination-link.pagination-next'
+        ).click()
+      })
+    }
+  })
+
+  describe('Navigate to Random Collection', () => {
+    it('Click on random button', () => {
+      cy.get('.tooltip-trigger > .button').click()
     })
   })
 })
