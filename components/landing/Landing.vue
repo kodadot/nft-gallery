801fce735dfb51a36f8ea507292e1c61328d4539
roiLeo <medina.leo42@gmail.com>
2022-02-21 08:22:11 +0100

    ✨ landing UI rework test

--- components/landing/Landing.vue
+++ components/landing/Landing.vue
@@ -1,43 +1,40 @@
 <template>
   <section class="homepage section">
     <div class="container">
-      <div class="hero is-fullheight">
-        <div class="columns">
-          <div class="column">
+      <div class="columns">
+        <div class="column">
+          <img
+            src="~/assets/KODADOT.png"
+            alt="First NFT market explorer on Kusama and Polkadot"
+            class="mb-5"
+            height="60" />
+          <h3
+            class="title is-1 is-flex is-flex-direction-column uppercase homepage__heading">
+            <span class="text__stroked my-5">
+              Discover, collect and sell Kusama NFTs
+            </span>
+            <span class="title is-6 homepage__subtitle uppercase text-semibold">
+              Built on
+              <span class="text-bold text-primary"> {{ buildOn }} </span>
+            </span>
+          </h3>
+          <a
+            href="https://discord.gg/35hzy2dXXh"
+            target="_blank"
+            rel="noopener noreferrer">
             <img
-              src="~/assets/KODADOT.png"
-              alt="First NFT market explorer on Kusama and Polkadot"
-              class="mb-5"
-              height="60" />
-            <h3
-              class="title is-1 is-flex is-flex-direction-column uppercase homepage__heading">
-              <span class="text__stroked my-5">
-                Discover, collect and sell Kusama NFTs
-              </span>
-              <span
-                class="title is-6 homepage__subtitle uppercase text-semibold">
-                Built on
-                <span class="text-bold text-primary"> {{ buildOn }} </span>
-              </span>
-            </h3>
-            <a
-              href="https://discord.gg/35hzy2dXXh"
-              target="_blank"
-              rel="noopener noreferrer">
-              <img
-                src="https://badgen.net/discord/online-members/35hzy2dXXh"
-                alt="Discord" />
-            </a>
-          </div>
-          <div class="column">
-            <!-- <CuratedList /> -->
-          </div>
+              src="https://badgen.net/discord/online-members/35hzy2dXXh"
+              alt="Discord" />
+          </a>
+        </div>
+        <div class="column">
+          <CuratedList />
         </div>
       </div>
       <div>
         <!-- <CuratedList class="my-5"/> -->
-        <CuratedList class="my-5" />
-        <!-- <LatestSales class="my-5" /> -->
+        <!-- <CuratedList class="my-5" /> -->
+        <LatestSales class="my-5" />
         <NewestList class="my-5" />
       </div>
     </div>
