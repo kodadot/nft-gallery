1f099d24d8d915f34488e5043bd2e8129a8fbcd9
Jarsen136 <695552027@qq.com>
2022-03-19 23:39:45 +0800

    fix: refresh page index after searching new keyword

--- components/rmrk/Gallery/Gallery.vue
+++ components/rmrk/Gallery/Gallery.vue
@@ -306,8 +306,9 @@ export default class Gallery extends mixins(PrefixMixin) {
   }
 
   @Watch('$route.query.search')
-  protected onIdChange(val: string, oldVal: string) {
+  protected onSearchChange(val: string, oldVal: string) {
     if (shouldUpdate(val, oldVal)) {
+      this.currentValue = 1
       this.searchQuery.search = val || ''
     }
   }
