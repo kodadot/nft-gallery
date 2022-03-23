e53063d289f6651edb1897eb38268275ee44db8c
Preschian Febryantara <preschian27@gmail.com>
2022-03-23 11:58:09 +0700

    test ignore deepsource invalid rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
     'max-classes-per-file': ['error', 2],
+    // skipcq
     'vue/multi-word-component-names': [
       'error',
       {
