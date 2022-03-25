5dd5215d76cc27ed152b7050cc92243b7ec7e51a
roiLeo <medina.leo42@gmail.com>
2022-03-25 09:14:59 +0100

    Update layouts/full-width-layout.vue

--- layouts/full-width-layout.vue
+++ layouts/full-width-layout.vue
@@ -7,7 +7,7 @@
         :hasImg="false"
         errorTitle="Offline Detected"
         errorSubtitle="Please check your network connections" />
-      <Nuxt />
+      <Nuxt v-else />
     </main>
     <Footer />
   </div>
