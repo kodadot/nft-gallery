c2a8e17a8c2d5d514b07badb5224a64dfcf3a9e7
Preschian Febryantara <preschian27@gmail.com>
2022-04-01 19:44:58 +0700

    add type in utils/visitedNFTs

--- utils/localStorage.ts
+++ utils/localStorage.ts
@@ -1,4 +1,14 @@
-export const visitedNFTs = () => {
+export type VisitedNFT = {
+  collection: string
+  date: string
+  id: string
+  image: string
+}
+
+/**
+ * Get visited NFTs from localStorage
+ */
+export const visitedNFTs = (): VisitedNFT[] => {
   const getHistory = localStorage.getItem('history')
   if (!getHistory) {
     return []
