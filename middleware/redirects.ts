7f6a88775d368209c9073838b9c0097a10867438
Darshana Prachi <prachirai00@gmail.com>
2022-04-11 16:28:54 +0530

    route minor change

--- middleware/redirects.ts
+++ middleware/redirects.ts
@@ -2,9 +2,9 @@ export default function ({ store, redirect, route }): void {
   const prefix = store.getters.currentUrlPrefix
 
   if (route.path.endsWith('collections')) {
-    return redirect(`/${prefix}/explore`)
+    return redirect(`/${prefix}/explore?tab=COLLECTION`)
   }
   if (route.path.endsWith('gallery')) {
-    return redirect(`/${prefix}/explore?tab=GALLERY`)
+    return redirect(`/${prefix}/explore`)
   }
 }
