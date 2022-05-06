2dd31d992255cf2cfdbf582d49588d618b97ef6c
Jarsen136 <695552027@qq.com>
2022-05-06 23:03:06 +0800

    remove unused test packages

--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vitest/config'
 import { resolve } from 'path'
-import { createVuePlugin } from 'vite-plugin-vue2'
 
 export default defineConfig({
   test: {
@@ -20,5 +19,4 @@ export default defineConfig({
       '~': resolve(__dirname, './'),
     },
   },
-  plugins: [createVuePlugin()],
 })
