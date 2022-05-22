4fb671ed568cfe3a745cdc864673f45ac9c95416
KoKo <kngzhi@hotmail.com>
2022-05-22 14:37:37 +0800

    refacotr: extract utility

--- components/hot/HotTable.vue
+++ components/hot/HotTable.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <Loader :value="$fetchState.pending" />
-    <b-table :data="data" hoverable class="series-sticky-header">
+    <b-table
+      :data="data"
+      :columns="columns"
+      hoverable
+      class="series-sticky-header">
       <template #empty>
         <div v-if="!$fetchState.pending" class="has-text-centered">
           {{ $t('spotlight.empty') }}
@@ -18,12 +22,13 @@ import hotNfts from '@/queries/rmrk/subsquid/hotNfts.graphql'
 import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 import groupBy from 'lodash/groupBy'
 import sortBy from 'lodash/sortBy'
-import formatBalance from '@/utils/formatBalance'
-import { getVolume } from '@/utils/math'
 import { RowHot, SubsquidHotNft } from './types'
 
 import PrefixMixin from '~/utils/mixins/prefixMixin'
 import ChainMixin from '@/utils/mixins/chainMixin'
+import formatBalance from '@/utils/formatBalance'
+import { getVolume } from '@/utils/math'
+import { parseDate } from '@/utils/datetime'
 
 const components = {
   Money: () => import('@/components/shared/format/Money.vue'),
@@ -34,7 +39,15 @@ const components = {
 
 @Component({ components })
 export default class HotTable extends mixins(PrefixMixin, ChainMixin) {
-  protected data: [RowHot] = []
+  protected data: RowHot[] = []
+  protected columns = [
+    { field: 'id', label: 'N°', numeric: true },
+    { field: 'name', label: 'Series Name' },
+    { field: 'totalVolume', label: 'Total Volume' },
+    { field: 'latestSoldSize', label: 'Last Sale Size(KSM)' },
+    { field: 'latestSoldTime', label: 'Time Since Last Sale' },
+    { field: 'medianDate', label: 'Median Time Between Sales' },
+  ]
 
   private toKSM(amount) {
     return formatBalance(amount, this.decimals, this.unit)
@@ -44,7 +57,7 @@ export default class HotTable extends mixins(PrefixMixin, ChainMixin) {
     const data = await this.fetchHotNfts()
     const collectionMap = groupBy(data, 'nft.collection.id')
     const sortOrder = [...new Set(data.map((e) => e.nft.collection.id))]
-    const result: [RowHot] = sortOrder.map((colId) => {
+    const result: RowHot[] = sortOrder.map((colId, idx) => {
       const collection = collectionMap[colId][0].nft.collection
       const nfts = sortBy(collectionMap[colId], 'timestamp').reverse()
       const totalVolume = this.toKSM(getVolume(nfts))
@@ -52,12 +65,13 @@ export default class HotTable extends mixins(PrefixMixin, ChainMixin) {
       const latestSale = nfts[0]
       const medianIdx = Math.floor(buys / 2)
       return {
+        id: idx + 1,
         name: collection.name,
         totalVolume,
         buys,
         latestSoldSize: this.toKSM(latestSale.meta),
         latestSoldTime: formatDistanceToNow(new Date(latestSale.timestamp)),
-        medianDate: nfts[medianIdx].timestamp,
+        medianDate: parseDate(nfts[medianIdx].timestamp),
         nfts,
       }
     })
