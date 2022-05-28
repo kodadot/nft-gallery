176f78405b72478e864d9ef4720cc9a742dbada1
Viki Val <viktorko99@gmail.com>
2022-05-28 15:36:38 +0200

    :tada: sandbox

---
+++ pages/sandbox/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>SanboxWork!</div>
+  <!-- <SandboxedGalleryItem /> -->
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+const components = {
+  SandboxedGalleryItem: () => import('@/components/sandbox/SandboxedGalleryItem.vue')
+}
+
+@Component({ components })
+export default class SandboxedGallery extends Vue {
+}
+</script>
