3613c4c6a0f7819e6fdcd6bb8c7a7d46e298cb5e
Viki Val <viktorko99@gmail.com>
2022-05-28 19:14:22 +0200

    :zap: Labeled text is here <3

---
+++ components/shared/gallery/LabeledText.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p class="label">
+      {{ $t(label) }}
+    </p>
+    <p class="subtitle is-size-6">
+      <slot>
+        <b-skeleton :active="isLoading"></b-skeleton>
+      </slot>
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class LabeledText extends Vue {
+  @Prop({ type: String, required: true, default: 'general.label' })
+  public label!: string
+  @Prop({ type: Boolean, default: false })
+  public isLoading!: string
+
+}
+</script>
