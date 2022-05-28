da4f512fab2545045a409db6c707f8c33589b438
Viki Val <viktorko99@gmail.com>
2022-05-28 15:18:54 +0200

    :zap: can check if it is v0 or v1 cid

--- utils/ipfs.ts
+++ utils/ipfs.ts
@@ -20,3 +20,15 @@ export const extractCid = (ipfsLink?: string): string => {
 
   return match[1]
 }
+
+const isV0Cid = (cid: string): boolean => {
+  return cid.length === 46 && cid.startsWith('Qm')
+}
+
+const isV1Cid = (cid: string): boolean => {
+  return cid.length === 59 && cid.startsWith('baf')
+}
+
+export const isCID = (ipfsLink: string): boolean => {
+  return isV0Cid(ipfsLink) || isV1Cid(ipfsLink)
+}
