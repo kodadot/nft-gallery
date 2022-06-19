e840e384c9e3c2b54a890ec319c8c1849bc11373
Viki Val <viktorko99@gmail.com>
2022-06-19 15:04:05 +0200

    :tada: Offers here

---
+++ components/bsx/Offer/OfferList.vue
@@ -0,0 +1,88 @@
+<template>
+  <CollapseCardWrapper label="offers">
+    <p class="title is-size-4 has-text-success">
+      {{ $t('nft.offer.count', [total]) }} 「🌱」
+    </p>
+    <OfferTable :offers="offers" @select="submit" />
+  </CollapseCardWrapper>
+</template>
+
+<script lang="ts">
+import { Component, mixins, Prop, Vue, Watch } from 'nuxt-property-decorator'
+import { isSameAccount } from '~/utils/account'
+import AuthMixin from '~/utils/mixins/authMixin'
+import MetaTransactionMixin from '~/utils/mixins/metaMixin'
+import { Offer, OfferResponse } from './types'
+import Connector from '@kodadot1/sub-api'
+import { notificationTypes, showNotification } from '~/utils/notification'
+import PrefixMixin from '~/utils/mixins/prefixMixin'
+import { createTokenId } from '~/components/unique/utils'
+import offerListByNftId from '@/queries/subsquid/bsx/offerListByNftId.graphql'
+
+const components = {
+  CollapseCardWrapper: () =>
+    import('@/components/shared/collapse/CollapseCardWrapper.vue'),
+  OfferTable: () => import('@/components/bsx/Offer/OfferTable.vue'),
+}
+
+@Component({ components })
+export default class OfferList extends mixins(
+  AuthMixin,
+  MetaTransactionMixin,
+  PrefixMixin
+) {
+  protected offers: Offer[] = []
+  protected total = 0
+  @Prop(String) public currentOwnerId!: string
+  @Prop(String) public collectionId!: string
+  @Prop(String) public nftId!: string
+
+  get isOwner(): boolean {
+    return Boolean(
+      this.currentOwnerId &&
+        this.accountId &&
+        isSameAccount(this.currentOwnerId, this.accountId)
+    )
+  }
+
+  fetch() {
+    // this.fetchData()
+  }
+
+  protected async fetchOffers() {
+    try {
+      const { data } = await this.$apollo.query<OfferResponse>({
+        client: this.urlPrefix,
+        query: offerListByNftId,
+        variables: { id: createTokenId(this.collectionId, this.nftId) },
+      })
+
+      this.offers = data.offers
+      this.total = 0
+    } catch (e) {
+      this.$consola.error(e)
+    }
+  }
+
+  protected async submit(maker: string) {
+    const { collectionId, nftId } = this
+    try {
+      const { api } = Connector.getInstance()
+      this.initTransactionLoader()
+      const cb = api.tx.marketplace.acceptOffer
+      const args = [collectionId, nftId, maker]
+
+      await this.howAboutToExecute(this.accountId, cb, args, (blockNumber) => {
+        showNotification(
+          `[OFFER] THIS NFT belongs to ${maker} since block ${blockNumber}`,
+          notificationTypes.success
+        )
+      })
+    } catch (e: any) {
+      showNotification(`[OFFER::ERR] ${e}`, notificationTypes.danger)
+      this.$consola.error(e)
+      this.isLoading = false
+    }
+  }
+}
+</script>
---
+++ components/bsx/Offer/OfferTable.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-table :data="attributes" :columns="columns" />
+</template>
+
+<script lang="ts">
+import { Attribute, emptyArray } from '@kodadot1/minimark'
+import { Component, Prop, Vue, Watch } from 'nuxt-property-decorator'
+import { Offer } from './types'
+
+@Component({})
+export default class OfferTable extends Vue {
+  @Prop({ type: Array, default: () => emptyArray<Attribute>() })
+  public offers!: Offer[]
+}
+</script>
