1448790f0d94c0059946678063679690b7d46bbc
Jarsen <jarsen.dev@gmail.com>
2022-07-05 22:58:31 +0800

    format price on bsx

--- components/shared/format/Money.vue
+++ components/shared/format/Money.vue
@@ -1,7 +1,13 @@
 <template>
   <div :class="['money', { 'is-inline-block': inline }]">
     <span v-if="!hideUnit">
-      {{ value | checkInvalidBalance | formatBalance(decimals, unit) }}
+      {{
+        value
+          | checkInvalidBalance
+          | formatBalance(decimals, '')
+          | round(2, !isBsx)
+      }}
+      {{ unit }}
     </span>
     <span v-else>
       {{ value | checkInvalidBalance | formatBalance(decimals, '') | round(2) }}
@@ -10,12 +16,21 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'nuxt-property-decorator'
+import { Component, Prop, mixins } from 'nuxt-property-decorator'
 import { checkInvalidBalanceFilter } from '@/utils/formatBalance'
+import PrefixMixin from '~/utils/mixins/prefixMixin'
+
 @Component({
   filters: {
     checkInvalidBalance: checkInvalidBalanceFilter,
-    round: function roundOffNumber(value, limit) {
+    round: function roundOffNumber(
+      value: string,
+      limit: number,
+      disableFilter: boolean
+    ) {
+      if (disableFilter) {
+        return value
+      }
       // `undefined` params in toLocaleString() means use host default language
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString#using_options
       return Number(value.replace(/,/g, '')).toLocaleString(undefined, {
@@ -25,12 +40,16 @@ import { checkInvalidBalanceFilter } from '@/utils/formatBalance'
     },
   },
 })
-export default class Money extends Vue {
+export default class Money extends mixins(PrefixMixin) {
   @Prop({ default: 0 }) readonly value: number | string | undefined
   @Prop(Boolean) readonly inline!: boolean
   @Prop(Boolean) readonly hideUnit!: boolean
 
   private readonly coinId: string = 'kusama'
+
+  get isBsx() {
+    return this.urlPrefix === 'bsx'
+  }
 
   get chainProperties() {
     return this.$store.getters['chain/getChainProperties']
