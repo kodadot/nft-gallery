d0099374dbd1b4934ff22cd7f102033bd06c701e
zhengow <313407040@qq.com>
2022-07-09 00:49:01 +0800

    feat: calculate 14days expiration for `offer`

--- components/bsx/Offer/OfferTable.vue
+++ components/bsx/Offer/OfferTable.vue
@@ -20,13 +20,12 @@
       <Money :value="props.row.price" inline />
     </b-table-column>
     <b-table-column
-      v-if="!isBsxStats"
+      v-if="isBsxStats"
       cell-class="is-vcentered is-narrow"
       field="expiration"
       :label="$t('offer.expiration')"
-      v-slot="props"
       sortable>
-      {{ props.row.expiration }}
+      {{ expiration }}
     </b-table-column>
     <b-table-column
       v-if="!isBsxStats"
@@ -91,6 +90,7 @@ import { Attribute, emptyArray } from '@kodadot1/minimark'
 import { Component, Emit, Prop, Vue } from 'nuxt-property-decorator'
 import { Offer } from './types'
 import { formatDistanceToNow } from 'date-fns'
+import onApiConnect from '~/utils/api/general'
 
 const components = {
   Identity: () => import('@/components/shared/format/Identity.vue'),
@@ -104,6 +104,19 @@ export default class OfferTable extends Vue {
   @Prop(Boolean) public isOwner!: boolean
   @Prop(String) public accountId!: string
   @Prop(Boolean) public isBsxStats!: boolean
+  private expiration = 'loading' // expiration = current block + number of blocks * 14
+
+  created() {
+    onApiConnect(async (api) => {
+      const BLOCK_OFFSET = 5 // time between submit & finalization
+      const BLOCK_PER_DAY_COUNT = 7200 // 7200 = 86400 / 12
+      const DAY_COUNT = 14 // two weeks
+      const currentBlock = await api.query.system.number()
+      const expiration =
+        currentBlock.toNumber() + BLOCK_OFFSET + BLOCK_PER_DAY_COUNT * DAY_COUNT
+      this.expiration = expiration.toString()
+    })
+  }
 
   @Emit('select')
   tellFrens(caller: string) {
