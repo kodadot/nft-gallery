a9771588f85c7d661efb6e9fda375cf9bedb23b9
zhengow <313407040@qq.com>
2022-07-18 21:39:32 +0800

    feat: Expand ability to set offer expiration in days

---
+++ components/base/DaysSelect.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-field :label="$t('offer expires in')">
+    <b-select placeholder="Select a collection" v-model="selectedDays" expanded>
+      <option disabled selected value="">--</option>
+      <option v-for="option in days" :value="option" :key="option">
+        {{ option }} {{ $t('days') }}
+      </option>
+    </b-select>
+  </b-field>
+</template>
+
+<script lang="ts">
+import { Component, Vue, VModel, Prop } from 'nuxt-property-decorator'
+import { BaseMintedCollection as MintedCollection } from './types'
+
+@Component({})
+export default class CollectionSelect extends Vue {
+  @VModel({ default: 14 }) selectedDays!: number
+  @Prop({ type: Array, default: () => [] }) days!: number[]
+}
+</script>
