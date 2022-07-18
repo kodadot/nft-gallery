64e7a45331df58dc74305196c1d92bfad1f7d7df
petersopko <petersopko@protonmail.com>
2022-07-18 16:10:08 +0200

    add_random_wallet_login

---
+++ pages/e2e-login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'nuxt-property-decorator'
+import { mnemonicGenerate } from '@polkadot/util-crypto'
+import keyring from '@polkadot/ui-keyring'
+
+@Component({})
+export default class E2ELogin extends Vue {
+  created() {
+    const mnemonic = mnemonicGenerate(12)
+    const { pair, json } = keyring.addUri(mnemonic, '', {
+      name: 'mnemonic acc',
+    })
+    keyring.addPair(pair, '')
+    const account = pair.address
+    this.$store.dispatch('setAuth', { address: account })
+    localStorage.setItem('kodaauth', account)
+    this.$router.push('/')
+  }
+}
+</script>
