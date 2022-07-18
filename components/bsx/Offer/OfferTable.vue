01778d2aba2da62ed335be7688ce36076b03a512
zhengow <313407040@qq.com>
2022-07-18 23:20:38 +0800

    replace longer part to `...`

--- components/bsx/Offer/OfferTable.vue
+++ components/bsx/Offer/OfferTable.vue
@@ -164,7 +164,7 @@ export default class OfferTable extends Vue {
 </script>
 <style scoped>
 .limit-width-text {
-  max-width: 50ch;
+  max-width: 20ch;
   overflow: hidden;
   text-overflow: ellipsis;
 }
