a96d28f19d28cc8db7faa157d0bae70843d31009
Viki Val <viktorko99@gmail.com>
2022-07-19 14:01:37 +0200

    :construction: new apin in components/unique/apiConstants.ts

--- components/unique/apiConstants.ts
+++ components/unique/apiConstants.ts
@@ -1,21 +1,17 @@
 import type { ApiPromise } from '@polkadot/api'
 
 export function getInstanceDeposit(api: ApiPromise): bigint {
-  // const { api } = Connector.getInstance()
   return api.consts?.uniques?.instanceDeposit?.toBigInt()
 }
 
 export function getMetadataDeposit(api: ApiPromise): bigint {
-  // const { api } = Connector.getInstance()
   return api.consts?.uniques?.metadataDepositBase?.toBigInt()
 }
 
 export function getDepositPerByte(api: ApiPromise): bigint {
-  // const { api } = Connector.getInstance()
   return api.consts?.uniques?.depositPerByte?.toBigInt()
 }
 
 export function getclassDeposit(api: ApiPromise): bigint {
-  // const { api } = Connector.getInstance()
   return api.consts?.uniques?.classDeposit?.toBigInt()
 }
