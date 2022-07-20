1c4feb520d5ca856114960462e4740ad903fe91a
Viki Val <viktorko99@gmail.com>
2022-07-20 16:06:03 +0200

    :broom: cleaned up configs

---
+++ utils/config/chain.config.ts
@@ -0,0 +1,29 @@
+import { ChainProperties } from '../api/Query'
+import { Config, Prefix } from './types'
+
+export const toChainProperty = (
+  ss58Format: number,
+  tokenDecimals: number,
+  tokenSymbol: string
+): ChainProperties => {
+  return {
+    ss58Format,
+    tokenDecimals,
+    tokenSymbol,
+  }
+}
+
+const DEFAULT_CHAIN_PROPERTIES: ChainProperties = toChainProperty(2, 12, 'KSM')
+
+const chainPropertyMap: Config<ChainProperties> = {
+  rmrk: DEFAULT_CHAIN_PROPERTIES,
+  bsx: toChainProperty(10041, 12, 'BSX'),
+  statemine: DEFAULT_CHAIN_PROPERTIES,
+  westmint: DEFAULT_CHAIN_PROPERTIES,
+  moonsama: toChainProperty(1285, 12, 'MOVR'),
+}
+
+export const chainPropListOf = (prefix: Prefix | string): ChainProperties => {
+  console.log('prefix2: ', prefix, chainPropertyMap[prefix])
+  return chainPropertyMap[prefix]
+}
