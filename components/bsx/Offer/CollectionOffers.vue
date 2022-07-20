6ba4a1807842d953c5fc45c5ff8b99ea0d4a26f8
zhengow <313407040@qq.com>
2022-07-21 00:21:13 +0800

    feat: add filter status

--- components/bsx/Offer/CollectionOffers.vue
+++ components/bsx/Offer/CollectionOffers.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
-    <Loader v-model="isLoading" :status="status" />
+    <b-select v-model="selectedStatus">
+      <option
+        v-for="option in uniqType"
+        :value="option.type"
+        :key="option.type">
+        {{ option.value }}
+      </option>
+    </b-select>
     <OfferTable
-      :offers="offers"
+      :offers="showOffers"
       :accountId="accountId"
       is-bsx-stats
       is-collection />
@@ -12,32 +19,53 @@
 <script lang="ts">
 import { Component, mixins, Prop } from 'nuxt-property-decorator'
 import AuthMixin from '~/utils/mixins/authMixin'
-import MetaTransactionMixin from '~/utils/mixins/metaMixin'
 import { Offer, OfferResponse } from './types'
 import PrefixMixin from '~/utils/mixins/prefixMixin'
 import offerListByCollectionId from '@/queries/subsquid/bsx/offerListByCollectionId.graphql'
+import { AllOfferStatusType } from '~/utils/offerStatus'
 
 const components = {
-  Loader: () => import('@/components/shared/Loader.vue'),
   OfferTable: () => import('@/components/bsx/Offer/OfferTable.vue'),
 }
 
 @Component({ components })
-export default class CollectionOffers extends mixins(
-  AuthMixin,
-  MetaTransactionMixin,
-  PrefixMixin
-) {
+export default class CollectionOffers extends mixins(AuthMixin, PrefixMixin) {
   @Prop(String) public collectionId!: string
   protected offers: Offer[] = []
-  protected total = 0
+  protected showOffers: Offer[] = []
+  protected status: AllOfferStatusType = AllOfferStatusType.ALL
+  // protected uniqType = [{value: 'All', type: 'all'}, {value: 'Test', type: 'test'}]
+
+  get uniqType(): { type: AllOfferStatusType; value: string }[] {
+    const statusSet = new Set(this.offers.map((offer) => offer.status))
+    const singleEventList = Array.from(statusSet).map((type) => ({
+      type: type as AllOfferStatusType,
+      value: AllOfferStatusType[type],
+    }))
+    return [{ type: AllOfferStatusType.ALL, value: 'All' }, ...singleEventList]
+  }
 
   fetch() {
     this.fetchOffers()
   }
 
+  get selectedStatus() {
+    return this.status
+  }
+
+  set selectedStatus(value: AllOfferStatusType) {
+    if (value === AllOfferStatusType.ALL) {
+      this.showOffers = this.offers.concat()
+    } else {
+      this.showOffers = this.offers.filter((offer) => offer.status === value)
+    }
+    this.status = value
+  }
+
   protected setResponse(response: OfferResponse) {
     this.offers = response.offers
+    console.log(response.offers)
+    this.showOffers = this.offers.concat()
   }
 
   protected async fetchOffers() {
