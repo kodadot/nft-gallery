b707efee3ad2bcceedf324405c90556287f262f4
Viki Val <viktorko99@gmail.com>
2022-07-25 13:14:50 +0200

    :coffin: removed dead singleton connection

--- plugins/endpoint.ts
+++ plugins/endpoint.ts
@@ -1,9 +1,1 @@
 import '@polkadot/api-augment'
-// import Connector from '@kodadot1/sub-api'
-
-// export default ({ store }): void => {
-//   const endpoint = store.state.setting.apiUrl
-//   const { getInstance: Api } = Connector
-//   console.log('[API PLUGIN]', endpoint)
-//   Api().connect(endpoint)
-// }
