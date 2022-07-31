27e50d534fb75dce936de90d651f99b797970bef
Prachi Darshana <prachirai00@gmail.com>
2022-07-30 17:13:08 -0700

    #3333 Add explainer text in create nft

--- components/base/CollectionSelect.vue
+++ components/base/CollectionSelect.vue
@@ -1,17 +1,24 @@
 <template>
-  <b-field
-    :label="$t('collection')"
-    :message="$t('Select collection where do you want mint your token')">
-    <b-select
-      placeholder="Select a collection"
-      v-model="selectedCollection"
-      expanded>
-      <option disabled selected value="">--</option>
-      <option v-for="option in collections" :value="option" :key="option.id">
-        {{ option.name || option.id }} ({{ option.totalCount }})
-      </option>
-    </b-select>
-  </b-field>
+  <div>
+    <div class="mb-2">
+      <small>
+        {{ $t('createNftExplainer') }}
+      </small>
+    </div>
+    <b-field
+      :label="$t('collection')"
+      :message="$t('Select collection where do you want mint your token')">
+      <b-select
+        placeholder="Select a collection"
+        v-model="selectedCollection"
+        expanded>
+        <option disabled selected value="">--</option>
+        <option v-for="option in collections" :value="option" :key="option.id">
+          {{ option.name || option.id }} ({{ option.totalCount }})
+        </option>
+      </b-select>
+    </b-field>
+  </div>
 </template>
 
 <script lang="ts">
