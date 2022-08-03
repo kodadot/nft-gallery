f2ce063ae8feeb95a2db6a65e14f82b658d834c0
Preschian Febryantara <preschian27@gmail.com>
2022-08-03 17:26:59 +0700

    remove data by average

--- components/bsx/Collection/ChartContainer.vue
+++ components/bsx/Collection/ChartContainer.vue
@@ -6,7 +6,6 @@
 
 <script lang="ts">
 import { Component, mixins } from 'nuxt-property-decorator'
-import format from 'date-fns/format'
 import PrefixMixin from '@/utils/mixins/prefixMixin'
 import ChainMixin from '@/utils/mixins/chainMixin'
 import formatBalance from '@/utils/formatBalance'
@@ -63,26 +62,16 @@ export default class ChartContainer extends mixins(PrefixMixin, ChainMixin) {
 
     for (const [index, element] of data.entries()) {
       const items = element.data.events
-      const average = {}
 
       for (const item of items) {
         const value = this.formatValue(item.meta)
-        const label = format(new Date(item.timestamp), 'dd MMM yy')
 
-        if (!average[label]) {
-          average[label] = {
-            count: 0,
-            value: 0,
-          }
-        }
-        average[label] = {
-          count: average[label].count + 1,
-          value: average[label].value + value / (average[label].count || 1),
-          date: new Date(item.timestamp),
-        }
+        this.priceData[index].push({
+          count: 1,
+          value,
+          date: item.timestamp,
+        })
       }
-
-      this.priceData[index].push(...(Object.values(average) as ChartData[]))
     }
   }
 }
