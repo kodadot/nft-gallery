1989c3744f35885fddba91843fc03ca95ef3b44a
Preschian Febryantara <preschian27@gmail.com>
2022-08-04 20:40:18 +0700

    use nested description in test case

--- components/shared/BalanceInput.test.ts
+++ components/shared/BalanceInput.test.ts
@@ -8,82 +8,100 @@ import BalanceInput from '@/components/shared/BalanceInput.vue'
 const localVue = createLocalVue()
 localVue.use(Buefy)
 
-test('BalanceInput.vue', async () => {
+let wrapper, input, options
+
+describe('BalanceInput.vue', async () => {
   vi.useFakeTimers()
 
-  const wrapper = mount(BalanceInput, {
-    attachTo: document.body,
-    mocks: {
-      $t: () => '',
-      $config: {
-        prefix: 'rmrk',
+  beforeAll(() => {
+    wrapper = mount(BalanceInput, {
+      propsData: {
+        min: 10,
+        max: 100,
       },
-      $store: {
-        getters: {
-          currentUrlPrefix: 'rmrk',
+      attachTo: document.body,
+      mocks: {
+        $t: () => '',
+        $config: {
+          prefix: 'rmrk',
+        },
+        $store: {
+          getters: {
+            currentUrlPrefix: 'rmrk',
+          },
         },
       },
-    },
-    localVue,
+      localVue,
+    })
+    input = wrapper.find('[data-testid="balance-input"]')
+    options = wrapper
+      .find('[data-testid="balance-input-select"]')
+      .findAll('option')
   })
-  const input = wrapper.find('[data-testid="balance-input"]')
-  const options = wrapper
-    .find('[data-testid="balance-input-select"]')
-    .findAll('option')
 
-  // test focus
-  await (wrapper.vm as any).focusInput()
-  expect(input.element).toBe(document.activeElement)
+  it('should focus on input element', async () => {
+    await (wrapper.vm as any).focusInput()
+    expect(input.element).toBe(document.activeElement)
+  })
 
-  // check html5validity
-  await wrapper.setProps({ min: 10, max: 100 })
-  await input.setValue('0')
-  expect(await (wrapper.vm as any).checkValidity()).toBe(false)
-  await input.setValue('50')
-  expect(await (wrapper.vm as any).checkValidity()).toBe(true)
-  await input.setValue('900')
-  expect(await (wrapper.vm as any).checkValidity()).toBe(false)
+  it('should check input with html5validity', async () => {
+    await input.setValue('0')
+    expect(await (wrapper.vm as any).checkValidity()).toBe(false)
+    await input.setValue('50')
+    expect(await (wrapper.vm as any).checkValidity()).toBe(true)
+    await input.setValue('900')
+    expect(await (wrapper.vm as any).checkValidity()).toBe(false)
+  })
 
-  // set value from props
-  await wrapper.setProps({ value: 3 })
-  expect((input.element as HTMLInputElement).value).toBe('3')
+  it('should set proper value from props', async () => {
+    await wrapper.setProps({ value: 3 })
+    expect((input.element as HTMLInputElement).value).toBe('3')
+  })
 
-  // check value
-  await input.setValue('1')
-  vi.runAllTimers()
-  expect(wrapper.emitted().input).toBeTruthy()
-  expect(wrapper.emitted().input?.at(-1)).toStrictEqual(['1000000000000', '1'])
+  it('should convert value based on unit', async () => {
+    await input.setValue('1')
+    vi.runAllTimers()
+    expect(wrapper.emitted().input).toBeTruthy()
+    expect(wrapper.emitted().input?.at(-1)).toStrictEqual([
+      '1000000000000',
+      '1',
+    ])
+  })
 
-  // change unit
-  await options.at(6).setSelected()
-  await input.setValue('2')
-  expect(wrapper.find('option:checked').element.textContent).toBe(' Kilo ')
-  vi.runAllTimers()
-  expect(wrapper.emitted().input).toBeTruthy()
-  expect(wrapper.emitted().input?.at(-1)).toStrictEqual([
-    '2000000000000000000',
-    '2',
-  ])
+  it('should convert value based on selected unit', async () => {
+    await options.at(6).setSelected()
+    await input.setValue('2')
+    expect(wrapper.find('option:checked').element.textContent).toBe(' Kilo ')
+    vi.runAllTimers()
+    expect(wrapper.emitted().input).toBeTruthy()
+    expect(wrapper.emitted().input?.at(-1)).toStrictEqual([
+      '2000000000000000000',
+      '2',
+    ])
+  })
 
-  // if props calculate = false
-  await options.at(5).setSelected()
-  await wrapper.setProps({ calculate: false })
-  await input.setValue('4')
-  vi.runAllTimers()
-  expect(wrapper.emitted().input).toBeTruthy()
-  expect(wrapper.emitted().input?.at(-1)).toStrictEqual([4, '4'])
+  it('should not calculate the unit if set calculate to false through props', async () => {
+    await options.at(5).setSelected()
+    await wrapper.setProps({ calculate: false })
+    await input.setValue('4')
+    vi.runAllTimers()
+    expect(wrapper.emitted().input).toBeTruthy()
+    expect(wrapper.emitted().input?.at(-1)).toStrictEqual([4, '4'])
+  })
 
-  // if value invalid
-  await wrapper.setProps({ calculate: true })
-  await input.setValue('+e-')
-  vi.runAllTimers()
-  expect(wrapper.emitted().input).toBeTruthy()
-  expect(wrapper.emitted().input?.at(-1)).toStrictEqual(['0', ''])
+  it('should check if the value is invalid', async () => {
+    await wrapper.setProps({ calculate: true })
+    await input.setValue('+e-')
+    vi.runAllTimers()
+    expect(wrapper.emitted().input).toBeTruthy()
+    expect(wrapper.emitted().input?.at(-1)).toStrictEqual(['0', ''])
+  })
 
-  // if value zero
-  await input.setValue('0')
-  vi.runAllTimers()
-  await options.at(6).setSelected()
-  expect(wrapper.emitted().input).toBeTruthy()
-  expect(wrapper.emitted().input?.at(-1)).toStrictEqual(['0', '0'])
+  it('should check if the value is zero', async () => {
+    await input.setValue('0')
+    vi.runAllTimers()
+    await options.at(6).setSelected()
+    expect(wrapper.emitted().input).toBeTruthy()
+    expect(wrapper.emitted().input?.at(-1)).toStrictEqual(['0', '0'])
+  })
 })
