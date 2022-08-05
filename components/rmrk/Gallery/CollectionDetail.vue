69a35208bf4cd1c7b0216197d00709456c939c94
Junwei Chen <kngzhi@hotmail.com>
2022-08-05 20:17:29 +0800

    feat: calc volume

--- components/rmrk/Gallery/CollectionDetail.vue
+++ components/rmrk/Gallery/CollectionDetail.vue
@@ -48,7 +48,7 @@
 <script lang="ts">
 import { Component, Prop, Vue } from 'nuxt-property-decorator'
 import { Interaction, NFT } from '@/components/rmrk/service/scheme'
-import { getVolume, pairListBuyEvent } from '@/utils/math'
+import { getVolume } from '@/utils/math'
 
 const components = {
   Money: () => import('@/components/shared/format/Money.vue'),
@@ -60,10 +60,7 @@ export default class CollectionDetail extends Vue {
   @Prop() public name!: string
 
   get saleEvents(): Interaction[] {
-    return this.nfts
-      .map((nft) => nft.events)
-      .map(pairListBuyEvent)
-      .flat()
+    return this.nfts.map((nft) => nft.events).flat()
   }
 
   get collectionLength(): number {
