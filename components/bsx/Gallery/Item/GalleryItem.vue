1780867af889add0ab4755387eb2bb6bb60ee1f9
Preschian Febryantara <preschian27@gmail.com>
2022-08-06 17:23:56 +0700

    fix: bsx - not show collection name after fresh mint

--- components/bsx/Gallery/Item/GalleryItem.vue
+++ components/bsx/Gallery/Item/GalleryItem.vue
@@ -301,7 +301,7 @@ export default class GalleryItem extends mixins(
   }
 
   public async fetchCollectionItems() {
-    const collectionId = this.nft?.collection.id
+    const collectionId = this.nft?.collection?.id
     if (collectionId) {
       // cancel request and get ids from store in case we already fetched collection data before
       if (this.$store.state.history?.currentCollection?.id === collectionId) {
@@ -352,7 +352,7 @@ export default class GalleryItem extends mixins(
     availableActions.unlistNft()
   }
 
-  private async fetchNftData() {
+  private async fetchNftData(retryCount = 0) {
     const query = await resolveQueryPath(this.urlPrefix, 'nftById')
     const nft = await this.$apollo.query({
       query: query.default,
@@ -368,7 +368,11 @@ export default class GalleryItem extends mixins(
     if (!nftEntity) {
       this.$consola.warn(`No NFT with ID ${this.id} fallback to RPC Node`)
       this.fetchRPCMetadata()
-      // showNotification(`No NFT with ID ${this.id}`, notificationTypes.warn)
+
+      if (retryCount < 3) {
+        this.fetchNftData(retryCount + 1)
+      }
+
       return
     }
 
