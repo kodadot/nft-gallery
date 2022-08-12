18ec35d08bb9acf6bcd4236229b498780c5d2d92
Viki Val <viktorko99@gmail.com>
2022-08-12 18:24:26 +0200

    :construction_worker: account balance will also handle tokens

--- components/shared/AccountBalance.vue
+++ components/shared/AccountBalance.vue
@@ -1,18 +1,26 @@
 <template>
   <p class="subtitle is-size-6" v-if="accountId">
     <span>{{ $t('general.balance') }}: </span>
-    <Money :value="balance" inline />
+    <TokenMoney v-if="tokenId" :value="balance" :tokenId="tokenId" inline />
+    <Money v-else :value="balance" inline />
   </p>
 </template>
 
 <script lang="ts">
-import { Component, mixins } from 'nuxt-property-decorator'
+import { Component, mixins, Prop } from 'nuxt-property-decorator'
 import AuthMixin from '@/utils/mixins/authMixin'
 
 const components = {
   Money: () => import('@/components/shared/format/Money.vue'),
+  TokenMoney: () => import('@/components/bsx/format/TokenMoney.vue'),
 }
 
 @Component({ components })
-export default class AccountBalance extends mixins(AuthMixin) {}
+export default class AccountBalance extends mixins(AuthMixin) {
+  @Prop({ type: String, default: '' }) readonly tokenId!: string
+
+  get realBalance(): string {
+    return !this.tokenId ? this.balance : '0'
+  }
+}
 </script>
