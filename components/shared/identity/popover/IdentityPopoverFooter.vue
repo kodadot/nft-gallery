59814002509f27d0a4e638e15725d8574563d86f
Preschian Febryantara <preschian27@gmail.com>
2022-08-16 16:59:48 +0700

    remove unused attrs

--- components/shared/identity/popover/IdentityPopoverFooter.vue
+++ components/shared/identity/popover/IdentityPopoverFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="" class="popover-stats-container pt-3">
+  <div class="popover-stats-container pt-3">
     <div class="has-text-centered">
       <p class="has-text-weight-bold is-size-6">{{ totalCollected }}</p>
       <span class="is-size-7 is-uppercase">{{ $t('profile.collected') }}</span>
