2c97c16bfe3599ad66e8b3bcc4f0c4a86b914050
Junwei Chen <kngzhi@hotmail.com>
2022-08-17 18:18:34 +0800

    feat: message notify component

---
+++ components/shared/MessageNotify.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-message class="message-box" type="is-primary">
+      <div class="columns">
+        <div class="column is-four-fifths">
+          <p class="title is-3 has-text-white">
+            {{ title }}
+          </p>
+          <p class="subtitle is-size-5 subtitle-text">
+            {{ subtitle }}
+          </p>
+        </div>
+        <div class="column">
+          <Sharing :enable-download="enableDownload" is-primary="{true}" />
+        </div>
+      </div>
+    </b-message>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+@Component<MessageNotify>({
+  components: {
+    Sharing: () => import('@/components/rmrk/Gallery/Item/Sharing.vue'),
+  },
+})
+export default class MessageNotify extends Vue {
+  @Prop() public title!: string
+  @Prop() public subtitle!: string
+  @Prop({ default: false }) public enableDownload?: boolean
+}
+</script>
