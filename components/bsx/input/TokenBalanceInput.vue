4d1281808bd975d8809909cca2c036ea774aeb8a
Viki Val <viktorko99@gmail.com>
2022-08-18 16:15:17 +0200

    :construction_worker: WIP TokenBalanceInput is Using store

--- components/bsx/input/TokenBalanceInput.vue
+++ components/bsx/input/TokenBalanceInput.vue
@@ -8,18 +8,19 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, VModel, Vue } from 'nuxt-property-decorator'
+import { Component, mixins, Prop, VModel, Vue } from 'nuxt-property-decorator'
 import BasicBalanceInput from '@/components/shared/form/BasicBalanceInput.vue'
 import { useApollo } from '~/utils/config/useApollo'
 import assetById from '@/queries/subsquid/bsx/assetById.graphql'
 import { AssetItem } from '../Asset/types'
+import AssetMixin from '@/utils/mixins/assetMixin'
 
 @Component({
   components: {
     BasicBalanceInput,
   },
 })
-export default class TokenBalanceInput extends Vue {
+export default class TokenBalanceInput extends mixins(AssetMixin) {
   @VModel() vValue!: string | number
   @Prop({ type: String, required: false, default: '5' }) tokenId!:
     | string
@@ -30,6 +31,18 @@ export default class TokenBalanceInput extends Vue {
 
   protected unit = 'BSX'
   protected decimals = 12
+
+  // get asset() {
+  //   return this.assetIdOf(this.tokenId)
+  // }
+
+  // get unit() {
+  //   return this.asset.symbol
+  // }
+
+  // get decimals() {
+  //   return this.asset.decimals
+  // }
 
   // TODO: rework into the store
   async fetch() {
