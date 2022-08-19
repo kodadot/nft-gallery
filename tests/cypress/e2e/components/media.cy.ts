3cecc1be3c65229dc81ea862a005c9d0d6070429
Preschian Febryantara <preschian@users.noreply.github.com>
2022-08-19 09:10:25 +0700

    adjust test case

--- tests/cypress/e2e/components/media.cy.ts
+++ tests/cypress/e2e/components/media.cy.ts
@@ -5,7 +5,6 @@ const mediaType = [
     description: 'Mutagen is mined in a Laboratory',
     collection: 'Kusama Ape Game',
     creator: 'Kusama Ape Club',
-    tag: 'img',
     tagRelated: 'img',
     type: 'image',
   },
@@ -15,9 +14,8 @@ const mediaType = [
     description: 'Class: "Zaphkiel" Cruiser',
     collection: 'Budget Spaceships',
     creator: 'cwh',
-    tag: 'model-viewer',
     tagRelated: 'model-viewer',
-    type: '3D model',
+    type: '3d',
   },
   {
     url: '/rmrk/gallery/9684463-c8205518d881699b3e-%E2%9D%84%EF%B8%8F-01_FROM_SINGULARITY-0000000000000005',
@@ -25,7 +23,6 @@ const mediaType = [
     description: '',
     collection: 'glaciers mood',
     creator: 'fr.om',
-    tag: 'audio',
     tagRelated: 'img',
     type: 'audio',
   },
@@ -35,71 +32,53 @@ const mediaType = [
     description: '',
     collection: '[ polkawear ]',
     creator: 'mad4ox',
-    tag: 'video',
     tagRelated: 'img',
     type: 'video',
   },
 ]
 
-describe('render Media.vue component', () => {
+describe('Media component', () => {
   mediaType.forEach(
-    ({
-      url,
-      title,
-      description,
-      collection,
-      creator,
-      tag,
-      tagRelated,
-      type,
-    }) => {
-      describe(`should render ${type}`, () => {
-        beforeEach(() => {
-          cy.visit(url)
-          cy.waitForNetworkIdle('+(GET|HEAD)', '*', 2000)
-        })
+    ({ url, title, description, collection, creator, tagRelated, type }) => {
+      it(`should render ${type} in Media component`, () => {
+        cy.visit(url)
+        cy.getCy(`type-${type}`)
+          .should('exist')
+          .then(() => {
+            // title
+            cy.getCy('item-title').should('contain.text', title)
 
-        it('should render item title', () => {
-          cy.getCy('item-title').should('contain.text', title)
-        })
+            // collection name
+            cy.getCy('item-collection').should('contain.text', collection)
 
-        it('should render item collection name', () => {
-          cy.getCy('item-collection').should('contain.text', collection)
-        })
+            // creator name
+            cy.getCy('item-creator').should('contain.text', creator)
 
-        it('should render item creator name', () => {
-          cy.getCy('item-creator').should('contain.text', creator)
-        })
+            cy.get('body').then(($body) => {
+              // owner
+              if ($body.find('[data-cy="item-owner"]').length) {
+                cy.getCy('item-owner')
+                  .find('[data-cy="identity"]')
+                  .should('exist')
+              }
 
-        it('should check owner or description', () => {
-          cy.get('body').then(($body) => {
-            if ($body.find('[data-cy="item-owner"]').length) {
-              cy.getCy('item-owner')
-                .find('[data-cy="identity"]')
-                .should('exist')
-            }
+              // description
+              if ($body.find('[data-cy="item-description"]').length) {
+                cy.getCy('item-description')
+                  .find('.description-wrapper')
+                  .should('contain.text', description)
+              }
+            })
 
-            if ($body.find('[data-cy="item-description"]').length) {
-              cy.getCy('item-description')
-                .find('.description-wrapper')
-                .should('contain.text', description)
-            }
+            // same item in related carousel
+            cy.getCy('carousel-related').find(tagRelated).should('exist')
+
+            // item in collection list
+            cy.getCy('item-collection').should('exist').click()
+            cy.location('pathname').should('include', '/collection/')
+            cy.waitForNetworkIdle('+(GET|HEAD)', '*', 1000)
+            cy.getCy(`type-${type}`).should('exist')
           })
-        })
-
-        it('should render item', () => {
-          cy.getCy('item-media').find(tag).should('exist')
-        })
-
-        it('should render item in related carousel', () => {
-          cy.getCy('carousel-related').find(tagRelated).should('exist')
-        })
-
-        it('should render item in collection list', () => {
-          cy.getCy('item-collection').click()
-          cy.waitForNetworkIdle('+(GET|HEAD)', '*', 1000)
-          cy.getCy('0').find(tag).should('exist')
-        })
       })
     }
   )
