b8daa88ea677961c71c63bba51fa4928866cd620
Preschian Febryantara <preschian27@gmail.com>
2022-08-21 18:42:28 +0700

    increase idle time

--- tests/cypress/e2e/components/media.cy.ts
+++ tests/cypress/e2e/components/media.cy.ts
@@ -85,7 +85,7 @@ describe('Media component', () => {
         cy.getCy('item-collection').scrollIntoView().click()
         cy.location('pathname').should('include', '/collection/')
         cy.getCy('small-display').click().scrollIntoView()
-        cy.waitForNetworkIdle('*', '*', 1000)
+        cy.waitForNetworkIdle('*', '*', 3000)
         cy.document().then((doc) => {
           const totalItems = doc.querySelectorAll(
             `#infinite-scroll-container ${tagRelated}`
