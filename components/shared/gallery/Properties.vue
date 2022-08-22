c4abaf86f4d9d4dc2fd8b814eb8119c6adf41831
Jarsen <695552027@qq.com>
2022-08-22 23:29:07 +0800

    remove scoped

--- components/shared/gallery/Properties.vue
+++ components/shared/gallery/Properties.vue
@@ -7,7 +7,7 @@
         class="column is-4">
         <div class="notification is-primary is-light">
           <div
-            class="is-size-7 has-text-grey-lighter is-uppercase attribute-key">
+            class="is-size-7 has-text-grey-lighter is-uppercase properties-attribute-key">
             {{ attribute[fieldKey] === 'undefined' ? '' : attribute[fieldKey] }}
           </div>
           <div class="is-size-5 has-text-white">{{ attribute.value }}</div>
@@ -34,8 +34,8 @@ export default class Properties extends Vue {
   @Prop({ type: String, default: 'trait_type' }) public fieldKey!: string
 }
 </script>
-<style scoped>
-.attribute-key {
+<style>
+.properties-attribute-key {
   min-height: 18px;
 }
 </style>
