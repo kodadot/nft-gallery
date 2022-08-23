3cfbc91fa1598028b6d54e15525b8c1ae115c037
Viki Val <viktorko99@gmail.com>
2022-08-23 14:11:50 +0200

    :zap: TokenBalanceInput powered by vuex

--- components/bsx/input/TokenBalanceInput.vue
+++ components/bsx/input/TokenBalanceInput.vue
@@ -8,12 +8,9 @@
 </template>
 
 <script lang="ts">
-import { Component, mixins, Prop, VModel } from 'nuxt-property-decorator'
 import BasicBalanceInput from '@/components/shared/form/BasicBalanceInput.vue'
-import { useApollo } from '~/utils/config/useApollo'
-import assetById from '@/queries/subsquid/bsx/assetById.graphql'
-import { AssetItem } from '../Asset/types'
 import AssetMixin from '@/utils/mixins/assetMixin'
+import { Component, Prop, VModel, mixins } from 'nuxt-property-decorator'
 
 @Component({
   components: {
@@ -25,37 +22,19 @@ export default class TokenBalanceInput extends mixins(AssetMixin) {
   @Prop({ type: String, required: false, default: '5' }) tokenId!:
     | string
     | number
-  @Prop({ type: String, default: 'bsx' }) prefix!: string
   // @Prop({ type: Number, default: '0' }) min!: number
   // @Prop({ type: Number, default: undefined }) max!: number
 
-  protected unit = 'BSX'
-  protected decimals = 12
+  get asset() {
+    return this.assetIdOf(this.tokenId)
+  }
 
-  // get asset() {
-  //   return this.assetIdOf(this.tokenId)
-  // }
+  get unit() {
+    return this.asset.symbol
+  }
 
-  // get unit() {
-  //   return this.asset.symbol
-  // }
-
-  // get decimals() {
-  //   return this.asset.decimals
-  // }
-
-  // TODO: rework into the store
-  async fetch() {
-    const { asset } = await useApollo<this, { asset: AssetItem }>(
-      this.$apollo,
-      this.prefix,
-      assetById,
-      { id: this.tokenId }
-    )
-    if (asset) {
-      this.unit = asset.symbol
-      this.decimals = asset.decimals
-    }
+  get decimals() {
+    return this.asset.decimals
   }
 
   public checkValidity() {
