036c049a07736e1f3d541caf11d5dd6b8426e3a9
kkukelka <17953978+kkukelka@users.noreply.github.com>
2022-08-25 09:00:20 +0200

    refactor: dont use hasOwnProperty

--- tests/utils/graphql-test-utils.ts
+++ tests/utils/graphql-test-utils.ts
@@ -1,10 +1,7 @@
 // Utility to match GraphQL mutation based on the operation name
 export const hasOperationName = (req, operationName) => {
   const { body } = req
-  return (
-    // eslint-disable-next-line no-prototype-builtins
-    body.hasOwnProperty('operationName') && body.operationName === operationName
-  )
+  return body?.operationName && body.operationName === operationName
 }
 
 // Alias query if operationName matches
