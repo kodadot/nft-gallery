5c02f798988c6046b471834453ec4d1770629f22
Preschian Febryantara <preschian27@gmail.com>
2022-08-29 20:32:54 +0700

    show visited if > 3

--- components/carousel/CarouselTypeVisited.vue
+++ components/carousel/CarouselTypeVisited.vue
@@ -1,10 +1,16 @@
 <template>
-  <CarouselIndex :title="`${$t('nft.visited')}`" :nfts="nfts" />
+  <CarouselIndex
+    v-if="showCarousel"
+    :title="`${$t('nft.visited')}`"
+    :nfts="nfts" />
 </template>
 
 <script lang="ts" setup>
+import { MIN_CAROUSEL_NFT } from '@/utils/constants'
+
 import CarouselIndex from './CarouselIndex.vue'
 import { useCarouselVisited } from './utils/useCarousel'
 
 const { nfts } = useCarouselVisited()
+const showCarousel = computed(() => nfts.value.length > MIN_CAROUSEL_NFT)
 </script>
