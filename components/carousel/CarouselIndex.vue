b92ad993f4e18a1ced1292531a0fc340cc675fe3
Preschian Febryantara <preschian27@gmail.com>
2022-08-31 16:13:39 +0700

    add use experiments

--- components/carousel/CarouselIndex.vue
+++ components/carousel/CarouselIndex.vue
@@ -1,35 +1,45 @@
 <template>
-  <div v-if="nfts.length" class="my-5">
-    <Loader v-model="isLoading" />
-
-    <div class="columns is-vcentered">
-      <div class="column is-four-fifths">
-        <h1 class="title is-2">{{ title }}</h1>
-        <p class="subtitle is-size-5">{{ subtitle }}</p>
+  <div>
+    <div v-if="redesign">
+      <div class="columns is-vcentered">
+        <div class="column is-four-fifths">
+          <h1 class="title is-2">{{ title }}</h1>
+        </div>
       </div>
-
-      <div class="column has-text-right">
-        <Pagination
-          v-if="actionType === 'pagination'"
-          v-model="page"
-          simple
-          preserve-scroll
-          :total="totalItems"
-          :per-page="1" />
-      </div>
-      <b-button
-        v-if="actionType === 'link' && linkUrl && linkText"
-        tag="nuxt-link"
-        type="is-primary"
-        inverted
-        outlined
-        icon-right="chevron-right"
-        :to="linkUrl">
-        {{ linkText }}
-      </b-button>
     </div>
 
-    <CarouselList :nfts="nfts" :page="page" :options="options" />
+    <div v-if="!redesign && nfts.length" class="my-5">
+      <Loader v-model="isLoading" />
+
+      <div class="columns is-vcentered">
+        <div class="column is-four-fifths">
+          <h1 class="title is-2">{{ title }}</h1>
+          <p class="subtitle is-size-5">{{ subtitle }}</p>
+        </div>
+
+        <div class="column has-text-right">
+          <Pagination
+            v-if="actionType === 'pagination'"
+            v-model="page"
+            simple
+            preserve-scroll
+            :total="totalItems"
+            :per-page="1" />
+        </div>
+        <b-button
+          v-if="actionType === 'link' && linkUrl && linkText"
+          tag="nuxt-link"
+          type="is-primary"
+          inverted
+          outlined
+          icon-right="chevron-right"
+          :to="linkUrl">
+          {{ linkText }}
+        </b-button>
+      </div>
+
+      <CarouselList :nfts="nfts" :page="page" :options="options" />
+    </div>
   </div>
 </template>
 
@@ -39,6 +49,8 @@ import CarouselList from './module-old/CarouselList.vue'
 
 import type { CarouselNFT } from '@/components/base/types'
 import type { RowSeries } from '@/components/series/types'
+
+const { redesign } = useExperiments()
 
 const props = defineProps<{
   title?: string
