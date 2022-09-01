49d843eaca55b4485d595df0c4dd413e7dba136c
Preschian Febryantara <preschian27@gmail.com>
2022-09-01 18:15:07 +0700

    redesign: landing page for new design

---
+++ components/landing/LandingPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="container">search</div>
+    <div class="container">spotlight</div>
+    <div class="container">top collections</div>
+    <div class="container">new listrings</div>
+    <div class="container">latest sales</div>
+    <div class="container">featured articles</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+// sample
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 500px;
+  border-top: 1px solid black;
+}
+</style>
