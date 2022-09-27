0cbccf19f20474cb526bb839b7101fc4f6ba965f
Preschian Febryantara <preschian27@gmail.com>
2022-09-27 13:11:28 +0700

    adjust identity ts

--- plugins/userBalance.ts
+++ plugins/userBalance.ts
@@ -1,12 +1,7 @@
-import { getChainEndpointByPrefix } from '@/utils/chain'
-
 export default ({ store }): void => {
   const account = store.getters.getAuthAddress
   if (account) {
     // Fetch and Subscribe to balance changes
-    store.dispatch('fetchBalance', {
-      address: account,
-      apiUrl: getChainEndpointByPrefix(store.getters.currentUrlPrefix),
-    })
+    store.dispatch('fetchBalance', { address: account })
   }
 }
