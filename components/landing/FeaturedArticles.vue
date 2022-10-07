8c0f37c38a8eb7fc4a4a38011bdb87d61edbf135
Preschian Febryantara <preschian27@gmail.com>
2022-10-07 21:51:45 +0700

    disable prettier on quote

--- components/landing/FeaturedArticles.vue
+++ components/landing/FeaturedArticles.vue
@@ -50,15 +50,18 @@ const articles: Articles[] = [
   {
     title: 'RIDE ON ART #1 - ZLUCHKAYAAA',
     description:
-      "Hello everyone, my name is Claire, and I'm working in KodaDot as marketing and social media manager. I'll introduce you some of the most talented artists we have worked with.",
+      // prettier-ignore
+      'Hello everyone, my name is Claire, and I\'m working in KodaDot as marketing and social media manager. I\'ll introduce you some of the most talented artists we have worked with.',
     image:
       'https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F52d9f1cf-457b-4855-ba14-e662c607400c_1200x675.png',
     link: 'https://kodadot.substack.com/p/ride-on-art-1-zluchkayaaa',
   },
   {
-    title: "THE ARTIST'S INSIGHT #1 - Clown World House",
+    // prettier-ignore
+    title: 'THE ARTIST\'S INSIGHT #1 - Clown World House',
     description:
-      "Are you thinking about minting your completely first NFT collection? But you don't know how to start? I'll try to bring you some advice from people who do this for a living and have enough experience.",
+      // prettier-ignore
+      'Are you thinking about minting your completely first NFT collection? But you don\'t know how to start? I\'ll try to bring you some advice from people who do this for a living and have enough experience.',
     image:
       'https://substackcdn.com/image/fetch/w_1456,c_limit,f_webp,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F6fe1fa7e-013f-4770-9b93-1bc84775ded2_1200x675.png',
     link: 'https://kodadot.substack.com/p/the-artists-inside-1-clown-world',
