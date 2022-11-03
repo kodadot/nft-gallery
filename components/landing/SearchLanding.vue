9a7240b027e8025bbd8c08d29f6a6f9e0a041fa0
Prachi Darshana <prachirai00@gmail.com>
2022-11-03 16:12:48 -0700

    overlap issue fix

--- components/landing/SearchLanding.vue
+++ components/landing/SearchLanding.vue
@@ -1,34 +1,36 @@
 <template>
   <section
-    class="landing-search is-flex is-flex-direction-column is-justify-content-center is-align-items-center">
-    <h1
-      class="title is-size-1 is-size-2-mobile has-text-weight-bold has-text-centered">
-      <span>{{ $t('search.landingTitle1') }}</span>
-      {{ $t('search.landingTitle2') }}
-      <span> {{ $t('search.landingTitle3') }}</span>
-    </h1>
-    <span
-      class="subtitle is-size-1 is-size-2-mobile has-text-weight-bold has-text-centered is-capitalized">
-      {{ $t('search.landingSubtitle') }}
-    </span>
-    <LazySearch
-      hide-filter
-      class="landing-search-bar"
-      search-column-class="is-flex-grow-1" />
-    <div class="is-flex is-justify-content-center is-flex-wrap-wrap">
-      <a
-        v-for="chain in chainList"
-        :key="chain.value"
-        :class="[
-          'mr-2 mb-2',
-          'chain-option',
-          { active: urlPrefix === chain.value },
-        ]"
-        @click="switchChain(chain.value)">
-        {{ chain.text }}
-      </a>
-    </div>
+    class="landing-search is-flex is-justify-content-space-between is-align-items-center">
     <img class="landing-search-left is-hidden-touch" :src="landingImage[0]" />
+    <div>
+      <h1
+        class="title is-size-1 is-size-2-mobile has-text-weight-bold has-text-centered">
+        <span>{{ $t('search.landingTitle1') }}</span>
+        {{ $t('search.landingTitle2') }}
+        <span> {{ $t('search.landingTitle3') }}</span>
+      </h1>
+      <span
+        class="subtitle is-size-1 is-size-2-mobile has-text-weight-bold has-text-centered is-capitalized">
+        {{ $t('search.landingSubtitle') }}
+      </span>
+      <LazySearch
+        hide-filter
+        class="landing-search-bar"
+        search-column-class="is-flex-grow-1" />
+      <div class="is-flex is-justify-content-center is-flex-wrap-wrap">
+        <a
+          v-for="chain in chainList"
+          :key="chain.value"
+          :class="[
+            'mr-2 mb-2',
+            'chain-option',
+            { active: urlPrefix === chain.value },
+          ]"
+          @click="switchChain(chain.value)">
+          {{ chain.text }}
+        </a>
+      </div>
+    </div>
     <img class="landing-search-right is-hidden-touch" :src="landingImage[1]" />
   </section>
 </template>
