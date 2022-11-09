60f0c9eb47ffebabd81ac71412f0b90c4286a56c
Preschian Febryantara <preschian27@gmail.com>
2022-11-09 11:13:45 +0700

    feat: media component for gallery item

---
+++ libs/ui/src/components/MediaObject/type/ImageMedia.vue
@@ -0,0 +1,12 @@
+<template>
+  <figure>
+    <img class="is-block" :src="src" :alt="alt" data-cy="type-image" />
+  </figure>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  src?: string
+  alt?: string
+}>()
+</script>
