a820d0f78005e0d101bda1d2a33db96315cbab7e
Preschian Febryantara <preschian27@gmail.com>
2022-11-09 21:09:36 +0700

    chore: rename to MediaItem

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,4 +1,4 @@
 export { default as CardArticle } from './components/CardArticle/CardArticle.vue'
-export { default as MediaObject } from './components/MediaObject/MediaObject.vue'
+export { default as MediaItem } from './components/MediaItem/MediaItem.vue'
 
 export { default as SampleButton } from './components/TheButton/TheButton.vue'
