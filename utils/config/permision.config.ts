3cd7845711ddc3bf7f3efec9c4b09e1b3dd58c25
nevermin9 <chizzza@gmail.com>
2022-11-10 11:42:07 +0100

    4268 Turn off tests for commented code

--- utils/config/permision.config.ts
+++ utils/config/permision.config.ts
@@ -1,6 +1,8 @@
 import { Config, Prefix } from './types'
 
 const hasCreate: Config<boolean> = {
+  // After *bsx* & *snek* are true again, turn on tests
+  // tests/cypress/e2e/basicE2E.cy.ts 6th line, 16th line checkNavbar (for bsx & snek)
   rmrk: true,
   bsx: false,
   statemine: true,
