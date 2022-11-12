211c945ae1dd31b55586cac638c1d56fcdd225b6
daiagi <daiagi@gmail.com>
2022-11-12 12:43:11 +0700

    replace av-waveform with native audio element

--- components/media/type/AudioMedia.vue
+++ components/media/type/AudioMedia.vue
@@ -1,26 +1,21 @@
 <template>
-  <div>
-    <av-waveform
-      class="media-audio__player"
-      :audio-src="src"
-      audio-class="media-audio__audio"
-      canv-class="media-audio__canvas"
-      played-line-color="rgba(0,0,0,0.74)"
-      noplayed-line-color="#d32e79"
-      playtime-font-color="rgba(0,0,0,0.74)"
+  <div class="is-flex is-justify-content-center is-align-content-center">
+    <audio
+      :controls="controls"
+      :muted="preview"
+      :src="src"
+      :type="mimeType"
+      controlslist="nodownload"
       data-cy="type-audio" />
   </div>
 </template>
 
 <script lang="ts" setup>
-defineProps<{
+const props = defineProps<{
   src?: string
+  mimeType?: string
+  preview?: boolean
 }>()
-</script>
 
-<style>
-div.media-audio__player div > canvas.media-audio__canvas,
-div.media-audio__player > div > audio.media-audio__audio {
-  width: 100% !important;
-}
-</style>
+const controls = computed(() => !props.preview)
+</script>
