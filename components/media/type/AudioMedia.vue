2537b6283d6bd219c7cdf250a43ccee2ff334b80
daiagi <daiagi@gmail.com>
2022-11-13 11:48:08 +0700

    Revert "replace av-waveform with native audio element"
    This reverts commit 211c945ae1dd31b55586cac638c1d56fcdd225b6.

--- components/media/type/AudioMedia.vue
+++ components/media/type/AudioMedia.vue
@@ -1,21 +1,26 @@
 <template>
-  <div class="is-flex is-justify-content-center is-align-content-center">
-    <audio
-      :controls="controls"
-      :muted="preview"
-      :src="src"
-      :type="mimeType"
-      controlslist="nodownload"
+  <div>
+    <av-waveform
+      class="media-audio__player"
+      :audio-src="src"
+      audio-class="media-audio__audio"
+      canv-class="media-audio__canvas"
+      played-line-color="rgba(0,0,0,0.74)"
+      noplayed-line-color="#d32e79"
+      playtime-font-color="rgba(0,0,0,0.74)"
       data-cy="type-audio" />
   </div>
 </template>
 
 <script lang="ts" setup>
-const props = defineProps<{
+defineProps<{
   src?: string
-  mimeType?: string
-  preview?: boolean
 }>()
-
-const controls = computed(() => !props.preview)
 </script>
+
+<style>
+div.media-audio__player div > canvas.media-audio__canvas,
+div.media-audio__player > div > audio.media-audio__audio {
+  width: 100% !important;
+}
+</style>
