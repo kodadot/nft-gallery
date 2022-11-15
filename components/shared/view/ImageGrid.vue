2536b3975f71a066016f4e795843e926443ae777
Viki Val <viktorko99@gmail.com>
2022-11-15 20:40:17 +0100

    :sparkles: ImageGrid

---
+++ components/shared/view/ImageGrid.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="columns is-multiline is-mobile">
+    <div
+      v-for="(src, index) in props.images"
+      :key="index"
+      class="column is-half">
+      <SelectableImage :index="index" :src="src" @click="handleClick" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  images: string[]
+  disabled: boolean
+}>()
+
+const emit = defineEmits(['select'])
+const SelectableImage = defineAsyncComponent(
+  () => import('@/components/shared/view/SelectableImage.vue')
+)
+
+const handleClick = (index: number) => {
+  emit('select', props.images[index])
+}
+</script>
