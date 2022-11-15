1e9a73b95823f2cfe475c096e08223ae8a6a80d9
Viki Val <viktorko99@gmail.com>
2022-11-15 20:39:12 +0100

    :sparkles: SelectableImage

---
+++ components/shared/view/SelectableImage.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-image
+    ratio="1by1"
+    class="image selectable-image"
+    :class="{ 'is-selected': selected }"
+    :src="src"
+    alt="Some Image"
+    @click.native="handleClick"></b-image>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  src: string[]
+  index: number
+}>()
+
+const selected = ref(false)
+const emit = defineEmits(['click', 'remove'])
+
+const handleClick = () => {
+  selected.value = !selected.value
+  emit(selected ? 'click' : 'remove', props.index)
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/abstracts/variables';
+.is-selected {
+  border: 2px $primary solid;
+}
+.selectable-image:hover {
+  border: 2px $lightpink solid;
+  cursor: pointer;
+}
+</style>
