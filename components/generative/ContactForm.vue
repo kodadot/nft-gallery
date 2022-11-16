8380ccbc42507f33badff35c6888fd46506a72d2
Viki Val <viktorko99@gmail.com>
2022-11-16 16:25:53 +0100

    :sparkles: ContactForm.vue

---
+++ components/generative/ContactForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <br />
+    <p class="title is-size-3">Enter your email</p>
+
+    <BasicInput
+      v-model="email"
+      required
+      :label="$t('mint.nft.name.label')"
+      :message="$t('mint.nft.name.message')"
+      :placeholder="$t('mint.nft.name.placeholder')"
+      expanded
+      type="email"
+      spellcheck="true"
+      data-cy="input-name" />
+
+    <SubmitButton label="generate" @click="submit" />
+  </section>
+</template>
+
+<script setup lang="ts">
+const BasicInput = defineAsyncComponent(
+  () => import('@/components/shared/form/BasicInput.vue')
+)
+const SubmitButton = defineAsyncComponent(
+  () => import('@/components/base/SubmitButton.vue')
+)
+
+const email = ref('')
+const emit = defineEmits(['select'])
+
+const submit = async () => {
+  emit('select', email.value)
+}
+</script>
