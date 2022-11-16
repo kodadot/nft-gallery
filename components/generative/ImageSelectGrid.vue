7c4c74b9f817d4ed0f69c72c3fdd96706142768b
Viki Val <viktorko99@gmail.com>
2022-11-16 16:25:57 +0100

    :sparkles: ImageSelectGrid.vue

---
+++ components/generative/ImageSelectGrid.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p class="title is-size-3">Select your 1 waifu</p>
+    <ImageGrid
+      v-if="predicion && predicion.output"
+      :images="predicion.output"
+      :disabled="selectedImage"
+      @update="handleSelectImage" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PredictionStatus } from '@/services/replicate'
+
+const ImageGrid = defineAsyncComponent(
+  () => import('@/components/shared/view/ImageGrid.vue')
+)
+
+const emit = defineEmits(['select'])
+
+const handleSelectImage = (image: string) => {
+  emit('select', image)
+}
+
+const props = defineProps<{
+  predicion: PredictionStatus
+}>()
+</script>
