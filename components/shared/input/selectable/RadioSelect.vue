fd58a87b3f5f675f3ff230d94442df8ff7b96409
Viki Val <viktorko99@gmail.com>
2022-11-18 16:38:24 +0100

    :sparkles: radioselect

---
+++ components/shared/input/selectable/RadioSelect.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-field v-if="options">
+    <b-radio-button
+      v-for="option in options"
+      :key="option"
+      :value="value"
+      :native-value="option"
+      type="is-primary"
+      class="collection-radio-btn"
+      @input="handleInput">
+      <span>{{ option }}</span>
+    </b-radio-button>
+  </b-field>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['input'])
+
+const props = defineProps<{
+  value: string
+  options: string[]
+}>()
+
+const handleInput = (value: string) => {
+  emit('input', value)
+}
+</script>
