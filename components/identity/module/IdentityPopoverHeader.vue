3302a4effd337f1aa7b32e651c5771ff1a40fa8d
Prachi Darshana <prachirai00@gmail.com>
2022-11-17 20:18:33 -0800

    #4337 hover card updates

--- components/identity/module/IdentityPopoverHeader.vue
+++ components/identity/module/IdentityPopoverHeader.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="popover-header pb-2">
     <h6 class="popover-user-heading">{{ $t('user') }}</h6>
-    <nuxt-link
-      class="is-size-6 mb-1 break-word"
-      :to="`/${urlPrefix}/u/${address}`">
-      <span data-cy="identity-display">
-        {{ identity?.display || shortenedAddress }}</span
-      >
-    </nuxt-link>
+    <div class="is-flex is-align-items-center is-justify-content-space-between">
+      <div class="is-flex is-align-items-center">
+        <nuxt-link
+          class="is-size-6 break-word mr-2"
+          :to="`/${urlPrefix}/u/${address}`">
+          <span data-cy="identity-display">
+            {{ identity?.display || shortenedAddress }}</span
+          >
+        </nuxt-link>
+        <b-icon
+          v-clipboard:copy="address"
+          icon="copy"
+          size="is-small"
+          class="copy-icon"
+          data-cy="identity-clipboard"
+          @click.native="toast('Copied to clipboard')"></b-icon>
+      </div>
+      <a
+        v-if="identity?.twitter"
+        :href="`https://twitter.com/${identity?.twitter}`"
+        target="_blank"
+        rel="noopener noreferrer"
+        data-cy="identity-twitter">
+        <b-icon pack="fab" icon="twitter" />
+      </a>
+    </div>
   </div>
 </template>
 
@@ -19,6 +38,11 @@ const shortenedAddress = inject('shortenedAddress')
 
 const identity = inject<{ [x: string]: string }>('identity')
 const { urlPrefix } = usePrefix()
+const { $buefy } = useNuxtApp()
+
+const toast = (message: string) => {
+  $buefy.toast.open(message)
+}
 </script>
 
 <style lang="scss" scoped>
@@ -26,6 +50,7 @@ const { urlPrefix } = usePrefix()
 
 .copy-icon {
   cursor: pointer;
+  color: $k-blue;
 }
 
 .popover-image {
@@ -45,6 +70,9 @@ const { urlPrefix } = usePrefix()
 
   a {
     color: $k-blue !important;
+    &:hover {
+      color: $k-blue !important;
+    }
   }
 }
 </style>
