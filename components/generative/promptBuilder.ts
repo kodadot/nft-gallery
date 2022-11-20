fee2ca1d5f7558ebf60d6d350128755b202167f8
Viki Val <viktorko99@gmail.com>
2022-11-20 23:34:04 +0100

    :zap: naive impl of prompt builder

---
+++ components/generative/promptBuilder.ts
@@ -0,0 +1,37 @@
+export type Options = {
+  gender: string
+  vibe: string
+  framing: string
+  filmType: string
+  style: string
+  inspiredBy: string
+  having: string
+  wearing: string
+  art: string
+  lighting: string
+}
+
+const OF = 'of'
+const BY = 'by'
+const COMMA = ','
+
+export const buildPrompt = (options: Options): string => {
+  const { gender, framing, art, style, having, wearing, lighting, inspiredBy } =
+    options
+  const prompt = [
+    framing,
+    style || '',
+    art,
+    OF,
+    gender,
+    having ? `having ${having} ${COMMA}` : '',
+    wearing ? `wearing ${wearing} ${COMMA}` : '',
+    lighting ? `${lighting} lighting ${COMMA}` : '',
+    inspiredBy ? `${BY} ${inspiredBy}` : '',
+  ]
+    .filter((x) => x !== '')
+    .join(' ')
+  return prompt
+  // const { gender, framing, art, style } = options
+  // return `${framing} ${style || ''} ${art} of ${gender} ${having} ${wearing}`
+}
