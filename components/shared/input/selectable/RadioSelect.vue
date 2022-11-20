dce51af0e3ef919aeebc3cce60c0b7fcac95dc56
Viki Val <viktorko99@gmail.com>
2022-11-20 23:33:19 +0100

    :bug: ability to select none

--- components/shared/input/selectable/RadioSelect.vue
+++ components/shared/input/selectable/RadioSelect.vue
@@ -1,5 +1,18 @@
 <template>
-  <b-field v-if="options">
+  <component
+    :is="separated ? 'div' : 'b-field'"
+    v-if="options"
+    :class="{ 'columns is-multiline': separated }">
+    <b-radio-button
+      v-if="showEmpty"
+      :value="value"
+      native-value=""
+      type="is-primary"
+      class="collection-radio-btn"
+      :class="{ 'column is-half': separated }"
+      @input="handleInput">
+      <span><b>NONE</b></span>
+    </b-radio-button>
     <b-radio-button
       v-for="option in options"
       :key="option"
@@ -7,10 +20,11 @@
       :native-value="option"
       type="is-primary"
       class="collection-radio-btn"
+      :class="{ 'column is-half': separated }"
       @input="handleInput">
       <span>{{ option }}</span>
     </b-radio-button>
-  </b-field>
+  </component>
 </template>
 
 <script setup lang="ts">
@@ -19,6 +33,8 @@ const emit = defineEmits(['input'])
 const props = defineProps<{
   value: string
   options: string[]
+  showEmpty?: boolean
+  separated: boolean
 }>()
 
 const handleInput = (value: string) => {
