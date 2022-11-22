e91ebe283dceb6a3a3b13ba2d3b8c2110eca6062
daiagi <daiagi@gmail.com>
2022-11-22 11:50:12 +0700

    initial commit

---
+++ libs/ui/src/components/NeoDropdown/NeoDropdown.story.vue
@@ -0,0 +1,22 @@
+<template>
+  <Story title="NeoDropDown" :layout="{ type: 'grid', width: '200px' }">
+    <Variant title="small neo button">
+      <NeoDropdown :label="label" size="small" />
+    </Variant>
+    <!-- <Variant title="default neo button">
+      <NeoDropdown :label="label" />
+    </Variant>
+    <Variant title="medium neo button">
+      <NeoDropdown :label="label" size="medium" />
+    </Variant>
+    <Variant title="large neo button">
+      <NeoDropdown :label="label" size="large" />
+    </Variant> -->
+  </Story>
+</template>
+
+<script lang="ts" setup>
+import NeoDropdown from './NeoDropdown.vue'
+
+const label = 'Sample'
+</script>
---
+++ libs/ui/src/components/NeoDropdown/NeoDropdown.vue
@@ -0,0 +1,54 @@
+<template>
+  <o-dropdown aria-role="list">
+    <template #trigger="{ active }">
+      <o-button variant="primary">
+        <span>Click me!</span>
+        <o-icon :icon="active ? 'caret-up' : 'caret-down'" />
+      </o-button>
+    </template>
+
+    <o-dropdown-item aria-role="listitem"> Action </o-dropdown-item>
+    <o-dropdown-item aria-role="listitem"> Another action </o-dropdown-item>
+    <o-dropdown-item aria-role="listitem"> Something else </o-dropdown-item>
+  </o-dropdown>
+</template>
+
+<script lang="ts" setup>
+import { ODropdown, ODropdownItem } from '@oruga-ui/oruga'
+
+defineProps<{
+  size?: string
+  icon?: string
+  label?: string
+}>()
+</script>
+
+<style lang="scss" scoped>
+@import '@oruga-ui/oruga/dist/oruga.min.css';
+@import '@oruga-ui/oruga/dist/oruga-full-vars.min.css';
+
+.is-neo {
+  border-radius: 0;
+  color: hsl(0deg, 0%, 4%);
+  background: hsl(0deg, 0%, 100%);
+  border: 1px solid hsl(0deg, 0%, 4%);
+  -webkit-box-shadow: 4px 4px hsl(0deg, 0%, 4%);
+  box-shadow: 4px 4px hsl(0deg, 0%, 4%);
+
+  &:hover {
+    background: #ffe5f3 !important;
+  }
+}
+
+.dark-mode .is-neo {
+  color: hsl(0deg, 0%, 100%);
+  background: hsl(330, 4%, 9%);
+  border: 1px solid hsl(0deg, 0%, 100%);
+  -webkit-box-shadow: 4px 4px hsl(0deg, 0%, 100%);
+  box-shadow: 4px 4px hsl(0deg, 0%, 100%);
+
+  &:hover {
+    background: #ff47ac !important;
+  }
+}
+</style>
