f3058ef618f858b960b77a3b4589c447c3487ea5
Viki Val <viktorko99@gmail.com>
2022-11-27 16:23:03 +0000

    :sparkles: such creative

--- components/generative/promptBuilder.ts
+++ components/generative/promptBuilder.ts
@@ -1,3 +1,12 @@
+import { PredictionStatus } from '@/services/replicate'
+import { pinImageSafe } from '@/utils/safePin'
+import {
+  Attribute,
+  createMetadata,
+  unSanitizeIpfsUrl,
+} from '@kodadot1/minimark'
+import { pinJson } from '@/services/nftStorage'
+
 export type Options = {
   gender: string
   vibe: string
@@ -37,6 +46,75 @@ export const buildPrompt = (options: Options): string => {
   // return `${framing} ${style || ''} ${art} of ${gender} ${having} ${wearing}`
 }
 
-export const buildDescription = (prompt: string): string => {
-  return prompt
+// I am an anime waifu from Sub0 generated using Stable Diffusion.
+// And I would like to say something about this picture:
+// The picture that you see is a portrait oil painting of me.
+// As you can see, I have these cute cat ears and wearing comfy kimono.
+// Why does this picture look so kawaii? I think it is because of the soft lighting.
+// And the vibe? Art was insipired by artist Yaoki
+
+export const buildDescription = (options: Options): string => {
+  const { gender, framing, art, style, having, wearing, lighting, inspiredBy } =
+    options
+  let base = `I am an anime ${gender} from Sub0 generated using Stable Diffusion.\n
+  And I would like to say something about this picture:\n\n
+  The picture that you see is a ${framing} ${style} ${art} of me.\n
+  `
+
+  if (having) {
+    base += `As you can see, I have ${having}.\n`
+  }
+
+  if (wearing) {
+    base += `I am wearing ${wearing}.\n`
+  }
+
+  if (lighting) {
+    base += `Why does this picture look so kawaii? I think it is because of the ${lighting} lighting.\n`
+  }
+
+  if (inspiredBy) {
+    base += `And the vibe? Art was inspired by ${inspiredBy}.\n`
+  }
+
+  return base
+}
+
+export const buildAttributes = (options: Options): Attribute[] => {
+  return Object.entries(options).map(([key, value]) => ({
+    trait_type: key,
+    value,
+  }))
+}
+
+export const buildMetadata = async (
+  url: string,
+  options: Options,
+  { input: replicate }: PredictionStatus
+): string => {
+  const blob = await $fetch<Blob>(url, { responseType: 'blob' })
+  const file = new File([blob], 'image.png', { type: 'image/png' })
+  const imageHash = await pinImageSafe(file, 'token')
+  const attributes = buildAttributes(options)
+  const description = buildDescription(options)
+  const name = `Sub0 Anime ${options.gender}`
+
+  const meta = createMetadata(
+    name,
+    description,
+    imageHash,
+    undefined,
+    attributes,
+    undefined,
+    'image/png'
+  )
+
+  const replicableMeta = {
+    ...meta,
+    replicate,
+  }
+
+  const metaHash = await pinJson(replicableMeta, imageHash)
+
+  return unSanitizeIpfsUrl(metaHash)
 }
