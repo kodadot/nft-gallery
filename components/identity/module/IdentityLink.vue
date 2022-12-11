c3c824cac2bf39c04c7d4c344ae0136e85e90342
Peter Sopko <petersopko@protonmail.com>
2022-12-11 10:44:28 +0100

    :sparkles: identity link

---
+++ components/identity/module/IdentityLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <a :href="`https://kusama.subscan.io/account/${address}`">
+    {{ shortenedAddress }}
+  </a>
+</template>
+
+<script lang="ts" setup>
+import { GenericAccountId } from '@polkadot/types/generic/AccountId'
+
+type Address = string | GenericAccountId | undefined
+
+defineProps<{
+  shortenedAddress?: string | object
+  address?: Address
+}>()
+</script>
