46830368069f231193699e21be69dca1e4839fd4
Peter Sopko <petersopko@protonmail.com>
2022-12-11 18:27:08 +0100

    :construction: assets here 2

--- components/rmrk/Profile/ProfileAssetsList.vue
+++ components/rmrk/Profile/ProfileAssetsList.vue
@@ -3,6 +3,11 @@
     <div class="is-flex is-justify-content-space-between">
       <div class="has-text-left has-text-grey is-size-7">
         {{ 'Asset' }}
+        <div>
+          <span class="has-text-weight-bold">
+            {{ 'Balance' }}
+          </span>
+        </div>
       </div>
       <div class="has-text-left has-text-grey is-size-7">
         {{ 'Balance' }}
@@ -13,7 +18,7 @@
     </div>
     <hr class="my-2" />
     <div>
-      {{ assetList }}
+      <!--      {{ assetList }}-->
       <!--      <SimpleAccountBalance-->
       <!--        v-for="token in tokens"-->
       <!--        :key="token"-->
@@ -22,3 +27,77 @@
     </div>
   </div>
 </template>
+<script lang="ts" setup>
+import assetListByIdList from '@/queries/subsquid/bsx/assetListByIdList.graphql'
+// import { setDefaultFeeToken } from '@/utils/api/bsx/extrinsics'
+import {
+  // getAssetIdByAccount,
+  getAsssetBalance,
+  getKusamaAssetId,
+} from '@/utils/api/bsx/query'
+import { useApollo } from '@/utils/config/useApollo'
+import { mapToId } from '@/utils/mappers'
+import { notificationTypes, showNotification } from '@/utils/notification'
+import { AssetItem, AssetListQueryResponse } from '@/components/bsx/Asset/types'
+import Loader from '@/components/shared/Loader.vue'
+import AssetTable from '@/components/bsx/Asset/AssetTable.vue'
+
+const { accountId } = useAuth()
+
+const { urlPrefix, client } = usePrefix()
+const { $apollo, $consola, $set } = useNuxtApp()
+
+const { howAboutToExecute, initTransactionLoader, isLoading, status } =
+  useMetaTransaction()
+const assetList = ref<AssetItem[]>([])
+// const currentAsset = ref<string>('0')
+
+const loadAssets = async () => {
+  try {
+    const { assetList: newAssetList } = await useApollo<
+      any,
+      AssetListQueryResponse
+    >($apollo as any, urlPrefix.value, assetListByIdList, {
+      ids: ['0', getKusamaAssetId(client.value), '6'],
+    })
+    assetList.value = newAssetList
+    fetchAccountBalance()
+  } catch (e) {
+    $consola.warn(e)
+    showNotification('Unable to load assets')
+  }
+}
+
+const fetchAccountBalance = async () => {
+  const { apiInstance } = useApi()
+  const api = await apiInstance.value
+  const mapper = (id: string) => getAsssetBalance(api, accountId.value, id)
+  assetList.value.map(mapToId).map(mapper).forEach(updatedBalanceFor)
+}
+
+const updatedBalanceFor = async (balance: Promise<string>, index: number) => {
+  try {
+    $set(assetList.value, index, {
+      ...assetList.value[index],
+      balance: await balance,
+    })
+  } catch (e) {
+    console.warn('Unable to fetch balance', e)
+  }
+}
+
+// const fetchCurrentToken = async (): Promise<void> => {
+//   const { apiInstance } = useApi()
+//   const api = await apiInstance.value
+//   const currentToken = await getAssetIdByAccount(api, accountId.value)
+//   currentAsset.value = currentToken
+// }
+
+watch(
+  () => accountId.value,
+  async () => {
+    await loadAssets()
+  },
+  { immediate: true }
+)
+</script>
