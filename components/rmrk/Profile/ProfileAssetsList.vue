c869be90fef52462711a571bda545d341a8680ad
Peter Sopko <petersopko@protonmail.com>
2022-12-11 19:15:16 +0100

    :zap: poc assets in dropdown

--- components/rmrk/Profile/ProfileAssetsList.vue
+++ components/rmrk/Profile/ProfileAssetsList.vue
@@ -1,46 +1,29 @@
 <template>
-  <div class="profile-assets-list">
-    <div class="is-flex is-justify-content-space-between">
-      <div class="has-text-left has-text-grey is-size-7">
-        {{ 'Asset' }}
-        <div>
-          <span class="has-text-weight-bold">
-            {{ 'Balance' }}
-          </span>
-        </div>
-      </div>
-      <div class="has-text-left has-text-grey is-size-7">
-        {{ 'Balance' }}
-      </div>
-      <div class="has-text-left has-text-grey is-size-7">
-        {{ 'USD' }}
-      </div>
-    </div>
-    <hr class="my-2" />
-    <div>
-      <!--      {{ assetList }}-->
-      <!--      <SimpleAccountBalance-->
-      <!--        v-for="token in tokens"-->
-      <!--        :key="token"-->
-      <!--        class="is-size-6"-->
-      <!--        :token-id="token" />-->
-    </div>
-  </div>
+  <table>
+    <thead>
+      <tr>
+        <th class="has-text-grey">Symbol</th>
+        <th class="has-text-grey">Balance</th>
+        <th class="has-text-grey">id</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="asset in assetList" :key="asset.id">
+        <td>{{ asset.symbol }}</td>
+        <td><Money :value="asset.balance" inline hide-unit /></td>
+        <td>{{ asset.id }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 <script lang="ts" setup>
 import assetListByIdList from '@/queries/subsquid/bsx/assetListByIdList.graphql'
-// import { setDefaultFeeToken } from '@/utils/api/bsx/extrinsics'
-import {
-  // getAssetIdByAccount,
-  getAsssetBalance,
-  getKusamaAssetId,
-} from '@/utils/api/bsx/query'
+import { getAsssetBalance, getKusamaAssetId } from '@/utils/api/bsx/query'
 import { useApollo } from '@/utils/config/useApollo'
 import { mapToId } from '@/utils/mappers'
 import { notificationTypes, showNotification } from '@/utils/notification'
 import { AssetItem, AssetListQueryResponse } from '@/components/bsx/Asset/types'
-import Loader from '@/components/shared/Loader.vue'
-import AssetTable from '@/components/bsx/Asset/AssetTable.vue'
+import Money from '~/components/shared/format/Money.vue'
 
 const { accountId } = useAuth()
 
@@ -50,7 +33,6 @@ const { $apollo, $consola, $set } = useNuxtApp()
 const { howAboutToExecute, initTransactionLoader, isLoading, status } =
   useMetaTransaction()
 const assetList = ref<AssetItem[]>([])
-// const currentAsset = ref<string>('0')
 
 const loadAssets = async () => {
   try {
@@ -86,13 +68,6 @@ const updatedBalanceFor = async (balance: Promise<string>, index: number) => {
   }
 }
 
-// const fetchCurrentToken = async (): Promise<void> => {
-//   const { apiInstance } = useApi()
-//   const api = await apiInstance.value
-//   const currentToken = await getAssetIdByAccount(api, accountId.value)
-//   currentAsset.value = currentToken
-// }
-
 watch(
   () => accountId.value,
   async () => {
@@ -101,3 +76,18 @@ watch(
   { immediate: true }
 )
 </script>
+<style lang="scss" scoped>
+@import '@/styles/abstracts/variables';
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+  border-spacing: 0;
+  border-radius: 0.25rem;
+  overflow: hidden;
+
+  tr:last-child {
+    border-bottom: none;
+  }
+}
+</style>
