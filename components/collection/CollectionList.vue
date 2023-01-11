307a36a244e6523a70dbfe753f959c63601089cc
Preschian Febryantara <preschian27@gmail.com>
2023-01-11 23:52:20 +0700

    revert: prefetchPage

--- components/collection/CollectionList.vue
+++ components/collection/CollectionList.vue
@@ -53,12 +53,15 @@ import {
   Collection,
   CollectionWithMeta,
   Metadata,
+  NFTMetadata,
 } from '@/components/rmrk/service/scheme'
 import { SearchQuery } from '@/components/rmrk/Gallery/search/types'
 import 'lazysizes'
 import collectionListWithSearch from '@/queries/subsquid/general/collectionListWithSearch.graphql'
 import PrefixMixin from '~/utils/mixins/prefixMixin'
 import InfiniteScrollMixin from '~/utils/mixins/infiniteScrollMixin'
+import { mapOnlyMetadata } from '~/utils/mappers'
+import { processMetadata } from '~/utils/cachingStrategy'
 import { getDenyList } from '~/utils/prefix'
 
 interface Image extends HTMLImageElement {
@@ -194,6 +197,31 @@ export default class CollectionList extends mixins(
     }
 
     this.isLoading = false
+    this.prefetchPage(this.offset + this.first, this.offset + 3 * this.first)
+  }
+
+  public async prefetchPage(offset: number, prefetchLimit: number) {
+    try {
+      const collections = this.$apollo.query({
+        query: collectionListWithSearch,
+        client: this.client,
+        variables: {
+          first: this.first,
+          offset,
+        },
+      })
+      const {
+        data: { collectionEntities: collectionList },
+      } = await collections
+      const metadataList: string[] = collectionList.map(mapOnlyMetadata)
+      processMetadata<NFTMetadata>(metadataList)
+    } catch (e: any) {
+      this.$consola.warn('[PREFETCH] Unable fo fetch', offset, e.message)
+    } finally {
+      if (offset <= prefetchLimit) {
+        this.prefetchPage(offset + this.first, prefetchLimit)
+      }
+    }
   }
 
   @Watch('$route.query.search')
