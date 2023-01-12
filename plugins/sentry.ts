a2798c6cb88c57decf5ed8ed46397f252a05d1b9
Jarsen <695552027@qq.com>
2023-01-12 22:51:06 +0800

    fix: deepsource issue

--- plugins/sentry.ts
+++ plugins/sentry.ts
@@ -1,9 +1,9 @@
-import * as Sentry from '@sentry/browser'
+import { Replay } from '@sentry/browser'
 
 // docs: https://docs.sentry.io/platforms/javascript/guides/vue/session-replay/
 export default function () {
   return [
-    new Sentry.Replay({
+    new Replay({
       maskAllText: true,
       blockAllMedia: true,
     }),
