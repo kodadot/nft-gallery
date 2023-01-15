24429e949786f05260f8356a06fc09d12d8fba53
Preschian Febryantara <preschian27@gmail.com>
2023-01-16 00:13:07 +0700

    chore: remove patches and qrcode to composition api

--- components/shared/QRCode.vue
+++ components/shared/QRCode.vue
@@ -10,67 +10,20 @@
     }" />
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue, Watch } from 'nuxt-property-decorator'
+<script setup lang="ts">
 import VueQrcode from '@chenfengyuan/vue-qrcode'
-import shouldUpdate from '@/utils/shouldUpdate'
 
-@Component({
-  components: {
-    VueQrcode,
-  },
-})
-export default class QRCodeComponent extends Vue {
-  @Prop({ type: String, required: true }) public text!: string
-  @Prop({ type: Number, required: false, default: 256 }) public size!: number
-  @Prop({ type: String, required: false, default: '#000' })
-  public color!: string
-  @Prop({ type: String, required: false, default: '#FFF' })
-  public bgColor!: string
-  @Prop({
-    type: String,
-    validator: function (value) {
-      return value === 'L' || value === 'M' || value === 'Q' || value === 'H'
-    },
-    required: false,
-    default: 'H',
-  })
-  public errorLevel!: string
-
-  protected qrCode: Record<string, any> | null = null
-
-  // public mounted(): void {
-  //   this.qrCode = new QRCode(this.$el, {
-  //     text: this.text,
-  //     width: this.size,
-  //     height: this.size,
-  //     colorDark: this.color,
-  //     colorLight: this.bgColor,
-  //     correctLevel: QRCode.CorrectLevel[this.errorLevel],
-  //   })
-  // }
-
-  protected clear(): void {
-    this.qrCode?.clear()
+withDefaults(
+  defineProps<{
+    text: string
+    size?: number
+    color?: string
+    bgColor?: string
+  }>(),
+  {
+    size: 256,
+    color: '#000',
+    bgColor: '#FFF',
   }
-
-  protected makeCode(text: string): void {
-    this.qrCode?.makeCode(text)
-  }
-
-  @Watch('text')
-  handleTextChange(val: string, oldVal: string): void {
-    if (shouldUpdate(val, oldVal)) {
-      this.clear()
-      this.makeCode(val)
-    }
-  }
-}
+)
 </script>
-
-<style>
-.qr-code-wrapper > canvas > img,
-.qr-code-wrapper img {
-  margin: auto !important;
-}
-</style>
