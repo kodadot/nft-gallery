02d7957c4ae69bbfd2648c2b62d6eb5391a2d1a5
Peter Sopko <petersopko@protonmail.com>
2023-01-15 12:14:32 +0100

    :zap: loop for routes

--- tests/cypress/e2e/explore.cy.ts
+++ tests/cypress/e2e/explore.cy.ts
@@ -1,84 +1,6 @@
-describe('exploreRmrkCollections', () => {
+const testCollections = (route) => {
   it('loadExplore', () => {
-    cy.visit('/rmrk/explore')
-  })
-
-  it('exploreTabs', () => {
-    cy.getCy('tabs')
-      .should('be.visible')
-      .within(() => {
-        cy.contains('Collections').should('be.visible')
-        cy.contains('Items').should('be.visible')
-      })
-  })
-
-  it('collectionsSort', () => {
-    cy.getCy('collection-sort-by').should('be.visible')
-    cy.getCy('collection-sort-by').select('Old first')
-    cy.getCy('collection-sort-by').select('New first')
-  })
-})
-
-describe('exploreRmrkGallery', () => {
-  it('loadExplore', () => {
-    cy.visit('/rmrk/explore/items?page=1')
-  })
-
-  it('exploreTabs', () => {
-    cy.getCy('tabs')
-      .should('be.visible')
-      .within(() => {
-        cy.contains('Collections').should('be.visible')
-        cy.contains('Items').should('be.visible')
-      })
-  })
-
-  it('expandGallerySearch', () => {
-    cy.getCy('expand-search').click({ force: true })
-  })
-
-  it('galleryInputFields', () => {
-    cy.getCy('input-min').type('100', { force: true })
-    cy.getCy('apply').click({ force: true })
-  })
-
-  it('gallerySortRmrk', () => {
-    cy.waitForNetworkIdle('POST', '*', 1000)
-    // TODO: clean up selector -> too many elements for data-cy
-    cy.get(
-      '.gallery > :nth-child(1) > .collapse > #sortAndFilter > :nth-child(1) > .mb-0 > .dropdown > .dropdown-trigger > [data-cy="gallery-sort-by"]'
-    ).click()
-    cy.get('[data-cy="Recently Created"]').should('be.visible')
-    cy.get('[data-cy="Oldest"]').should('be.visible')
-    cy.get('[data-cy="Price: High to Low"]').should('be.visible')
-    cy.get('[data-cy="Price: Low to High"]').should('be.visible')
-    cy.get('[data-cy="Recently Interacted"]').should('be.visible')
-    cy.get('[data-cy="Unpopular"]').should('be.visible')
-    // cy.get('[data-cy="Most reacted"]').should('be.visible')
-    // TODO: clean up selector -> too many elements for data-cy
-    cy.get(
-      '[pricemin="100000000000000"] > .collapse > #sortAndFilter > :nth-child(1) > .mb-0 > .dropdown > .dropdown-menu > .dropdown-content > [data-cy="Price: Low to High"]'
-    ).click()
-  })
-
-  it('galleryBuyNow', () => {
-    cy.getCy('buy-now').within(() => {
-      return cy.get('[type="checkbox"]').check({ force: true, timeout: 5000 })
-    })
-    cy.getCy('item-index-0')
-      .getCy('card-money')
-      .invoke('text')
-      .then((text) => {
-        if (!(parseFloat(text.replace(',', '')) >= 100)) {
-          throw '[ERROR] Gallery BUY NOW is not working'
-        }
-      })
-  })
-})
-
-describe('exploreSnekCollections', () => {
-  it('loadExplore', () => {
-    cy.visit('/snek/explore')
+    cy.visit(route)
   })
 
   it('exploreTabs', () => {
@@ -107,12 +29,13 @@ describe('exploreSnekCollections', () => {
     cy.get('[data-cy="collection-index-2"]').should('exist')
     cy.get('[data-cy="collection-index-3"]').should('exist')
   })
-})
+}
 
-describe('exploreSnekGallery', () => {
+const testItems = (route) => {
   it('loadExplore', () => {
-    cy.visit('/snek/explore')
+    cy.visit(route)
   })
+
   it('exploreTabs', () => {
     cy.getCy('tabs')
       .should('be.visible')
@@ -122,21 +45,17 @@ describe('exploreSnekGallery', () => {
       })
   })
 
-  it('expandGallerySearch', () => {
-    cy.getCy('tabs')
-      .should('be.visible')
-      .within(() => {
-        cy.contains('Items').click()
-      })
+  it('itemsExpandSearch', () => {
     cy.getCy('expand-search').click({ force: true })
   })
 
-  it('galleryInputFields', () => {
+  it('itemsInputFields', () => {
     cy.getCy('input-min').type('100', { force: true })
     cy.getCy('apply').click({ force: true })
   })
 
-  it('gallerySortSnek', () => {
+  it('itemsSort', () => {
+    cy.waitForNetworkIdle('POST', '*', 1000)
     // TODO: clean up selector -> too many elements for data-cy
     cy.get(
       '.gallery > :nth-child(1) > .collapse > #sortAndFilter > :nth-child(1) > .mb-0 > .dropdown > .dropdown-trigger > [data-cy="gallery-sort-by"]'
@@ -153,7 +72,7 @@ describe('exploreSnekGallery', () => {
     ).click()
   })
 
-  it('galleryBuyNow', () => {
+  it('itemsBuyNow', () => {
     cy.getCy('buy-now').within(() => {
       return cy.get('[type="checkbox"]').check({ force: true, timeout: 5000 })
     })
@@ -165,5 +84,22 @@ describe('exploreSnekGallery', () => {
           throw '[ERROR] Gallery BUY NOW is not working'
         }
       })
+  })
+}
+describe('exploreCollections', () => {
+  const routes = ['/rmrk/explore/collectibles', '/bsx/explore/collectibles']
+  routes.forEach((route) => {
+    describe(`Collections explore at ${route}`, () => {
+      testCollections(route)
+    })
+  })
+})
+
+describe('exploreItems', () => {
+  const routes = ['/rmrk/explore/items?page=1', '/bsx/explore/items?page=1']
+  routes.forEach((route) => {
+    describe(`Testing route ${route}`, () => {
+      testItems(route)
+    })
   })
 })
