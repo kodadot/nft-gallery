5b286d23a4d174113f94df77840555432608ab95
Preschian Febryantara <preschian27@gmail.com>
2023-01-17 11:56:35 +0700

    fix: workbox always true

--- plugins/pwa.ts
+++ plugins/pwa.ts
@@ -7,30 +7,28 @@ import { clearSession } from '@/utils/cachingStrategy'
 export default async () => {
   const workbox = new Workbox('/sw.js')
 
-  if (workbox) {
-    console.info('workbox detected')
+  console.info('workbox detected', workbox)
 
-    workbox.addEventListener('installed', (event) => {
-      consola.info(
-        'App is being served from cache by a service worker.\n' +
-          'For more details, visit https://pwa.nuxtjs.org/'
-      )
+  workbox.addEventListener('installed', (event) => {
+    consola.info(
+      'App is being served from cache by a service worker.\n' +
+        'For more details, visit https://pwa.nuxtjs.org/'
+    )
 
-      if (event.isUpdate) {
-        consola.info('New content is available, please refresh.')
-        const notif = Notification.open({
-          message: 'New version is ready. Close to upgrade.',
-          queue: false,
-          type: 'is-info is-dark',
-          position: 'is-top-left',
-          indefinite: true,
-          hasIcon: true,
-        })
+    if (event.isUpdate) {
+      consola.info('New content is available, please refresh.')
+      const notif = Notification.open({
+        message: 'New version is ready. Close to upgrade.',
+        queue: false,
+        type: 'is-info is-dark',
+        position: 'is-top-left',
+        indefinite: true,
+        hasIcon: true,
+      })
 
-        notif.$on('close', () => {
-          clearSession()
-        })
-      }
-    })
-  }
+      notif.$on('close', () => {
+        clearSession()
+      })
+    }
+  })
 }
