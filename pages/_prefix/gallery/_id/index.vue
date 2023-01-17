6febb95921996f1a01392326fe3da5774fab7c02
Jarsen <695552027@qq.com>
2023-01-17 21:56:10 +0800

    refactor: vue3 implement of gallery index

---
+++ pages/_prefix/gallery/_id/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <GalleryItem />
+</template>
+
+<script lang="ts" setup>
+import GalleryItem from '@/components/gallery/GalleryItem.vue'
+
+const { $store } = useNuxtApp()
+const { urlPrefix } = usePrefix()
+const route = useRoute()
+const router = useRouter()
+
+onBeforeMount(() => {
+  const prefix = route.params.prefix
+
+  if (urlPrefix.value !== prefix) {
+    $store.dispatch('setUrlPrefix', prefix)
+    router.go(0)
+  }
+})
+</script>
