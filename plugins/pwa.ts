039abf622958f6ba31273069f616ebd5c002b3b4
Preschian Febryantara <preschian27@gmail.com>
2023-01-17 11:51:30 +0700

    fix: use workbox-window package

--- plugins/pwa.ts
+++ plugins/pwa.ts
@@ -1,24 +1,23 @@
 import { NotificationProgrammatic as Notification } from 'buefy'
+import consola from 'consola'
+import { Workbox } from 'workbox-window'
+
 import { clearSession } from '@/utils/cachingStrategy'
 
-declare global {
-  interface Window {
-    $workbox: any
-  }
-}
-
 export default async () => {
-  const workbox = await window.$workbox
+  const workbox = new Workbox('/sw.js')
 
   if (workbox) {
+    console.info('workbox detected')
+
     workbox.addEventListener('installed', (event) => {
-      console.log(
+      consola.info(
         'App is being served from cache by a service worker.\n' +
           'For more details, visit https://pwa.nuxtjs.org/'
       )
 
       if (event.isUpdate) {
-        console.log('New content is available, please refresh.')
+        consola.info('New content is available, please refresh.')
         const notif = Notification.open({
           message: 'New version is ready. Close to upgrade.',
           queue: false,
