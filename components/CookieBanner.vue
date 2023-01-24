98623b6c770275d2f3b61fe9d8722c0c312d8069
roiLeo <medina.leo42@gmail.com>
2023-01-24 12:07:35 +0100

    ✨ vue-gtag plugin opt-out

--- components/CookieBanner.vue
+++ components/CookieBanner.vue
@@ -20,24 +20,22 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
+<script lang="ts" setup>
+import { useState } from 'vue-gtag-next'
 
-@Component({
-  components: {}, // do not remove!
-})
-export default class CookieBanner extends Vue {
-  public hasDisplayedCookieBanner: boolean =
-    localStorage.getItem('cookies_enabled') !== null || false
-
-  public acceptCookies() {
-    localStorage.setItem('cookies_enabled', '1')
-    this.hasDisplayedCookieBanner = true
+const { isEnabled } = useState()
+const hasDisplayedCookieBanner = ref(
+  localStorage.getItem('cookies_enabled') !== null || false
+)
+const acceptCookies = () => {
+  if (isEnabled) {
+    isEnabled.value = true
   }
-
-  public declineCookies() {
-    localStorage.setItem('cookies_enabled', '0')
-    this.hasDisplayedCookieBanner = true
-  }
+  localStorage.setItem('cookies_enabled', '1')
+  hasDisplayedCookieBanner.value = true
+}
+const declineCookies = () => {
+  localStorage.setItem('cookies_enabled', '0')
+  hasDisplayedCookieBanner.value = true
 }
 </script>
