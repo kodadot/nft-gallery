792fe93cf6418788ee1e323c2737b5b58baad2c9
roiLeo <medina.leo42@gmail.com>
2023-01-24 11:50:42 +0100

    ✨ vue-gtag plugin

---
+++ plugins/vue-gtag.client.ts
@@ -0,0 +1,7 @@
+import VueGtag from 'vue-gtag-next'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueGtag, {
+    property: { id: nuxtApp.$config.public.googleAnalyticsId },
+  })
+})
