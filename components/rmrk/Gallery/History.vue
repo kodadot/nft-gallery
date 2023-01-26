326ce976bdab337b0cec0847a2bea12f3f735ea7
Preschian Febryantara <preschian27@gmail.com>
2023-01-26 12:41:10 +0700

    revert: history id

--- components/rmrk/Gallery/History.vue
+++ components/rmrk/Gallery/History.vue
@@ -377,8 +377,7 @@ export default class History extends mixins(
       event['Block'] = String(newEvent['blockNumber'])
 
       // ID for b-table: Use a unique key of your data Object for each row.
-      event['ID'] =
-        newEvent['timestamp'] + newEvent['id'] + newEvent['interaction']
+      event['ID'] = newEvent['timestamp'] + newEvent['id']
 
       // Push to chart data
       if (newEvent['interaction'] === Interaction.LIST) {
