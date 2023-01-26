d66bf8faa3cf9aa32301a0f8f7e1e013ef416cc0
Preschian Febryantara <preschian27@gmail.com>
2023-01-26 12:35:49 +0700

    feat: sort options on explore

---
+++ components/explore/ExploreIndex.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="explore is-flex">
+    <ExploreTabs />
+    <ExploreSort />
+  </div>
+</template>
+
+<script setup lang="ts" scoped>
+import ExploreTabs from './ExploreTabs.vue'
+import ExploreSort from './ExploreSort.vue'
+</script>
+
+<style lang="scss" scoped>
+.explore {
+  gap: 1rem;
+}
+</style>
---
+++ components/explore/ExploreSort.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <p>{{ selectedSort }}</p>
+    <o-dropdown v-model="selectedSort" multiple aria-role="list">
+      <template #trigger>
+        <NeoButton type="button">
+          <span>Sort By: {{ selectedSort.length }}</span>
+        </NeoButton>
+      </template>
+
+      <NeoDropdownItem
+        v-for="option in options"
+        :key="option.value"
+        aria-role="listitem"
+        :value="option.value">
+        <span>{{ option.text }}</span>
+      </NeoDropdownItem>
+    </o-dropdown>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { NeoButton, NeoDropdownItem } from '@kodadot1/brick'
+import { ODropdown } from '@oruga-ui/oruga'
+
+const selectedSort = ref([])
+const options = [
+  {
+    value: 'price_ASC',
+    text: 'Price: Low to High',
+  },
+  {
+    value: 'price_DESC',
+    text: 'Price: High to Low',
+  },
+  {
+    value: 'created_ASC',
+    text: 'Created: Old to New',
+  },
+  {
+    value: 'created_DESC',
+    text: 'Created: New to Old',
+  },
+]
+</script>
+
+<style scoped></style>
