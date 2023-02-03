9c6ff7b6b91dd05b501e65c7bb53d7a6069911dd
Jarsen <695552027@qq.com>
2023-02-03 18:17:10 +0800

    feat: redesign teleport address

--- components/teleport/Teleport.vue
+++ components/teleport/Teleport.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="is-flex is-justify-content-center">
-    <div>
+    <div class="teleport-container">
       <Loader v-model="isLoading" />
       <p class="is-size-3 has-text-weight-bold">Teleport</p>
-      <div class="mb-4">
+      <div class="mb-5">
         <h1 class="has-text-weight-bold">From</h1>
         <TeleportTabs
           :tabs="fromTabs"
@@ -12,29 +12,30 @@
       </div>
 
       <div
-        class="mb-4 is-flex is-justify-content-space-between is-align-items-center input-wrapper">
+        class="mb-5 is-flex is-justify-content-space-between is-align-items-center input-wrapper">
         <input
           v-model="amount"
           class="transfer-amount is-flex is-align-items-center"
           type="number"
           placeholder="type your amount"
-          min="0" />
-        <div class="px-4">KSM</div>
+          :min="0" />
+        <div
+          class="token is-flex is-align-items-center is-justify-content-center">
+          KSM
+        </div>
       </div>
 
-      <div class="mb-4">
+      <div class="mb-5">
         <h1 class="has-text-weight-bold">To</h1>
         <TeleportTabs
-          :tabs="toTabs"
+          :tabs="fromTabs"
           :value="toChain"
           @select="onToChainChange" />
       </div>
-      <div
-        class="mb-4 is-flex is-justify-content-space-between is-align-items-center input-wrapper">
-        <input
-          v-model="toAddress"
-          class="transfer-amount is-flex is-align-items-center"
-          :placeholder="`Input recipient address (${toChain})`" />
+
+      <div class="mb-5">
+        You will receive {{ amount || 0 }} KSM on {{ toChain }} to
+        <span class="has-text-info">{{ shortAddress(toAddress) }}</span>
       </div>
 
       <NeoButton
@@ -43,7 +44,7 @@
         class="is-size-6 submit-button"
         :loading="isLoading"
         variant="k-accent"
-        @click.native="sendXCM(accountId)" />
+        @click.native="sendXCM" />
     </div>
   </section>
 </template>
@@ -54,7 +55,7 @@ import { web3Enable } from '@polkadot/extension-dapp'
 import '@polkadot/api-augment'
 import { toDefaultAddress } from '@/utils/account'
 import { getAddress } from '@/utils/extension'
-import { Chain } from '@/utils/teleport'
+import { Chain, chainToPrefixMap } from '@/utils/teleport'
 import { notificationTypes, showNotification } from '@/utils/notification'
 import useAuth from '@/composables/useAuth'
 import Loader from '@/components/shared/Loader.vue'
@@ -63,19 +64,21 @@ import * as paraspell from '@paraspell/sdk'
 import { txCb } from '@/utils/transactionExecutor'
 import TeleportTabs from './TeleportTabs'
 import { NeoButton } from '@kodadot1/brick'
+import { getss58AddressByPrefix } from '@/utils/account'
 
 const { accountId } = useAuth()
-
+const { assets } = usePrefix()
 const chains = ref([Chain.KUSAMA, Chain.BASILISK])
 const fromChain = ref(Chain.KUSAMA) //Selected origin parachain
 const toChain = ref(Chain.BASILISK) //Selected destination parachain
-const toAddress = ref('') //Recipient address is stored here
 const amount = ref() //Required amount to be transfered is stored here
+
 const currency = ref('KSM') //Selected currency is stored here
 const isLoading = ref(false)
+import shortAddress from '@/utils/shortAddress'
 
 const resetStatus = () => {
-  amount.value = 0
+  amount.value = undefined
   isLoading.value = false
 }
 
@@ -89,7 +92,7 @@ const fromTabs = [
     value: Chain.BASILISK,
   },
 ]
-const toTabs = fromTabs.reverse()
+
 const getAnotherOption = (val) => {
   return chains.value.find((chain) => chain !== val) || Chain.KUSAMA
 }
@@ -102,13 +105,21 @@ const onToChainChange = (val) => {
   toChain.value = val
   fromChain.value = getAnotherOption(val)
 }
+const getAddressByChain = (chain) => {
+  return getss58AddressByPrefix(accountId.value, chainToPrefixMap[chain])
+}
+const fromAddress = computed(() => getAddressByChain(fromChain.value))
+const toAddress = computed(() => getAddressByChain(toChain.value))
 
 //Used to create XCM transfer
-const sendXCM = async (address: string) => {
+const sendXCM = async () => {
+  if (!amount.value || amount.value < 0) {
+    return
+  }
   await web3Enable('Kodadot')
   let isFirstStatus = true
   isLoading.value = true
-
+  const amountValue = 10 ** assets(5).decimals * amount.value
   const transactionHandler = txCb(
     (blockHash) => {
       showNotification(
@@ -137,7 +148,7 @@ const sendXCM = async (address: string) => {
     isLoading.value = false
   }
 
-  const injector = await getAddress(toDefaultAddress(address))
+  const injector = await getAddress(toDefaultAddress(fromAddress.value))
 
   if (fromChain.value === Chain.KUSAMA) {
     const wsProvider = new WsProvider('wss://public-rpc.pinknode.io/kusama')
@@ -146,12 +157,16 @@ const sendXCM = async (address: string) => {
     const promise = paraspell.xcmPallet.transferRelayToPara(
       apiKusama,
       Chain.BASILISK,
-      amount.value,
+      amountValue,
       toAddress.value
     )
 
     promise
-      .signAndSend(address, { signer: injector.signer }, transactionHandler)
+      .signAndSend(
+        fromAddress.value,
+        { signer: injector.signer },
+        transactionHandler
+      )
       .catch(errorHandler)
   } else if (fromChain.value === Chain.BASILISK) {
     const wsProvider = new WsProvider('wss://rpc.basilisk.cloud')
@@ -162,36 +177,53 @@ const sendXCM = async (address: string) => {
       Chain.BASILISK,
       currency.value,
       1,
-      amount.value,
+      amountValue,
       toAddress.value
     )
 
     promise
-      .signAndSend(address, { signer: injector.signer }, transactionHandler)
+      .signAndSend(
+        fromAddress.value,
+        { signer: injector.signer },
+        transactionHandler
+      )
       .catch(errorHandler)
   }
 }
 </script>
 <style lang="scss" scoped>
 @import '@/styles/abstracts/variables.scss';
+.teleport-container {
+  max-width: 30rem;
+
+  .submit-button {
+    width: 100%;
+    height: 3.5rem;
+  }
+}
 
 .input-wrapper {
   border: 1px solid $black;
-}
-.transfer-amount {
-  border: none;
-  border-right: 1px solid black;
-  padding: 0 0.5rem;
-  height: 54px;
-  outline: none;
-  width: 100%;
+  .token {
+    width: 16rem;
+  }
+  .transfer-amount {
+    border: none;
+    border-right: 1px solid black;
+    padding: 0 0.5rem;
+    height: 54px;
+    outline: none;
+    width: 100%;
 
-  &::-webkit-outer-spin-button,
-  &::-webkit-inner-spin-button {
-    -webkit-appearance: none !important;
+    &::-webkit-outer-spin-button,
+    &::-webkit-inner-spin-button {
+      -webkit-appearance: none !important;
+    }
   }
 }
-.submit-button {
-  width: 100%;
+.dark-mode {
+  .input-wrapper {
+    border-color: $white;
+  }
 }
 </style>
