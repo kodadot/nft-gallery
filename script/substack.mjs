dcc21dabbaffbfbc2b4fa9f342e40e21797bdb49
Preschian Febryantara <preschian27@gmail.com>
2023-02-03 21:28:51 +0700

    feat: generate post from substack api

--- script/substack.mjs
+++ script/substack.mjs
@@ -1,30 +1,21 @@
 import fetch from 'node-fetch'
-import { load } from 'cheerio'
 import fs from 'fs/promises'
 import consola from 'consola'
 
 async function init() {
   try {
     const response = await fetch(
-      'https://kodadot.substack.com/archive?sort=new'
+      'https://kodadot.substack.com/api/v1/archive?sort=new&search=&offset=0&limit=3'
     )
-    const body = await response.text()
-    const $dom = load(body)
-    const list = $dom(
-      '.portable-archive-list .post-preview.portable-archive-post.has-image'
-    )
-
-    const post = list
-      .slice(0, 3)
-      .map((i, el) => {
-        const title = $dom(el).find('.post-preview-title').text()
-        const description = $dom(el).find('.post-preview-description').text()
-        const image = $dom(el).find('.post-preview-image source').attr('srcset')
-        const link = $dom(el).find('.post-preview-title').attr('href')
-
-        return { title, description, image, link }
-      })
-      .toArray()
+    const json = await response.json()
+    const post = json.map((article) => {
+      return {
+        title: article.title,
+        description: article.description,
+        image: article.cover_image,
+        link: article.canonical_url,
+      }
+    })
 
     await fs.writeFile(
       './script/substack.json',
