90a759ca29e716ace89e44a3d0a1fcabd2414c7b
Preschian Febryantara <preschian27@gmail.com>
2023-02-09 11:34:44 +0700

    fix: check sw in navigator

--- plugins/pwa.ts
+++ plugins/pwa.ts
@@ -1,34 +1,36 @@
 import { NotificationProgrammatic as Notification } from 'buefy'
 import consola from 'consola'
-import { Workbox } from 'workbox-window'
 
 import { clearSession } from '@/utils/cachingStrategy'
 
-export default () => {
-  const workbox = new Workbox('/sw.js')
+export default async () => {
+  if ('serviceWorker' in navigator) {
+    const { Workbox } = await import('workbox-window')
+    const workbox = new Workbox('/sw.js')
 
-  consola.info('workbox detected', workbox)
+    consola.info('workbox detected', workbox)
 
-  workbox.addEventListener('installed', (event) => {
-    consola.info(
-      'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://pwa.nuxtjs.org/'
-    )
+    workbox.addEventListener('installed', (event) => {
+      consola.info(
+        'App is being served from cache by a service worker.\n' +
+          'For more details, visit https://pwa.nuxtjs.org/'
+      )
 
-    if (event.isUpdate) {
-      consola.info('New content is available, please refresh.')
-      const notif = Notification.open({
-        message: 'New version is ready. Close to upgrade.',
-        queue: false,
-        type: 'is-info is-dark',
-        position: 'is-top-left',
-        indefinite: true,
-        hasIcon: true,
-      })
+      if (event.isUpdate) {
+        consola.info('New content is available, please refresh.')
+        const notif = Notification.open({
+          message: 'New version is ready. Close to upgrade.',
+          queue: false,
+          type: 'is-info is-dark',
+          position: 'is-top-left',
+          indefinite: true,
+          hasIcon: true,
+        })
 
-      notif.$on('close', () => {
-        clearSession()
-      })
-    }
-  })
+        notif.$on('close', () => {
+          clearSession()
+        })
+      }
+    })
+  }
 }
