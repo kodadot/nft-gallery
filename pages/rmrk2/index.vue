e749a21885f72b67c1d08467429f67ca9febe572
Viki Val <viktorko99@gmail.com>
2023-02-16 16:12:59 +0100

    :tada: RMRK2

---
+++ pages/rmrk2/index.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({
+  components: {},
+})
+export default class LandingPage extends Vue {
+  layout() {
+    return 'full-width-layout'
+  }
+
+  middleware({ store, redirect }) {
+    // If the user is not authenticated
+    const prefix = store.getters.currentUrlPrefix
+    if (prefix !== 'movr') {
+      this.$consola.log('Not Moonriver')
+      store.dispatch('setUrlPrefix', 'movr')
+    }
+    setTimeout(() => redirect('/'))
+  }
+}
+</script>
