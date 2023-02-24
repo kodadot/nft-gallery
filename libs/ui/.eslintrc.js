22d0d1c25739362ffdefd3a4eb6ccd3bc509abba
roiLeo <medina.leo42@gmail.com>
2023-02-24 15:51:00 +0100

    🧹 lint code

--- libs/ui/.eslintrc.js
+++ libs/ui/.eslintrc.js
@@ -1,30 +1,12 @@
 module.exports = {
-  extends: ['plugin:vue/recommended'],
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: ['plugin:vue/recommended', 'prettier'],
   rules: {
     // override/add rules settings here, such as:
-    'no-trailing-spaces': 'error',
-    'unicorn/no-for-loop': 'error',
-    'brace-style': ['error', '1tbs', { allowSingleLine: false }],
-    'no-restricted-syntax': [
-      'error',
-      {
-        selector: 'IfStatement > :not(BlockStatement).consequent',
-        message: 'Use blockstatement after if, please check STYLE_GUIDE.md',
-      },
-    ],
-    'no-useless-catch': 1,
-    'no-var': 'error',
-    'linebreak-style': 0,
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-    'max-classes-per-file': ['error', 2],
-    'sort-imports': [
-      'warn',
-      {
-        ignoreDeclarationSort: true,
-        allowSeparatedGroups: true,
-      },
-    ],
-    'vue/multi-word-component-names': 'off',
+    // 'vue/no-unused-vars': 'error'
   },
 }
