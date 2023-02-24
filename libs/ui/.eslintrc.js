28479bd3affeaa8306d3961b1ff2c57155b4a02f
roiLeo <medina.leo42@gmail.com>
2023-02-24 15:48:06 +0100

    🧹 lint code

--- libs/ui/.eslintrc.js
+++ libs/ui/.eslintrc.js
@@ -2,6 +2,29 @@ module.exports = {
   extends: ['plugin:vue/recommended'],
   rules: {
     // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
+    'no-trailing-spaces': 'error',
+    'unicorn/no-for-loop': 'error',
+    'brace-style': ['error', '1tbs', { allowSingleLine: false }],
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'IfStatement > :not(BlockStatement).consequent',
+        message: 'Use blockstatement after if, please check STYLE_GUIDE.md',
+      },
+    ],
+    'no-useless-catch': 1,
+    'no-var': 'error',
+    'linebreak-style': 0,
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'max-classes-per-file': ['error', 2],
+    'sort-imports': [
+      'warn',
+      {
+        ignoreDeclarationSort: true,
+        allowSeparatedGroups: true,
+      },
+    ],
+    'vue/multi-word-component-names': 'off',
   },
 }
