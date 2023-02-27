19b0c3d62ab323d82f536e97217104f65f8e8570
daiagi <daiagi@gmail.com>
2023-02-27 19:26:21 +0700

    add Clear All button to breadcrumbs

--- components/shared/gallery/BreadcrumbsFilter.vue
+++ components/shared/gallery/BreadcrumbsFilter.vue
@@ -1,32 +1,63 @@
 <template>
   <b-field grouped group-multiline class="filters-tag">
-    <template v-for="(value, key) in breads">
-      <div v-if="value === 'true'" :key="key" class="control">
-        <!-- NeoTag -->
-        <NeoTag @close="closeTag(String(key))">
-          {{ queryMapTranslation[String(key)] }}
-        </NeoTag>
-      </div>
+    <template v-if="true">
+      <template v-for="(value, key) in breads">
+        <div v-if="value === 'true'" :key="key" class="control">
+          <!-- NeoTag -->
+          <NeoTag @close="closeTag(String(key))">
+            {{ queryMapTranslation[String(key)] }}
+          </NeoTag>
+        </div>
+        <div
+          v-if="key === 'search' && value != undefined"
+          :key="key"
+          class="control">
+          <NeoTag @close="removeSearch">
+            {{ `${$i18n.t('general.search')}: ${value}` }}
+          </NeoTag>
+        </div>
+      </template>
       <div
-        v-if="key === 'search' && value != undefined"
-        :key="key"
-        class="control">
-        <NeoTag @close="removeSearch">
-          {{ `${$i18n.t('general.search')}: ${value}` }}
-        </NeoTag>
+        v-if="isAnyFilterActive"
+        class="control py-1 is-clickable"
+        @click="clearAllFilters">
+        <span>Clear All</span>
       </div>
     </template>
   </b-field>
 </template>
 
 <script lang="ts" setup>
+import useReplaceUrl from '@/components/explore/filters/useReplaceUrl'
 import NeoTag from './NeoTag.vue'
 
 const route = useRoute()
-const router = useRouter()
-const { $i18n, $consola } = useNuxtApp()
+const { replaceUrl } = useReplaceUrl()
+const { $i18n } = useNuxtApp()
 
-const breads = computed(() => route.query)
+const breads = computed(() => {
+  const filters = { ...route.query }
+  delete filters.redesign
+  console.log(filters)
+  return filters
+})
+
+const isAnyFilterActive = computed(
+  () =>
+    Boolean(breads.value?.search) ||
+    Object.values(breads.value).includes('true')
+)
+
+const clearAllFilters = () => {
+  const clearedFilters = Object.keys(breads.value).reduce((filters, key) => {
+    if (key === 'search') {
+      return { ...filters, [key]: undefined }
+    }
+    return { ...filters, [key]: 'false' }
+  }, {})
+
+  replaceUrl(clearedFilters)
+}
 
 const queryMapTranslation = {
   listed: $i18n.t('sort.listed'),
@@ -35,42 +66,16 @@ const queryMapTranslation = {
 
 onMounted(() => {
   if (route.query.listed == undefined) {
-    router
-      .replace({
-        path: String(route.path),
-        query: {
-          ...route.query,
-          page: '1',
-          listed: 'true',
-        },
-      })
-      .catch($consola.warn)
+    replaceUrl({ listed: 'true' })
   }
 })
 
 const closeTag = (key: string) => {
-  router
-    .replace({
-      path: String(route.path),
-      query: {
-        ...route.query,
-        ...{ [key]: false },
-        page: '1',
-      },
-    })
-    .catch($consola.warn)
+  replaceUrl({ [key]: false })
 }
+
 const removeSearch = () => {
-  router
-    .replace({
-      path: String(route.path),
-      query: {
-        ...route.query,
-        search: undefined,
-        page: '1',
-      },
-    })
-    .catch($consola.warn)
+  replaceUrl({ search: undefined })
 }
 </script>
 
