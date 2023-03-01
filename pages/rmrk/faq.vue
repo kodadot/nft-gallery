5da4f5ef72d140d1205c4301472ed9ab14c7e7d8
Zen <kefengong@gmail.com>
2023-03-01 20:15:56 +0800

    fix: faq not working

--- pages/rmrk/faq.vue
+++ pages/rmrk/faq.vue
@@ -1,3 +1,9 @@
 <template>
   <FrequentlyAsked />
 </template>
+
+<script lang="ts" setup>
+const FrequentlyAsked = defineAsyncComponent(
+  () => import('@/components/common/FrequentlyAsked.vue')
+)
+</script>
