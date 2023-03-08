8c35d2e7b0712d0422c66d70e1ee19198c5196b6
daiagi <daiagi@gmail.com>
2023-03-08 21:47:38 +0700

    massmint - onboarding page

---
+++ components/massmint/OnBoarding.vue
@@ -0,0 +1,187 @@
+<template>
+  <div>
+    <div
+      class="is-flex is-align-items-center is-justify-content-center mb-6 px-3">
+      <div
+        class="is-size-1-desktop is-size-2-tablet is-size-3-mobile is-flex is-flex-grow-1 is-justify-content-center has-text-weight-bold">
+        Mass Mint Onboarding
+      </div>
+      <NeoButton :label="'Skip'" icon="arrow-right" icon-pack="fas" />
+    </div>
+    <div
+      class="carousel is-flex is-flex-direction-column is-align-items-center is-clipped is-relative">
+      <div
+        class="carousel-wrapper is-flex is-flex-wrap-nowrap"
+        :class="`slide-${currentSlide}`">
+        <div
+          v-for="(card, index) in cards"
+          :key="index"
+          class="carousel-card is-flex p-5 is-justify-content-start mobile-padding"
+          :class="{ 'not-active': index !== currentSlide }">
+          <div class="card__content">
+            <p
+              class="title is-size-2-desktop is-size-3-tablet is-size-5-mobile is-capitalized">
+              {{ card.title }}
+            </p>
+            <div class="content is-size-4-tablet is-size-5-mobile">
+              <VueMarkdown :source="card.content" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="is-flex is-justify-content-center my-6">
+      <span
+        v-for="(card, index) in cards"
+        :key="index"
+        class="carousel-dot mx-2"
+        :class="{ 'is-active': index === currentSlide }"></span>
+    </div>
+    <div class="is-flex is-justify-content-center">
+      <NeoButton
+        :label="currentSlide === 2 ? 'Done' : 'Next'"
+        class="is-flex-grow-1 limit-width"
+        variant="k-accent"
+        @click.native="nextSlide" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { NeoButton } from '@kodadot1/brick'
+import VueMarkdown from 'vue-markdown-render'
+
+interface Card {
+  title: string
+  content: string
+}
+
+const cards: Card[] = [
+  {
+    title: 'How to Name your NFTs',
+    content: `instructions:
+
+* **File Name:** the file name of your assets should be
+  **numeric** and **consecutive** 
+
+e.g. 
+* '1.png', '2.png', '3.png', or 
+* 'awesome_art_1', 'awesome_art_2' and so on...
+`,
+  },
+  {
+    title: 'description for your NFTs',
+    content: `
+Each NFT has a name, description and price.
+You can prepare this up front in a separate file
+so that you don’t have to do it manually for all of your NFTs.
+
+Instructions:
+* File type: .TXT, .JSON or CSV
+`,
+  },
+  {
+    title: 'You need to have A collection',
+    content: `In order to use the Mass Mint feature **you need to have an NFT collection created**
+     to which you will mint all of your NFTs. 
+
+If you don’t have a collection created yet, you will be able to create one in the Miss Mint.
+`,
+  },
+]
+
+const currentSlide = ref(0)
+
+const nextSlide = () => {
+  if (currentSlide.value < cards.length - 1) {
+    currentSlide.value++
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/abstracts/variables';
+
+$card-width-percents: 54%;
+$gap-percents: 5.5%;
+
+$max-card-width: 760px;
+$min-card-width: 225px;
+$max-card-height: 464px;
+$max-gap: 80px;
+
+$gap: min($gap-percents, $max-gap);
+$card-width: clamp($min-card-width, $card-width-percents, $max-card-width);
+
+$base-shift: calc((100% - $card-width) / 2);
+
+.carousel-wrapper {
+  //   margin-left: calc((100% - (#{$card-width})) / 2);
+  transition: transform 0.5s ease-in-out;
+  gap: #{$gap};
+
+  &.slide-0 {
+    transform: translateX($base-shift);
+  }
+
+  &.slide-1 {
+    transform: translateX(calc($base-shift + (-1 * ($card-width + $gap))));
+  }
+
+  &.slide-2 {
+    transform: translateX(calc($base-shift + (-2 * ($card-width + $gap))));
+  }
+}
+
+.carousel-card {
+  flex: 0 0 #{$card-width};
+  height: $max-card-height;
+
+  &.mobile-padding {
+    @include mobile {
+      padding: 0.75rem !important;
+    }
+  }
+  @include ktheme() {
+    box-shadow: theme('primary-shadow');
+    background: theme('background-color');
+    border: 1px solid theme('border-color');
+  }
+  &__content {
+    @include ktheme() {
+      color: theme('text-color') !important;
+    }
+    .content {
+      @include ktheme() {
+        color: theme('text-color') !important;
+      }
+    }
+  }
+  &.not-active {
+    opacity: 0.5;
+  }
+}
+
+.carousel-dot {
+  width: 10px;
+  height: 10px;
+  border-radius: 25%;
+  background-color: #ccc;
+  transition: background-color 0.2s ease-in-out;
+  @include ktheme() {
+    background-color: theme('k-shade');
+  }
+
+  &.is-active {
+    @include ktheme() {
+      background-color: theme('text-color');
+    }
+  }
+}
+
+.limit-width {
+  max-width: $card-width;
+  min-width: $min-card-width;
+}
+</style>
---
+++ pages/_prefix/massmint/onboarding.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="py-6">
+    <OnBoarding />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import OnBoarding from '@/components/massmint/OnBoarding.vue'
+const components = { OnBoarding }
+
+@Component<OnBoardingPage>({
+  components,
+  layout() {
+    return 'full-width-layout'
+  },
+  head() {
+    const title = 'Low minting fees and carbonless NFTs'
+    const metaData = {
+      title,
+      type: 'profile',
+      description: 'Buy Carbonless NFTs on Kusama',
+      url: `/${this.$route.params.prefix}/massmint/onboarding`,
+      image: `${this.$config.public.baseUrl}/k_card.png`,
+    }
+    return {
+      title,
+      meta: [...this.$seoMeta(metaData)],
+    }
+  },
+})
+export default class OnBoardingPage extends Vue {}
+</script>
