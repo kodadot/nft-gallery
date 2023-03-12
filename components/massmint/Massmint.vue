66d5f9364f0455220fe52eb97ac28d1b14a1898e
daiagi <daiagi@gmail.com>
2023-03-12 16:34:29 +0700

    continue

--- components/massmint/Massmint.vue
+++ components/massmint/Massmint.vue
@@ -20,16 +20,54 @@
     <section
       class="pt-6 is-flex is-flex-direction-column is-align-items-center">
       <p class="mb-4">Choose a collection to which you want to mint</p>
-      <NeoDropdown class="w-40 max-width">
+      <NeoDropdown
+        class="dropdown-width"
+        :disabled="!isLogIn"
+        @active-change="toggleOpen">
         <NeoButton
-          class="w-100"
+          v-if="!selectedCollection"
+          class="dropdown-width"
           :label="'Select Collection'"
-          icon="chevron-down" />
-        <template #items>
+          :icon="isDropdownopen ? 'chevron-up' : 'chevron-down'" />
+        <NeoButton
+          v-else
+          class="dropdown-width"
+          :icon="isDropdownopen ? 'chevron-up' : 'chevron-down'">
+          {{ selectedCollection }}
+          <o-icon
+            v-if="selectedCollection"
+            icon="check"
+            size="small"
+            variant="success"
+            class="ml-3" />
+        </NeoButton>
+        <template v-if="collectionsEntites?.length" #items>
           <NeoDropdownItem
             v-for="c in collectionsEntites"
             :key="c.id"
-            :item="c.name">
+            class="dropdown-width"
+            @click.native="selectCollection(c.name)">
+            <span class="px-5">
+              {{ c.name }}
+            </span>
+          </NeoDropdownItem>
+          <NeoDropdownItem class="dropdown-width">
+            <div>
+              <o-icon pack="fas" icon="plus" size="small" class="mr-1">
+              </o-icon>
+              <span>Create New Collection</span>
+            </div>
+          </NeoDropdownItem>
+        </template>
+        <template v-else #items>
+          <NeoDropdownItem disabled class="dropdown-width">
+            <span class="px-5"> No Collection </span>
+          </NeoDropdownItem>
+          <NeoDropdownItem class="dropdown-width">
+            <div>
+              <o-icon pack="fas" icon="plus" size="small" class="mr-1" />
+              <span>Create New Collection</span>
+            </div>
           </NeoDropdownItem>
         </template>
       </NeoDropdown>
@@ -39,6 +77,7 @@
 
 <script setup lang="ts">
 import { NeoButton, NeoDropdown, NeoDropdownItem } from '@kodadot1/brick'
+import { OIcon } from '@oruga-ui/oruga'
 import { useMassmintsStore } from '@/stores/massmint'
 import TabItem from '@/components/explore/tab/TabItem.vue'
 import { useMassMint } from './useMassMint'
@@ -47,10 +86,24 @@ const { $consola } = useNuxtApp()
 const router = useRouter()
 const route = useRoute()
 const { urlPrefix } = usePrefix()
+const { isLogIn, accountId } = useAuth()
 
 const tabs = ['Collection', 'NFT', 'Mass Mint']
 
 const { collectionsEntites } = useMassMint()
+const isDropdownopen = ref(false)
+const selectedCollection = ref<string>()
+
+watch(accountId, () => {
+  selectedCollection.value = undefined
+})
+
+const toggleOpen = () => {
+  isDropdownopen.value = !isDropdownopen.value
+}
+const selectCollection = (name: string) => {
+  selectedCollection.value = name
+}
 
 const toOnborading = () => {
   massMintStore.setVisitedOnboarding(false)
@@ -63,13 +116,21 @@ const toOnborading = () => {
 </script>
 
 <style lang="scss" scoped>
-.w-40 {
-  width: 40%;
+@import '@/styles/abstracts/variables';
+
+.dropdown-width {
+  width: 30rem;
 }
 .max-width {
   max-width: 466px;
 }
 .w-100 {
   width: 100%;
+}
+
+.o-icon--success {
+  @include ktheme() {
+    color: theme('k-green');
+  }
 }
 </style>
