7a02adc4f1ee44bded187427820baced5355abbc
Floyd Li <floyd.li@outlook.com>
2023-03-18 16:24:30 +0800

    feat: Increase card number on landing

--- components/carousel/CarouselIndex.vue
+++ components/carousel/CarouselIndex.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Description:
+ * @Author: Floyd Li (floyd.li@outlook.com)
+ * @Date: 2023-03-13 17:20:12
+ * @LastEditors: Floyd Li (floyd.li@outlook.com)
+ * @LastEditTime: 2023-03-18 16:21:22
+-->
 <template>
   <div v-if="nfts.length">
     <h2 class="title is-2">
@@ -8,11 +15,12 @@
       v-if="showCarousel"
       :nfts="nfts"
       :gallery-item-carousel="galleryItemCarousel"
-      :step="3" />
+      :step="steps" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { useWindowSize } from '@vueuse/core'
 import type { CarouselNFT } from '@/components/base/types'
 import type { RowSeries } from '@/components/series/types'
 
@@ -36,4 +44,18 @@ const itemUrl = computed(() => props.itemUrl || 'gallery')
 provide('itemUrl', itemUrl.value)
 
 const showCarousel = computed(() => props.nfts.length)
+
+const { width } = useWindowSize()
+const steps = computed(() => {
+  if (width.value > 1280) {
+    return 6
+  }
+  if (width.value > 1024) {
+    return 4
+  }
+  if (width.value > 768) {
+    return 2
+  }
+  return 1
+})
 </script>
