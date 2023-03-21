d24d0defaf10126bf1286ab37094d5a4ae5e90a8
daiagi <daiagi@gmail.com>
2023-03-21 20:44:04 +0700

    🔧 on Snek

--- components/bsx/Create/utils.ts
+++ components/bsx/Create/utils.ts
@@ -3,11 +3,6 @@ import { getAssetIdByAccount } from '@/utils/api/bsx/query'
 
 export type Token = 'BSX' | 'KSM'
 
-const tokenToTokenId: { [K in Token]: string } = {
-  BSX: '0',
-  KSM: '1',
-}
-
 export const ksmToBsx = (value: number): number => {
   const fiatStore = useFiatStore()
   const KSMToUsd = fiatStore.getCurrentKSMValue
@@ -18,9 +13,10 @@ export const ksmToBsx = (value: number): number => {
 export const getBalance = (token?: Token): number => {
   const { balance } = useAuth()
   const { $store } = useNuxtApp()
+  const { tokenId } = usePrefix()
 
   if (token === 'KSM') {
-    return $store.getters.getTokenBalanceOf(tokenToTokenId['KSM'])
+    return $store.getters.getTokenBalanceOf(tokenId.value)
   }
   return balance.value
 }
@@ -33,10 +29,17 @@ export const getFeesToken = async (): Promise<Token> => {
   const { apiInstance } = useApi()
   const { accountId } = useAuth()
   const { $consola } = useNuxtApp()
+  const bsxTokenId = '0'
   try {
     const api = await apiInstance.value
+
+    //this function returns
+    // '0' if paying in bsx (on Basilisk and Snek)
+    // '1' if paying in ksm (on basilisk)
+    // '5' if paying in ksm (on Snek)
     const tokenId = await getAssetIdByAccount(api, accountId.value)
-    return tokenId === tokenToTokenId['BSX'] ? 'BSX' : 'KSM'
+
+    return tokenId === bsxTokenId ? 'BSX' : 'KSM'
   } catch (e) {
     $consola.log(e)
   }
