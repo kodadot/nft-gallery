9493dabd7e97994b038c73c7e8ec5fb8167a1ce8
Prachi Darshana <prachirai00@gmail.com>
2023-03-21 19:43:09 -0700

    changes

--- components/massmint/UploadPicture/UploadPicture.vue
+++ components/massmint/UploadPicture/UploadPicture.vue
@@ -1,21 +1,19 @@
 <template>
-  <div>
-    <NeoCollapseStack>
-      <NeoCollapse>
-        <template #title> {{ $t('massmint.uploadPics') }}</template>
-        <template #content>
-          <DropUpload
-            preview
-            :label="$t('mint.collection.massmintDrop')"
-            accept="image/*, audio/*, .zip"
-            @input="selectedFile" />
-        </template>
-      </NeoCollapse>
-      <NeoCollapse>
-        <template #title> {{ $t('massmint.uploadDesc') }} </template>
-        <template #content> custom content </template>
-      </NeoCollapse>
-    </NeoCollapseStack>
+  <div class="collpase-massmint">
+    <NeoCollapse>
+      <template #title> {{ $t('massmint.uploadPics') }}</template>
+      <template #content>
+        <DropUpload
+          preview
+          :label="$t('mint.collection.massmintDrop')"
+          accept="image/*, audio/*, .zip"
+          @input="selectedFile" />
+      </template>
+    </NeoCollapse>
+    <NeoCollapse>
+      <template #title> {{ $t('massmint.uploadDesc') }} </template>
+      <template #content> custom content </template>
+    </NeoCollapse>
   </div>
 </template>
 
@@ -30,3 +28,16 @@ const selectedFile = (file) => {
   console.log('selected file', file)
 }
 </script>
+
+<style lang="scss" scoped>
+@import '@/styles/abstracts/variables';
+
+.collpase-massmint {
+  @include ktheme() {
+    background-color: theme('background-color');
+    color: theme('text-color');
+    border: 1px solid theme('border-color');
+    box-shadow: theme('primary-shadow');
+  }
+}
+</style>
