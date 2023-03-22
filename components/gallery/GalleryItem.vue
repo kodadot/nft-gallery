0d59a7b65f21615162c954bba9724eb116a2ea75
Floyd Li <floyd.li@outlook.com>
2023-03-22 23:08:17 +0800

    Revert "feat: use svg from design"
    This reverts commit a21547cc6ebf2db80b344ec4e6654b48e7ec7521.

--- components/gallery/GalleryItem.vue
+++ components/gallery/GalleryItem.vue
@@ -11,7 +11,7 @@
     <div class="columns is-variable is-6">
       <div class="column is-two-fifths is-relative">
         <a class="fullscreen-button" @click="isFullscreen = true">
-          <i class="icon-expand" />
+          <NeoIcon icon="expand" />
         </a>
         <MediaItem
           :key="nftImage"
@@ -227,17 +227,14 @@ $break-point-width: 930px;
   top: 2rem;
   z-index: 1;
   display: none;
+  @include ktheme() {
+    color: theme('text-color-inverse');
+    background: theme('background-color-inverse');
+    padding: 0 0.3rem;
+    opacity: 0.6;
+  }
 }
 
-i.icon-expand {
-  display: block;
-  height: 35px;
-  width: 35px;
-  background-image: url('@/static/expand_light.svg');
-}
-.dark-mode i.icon-expand {
-  background-image: url('@/static/expand_dark.svg');
-}
 .column:hover .fullscreen-button {
   display: block;
 }
