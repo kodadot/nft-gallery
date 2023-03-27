9aff17b8187fbd9ea202b4b0a061b71e35f15f31
Preschian F <preschian27@gmail.com>
2023-03-27 13:35:19 +0700

    refactor: remove preferences on vuex-persist

--- plugins/vuex-persist.ts
+++ plugins/vuex-persist.ts
@@ -2,10 +2,6 @@ import VuexPersistence from 'vuex-persist'
 
 export default ({ store }): void => {
   new VuexPersistence({
-    key: 'preferences',
-    storage: window.localStorage,
-  }).plugin(store)
-  new VuexPersistence({
     key: 'setting',
     storage: window.sessionStorage,
   }).plugin(store)
