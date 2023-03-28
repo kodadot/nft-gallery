1e3ae3e82f4bfb0756e77b22a7f8524aa3abcc3d
Preschian F <preschian27@gmail.com>
2023-03-28 14:24:23 +0700

    refactor: move @kodadot1/static

---
+++ libs/static/src/services.ts
@@ -0,0 +1,8 @@
+export const SERVICES: Record<string, string> = {
+  directUpload: 'https://direct-upload.w.kodadot.xyz/',
+  nftStorage: 'https://nft-storage.w.kodadot.xyz/',
+  seoCard: 'https://og-image-green-seven.vercel.app/',
+  replicate: 'https://replicate.w.kodadot.xyz/',
+  image: 'https://image.w.kodadot.xyz/',
+  imageBeta: 'https://image-beta.w.kodadot.xyz/',
+}
