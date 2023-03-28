24b8ef0adc47d413b3a4b56e6ccaabc156299519
Preschian F <preschian27@gmail.com>
2023-03-28 20:44:41 +0700

    refactor: explorers

--- libs/static/src/services.ts
+++ libs/static/src/services.ts
@@ -1,3 +1,6 @@
+import { oldKsm } from './chains'
+import type { BackwardPrefix, Prefix } from './types'
+
 export const SERVICES: Record<string, string> = {
   directUpload: 'https://direct-upload.w.kodadot.xyz/',
   nftStorage: 'https://nft-storage.w.kodadot.xyz/',
@@ -5,4 +8,21 @@ export const SERVICES: Record<string, string> = {
   replicate: 'https://replicate.w.kodadot.xyz/',
   image: 'https://image.w.kodadot.xyz/',
   imageBeta: 'https://image-beta.w.kodadot.xyz/',
+}
+
+export const EXPLORERS: Record<Prefix, string> = {
+  ksm: 'https://kusama.subscan.io/account/',
+  rmrk2: 'https://kusama.subscan.io/account/',
+  bsx: 'https://basilisk.subscan.io/account/',
+  snek: 'https://calamar.play.hydration.cloud/rococo%20basilisk/search?query=',
+  movr: 'https://moonriver.subscan.io/account/',
+  glmr: 'https://moonbeam.subscan.io/account/',
+}
+
+export const hasExplorer = (prefix: BackwardPrefix): boolean => {
+  return EXPLORERS[oldKsm(prefix)] !== undefined
+}
+
+export const getExplorer = (prefix: BackwardPrefix, id: string) => {
+  return EXPLORERS[oldKsm(prefix)] + id
 }
