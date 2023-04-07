2496c87195eb5899a93449b43f5a718fad7e6e05
Floyd Li <floyd.li@outlook.com>
2023-04-07 22:16:29 +0800

    fix: type waring

--- components/common/NotificationBox/useNotificationBox.ts
+++ components/common/NotificationBox/useNotificationBox.ts
@@ -2,6 +2,6 @@ import NotificationBoxModal from './NotificationBoxModal.vue'
 
 export const NotificationBoxModalConfig = {
   component: NotificationBoxModal,
-  canCancel: 'outside',
+  canCancel: ['outside'],
   customClass: 'notification-box-modal',
 }
