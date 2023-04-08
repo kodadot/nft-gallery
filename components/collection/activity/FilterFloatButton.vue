b2d20fedb221e4a9e9c7d1ef0c9d327e1973f235
Leo Anderson <128157824+leo-anderson-x@users.noreply.github.com>
2023-04-08 17:19:48 +0900

    extract `activeFilters` to a composable

--- components/collection/activity/FilterFloatButton.vue
+++ components/collection/activity/FilterFloatButton.vue
@@ -10,18 +10,14 @@
 <script setup lang="ts">
 import { NeoButton } from '@kodadot1/brick'
 import { usePreferencesStore } from '@/stores/preferences'
+import useActiveRouterFilters from '@/composables/useActiveRouterFilters'
 
-const route = useRoute()
 const preferencesStore = usePreferencesStore()
+const activeFilters = useActiveRouterFilters()
 
-const numOfActiveFilters = computed(() => {
-  const query = { ...route.query, redesign: undefined }
-  const activeFilters = Object.entries(query).filter(
-    ([key, value]) => (key === 'search' && Boolean(value)) || value === 'true'
-  )
-
-  return activeFilters.length
-})
+const numOfActiveFilters = computed(
+  () => Object.keys(activeFilters.value).length
+)
 const openMobileFilters = () => preferencesStore.setMobileFilterCollapse(true)
 </script>
 
