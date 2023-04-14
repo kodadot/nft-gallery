72c39430f44d566e4cee521b5c8bad77959082a4
Viki Val <viktorko99@gmail.com>
2023-04-14 15:10:28 +0200

    :label: RMRK2 forever

--- libs/static/src/names.ts
+++ libs/static/src/names.ts
@@ -4,7 +4,7 @@ export const NAMES: Record<Prefix, string> = {
   bsx: 'Basilisk',
   glmr: 'Moonbeam [Beta]',
   rmrk: 'Kusama',
-  ksm: 'RMRK 2.0 [Beta]',
+  ksm: 'RMRK2',
   movr: 'Moonriver [Beta]',
   snek: 'Snek [Rococo]',
 }
