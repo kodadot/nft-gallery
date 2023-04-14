8f572d82057483818ed7e0ae7da67b3eb39f61e8
roiLeo <medina.leo42@gmail.com>
2023-04-14 14:26:26 +0200

    🔧 windows workflow

---
+++ libs/ui/src/components/NeoModal/NeoModal.story.vue
@@ -0,0 +1,15 @@
+<template>
+  <Story title="NeoNftCard">
+    <Variant title="Nft Card">
+      <NeoModal v-model="modalActive" @close="modalActive = false">
+        Modal Content
+      </NeoModal>
+    </Variant>
+  </Story>
+</template>
+
+<script lang="ts" setup>
+import NeoModal from './NeoModal.vue'
+
+const modalActive = ref(true)
+</script>
