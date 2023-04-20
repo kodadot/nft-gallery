4ca5d3cfac8f739fa017ba1ca9f5d71504ddbc7e
Prachi Darshana <prachirai00@gmail.com>
2023-04-19 17:35:03 -0700

    #5726 card changes

--- libs/ui/src/components/NeoNftCard/NeoNftCard.vue
+++ libs/ui/src/components/NeoNftCard/NeoNftCard.vue
@@ -9,51 +9,52 @@
         :mime-type="nft.mimeType"
         :is-dark-mode="isDarkMode"
         :title="nft?.name" />
-    </nuxt-link>
-    <div
-      class="nft-media-info is-flex is-flex-direction-column"
-      :class="`nft-media-info__${variant}`">
-      <div class="is-flex is-flex-direction-column">
-        <span class="is-ellipsis has-text-weight-bold">{{
-          nft.name || '--'
-        }}</span>
-
-        <CollectionDetailsPopover
-          v-if="
-            variant !== 'minimal' && (nft.collection.name || nft.collection.id)
-          "
-          :show-delay="collectionPopoverShowDelay"
-          :nft="nft"
-          class="is-ellipsis">
-          <template #trigger>
-            <nuxt-link
-              :title="nft.collectionName || nft.collection.name"
-              :to="`/${prefix}/collection/${nft.collection.id}`"
-              class="is-size-7 nft-info-collection-name">
-              {{ nft.collection.name || '--' }}
-            </nuxt-link>
-          </template>
-        </CollectionDetailsPopover>
-      </div>
-
       <div
-        class="is-flex is-align-items-center mt-2 is-ellipsis nft-media-info-footer"
-        :class="[
-          showPrice
-            ? 'is-justify-content-space-between'
-            : 'is-justify-content-end',
-        ]">
-        <CommonTokenMoney
-          v-if="showPrice"
-          :value="nft.price"
-          data-cy="card-money" />
-        <span
-          v-if="variant !== 'minimal'"
-          class="chain-name is-capitalized is-size-7"
-          >{{ getChainNameByPrefix(prefix) }}</span
-        >
+        class="nft-media-info is-flex is-flex-direction-column"
+        :class="`nft-media-info__${variant}`">
+        <div class="is-flex is-flex-direction-column">
+          <span class="is-ellipsis has-text-weight-bold">{{
+            nft.name || '--'
+          }}</span>
+
+          <CollectionDetailsPopover
+            v-if="
+              variant !== 'minimal' &&
+              (nft.collection.name || nft.collection.id)
+            "
+            :show-delay="collectionPopoverShowDelay"
+            :nft="nft"
+            class="is-ellipsis">
+            <template #trigger>
+              <nuxt-link
+                :title="nft.collectionName || nft.collection.name"
+                :to="`/${prefix}/collection/${nft.collection.id}`"
+                class="is-size-7 nft-info-collection-name">
+                {{ nft.collection.name || '--' }}
+              </nuxt-link>
+            </template>
+          </CollectionDetailsPopover>
+        </div>
+
+        <div
+          class="is-flex is-align-items-center mt-2 is-ellipsis nft-media-info-footer"
+          :class="[
+            showPrice
+              ? 'is-justify-content-space-between'
+              : 'is-justify-content-end',
+          ]">
+          <CommonTokenMoney
+            v-if="showPrice"
+            :value="nft.price"
+            data-cy="card-money" />
+          <span
+            v-if="variant !== 'minimal'"
+            class="chain-name is-capitalized is-size-7"
+            >{{ getChainNameByPrefix(prefix) }}</span
+          >
+        </div>
       </div>
-    </div>
+    </nuxt-link>
   </div>
 </template>
 
