85b4e5ef2f806b45707bba075be9be9652075a78
Floyd Li <floyd.li@outlook.com>
2023-04-24 20:44:50 +0800

    fix: fix typo and add comments

--- components/shared/filters/modules/constants.ts
+++ components/shared/filters/modules/constants.ts
@@ -1,43 +1,42 @@
 export const POPULAR_COLLECTIONS = {
   ksm: [
-    'be15890524c6e9b359-WIZARD',
-    '66241b171becfd500e-EGG',
-    '342f12106eab6d904c-YOUDLE',
-    '3e6f301b0a0b1a0e4e-MRWH',
-    '8ac9852f42a86a395d-8GDSY',
-    '72559667d353e6dc0d-HUBIM',
-    '7a147cba01afccbf17-GA',
-    '9692fa834afcce2d3e-RNDM',
-    'a2a383db29d7601e4b-TONICS',
-    '800f8a914281765a7d-KITTY',
-    'c210bd52d397099b2c-JPS',
-    '7cf9daa38281a57331-BSS',
-    'f69287204232615e37-NEP4I',
-    '22708b368d163c8007-KV',
-    'dac5c7f54029d0e73c-HXAI8',
-    '2644199cf3652aaa78-KK01',
-    'b45943b4edbf6f5c0f-SKYLAB_ARTEMIS_R4VENS',
-    'dac5c7f54029d0e73c-XCHIMPZ',
-    'be15890524c6e9b359-TRUSTED',
-    'e63e926f7db5997e5c-STGR',
-    '702baf9434daae9f53-INKPIONEERS1',
-    'a0c4801b6dbf04c674-P1Z70',
-    '3629a3ff458d113e25-PUNK',
+    'be15890524c6e9b359-WIZARD', // Manta Wizard
+    '66241b171becfd500e-EGG', //KU EGGS
+    '342f12106eab6d904c-YOUDLE', //Youdles
+    '3e6f301b0a0b1a0e4e-MRWH', //Mr Whitehole
+    '8ac9852f42a86a395d-8GDSY', //EJVII's Collection
+    '72559667d353e6dc0d-HUBIM', //The Human Birds
+    '7a147cba01afccbf17-GA', //⁂ GenerativeAsterism
+    '9692fa834afcce2d3e-RNDM', //RNDM ART
+    'a2a383db29d7601e4b-TONICS', //Toxic Tonics
+    '800f8a914281765a7d-KITTY', //Kitty Paradise
+    'c210bd52d397099b2c-JPS', //JaPandas Squad
+    '7cf9daa38281a57331-BSS', //Budget Spaceships
+    'f69287204232615e37-NEP4I', //Hue Lady Set
+    '22708b368d163c8007-KV', //Kessel's Vessels Vol. 1
+    'dac5c7f54029d0e73c-HXAI8', // /// Chaos-DAO Apes ///
+    '2644199cf3652aaa78-KK01', //Kusama Kings
+    'b45943b4edbf6f5c0f-SKYLAB_ARTEMIS_R4VENS', //The Artemis Project - Code R4VEN
+    'dac5c7f54029d0e73c-XCHIMPZ', //XChimpz
+    'be15890524c6e9b359-TRUSTED', //Trusted Contributors
+    'e63e926f7db5997e5c-STGR', //STARGAZER
+    '702baf9434daae9f53-INKPIONEERS1', //ink! Pioneers Collection 1
+    'a0c4801b6dbf04c674-P1Z70', //420 Kusama Club
+    '3629a3ff458d113e25-PUNK', //RMRK Punks
   ],
   bsx: [
-    '186590971',
-    '3268254081',
-    '255118262',
-    '3929608780',
-    '3579963603',
-    '3929608780',
-    '608015335',
-    '1825819407',
-    '945672150',
-    '2349262476',
-    '3715754953',
-    '2385422779',
-    '2334273319',
-    '1635680444',
+    '1865909717', //Berliner
+    '3268254081', //Ancient Civilizations
+    '2551182625', //Luna Witches
+    '3579963603', //Hooker's Urban
+    '3929608780', //Sentinels of the New Age
+    '608015335', //Cascadia
+    '1825819407', //KoDragons
+    '945672150', //NAGA KADAL
+    '2349262476', //Beyond boundaries
+    '3715754953', //Coalesce
+    '2385422779', //Fli.
+    '2334273319', //Guardians and Creatures
+    '1635680444', //KODACHAINS
   ],
 }
