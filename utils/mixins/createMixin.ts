d89a812500ed3d1f092b2af704857fe780661850
Zen <kefengong@gmail.com>
2023-04-28 20:38:44 +0800

    fix: keep activeTab in url

--- utils/mixins/createMixin.ts
+++ utils/mixins/createMixin.ts
@@ -1,5 +1,6 @@
-import { Component, Vue } from 'nuxt-property-decorator'
+import { Component, Vue, Watch } from 'nuxt-property-decorator'
 import { showNotification } from '@/utils/notification'
+
 export const enum CreateComponent {
   Collection = 'Collection',
   NFT = 'NFT',
@@ -22,5 +23,19 @@ export default class CreateMixin extends Vue {
       (this.activeTab = targetIdx > -1 ? targetIdx : 0)
     showNotification('You will go to create nft in 2 seconds')
     setTimeout(delaySwitchFn, 2000)
+  }
+  @Watch('activeTab', { immediate: true })
+  onTabChange(newTab: number) {
+    const { replaceUrl } = useReplaceUrl()
+    replaceUrl({
+      tab: newTab === 1 ? CreateComponent.NFT : CreateComponent.Collection,
+    })
+  }
+  created() {
+    const route = useRoute()
+    const tab = route.query.tab
+    if (tab) {
+      this.activeTab = tab === CreateComponent.NFT ? 1 : 0
+    }
   }
 }
