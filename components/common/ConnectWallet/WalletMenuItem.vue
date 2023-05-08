2049d3361c46516447f1bdcce272bc4365b34772
Prachi Darshana <prachirai00@gmail.com>
2023-05-08 12:30:57 -0700

    auth icon fix

--- components/common/ConnectWallet/WalletMenuItem.vue
+++ components/common/ConnectWallet/WalletMenuItem.vue
@@ -23,7 +23,7 @@
       </div>
     </b-button>
     <div
-      v-if="(isAuth && walletAccounts.length === 0) || true"
+      v-if="isAuth && walletAccounts.length === 0"
       class="pl-5 pt-2 pb-2 is-flex is-align-items-center auth-tip">
       <NeoIcon icon="spinner-third" pack="fad" />
       <span class="has-text-grey is-size-7 pl-4">
