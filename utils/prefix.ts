c133881a13ead509cf9eca70bee08e712de78ec5
Viki Val <viktorko99@gmail.com>
2023-05-08 15:09:27 +0200

    :coffin: old code

--- utils/prefix.ts
+++ utils/prefix.ts
@@ -20,35 +20,5 @@ export function isRemark(prefix: string): boolean {
   return prefix === 'rmrk'
 }
 
-export function getSupportedClient(prefix: string): 'subquery' | 'subsquid' {
-  switch (prefix) {
-    case 'stmn':
-    case 'westmint':
-    case 'rmrk':
-      return 'subquery'
-    case 'bsx':
-    case 'snek':
-      return 'subsquid'
-    default:
-      return 'subsquid'
-  }
-}
-
-export function prefixBuildOnText(prefix: string): string {
-  switch (prefix) {
-    case 'bsx':
-    case 'snek':
-      return 'Basilisk NFT Pallet'
-    case 'stmn':
-    case 'westmint':
-      return 'Statemine Unique Pallet'
-    case 'movr':
-    case 'glmr':
-      return 'EVM Smart Contracts'
-    default:
-      return 'RMRK Protocol'
-  }
-}
-
 export const hasMarketplace = (prefix: string): boolean =>
   ['rmrk', 'bsx'].includes(prefix)
