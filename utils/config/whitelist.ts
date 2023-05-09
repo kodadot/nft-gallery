ae4d4ccdc0df417588e2307fa62eb7a9a4cfba92
Prachi Darshana <prachirai00@gmail.com>
2023-05-09 09:27:43 -0700

    #5886 remove whitelist rmrk2

--- utils/config/whitelist.ts
+++
@@ -1,7 +0,0 @@
-import { toDefaultAddress } from '@/utils/account'
-
-export const getRedirectToRmrk2HostnameWhitelist = () =>
-  [
-    'CuHWHNcBt3ASMVSJmcJyiBWGxxiWLyjYoYbGjfhL4ovoeSd',
-    'H56NQrVAH4oyJPiYVqhTsA3uTcfAdcNaYtkfEGai9jyVnhE',
-  ].map((address) => toDefaultAddress(address))
