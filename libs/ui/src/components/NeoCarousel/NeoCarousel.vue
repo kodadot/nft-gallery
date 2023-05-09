9b883796effb798b136d702e9b2b0bdea7fcc1ad
Preschian Febryantara <preschian@users.noreply.github.com>
2023-05-09 17:17:38 +0700

    feat: NeoCarousel

---
+++ libs/ui/src/components/NeoCarousel/NeoCarousel.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { OCarousel } from '@oruga-ui/oruga'
+
+export default {
+  extends: OCarousel,
+}
+</script>
+
+<style lang="scss">
+@import '@oruga-ui/oruga/src/scss/utilities/expressions.scss';
+@import '@oruga-ui/oruga/src/scss/utilities/variables.scss';
+@import '@oruga-ui/oruga/src/scss/utilities/animations.scss';
+@import '@oruga-ui/oruga/src/scss/utilities/helpers.scss';
+@import '@oruga-ui/oruga/src/scss/components/_carousel.scss';
+</style>
---
+++ libs/ui/src/components/NeoCarousel/NeoCarouselItem.vue
@@ -0,0 +1,7 @@
+<script lang="ts">
+import { OCarouselItem } from '@oruga-ui/oruga'
+
+export default {
+  extends: OCarouselItem,
+}
+</script>
