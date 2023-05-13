c6a2e1a0f7bc2ec9f29afed3f5501ff1ab9bd39b
Preschian Febryantara <preschian@users.noreply.github.com>
2023-05-13 17:40:53 +0700

    test: adjust mock login

--- pages/e2e-login.vue
+++ pages/e2e-login.vue
@@ -1,28 +1,40 @@
 <template>
-  <div></div>
+  <div>
+    <p data-cy="mockAddress">{{ mockAddress }}</p>
+  </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
+<script lang="ts" setup>
 import { mnemonicGenerate } from '@polkadot/util-crypto'
 import keyring from '@polkadot/ui-keyring'
-import PrefixMixin from '@/utils/mixins/prefixMixin'
-import { ss58Of } from '~~/utils/config/chain.config'
+import { ss58Of } from '@/utils/config/chain.config'
 
-@Component({})
-export default class E2ELogin extends PrefixMixin {
-  created() {
-    const mnemonic = mnemonicGenerate(12)
-    const { pair } = keyring.addUri(mnemonic, '', {
-      name: 'mnemonic acc',
-    })
-    // TODO: check 'loadAll' error, approx 1 in 10 tests fail without this
-    keyring.setSS58Format(ss58Of(this.urlPrefix))
-    keyring.addPair(pair, '')
-    const account = pair.address
-    this.$store.dispatch('setAuth', { address: account })
-    localStorage.setItem('kodaauth', account)
-    this.$router.push('/')
-  }
-}
+const { urlPrefix } = usePrefix()
+const mockAddress = ref(false)
+
+onMounted(() => {
+  const mnemonic = mnemonicGenerate(12)
+  const { pair } = keyring.addUri(mnemonic, '', {
+    name: 'mnemonic acc',
+  })
+
+  // TODO: check 'loadAll' error, approx 1 in 10 tests fail without this
+  keyring.setSS58Format(ss58Of(urlPrefix.value))
+  keyring.addPair(pair, '')
+  const account = pair.address
+
+  localStorage.setItem('kodaauth', account)
+
+  setTimeout(() => {
+    localStorage.setItem(
+      'identity',
+      JSON.stringify({
+        auth: { address: account, balance: {}, tokens: {} },
+        identities: {},
+      })
+    )
+
+    mockAddress.value = true
+  }, 1000)
+})
 </script>
