79e725e297224df84ea3a98b9b92b6d20183a552
Preschian Febryantara <preschian@users.noreply.github.com>
2023-05-17 19:56:48 +0700

    fix: translation, map ksm token id, unused import, selected chain bold

--- components/balance/MultipleBalances.vue
+++ components/balance/MultipleBalances.vue
@@ -2,14 +2,18 @@
   <div>
     <div class="balance">
       <div class="balance-row has-text-grey is-size-7">
-        <div>Chain</div>
-        <div class="has-text-right">Balance</div>
-        <div class="has-text-right">Token</div>
-        <div class="has-text-right">USD</div>
+        <div>{{ $t('general.chain') }}</div>
+        <div class="has-text-right">{{ $t('general.balance') }}</div>
+        <div class="has-text-right">{{ $t('general.token') }}</div>
+        <div class="has-text-right">{{ $t('general.usd') }}</div>
       </div>
 
       <div v-for="(chain, key) in availableChains" :key="key" class="is-size-7">
-        <div v-for="token in chain" :key="token.token" class="balance-row">
+        <div
+          v-for="token in chain"
+          :key="token.token"
+          class="balance-row"
+          :class="{ 'has-text-weight-bold': token.selected }">
           <div>{{ key }}</div>
           <div class="has-text-right">
             {{ token.balance }}
@@ -40,14 +44,16 @@ import { balanceOf } from '@kodadot1/sub-api'
 
 import format from '@/utils/format/balance'
 import { useFiatStore } from '@/stores/fiat'
+import { calculateExactUsdFromToken } from '@/utils/calculation'
+import { getAssetIdByAccount, getKusamaAssetId } from '@/utils/api/bsx/query'
 
 import type { PalletBalancesAccountData } from '@polkadot/types/lookup'
-import { calculateExactUsdFromToken } from '@/utils/calculation'
 
 interface Token {
   token: string
   balance: string
   usd: number
+  selected: boolean
 }
 
 interface Chain {
@@ -129,6 +135,11 @@ async function getBalance(chainName, token = 'KSM', tokenId = 0) {
     currentBalance = format(balance, chain.tokenDecimals, false)
   }
 
+  let selectedTokenId = String(tokenId)
+  if (chainName === 'Basilisk') {
+    selectedTokenId = await getAssetIdByAccount(api, prefixAddress)
+  }
+
   const balance = delimiter(currentBalance)
   const usd = calculateUsd(balance, token)
 
@@ -136,6 +147,7 @@ async function getBalance(chainName, token = 'KSM', tokenId = 0) {
     token,
     balance,
     usd,
+    selected: selectedTokenId === String(tokenId),
   })
   totalUsd.value.push(usd)
 
@@ -149,7 +161,7 @@ onMounted(async () => {
     getBalance('Kusama'),
     getBalance('Statemine'),
     getBalance('Basilisk', 'BSX'),
-    getBalance('Basilisk', 'KSM', 1),
+    getBalance('Basilisk', 'KSM', Number(getKusamaAssetId('bsx'))),
   ]).then(() => {
     status.value = 'done'
   })
@@ -157,8 +169,6 @@ onMounted(async () => {
 </script>
 
 <style scoped lang="scss">
-@import '@/styles/abstracts/variables';
-
 .balance {
   &-row {
     display: flex;
