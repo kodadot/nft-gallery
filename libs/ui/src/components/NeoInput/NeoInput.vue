270e2e12ead82b61850d2238de90f99973e81a75
Prachi Darshana <prachirai00@gmail.com>
2023-05-20 16:33:38 -0700

    input fixes

--- libs/ui/src/components/NeoInput/NeoInput.vue
+++ libs/ui/src/components/NeoInput/NeoInput.vue
@@ -1,56 +1,14 @@
-<template>
-  <o-input
-    v-model="inputValue"
-    :placeholder="placeholder"
-    class="neo-input"
-    v-bind="$attrs"
-    :style="heightStyle"
-    :type="type"
-    :maxlength="maxlength"
-    :min="min"
-    :max="max"
-    :step="step"
-    :validation-message="validationMessage"
-    :expanded="expanded"
-    :use-html5-validation="useHtml5Validation"
-    :disabled="disabled"
-    @input="updateValue" />
-</template>
-
-<script lang="ts" setup>
+<script lang="ts">
 import { OInput } from '@oruga-ui/oruga'
 
-const props = defineProps<{
-  value: string | number
-  height?: number | string
-  type?: string
-  placeholder?: string
-  maxlength?: number
-  min?: number
-  max?: number
-  step?: number
-  expanded?: boolean
-  disabled?: boolean
-  useHtml5Validation?: boolean
-  customClass?: string
-  validationMessage?: string
-}>()
-const emit = defineEmits(['input'])
-
-const inputValue = useVModel(props, 'value')
-const updateValue = (event) => {
-  emit('input', event)
+export default {
+  mixins: [OInput],
+  computed: {
+    rootClasses() {
+      return ['neo-input']
+    },
+  },
 }
-
-const heightStyle = computed(() => {
-  if (props.height) {
-    return {
-      '--neo-input-height':
-        typeof props.height === 'number' ? `${props.height}px` : props.height,
-    }
-  }
-  return {}
-})
 </script>
 
 <style lang="scss">
