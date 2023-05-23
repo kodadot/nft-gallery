cac3cf74449d6495fc65095028c154de5edb0c2c
roiLeo <medina.leo42@gmail.com>
2023-05-23 14:48:53 +0200

    ➖ remove vuex-persist dep

--- plugins/vuex-persist.ts
+++
@@ -1,9 +0,0 @@
-import VuexPersistence from 'vuex-persist'
-
-export default ({ store }): void => {
-  new VuexPersistence({
-    key: 'setting',
-    storage: window.localStorage,
-    modules: ['identity'],
-  }).plugin(store)
-}
