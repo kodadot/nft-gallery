35549f7fa0c9869d685377d1e3c11f40441a069a
Viki Val <viktorko99@gmail.com>
2023-05-24 17:59:44 +0200

    :wrench: stt config

--- utils/config/types.ts
+++ utils/config/types.ts
@@ -1,4 +1,4 @@
 import type { Prefix as ChainPrefix } from '@kodadot1/static'
 
-export type Prefix = ChainPrefix
+export type Prefix = ChainPrefix | 'stt'
 export type Config<T = boolean> = Record<Prefix, T>
