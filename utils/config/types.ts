560d13e0d48f05a19975656d2316206515d258e2
Viki Val <viktorko99@gmail.com>
2023-05-24 17:41:08 +0200

    :wrench: one prefix rules them all

--- utils/config/types.ts
+++ utils/config/types.ts
@@ -1,11 +1,4 @@
-export type Prefix =
-  | 'rmrk'
-  | 'bsx'
-  | 'stmn'
-  | 'westmint'
-  | 'movr'
-  | 'snek'
-  | 'glmr'
-  | 'ksm'
-  | 'dot'
+import type { Prefix as ChainPrefix } from '@kodadot1/static'
+
+export type Prefix = ChainPrefix
 export type Config<T = boolean> = Record<Prefix, T>
