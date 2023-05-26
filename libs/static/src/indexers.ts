1448b61987122f611a112415b2a76b8c0e453857
Viki Val <viktorko99@gmail.com>
2023-05-26 15:20:42 +0200

    :zap: latest stick

--- libs/static/src/indexers.ts
+++ libs/static/src/indexers.ts
@@ -11,7 +11,7 @@ export const INDEXERS: Config<SquidEndpoint> = {
   movr: 'https://squid.subsquid.io/antick/v/001-rc0/graphql',
   ksm: 'https://squid.subsquid.io/marck/v/v2/graphql',
   snek: 'https://squid.subsquid.io/snekk/v/004/graphql',
-  stmn: 'https://squid.subsquid.io/stick/v/v1/graphql',
+  stmn: 'https://squid.subsquid.io/stick/v/v3/graphql',
   dot: 'https://squid.subsquid.io/rubick/graphql', // TODO: change to dot indexer when available
 }
 
