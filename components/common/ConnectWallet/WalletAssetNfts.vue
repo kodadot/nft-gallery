b8ed14e4aff5297cfae706c534d98f581ab059c8
Preschian Febryantara <preschian@users.noreply.github.com>
2023-05-28 20:38:20 +0700

    fix: pass mimetype

--- components/common/ConnectWallet/WalletAssetNfts.vue
+++ components/common/ConnectWallet/WalletAssetNfts.vue
@@ -4,9 +4,10 @@
     <p class="has-text-grey is-size-7 mb-2">Recent NFTs</p>
     <div class="nfts is-flex is-justify-content-space-between">
       <MediaItem
-        v-for="nft in nfts"
+        v-for="nft in nfts.slice(0, 3)"
         :key="nft.meta.id"
-        :src="sanitizeIpfsUrl(nft.meta.image)" />
+        :src="sanitizeIpfsUrl(nft.meta.image)"
+        :mime-type="nft.type" />
     </div>
   </div>
 </template>
@@ -14,16 +15,20 @@
 <script setup lang="ts">
 import { MediaItem } from '@kodadot1/brick'
 import { sanitizeIpfsUrl } from '@/utils/ipfs'
+import { getNftMetadata } from '@/composables/useNft'
 
 import type { NFTWithMetadata } from '@/composables/useNft'
+import { getMimeType } from '~~/utils/gallery/media'
 
 const { accountId } = useAuth()
+const { client, urlPrefix } = usePrefix()
 
+const queryPrefix = client.value === 'ksm' ? 'chain-ksm' : client.value
 const { data } = useGraphql({
-  queryPrefix: 'subsquid',
+  queryPrefix,
   queryName: 'nftListWithSearch',
   variables: {
-    first: 3,
+    first: 10,
     search: [
       {
         currentOwner_eq: accountId.value,
@@ -36,7 +41,18 @@ const nfts = ref<NFTWithMetadata[]>([])
 
 watchEffect(() => {
   if (data.value?.nFTEntities) {
-    nfts.value = data.value.nFTEntities
+    const nftEntities = data.value.nFTEntities
+
+    nftEntities.forEach(async (nft) => {
+      const nftMetadata = await getNftMetadata(nft, urlPrefix.value)
+
+      if (nftMetadata.meta.image) {
+        const mimeType = await getMimeType(
+          sanitizeIpfsUrl(nftMetadata.meta.image)
+        )
+        nfts.value.push({ ...nftMetadata, type: mimeType })
+      }
+    })
   }
 })
 </script>
