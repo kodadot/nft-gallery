d65469333eeab09462a898a49adfb0cfc2fd1697
roiLeo <medina.leo42@gmail.com>
2023-05-30 09:34:53 +0200

    ✨ update NeoTable design

--- libs/ui/src/components/NeoTable/NeoTable.vue
+++ libs/ui/src/components/NeoTable/NeoTable.vue
@@ -12,5 +12,13 @@ export default {
 @import '@oruga-ui/oruga/src/scss/utilities/_variables.scss';
 @import '@oruga-ui/oruga/src/scss/utilities/_animations.scss';
 @import '@oruga-ui/oruga/src/scss/utilities/_helpers.scss';
+
+$table-border: 0;
+$table-border-radius: 0;
+$table-th-border: 0;
+$table-td-border: 0;
+$table-th-font-weight: 400;
+$table-th-color: #999999;
+
 @import '@oruga-ui/oruga/src/scss/components/_table.scss';
 </style>
