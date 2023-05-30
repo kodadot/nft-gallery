4e6e248fed8454dfc5c86e496a3bca7a25db9c34
daiagi <daiagi@gmail.com>
2023-05-30 18:36:34 +0700

    ⚡ statemine mass mint

--- composables/transaction/transactionMintToken.ts
+++ composables/transaction/transactionMintToken.ts
@@ -1,6 +1,7 @@
 import { execMintRmrk } from './mintToken/transactionMintRmrk'
 import { execMintBasilisk } from './mintToken/transactionMintBasilisk'
 import { MintTokenParams } from './types'
+import { execMintStatemine } from './mintToken/transactionMintStatemine'
 
 export function execMintToken({
   item,
@@ -15,6 +16,15 @@ export function execMintToken({
 
   if (item.urlPrefix === 'snek' || item.urlPrefix === 'bsx') {
     return execMintBasilisk({
+      item,
+      api,
+      executeTransaction,
+      isLoading,
+      status,
+    })
+  }
+  if (item.urlPrefix === 'stmn' || item.urlPrefix === 'stt') {
+    return execMintStatemine({
       item,
       api,
       executeTransaction,
