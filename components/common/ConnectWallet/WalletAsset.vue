35f3d8edfb6743a4c2497e9bb85530f7d6df69d9
Preschian Febryantara <preschian@users.noreply.github.com>
2023-06-06 19:04:43 +0700

    feat: remove redesign params

--- components/common/ConnectWallet/WalletAsset.vue
+++ components/common/ConnectWallet/WalletAsset.vue
@@ -1,21 +1,10 @@
 <template>
   <div class="is-flex is-flex-direction-column wallet-asset">
-    <WalletAssetSetIdentity v-if="!display && redesign" />
+    <WalletAssetSetIdentity v-if="!display" />
 
     <div class="is-flex is-flex-direction-column wallet-asset-container mt-4">
-      <WalletAssetIdentity v-if="redesign" />
-      <div v-else>
-        <div>
-          {{ walletName }}
-        </div>
-        <Identity
-          class="identity-address is-size-6"
-          :shortened-address="shortenedAddress"
-          :address="account"
-          show-clipboard />
-      </div>
-
-      <WalletAssetNfts v-if="redesign" />
+      <WalletAssetIdentity />
+      <WalletAssetNfts />
 
       <hr class="my-4" />
 
@@ -23,7 +12,7 @@
         <ProfileAssetsList v-if="isSnek" @totalValueChange="setTotalValue" />
         <MultipleBalances v-else />
 
-        <WalletAssetPortfolio v-if="redesign" />
+        <WalletAssetPortfolio />
       </div>
 
       <div v-if="isSnek">
@@ -37,29 +26,12 @@
       </div>
     </div>
 
-    <WalletAssetMenu v-if="redesign" />
-    <div
-      v-else
-      class="buttons is-justify-content-space-between is-flex-wrap-nowrap my-2 wallet-asset-container">
-      <NeoButton
-        class="button is-size-7 is-capitalized is-flex-grow-1"
-        :label="$i18n.t('general.change_account')"
-        variant="connect-dropdown"
-        @click.native="backToWallet" />
-      <NeoButton
-        class="button is-size-7 is-capitalized is-flex-grow-1"
-        :label="$i18n.t('profileMenu.disconnect')"
-        variant="connect-dropdown"
-        @click.native="disconnect()" />
-    </div>
+    <WalletAssetMenu />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { NeoButton } from '@kodadot1/brick'
-import { useWalletStore } from '@/stores/wallet'
 import { useIdentityStore } from '@/stores/identity'
-import { clearSession } from '@/utils/cachingStrategy'
 import useIdentity from '@/components/identity/utils/useIdentity'
 import WalletAssetIdentity from './WalletAssetIdentity.vue'
 import WalletAssetNfts from './WalletAssetNfts.vue'
@@ -67,9 +39,6 @@ import WalletAssetPortfolio from './WalletAssetPortfolio.vue'
 import WalletAssetMenu from './WalletAssetMenu.vue'
 import WalletAssetSetIdentity from './WalletAssetSetIdentity.vue'
 
-const Identity = defineAsyncComponent(
-  () => import('@/components/identity/module/IdentityLink.vue')
-)
 const MultipleBalances = defineAsyncComponent(
   () => import('@/components/balance/MultipleBalances.vue')
 )
@@ -77,33 +46,18 @@ const ProfileAssetsList = defineAsyncComponent(
   () => import('@/components/rmrk/Profile/ProfileAssetsList.vue')
 )
 
-const { redesign } = useExperiments()
-
 const totalValue = ref(0)
-const walletStore = useWalletStore()
 const identityStore = useIdentityStore()
 const { urlPrefix } = usePrefix()
 const { $i18n } = useNuxtApp()
 const { $consola } = useNuxtApp()
 
-const emit = defineEmits(['back'])
-
 const account = computed(() => identityStore.getAuthAddress)
-const walletName = computed(() => walletStore.getWalletName)
 const isSnek = computed(() => urlPrefix.value === 'snek')
 
-const { shortenedAddress, display } = useIdentity({
+const { display } = useIdentity({
   address: account.value,
 })
-
-const disconnect = () => {
-  identityStore.resetAuth()
-  clearSession()
-}
-
-const backToWallet = () => {
-  emit('back')
-}
 
 const setTotalValue = (value: number) => {
   totalValue.value = value
--- components/navbar/ProfileDropdown.vue
+++ components/navbar/ProfileDropdown.vue
@@ -1,181 +1,41 @@
 <template>
   <div class="is-flex is-align-items-center">
     <a
-      v-if="account && redesign"
+      v-if="account"
       class="navbar-item"
       role="button"
       @click="toggleWalletConnectModal">
       <Avatar :value="account" class="navbar__avatar-icon" :size="27" />
     </a>
-    <b-dropdown
-      v-else
-      position="is-bottom-left"
-      aria-role="menu"
-      :triggers="['hover']">
-      <template #trigger>
-        <a class="navbar-item" role="button">
-          <Avatar
-            v-if="account"
-            :value="account"
-            class="navbar__avatar-icon"
-            :size="27" />
-          <img v-else :src="profileIcon" />
-        </a>
-      </template>
-
-      <template v-if="account">
-        <b-dropdown-item has-link aria-role="menuitem">
-          <nuxt-link :to="`/${urlPrefix}/u/${account}`">
-            {{ $t('profile.page') }}
-          </nuxt-link>
-        </b-dropdown-item>
-        <b-dropdown-item has-link aria-role="menuitem">
-          <nuxt-link to="/settings">{{ $t('settings') }}</nuxt-link>
-        </b-dropdown-item>
-        <template v-if="chain === 'rmrk' || chain === 'ksm'">
-          <b-dropdown-item has-link aria-role="menuitem">
-            <a @click="showRampSDK">
-              {{ $t('credit') }}
-            </a>
-          </b-dropdown-item>
-          <b-dropdown-item has-link aria-role="menuitem">
-            <nuxt-link :to="{ name: 'identity' }">
-              {{ $t('identity.page') }}
-            </nuxt-link>
-          </b-dropdown-item>
+    <div v-else class="is-flex is-align-items-center">
+      <b-dropdown
+        position="is-bottom-left"
+        aria-role="menu"
+        :triggers="['hover']">
+        <template #trigger>
+          <a class="navbar-item" role="button">
+            <img :src="profileIcon" />
+          </a>
         </template>
-        <b-dropdown-item v-if="isSnekOrBsx" has-link aria-role="menuitem">
-          <nuxt-link :to="`/${urlPrefix}/incomingoffers`"
-            >{{ $t('incomingOffers') }}
-          </nuxt-link>
-        </b-dropdown-item>
-        <b-dropdown-item v-if="isSnekOrBsx" has-link aria-role="menuitem">
-          <nuxt-link :to="`/${urlPrefix}/assets`">{{ $t('assets') }}</nuxt-link>
+
+        <b-dropdown-item has-link aria-role="menuitem">
+          <a class="is-flex is-align-items-center" @click="toggleLanguageMenu">
+            {{ $t('profileMenu.language') }}&nbsp;<NeoIcon icon="globe" />
+          </a>
         </b-dropdown-item>
         <b-dropdown-item has-link aria-role="menuitem">
-          <nuxt-link :to="`/${urlPrefix}/transfer`">{{
-            $t('transfer')
-          }}</nuxt-link>
+          <ColorModeButton />
         </b-dropdown-item>
-        <b-dropdown-item has-link aria-role="menuitem">
-          <nuxt-link to="/teleport-bridge"
-            >{{ $t('navbar.teleportBridge') }}
-          </nuxt-link>
-        </b-dropdown-item>
-      </template>
+      </b-dropdown>
 
-      <b-dropdown-item has-link aria-role="menuitem">
-        <a class="is-flex is-align-items-center" @click="toggleLanguageMenu"
-          >{{ $t('profileMenu.language') }}
-          <svg
-            width="14"
-            height="13"
-            class="ml-2"
-            viewBox="0 0 14 13"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M1 6.5C1 9.8138 3.6862 12.5 7 12.5C10.3138 12.5 13 9.8138 13 6.5C13 3.1862 10.3138 0.5 7 0.5C3.6862 0.5 1 3.1862 1 6.5Z"
-              fill="white"
-              stroke="black"
-              stroke-width="0.9"
-              stroke-linecap="round"
-              stroke-linejoin="round" />
-            <path
-              d="M7.59993 0.530273C7.59993 0.530273 9.39993 2.90027 9.39993 6.50027C9.39993 10.1003 7.59993 12.4703 7.59993 12.4703M6.39993 12.4703C6.39993 12.4703 4.59993 10.1003 4.59993 6.50027C4.59993 2.90027 6.39993 0.530273 6.39993 0.530273M1.37793 8.60027H12.6219M1.37793 4.40027H12.6219"
-              stroke="black"
-              stroke-width="0.9"
-              stroke-linecap="round"
-              stroke-linejoin="round" />
-          </svg>
-        </a>
-      </b-dropdown-item>
-      <b-dropdown-item has-link aria-role="menuitem">
-        <ColorModeButton />
-      </b-dropdown-item>
-    </b-dropdown>
-
-    <a
-      v-if="account && !redesign"
-      class="navbar-item"
-      role="button"
-      @click="toggleWalletConnectModal">
-      <svg
-        width="28"
-        height="27"
-        viewBox="0 0 28 23"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg">
-        <rect
-          x="0.75"
-          y="1.25"
-          width="23.5"
-          height="20.5"
-          fill="none"
-          :stroke="isDarkMode ? 'white' : 'black'"
-          stroke-width="1.5" />
-        <g filter="url(#filter0_d_2684_755)">
-          <rect
-            x="15"
-            y="7.5"
-            width="12"
-            height="9"
-            :fill="isDarkMode ? 'black' : 'white'" />
-          <rect
-            x="15.75"
-            y="8.25"
-            width="10.5"
-            height="7.5"
-            :stroke="isDarkMode ? 'white' : 'black'"
-            stroke-width="1.5" />
-        </g>
-        <rect
-          x="19"
-          y="10.5"
-          width="3"
-          height="3"
-          rx="1.5"
-          :fill="isDarkMode ? 'white' : 'black'" />
-        <defs>
-          <filter
-            id="filter0_d_2684_755"
-            x="15"
-            y="7.5"
-            width="13"
-            height="10"
-            filterUnits="userSpaceOnUse"
-            color-interpolation-filters="sRGB">
-            <feFlood flood-opacity="0" result="BackgroundImageFix" />
-            <feColorMatrix
-              in="SourceAlpha"
-              type="matrix"
-              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-              result="hardAlpha" />
-            <feOffset dx="1" dy="1" />
-            <feComposite in2="hardAlpha" operator="out" />
-            <feColorMatrix
-              type="matrix"
-              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0" />
-            <feBlend
-              mode="normal"
-              in2="BackgroundImageFix"
-              result="effect1_dropShadow_2684_755" />
-            <feBlend
-              mode="normal"
-              in="SourceGraphic"
-              in2="effect1_dropShadow_2684_755"
-              result="shape" />
-          </filter>
-        </defs>
-      </svg>
-    </a>
-    <div v-if="!account">
-      <ConnectWalletButton
-        class="button-connect-wallet px-4"
-        variant="k-accent"
-        no-shadow
-        :modal-toggle-disabled="true"
-        @toggleConnectModal="toggleWalletConnectModal" />
+      <div v-if="!account">
+        <ConnectWalletButton
+          class="button-connect-wallet px-4"
+          variant="k-accent"
+          no-shadow
+          :modal-toggle-disabled="true"
+          @toggleConnectModal="toggleWalletConnectModal" />
+      </div>
     </div>
 
     <b-dropdown
@@ -229,7 +89,7 @@
 </template>
 
 <script lang="ts">
-import { NeoButton } from '@kodadot1/brick'
+import { NeoButton, NeoIcon } from '@kodadot1/brick'
 import { RampInstantSDK } from '@ramp-network/ramp-instant-sdk'
 import { Component, Prop, Ref, mixins } from 'nuxt-property-decorator'
 
@@ -242,11 +102,11 @@ import { langsFlags as langsFlagsList } from '@/utils/config/i18n'
 import AuthMixin from '@/utils/mixins/authMixin'
 import PrefixMixin from '@/utils/mixins/prefixMixin'
 import useApiMixin from '@/utils/mixins/useApiMixin'
-import ExperimentMixin from '@/utils/mixins/experimentMixin'
 
 const components = {
   Avatar,
   NeoButton,
+  NeoIcon,
   ConnectWalletButton: () =>
     import('@/components/shared/ConnectWalletButton.vue'),
   Identity: () => import('@/components/identity/IdentityIndex.vue'),
@@ -262,8 +122,7 @@ const components = {
 export default class ProfileDropdown extends mixins(
   PrefixMixin,
   AuthMixin,
-  useApiMixin,
-  ExperimentMixin
+  useApiMixin
 ) {
   @Prop() public value!: any
   @Prop() public showIncomingOffers!: boolean
