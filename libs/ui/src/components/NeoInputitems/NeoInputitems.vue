38c90e8d0fa2340689975014e31c59e26bc36e65
roiLeo <medina.leo42@gmail.com>
2023-06-15 17:46:30 +0200

    🔧 NeoInputitems (#6183)
    * 🔧 wip: NeoTaginput
    
    * 🔧 wip: NeoTaginput design
    
    * 🔧 dark mode
    
    * 🔧 InputeItem design autocomplete
    
    * 🔧 inputitem darkmode

---
+++ libs/ui/src/components/NeoInputitems/NeoInputitems.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { OInputitems } from '@oruga-ui/oruga'
+
+export default {
+  mixins: [OInputitems],
+}
+</script>
+
+<style lang="scss">
+@import './NeoInputitems.scss';
+</style>
