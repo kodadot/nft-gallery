8b2964ac865aafcd90667be174ac3698b2405c30
daiagi <daiagi@gmail.com>
2023-06-16 13:45:37 +0700

    cleanup

--- composables/transaction/mintCollection/transactionMintCollectionStatemine.ts
+++ composables/transaction/mintCollection/transactionMintCollectionStatemine.ts
@@ -13,7 +13,6 @@ export async function execMintCollectionStatemine(
   const { accountId } = useAuth()
 
   const cb = api.tx.utility.batchAll
-  debugger
 
   const { newCollectionId } = useNewCollectionId()
 
--- composables/transaction/mintCollection/useNewCollectionId.ts
+++ composables/transaction/mintCollection/useNewCollectionId.ts
@@ -1,7 +1,5 @@
 export function useNewCollectionId() {
-  console.log('useNewCollectionId')
   const { apiInstance } = useApi()
-
   const newCollectionId = ref<number>()
 
   const getCollectionId = async () => {
@@ -13,23 +11,6 @@ export function useNewCollectionId() {
   }
 
   getCollectionId()
-
-  // watch(data, () => {
-  //   console.log('data', data)
-  //   if (data.value) {
-  //     const collectionEntities = data.value.collectionEntities
-  //     const collectionList = unwrapSafe(collectionEntities)
-  //     const existingIds = collectionList.map((v) => v.id)
-  //     // get max id
-  //     const newId =
-  //       existingIds.length > 0
-  //         ? Math.max(...existingIds) + 1
-  //         : INITIAL_COLLECTION_ID
-
-  //     console.log('newId', newId)
-  //     newCollectionId.value = newId
-  //   }
-  // })
 
   return {
     newCollectionId,
