e085ca7098572c52f259a79626d837263f05511d
Jarsen <695552027@qq.com>
2023-06-21 01:03:33 +0800

    fix: drop page counter styling

--- components/collection/unlockable/CountdownTimer.vue
+++ components/collection/unlockable/CountdownTimer.vue
@@ -1,14 +1,15 @@
 <template>
-  <div ref="wrapper" class="px-4 py-4 keen-slider">
+  <div ref="wrapper" class="px-4 py-4 keen-slider background-inverse">
     <div
-      v-for="(section, index) in [1, 2, 3, 4]"
+      v-for="(section, index) in Array(10).fill(0)"
       :key="index"
       class="keen-slider__slide is-flex no-wrap">
       <div class="no-wrap">
-        <span class="mr-4 has-text-grey">Mint Phase Ends In</span
-        ><span
-          >{{ hours }} Hours : {{ minutes }} Minutes :
-          {{ seconds }} Seconds</span
+        <span class="mr-4 has-text-grey-inverse">Mint Phase Ends In</span
+        ><span class="has-text-inverse"
+          >{{ getPositiveNumber(hours) }} Hours :
+          {{ getPositiveNumber(minutes) }} Minutes :
+          {{ getPositiveNumber(seconds) }} Seconds</span
         >
       </div>
     </div>
@@ -20,7 +21,8 @@ import 'keen-slider/keen-slider.min.css'
 import { useKeenSlider } from 'keen-slider/vue.es'
 import { countDownTime } from './const'
 import { useCountDown } from './utils/useCountDown'
-const animation = { duration: 50000, easing: (t) => t }
+const animation = { duration: 30000, easing: (t) => t }
+const getPositiveNumber = (number: number) => (number >= 0 ? number : 0)
 
 const { hours, minutes, seconds } = useCountDown(countDownTime)
 
