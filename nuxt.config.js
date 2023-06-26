b74a3c5c2c1beef11171befd9f4832e2b28963fa
Jarsen <695552027@qq.com>
2023-06-26 22:54:11 +0800

    fix: remove unused import

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,6 @@ import * as fs from 'fs'
 import { defineNuxtConfig } from '@nuxt/bridge'
 import SentryWebpackPlugin from '@sentry/webpack-plugin'
 import { manifestIcons } from './utils/config/pwa'
-// import { isProduction } from './utils/chain'
 import { URLS, apolloClientConfig } from './utils/constants'
 
 const baseUrl = process.env.BASE_URL || 'http://localhost:9090'
