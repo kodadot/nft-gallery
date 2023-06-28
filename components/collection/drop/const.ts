9030238763f371e922160a0a64c24fa49a8d4e6b
Viki Val <viktorko99@gmail.com>
2023-06-28 12:27:50 +0200

    :wrench: updated consts

--- components/collection/drop/const.ts
+++ components/collection/drop/const.ts
@@ -3,7 +3,10 @@ import { endOfWeek } from 'date-fns'
 const now = new Date()
 export const countDownTime = endOfWeek(now).getTime()
 
-export const collectionId = '6'
-export const pricePerMint = '100000000' // '9000000000'
-export const DROP_CAMPAIGN = 'copenhagen'
-export const MINT_ADDRESS = 'Gn84LKb5HSxc3SACayxCzKQcWESRMcT1VUCqeZURfGj6ASi'
+export const STMN_COLLECTION_ID = '6'
+export const STT_COLLECTION_ID = '8'
+export const pricePerMint = '9000000000' // '100000000'
+export const displayPricePerMint = 1e10
+export const STMN_DROP_CAMPAIGN = 'copenhagen'
+export const STT_DROP_CAMPAIGN = 'copenhagen2'
+export const MINT_ADDRESS = '15CoYMEnJhhWHvdEPXDuTBnZKXwrJzMQdcMwcHGsVx5kXYvW'
