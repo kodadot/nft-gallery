91ccba65db758ff6968df3a79fedf11a57254e09
Viki Val <viktorko99@gmail.com>
2023-07-05 14:03:38 +0200

    6390 shanghai waifu (#6393)
    * :wrench: initial settings
    
    * :art: banner

--- components/collection/unlockable/utils.ts
+++ components/collection/unlockable/utils.ts
@@ -2,15 +2,15 @@ import { createMetadata, unSanitizeIpfsUrl } from '@kodadot1/minimark/utils'
 import { preheatFileFromIPFS } from '@/utils/ipfs'
 import { pinJson } from '@/services/nftStorage'
 
-export const UNLOCKABLE_CAMPAIGN = 'cph'
-export const UNLOCKABLE_NAME = 'Copenhagen Waifu'
+export const UNLOCKABLE_CAMPAIGN = 'sha'
+export const UNLOCKABLE_NAME = 'Shanghai Waifu'
 export const unlockableDesc = (value: number) => `
   This anime waifu is a demonstration of unlockables at [KodaDot](https://kodadot.xyz)
 
   Owner of this item has **access to $${value} voucher**, which can be claimed in **[KodaShop](https://shop.kodadot.xyz)**.
   KodaMerch is then shipped right to your door step.
 
-  It's **limited supply** and **only first 150 can claim** this voucher.
+  It's **limited supply** and **only first 100 can claim** this voucher.
 
   Enjoy and be quick 😉
 `
