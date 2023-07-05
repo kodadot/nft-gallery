209836a52bb10c165e8cbb3f02f6d1e99c615fc3
Prachi Darshana <prachirai00@gmail.com>
2023-07-05 15:35:56 -0700

    common css

--- components/explore/ExploreGrid.vue
+++ components/explore/ExploreGrid.vue
@@ -5,7 +5,6 @@
       :active="gridSize === 'large'"
       @click.native="changeGridLayout(smallGridLayout, 'large')" />
     <NeoButton
-      class="middle-icon"
       icon-left="grid"
       :active="gridSize === 'medium'"
       @click.native="changeGridLayout(smallGridLayout, 'medium')" />
@@ -34,10 +33,3 @@ const changeGridLayout = (layout: string, grid: string) => {
   preferencesStore.setGridSize(grid)
 }
 </script>
-
-<style lang="scss" scoped>
-.middle-icon {
-  border-left: 0 !important;
-  border-right: 0 !important;
-}
-</style>
