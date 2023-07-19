59d67f94f6811c914cebead24ce871c47aea1981
roiLeo <medina.leo42@gmail.com>
2023-07-19 09:21:54 +0200

    ✨ upgrade libs ui

--- libs/ui/histoire.config.ts
+++ libs/ui/histoire.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'histoire'
-import { HstVue } from '@histoire/plugin-vue2'
-import vue from '@vitejs/plugin-vue2'
+import { HstVue } from '@histoire/plugin-vue'
+import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
