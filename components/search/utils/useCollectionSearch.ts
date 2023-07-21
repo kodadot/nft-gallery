1f572480f5ed1f1bbc129bfb680320fc271ec23d
Jarsen <695552027@qq.com>
2023-07-22 00:43:25 +0800

    fix: collectionId in path of url

--- components/search/utils/useCollectionSearch.ts
+++ components/search/utils/useCollectionSearch.ts
@@ -1,16 +1,22 @@
-import { readParam } from '@/composables/useReplaceUrl'
+import { useSearchStore } from '@/stores/search'
 
 export const useCollectionSearch = () => {
   const query = useRoute().query
   const route = useRoute()
 
-  const collectionId = readParam(query?.collectionId)
+  const isMobile = computed(() => useWindowSize().width.value < 1024)
 
+  const searchStore = useSearchStore()
   const isCollectionSearchMode = computed(() => {
-    return route.name === 'prefix-collection-id' && collectionId
+    return (
+      route.name === 'prefix-collection-id' &&
+      searchStore.isCollectionSearchMode &&
+      !isMobile.value
+    )
   })
 
   return {
     isCollectionSearchMode,
+    setCollectionSearchMode: searchStore.setCollectionSearchMode,
   }
 }
