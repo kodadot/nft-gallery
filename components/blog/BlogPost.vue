b46e2e6e4ef68d155cc6c136c07eae42b9a851eb
Preschian Febryantara <preschian@users.noreply.github.com>
2023-07-24 16:14:40 +0700

    feat: KodaDot blog

---
+++ components/blog/BlogPost.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="article">
+    <div
+      class="is-flex is-justify-content-space-between has-text-grey is-size-5">
+      <div>{{ attributes.tags }}</div>
+      <div v-if="attributes.date">
+        {{ format(new Date(attributes.date), 'dd.MM.yyyy') }}
+      </div>
+    </div>
+    <h1>{{ attributes.title }}</h1>
+    <h2 v-if="attributes.subtitle">{{ attributes.subtitle }}</h2>
+    <img
+      v-if="attributes.image"
+      :src="attributes.image"
+      :alt="attributes.title"
+      width="100%" />
+    <component :is="singlePostComponent" />
+  </div>
+</template>
+
+<script setup>
+import { format } from 'date-fns'
+import { convertMarkdownToText } from '@/utils/markdown'
+
+const { $seoMeta } = useNuxtApp()
+const route = useRoute()
+
+const slug = route.params.slug
+const attributes = ref({})
+const singlePostComponent = ref('')
+
+onMounted(async () => {
+  const post = await import(`~/content/blog/${slug}.md`)
+
+  attributes.value = post.attributes
+  singlePostComponent.value = post.vue.component
+})
+
+const title = computed(() => attributes.value.title)
+const meta = computed(() => {
+  return [
+    ...$seoMeta({
+      title: attributes.value.title,
+      description: convertMarkdownToText(attributes.value.subtitle),
+      url: route.path,
+      image: attributes.value.image,
+    }),
+  ]
+})
+
+useNuxt2Meta({
+  title,
+  meta,
+})
+</script>
+
+<style lang="scss">
+.article {
+  margin: 0 auto;
+  max-width: 40rem;
+
+  h1 {
+    font-size: 3rem;
+    font-weight: 700;
+    margin: 1rem 0;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    margin-bottom: 1rem;
+  }
+
+  img {
+    margin-bottom: 1rem;
+  }
+
+  p {
+    margin-bottom: 1rem;
+  }
+}
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="content">
+    <div class="content-headline">
+      <h1>KodaDot Blog</h1>
+      <p>Let’s explore the NFT universe</p>
+    </div>
+
+    <div
+      v-for="post in posts"
+      :key="post.attributes.title"
+      class="content-list">
+      <img :src="post.attributes.image" :alt="post.attributes.title" />
+
+      <div class="content-list-card">
+        <div>
+          <div class="has-text-grey">{{ post.attributes.tags }}</div>
+          <div class="content-list-card-title">{{ post.attributes.title }}</div>
+          <div>{{ post.attributes.subtitle }}</div>
+        </div>
+
+        <div>
+          <NeoButton
+            no-shadow
+            rounded
+            tag="a"
+            size="small"
+            :href="getPermalink(post)"
+            icon="arrow-right-long">
+            View Article
+          </NeoButton>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { NeoButton } from '@kodadot1/brick'
+
+export default {
+  components: {
+    NeoButton,
+  },
+  async asyncData() {
+    const resolve = require.context('~/content/blog/', true, /\.md$/)
+    const imports = resolve.keys().map((key) => resolve(key))
+
+    return {
+      posts: imports,
+    }
+  },
+  methods: {
+    getPermalink(post) {
+      const filePath = post.meta.resourcePath
+      const fileName = filePath.match(/\/([^/]+)\.\w+$/)[1]
+
+      return fileName
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.content {
+  margin: 0 auto;
+  max-width: 60rem;
+
+  &-headline {
+    text-align: center;
+
+    h1 {
+      font-size: 3rem;
+      margin-bottom: 0.5rem;
+      color: #fff !important;
+      text-shadow: 1px 1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
+        -1px -1px 0 #000, 1px 0 0 #000, 0 1px 0 #000, -1px 0 0 #000,
+        0 -1px 0 #000, 4px 4px #000;
+    }
+
+    p {
+      font-size: 1.5rem;
+      font-weight: 500;
+      margin-bottom: 5rem;
+    }
+  }
+
+  &-list {
+    border-radius: 2.5rem;
+    border: 1px solid #000;
+    background: #fff;
+    box-shadow: 4px 4px 0px 0px #000;
+    display: flex;
+
+    img {
+      width: 40rem;
+    }
+
+    &-card {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      padding: 1.5rem;
+
+      &-title {
+        font-size: 1.25rem;
+        font-weight: 700;
+        margin-bottom: 1rem;
+      }
+    }
+  }
+}
+</style>
