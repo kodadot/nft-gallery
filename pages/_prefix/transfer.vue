544e142589747c680432a91c0264aacd3aa87954
Jarsen <695552027@qq.com>
2023-07-26 00:30:13 +0800

    refactor: redesign feature gating

--- pages/_prefix/transfer.vue
+++ pages/_prefix/transfer.vue
@@ -1,15 +1,27 @@
 <template>
-  <Transfer />
+  <Transfer v-if="isRedesign" />
+  <TransferOld v-else />
 </template>
 
 <script lang="ts">
 export default {
   name: 'TransferPage',
+
   components: {
     Transfer: () => import('@/components/transfer/Transfer.vue'),
+    TransferOld: () => import('@/components/transfer/TransferOld.vue'),
   },
   layout() {
     return 'centered-half-layout'
+  },
+  data() {
+    return {
+      isRedesign: false,
+    }
+  },
+  created() {
+    const { redesign } = useExperiments()
+    this.isRedesign = redesign.value
   },
 }
 </script>
