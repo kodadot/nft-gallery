c2dab0b1fba37b63f7dd07824acfa8261282b27f
daiagi <daiagi@gmail.com>
2023-07-31 13:20:06 +0700

    add shopping cart and buy now to ItemsGridImage

--- components/items/ItemsGrid/ItemsGridImage.vue
+++ components/items/ItemsGrid/ItemsGridImage.vue
@@ -8,23 +8,115 @@
     :variant="variant"
     :unloackable-icon="unlockableIcon"
     link="nuxt-link"
-    bind-key="to" />
+    bind-key="to">
+    <template #hover-action>
+      <div
+        v-if="!isOwner && Number(nft?.price)"
+        class="w-half is-flex btn-height">
+        <NeoButton
+          :label="buyLabel"
+          data-cy="item-buy"
+          no-shadow
+          class="is-flex-grow-1"
+          @click.native.prevent="onClickBuy" />
+        <NeoButton
+          data-cy="item-add-to-cart"
+          no-shadow
+          class="fixed-width p-1 no-border-left hover-color"
+          @click.native.prevent="onClickShoppingCart">
+          <img :src="cartIcon" class="image" />
+        </NeoButton>
+      </div>
+    </template>
+  </NeoNftCard>
 </template>
 
 <script setup lang="ts">
-import { NeoNftCard } from '@kodadot1/brick'
-
+import { NeoButton, NeoNftCard } from '@kodadot1/brick'
 import type { NftCardVariant } from '@kodadot1/brick'
 import type { NFTWithMetadata } from '@/composables/useNft'
+import { useShoppingCartStore } from '@/stores/shoppingCart'
+import { usePreferencesStore } from '@/stores/preferences'
+import { nftToShoppingCardItem } from '@/components/common/shoppingCart/utils'
+import { openShoppingCart } from '@/components/common/shoppingCart/ShoppingCartModalConfig'
+import { isOwner as checkOwner } from '@/utils/account'
 
 const { urlPrefix } = usePrefix()
 const { placeholder } = useTheme()
+const { accountId } = useAuth()
 const { unlockableIcon } = useUnlockableIcon()
+const shoppingCartStore = useShoppingCartStore()
+const preferencesStore = usePreferencesStore()
+const { $i18n } = useNuxtApp()
+const instance = getCurrentInstance()
 
 const props = defineProps<{
   nft: NFTWithMetadata
   variant?: NftCardVariant
 }>()
 
+const buyLabel = computed(() => {
+  if (shoppingCartStore.isItemInCart(props.nft.id)) {
+    return $i18n.t('shoppingCart.gotToCart')
+  }
+  return $i18n.t(
+    preferencesStore.getReplaceBuyNowWithYolo ? 'YOLO' : 'shoppingCart.buyNow'
+  )
+})
+
+const { cartIcon } = useShoppingCartIcon(props.nft.id)
+
 const { nft } = useNft(props.nft)
+
+const isOwner = computed(() =>
+  checkOwner(props.nft?.currentOwner, accountId.value)
+)
+
+const onClickBuy = () => {
+  if (shoppingCartStore.isItemInCart(props.nft.id)) {
+    openShoppingCart(instance)
+  } else {
+    shoppingCartStore.setItemToBuy(nftToShoppingCardItem(props.nft))
+    preferencesStore.setCompletePurchaseModal({
+      isOpen: true,
+      mode: 'buy-now',
+    })
+  }
+}
+
+const onClickShoppingCart = () => {
+  shoppingCartStore.setItem(nftToShoppingCardItem(props.nft))
+}
 </script>
+
+<style lang="scss" scoped>
+@import '@/styles/abstracts/variables';
+
+.w-half {
+  width: 50%;
+}
+
+.fixed-width {
+  min-width: 35px;
+}
+
+.btn-height {
+  height: 35px;
+}
+
+.no-border-left {
+  border-left: none !important;
+}
+
+.hover-color {
+  &:hover {
+    @include ktheme() {
+      background-color: theme('background-color-inverse');
+    }
+    // can this be used in the composable to remove assets?
+    img {
+      filter: invert(1);
+    }
+  }
+}
+</style>
