ef9ecef7525ec8b3224697c8a12c1b0961a920b3
Preschian Febryantara <preschian@users.noreply.github.com>
2023-08-11 14:32:15 +0700

    chore: comment sentry

--- utils/notification.ts
+++ utils/notification.ts
@@ -1,4 +1,5 @@
 import { NotificationProgrammatic as Notification } from 'buefy'
+import { captureMessage } from '@sentry/browser'
 import consola from 'consola'
 
 export type NotificationType = { type: string; actionText: string }
@@ -32,6 +33,7 @@ export const showNotification = (
 ): void => {
   if (params === notificationTypes.danger) {
     consola.error('[Notification Error]', message)
+    captureMessage(String(message))
     return
   }
   Notification.open({
