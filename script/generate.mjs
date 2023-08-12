549c847f6629d860ac732e0f7ebca4ebe44c54c3
Preschian Febryantara <preschian@users.noreply.github.com>
2023-08-13 04:22:24 +0700

    ci: always exit(1) on stderr

--- script/generate.mjs
+++ script/generate.mjs
@@ -10,14 +10,13 @@ generate.stdout.on('data', (data) => {
     process.exit(1)
   }
 })
+
 generate.stderr.on('data', (data) => {
   console.error(`stderr: ${data}`)
-
-  if (data.includes('failed to parse')) {
-    console.log('exit(1)')
-    process.exit(1)
-  }
+  console.log('exit(1)')
+  process.exit(1)
 })
+
 generate.on('close', (code) => {
   console.log(`child process exited with code ${code}`)
 })
