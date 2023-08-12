4057b52d8fb15048ace2f3679d440dcadc462554
John Philip <developerphilo@gmail.com>
2023-08-12 11:01:50 +0300

    fix: create card review changes

--- components/drops/CreateDrop.vue
+++ components/drops/CreateDrop.vue
@@ -1,249 +1,12 @@
 <template>
   <div class="drop-card border card-border-color">
     <div class="svg-icons">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="82"
-        height="88"
-        viewBox="0 0 82 88"
-        fill="none"
-        class="svg-one">
-        <rect
-          x="35.3119"
-          y="0.698843"
-          width="53.3519"
-          height="69.2252"
-          transform="rotate(30 35.3119 0.698843)"
-          fill="#F6F5F5"
-          stroke="black"
-          stroke-width="0.440925" />
-        <rect
-          x="35.3926"
-          y="1"
-          width="52.9109"
-          height="52.9109"
-          transform="rotate(30 35.3926 1)"
-          fill="#F4A6BE" />
-        <rect
-          opacity="0.54"
-          x="10.4487"
-          y="51.2589"
-          width="18.5188"
-          height="4.85017"
-          transform="rotate(30 10.4487 51.2589)"
-          fill="#F4A6BE" />
-        <rect
-          opacity="0.54"
-          x="7.14185"
-          y="56.9866"
-          width="18.5188"
-          height="2.20462"
-          transform="rotate(30 7.14185 56.9866)"
-          fill="#F4A6BE" />
-        <g style="mix-blend-mode: overlay">
-          <rect
-            x="37.2817"
-            y="18.8922"
-            width="14.1096"
-            height="14.1096"
-            rx="7.05479"
-            transform="rotate(30 37.2817 18.8922)"
-            fill="black" />
-        </g>
-        <g style="mix-blend-mode: overlay">
-          <rect
-            x="40.9663"
-            y="26.62"
-            width="24.2509"
-            height="24.2509"
-            rx="12.1254"
-            transform="rotate(30 40.9663 26.62)"
-            fill="black" />
-        </g>
-      </svg>
-      <!--  2nd svg -->
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="58"
-        height="62"
-        viewBox="0 0 58 62"
-        fill="none"
-        class="svg-two">
-        <rect
-          x="0.788071"
-          y="19.5604"
-          width="37.5446"
-          height="48.7149"
-          transform="rotate(-30 0.788071 19.5604)"
-          fill="#F6F5F5"
-          stroke="black"
-          stroke-width="0.310286" />
-        <rect
-          x="1"
-          y="19.6172"
-          width="37.2343"
-          height="37.2343"
-          transform="rotate(-30 1 19.6172)"
-          fill="#F4C2A6" />
-        <rect
-          opacity="0.54"
-          x="22.8528"
-          y="52.5029"
-          width="13.032"
-          height="3.41315"
-          transform="rotate(-30 22.8528 52.5029)"
-          fill="#F4C2A6" />
-        <rect
-          opacity="0.54"
-          x="25.1799"
-          y="56.5336"
-          width="13.032"
-          height="1.55143"
-          transform="rotate(-30 25.1799 56.5336)"
-          fill="#F4C2A6" />
-        <g style="mix-blend-mode: overlay">
-          <rect
-            x="12.5688"
-            y="24.7614"
-            width="9.92915"
-            height="9.92915"
-            rx="4.96457"
-            transform="rotate(-30 12.5688 24.7614)"
-            fill="black" />
-        </g>
-        <g style="mix-blend-mode: overlay">
-          <rect
-            x="18.575"
-            y="25.2349"
-            width="17.0657"
-            height="17.0657"
-            rx="8.53286"
-            transform="rotate(-30 18.575 25.2349)"
-            fill="black" />
-        </g>
-      </svg>
-      <!--   3rd svg -->
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="53"
-        height="62"
-        viewBox="0 0 53 62"
-        fill="none"
-        class="svg-third">
-        <rect
-          x="0.798029"
-          y="11.127"
-          width="39.9079"
-          height="51.7813"
-          transform="rotate(-15 0.798029 11.127)"
-          fill="#F6F5F5"
-          stroke="black"
-          stroke-width="0.329817" />
-        <rect
-          x="1"
-          y="11.2437"
-          width="39.5781"
-          height="39.5781"
-          transform="rotate(-15 1 11.2437)"
-          fill="#F4A6E7" />
-        <rect
-          opacity="0.54"
-          x="14.3896"
-          y="51.0203"
-          width="13.8523"
-          height="3.62799"
-          transform="rotate(-15 14.3896 51.0203)"
-          fill="#F4A6E7" />
-        <rect
-          opacity="0.54"
-          x="15.6702"
-          y="55.799"
-          width="13.8523"
-          height="1.64909"
-          transform="rotate(-15 15.6702 55.799)"
-          fill="#F4A6E7" />
-        <g style="mix-blend-mode: overlay">
-          <rect
-            x="11.4629"
-            y="19.708"
-            width="10.5541"
-            height="10.5541"
-            rx="5.27707"
-            transform="rotate(-15 11.4629 19.708)"
-            fill="black" />
-        </g>
-        <g style="mix-blend-mode: overlay">
-          <rect
-            x="17.499"
-            y="21.8466"
-            width="18.1399"
-            height="18.1399"
-            rx="9.06997"
-            transform="rotate(-15 17.499 21.8466)"
-            fill="black" />
-        </g>
-      </svg>
-      <!-- 4th svg -->
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="66"
-        height="76"
-        viewBox="0 0 66 76"
-        fill="none"
-        class="svg-four">
-        <rect
-          x="17.2878"
-          y="0.750779"
-          width="49.2441"
-          height="63.8953"
-          transform="rotate(15 17.2878 0.750779)"
-          fill="#F6F5F5"
-          stroke="black"
-          stroke-width="0.406976" />
-        <rect
-          x="17.4316"
-          y="1"
-          width="48.8372"
-          height="48.8372"
-          transform="rotate(15 17.4316 1)"
-          fill="#A6B7F4" />
-        <rect
-          opacity="0.54"
-          x="7.19922"
-          y="51.7675"
-          width="17.093"
-          height="4.47674"
-          transform="rotate(15 7.19922 51.7675)"
-          fill="#A6B7F4" />
-        <rect
-          opacity="0.54"
-          x="5.61914"
-          y="57.6641"
-          width="17.093"
-          height="2.03488"
-          transform="rotate(15 5.61914 57.6641)"
-          fill="#A6B7F4" />
-        <g style="mix-blend-mode: overlay">
-          <rect
-            x="23.3904"
-            y="16.5006"
-            width="13.0232"
-            height="13.0232"
-            rx="6.51162"
-            transform="rotate(15 23.3904 16.5006)"
-            fill="black" />
-        </g>
-        <g style="mix-blend-mode: overlay">
-          <rect
-            x="28.5212"
-            y="22.5101"
-            width="22.3837"
-            height="22.3837"
-            rx="11.1919"
-            transform="rotate(15 28.5212 22.5101)"
-            fill="black" />
-        </g>
-      </svg>
+      <img :src="createdDrop1" class="svg-one" />
+      <img :src="createdDrop2" class="svg-two" />
+      <img :src="Ellipse" class="ellipse" />
+      <img :src="createdDrop3" class="svg-three" />
+      <img :src="Vector" class="vector" />
+      <img :src="createdDrop4" class="svg-four" />
     </div>
     <div class="inner_card">
       <p>Create Your Own</p>
@@ -259,8 +22,13 @@
   </div>
 </template>
 <script setup lang="ts">
+import createdDrop1 from '@/assets/drops/create_drop1.svg'
+import createdDrop2 from '@/assets/drops/create_drop2.svg'
+import createdDrop3 from '@/assets/drops/create_drop3.svg'
+import createdDrop4 from '@/assets/drops/create_drop4.svg'
+import Ellipse from '@/assets/drops/Ellipse_36.svg'
+import Vector from '@/assets/drops/Vector_24.svg'
 import { NeoButton } from '@kodadot1/brick'
-// const link = ref('https://form.kodadot.xyz/drop-page-interest')
 </script>
 <style scoped lang="scss">
 @import '@/styles/abstracts/variables';
@@ -269,16 +37,13 @@ import { NeoButton } from '@kodadot1/brick'
   height: 141px;
   display: flex;
   align-items: center;
-  justify-content: space-around;
+  column-gap: 2.5rem;
   border: 1px solid #cecbcb;
   cursor: pointer;
   a:hover {
     color: unset;
   }
   &:hover {
-    .drop-card-banner {
-      opacity: 0.85;
-    }
     @include ktheme() {
       border-color: theme('border-color');
     }
@@ -288,6 +53,9 @@ import { NeoButton } from '@kodadot1/brick'
     background-size: cover;
     background-repeat: no-repeat;
   }
+}
+.light-mode .drop-card {
+  background-color: white;
 }
 .svg-icons {
   width: 154px;
@@ -303,9 +71,9 @@ import { NeoButton } from '@kodadot1/brick'
   position: absolute;
   top: 68.6171875px;
   left: 32px;
-  z-index: 20;
+  z-index: 10;
 }
-.svg-third {
+.svg-three {
   position: absolute;
   top: 20.24px;
   left: 45px;
@@ -315,7 +83,18 @@ import { NeoButton } from '@kodadot1/brick'
   top: 19px;
   left: 98.431640625px;
 }
+.ellipse {
+  position: absolute;
+}
+.vector {
+  position: absolute;
+  top: 20px;
+  left: 50.43px;
+}
 .neo-button:hover {
-  background-color: #fff0f8;
+  background-color: rgba(255, 240, 248, 1);
+}
+.dark-mode .neo-button:hover {
+  color: rgb(45, 45, 45);
 }
 </style>
