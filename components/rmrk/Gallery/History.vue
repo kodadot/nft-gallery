f24af0e2072df7eb5b4c2cc60e62bd85477c9c3e
roiLeo <medina.leo42@gmail.com>
2023-08-14 11:58:43 +0200

    🔧 History Table composition api

--- components/rmrk/Gallery/History.vue
+++ components/rmrk/Gallery/History.vue
@@ -88,17 +88,11 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Watch, mixins } from 'nuxt-property-decorator'
-import { Debounce } from 'vue-debounce-decorator'
+<script lang="ts" setup>
 import { Interaction } from '@kodadot1/minimark/v1'
 import { formatDistanceToNow } from 'date-fns'
 
 import { exist } from '@/utils/exist'
-
-import ChainMixin from '@/utils/mixins/chainMixin'
-import KeyboardEventsMixin from '@/utils/mixins/keyboardEventsMixin'
-import PrefixMixin from '@/utils/mixins/prefixMixin'
 import { usePreferencesStore } from '@/stores/preferences'
 
 import {
@@ -108,287 +102,245 @@ import {
   parseDate,
   wrapEventNameWithIcon,
 } from '@/utils/historyEvent'
-import shortAddress from '@/utils/shortAddress'
 
 import { Interaction as EventInteraction } from '../service/scheme'
 import {
-  NeoIcon,
   NeoSelect,
   NeoTable,
   NeoTableColumn,
   NeoTooltip,
 } from '@kodadot1/brick'
 
-const components = {
-  Identity: () => import('@/components/identity/IdentityIndex.vue'),
-  Pagination: () => import('@/components/rmrk/Gallery/Pagination.vue'),
-  BlockExplorerLink: () => import('@/components/shared/BlockExplorerLink.vue'),
-  CommonTokenMoney: () => import('@/components/shared/CommonTokenMoney.vue'),
-  NeoIcon,
-  NeoTable,
-  NeoTableColumn,
-  NeoTooltip,
-  NeoSelect,
-}
-
-type TableRowItem = {
-  id: string
-  name: string
-}
-
-type TableRow = {
-  Type: HistoryEventType
-  Item?: TableRowItem // only in collection
-  From: string
-  To: string
-  Amount: string
-  Date: string
-  Block: string
-}
+import Identity from '@/components/identity/IdentityIndex.vue'
+import Pagination from '@/components/rmrk/Gallery/Pagination.vue'
+import BlockExplorerLink from '@/components/shared/BlockExplorerLink.vue'
+import CommonTokenMoney from '@/components/shared/CommonTokenMoney.vue'
 
 type ChartData = {
   buy: any[]
   list: any[]
 }
-
-@Component({ components })
-export default class History extends mixins(
-  PrefixMixin,
-  ChainMixin,
-  KeyboardEventsMixin
-) {
-  @Prop({ type: Array }) public events!: EventInteraction[]
-  @Prop({ type: Boolean, default: true })
-  private readonly openOnDefault!: boolean
-  @Prop({ type: Boolean, default: false }) hideCollapse!: boolean
-  @Prop({ type: Boolean, default: false }) displayItem!: boolean
-
-  private currentPage = parseInt(this.$route.query?.page as string) || 1
-  private event: HistoryEventType = HistoryEventType.BUY
-
-  protected data: TableRow[] = []
-  protected copyTableData: TableRow[] = []
-  public isOpen = false
-  public shortAddress = shortAddress
-  private preferencesStore = usePreferencesStore()
-
-  public async created() {
-    this.initKeyboardEventHandler({
-      e: this.bindExpandEvents,
-    })
+const prop = withDefaults(
+  defineProps<{
+    events?: EventInteraction[]
+    openOnDefault?: boolean
+    hideCollapse?: boolean
+    displayItem?: boolean
+  }>(),
+  {
+    events: () => [],
+    openOnDefault: true,
+    hideCollapse: false,
+    displayItem: false,
   }
+)
+const emit = defineEmits(['setPriceChartData'])
 
-  public mounted() {
-    exist(this.$route.query.event, (val) => {
-      this.event = (val as HistoryEventType) ?? HistoryEventType.ALL
-    })
-    this.isOpen = this.openOnDefault
-  }
+const { $i18n, $consola, $route, $router } = useNuxtApp()
+const { decimals } = useChain()
+const { urlPrefix } = usePrefix()
 
-  private bindExpandEvents(event) {
-    if (event.key === 'h') {
-      this.isOpen = !this.isOpen
+const currentPage = ref(parseInt($route.query?.page) || 1)
+const event = ref<HistoryEventType>(HistoryEventType.BUY)
+const data = ref([])
+const copyTableData = ref([])
+const isOpen = ref(false)
+const preferencesStore = usePreferencesStore()
+
+onMounted(() => {
+  exist($route.query.event, (val) => {
+    event.value = (val as HistoryEventType) ?? HistoryEventType.ALL
+  })
+  isOpen.value = prop.openOnDefault
+})
+
+const total = computed(() => data.value.length)
+const itemsPerPage = computed(() => preferencesStore.getHistoryItemsPerPage)
+const showList = computed(() => {
+  const endIndex = currentPage.value * itemsPerPage.value
+  return data.value.slice(endIndex - itemsPerPage.value, endIndex)
+})
+const uniqType = computed(() => {
+  const eventSet = new Set(copyTableData.value.map((v) => v.Type))
+  const singleEventList = Array.from(eventSet).map((type) => ({
+    type,
+    value: getEventDisplayName(type as Interaction),
+  }))
+  return [{ type: HistoryEventType.ALL, value: 'All' }, ...singleEventList]
+})
+const isToColumnVisible = computed(() => {
+  return [HistoryEventType.ALL, Interaction.BUY, Interaction.SEND].includes(
+    event.value
+  )
+})
+const isPercentageColumnVisible = computed(() => {
+  return [HistoryEventType.ALL, Interaction.BUY].includes(event.value)
+})
+const selectedEvent = computed({
+  get: () => event.value,
+  set: (value: HistoryEventType) => {
+    if (value) {
+      currentPage.value = 1
+      event.value = value
+      replaceUrl(value)
     }
-  }
+  },
+})
 
-  get total(): number {
-    return this.data.length
+const getEventDisplayName = (type: Interaction) => {
+  return wrapEventNameWithIcon(type, $i18n.t(`nft.event.${type}`) as string)
+}
+const percentageTextClassName = (percentage: number) => {
+  if (percentage > 0) {
+    return 'has-text-success'
+  } else if (percentage < 0) {
+    return 'has-text-danger'
   }
-
-  get itemsPerPage(): number {
-    return this.preferencesStore.getHistoryItemsPerPage
-  }
-
-  get showList(): TableRow[] {
-    const endIndex = this.currentPage * this.itemsPerPage
-    return this.data.slice(endIndex - this.itemsPerPage, endIndex)
-  }
-
-  getEventDisplayName(type: Interaction) {
-    return wrapEventNameWithIcon(type, this.$t(`nft.event.${type}`) as string)
-  }
-
-  get uniqType(): { type: HistoryEventType; value: string }[] {
-    const eventSet = new Set(this.copyTableData.map((v) => v.Type))
-    const singleEventList = Array.from(eventSet).map((type) => ({
-      type,
-      value: this.getEventDisplayName(type as Interaction),
-    }))
-    return [{ type: HistoryEventType.ALL, value: 'All' }, ...singleEventList]
-  }
-
-  get isToColumnVisible() {
-    return [HistoryEventType.ALL, Interaction.BUY, Interaction.SEND].includes(
-      this.event
-    )
-  }
-
-  get isPercentageColumnVisible() {
-    return [HistoryEventType.ALL, Interaction.BUY].includes(this.event)
-  }
-
-  get selectedEvent(): HistoryEventType {
-    return this.event
-  }
-
-  set selectedEvent(event: HistoryEventType) {
-    if (event) {
-      this.currentPage = 1
-      this.event = event
-      this.replaceUrl(event)
-    }
-  }
-
-  protected updateDataByEvent() {
-    const event = this.event
-    this.data =
-      event === HistoryEventType.ALL
-        ? this.copyTableData
-        : [...new Set(this.copyTableData.filter((v) => v.Type === event))]
-  }
-
-  private percentageTextClassName(percentage: number) {
-    if (percentage > 0) {
-      return 'has-text-success'
-    } else if (percentage < 0) {
-      return 'has-text-danger'
-    }
-    return ''
-  }
-
-  @Debounce(100)
-  replaceUrl(value: string, key = 'event') {
-    this.$router
+  return ''
+}
+const replaceUrl = (value: string, key = 'event') => {
+  if ($route.query[key] !== value) {
+    $router
       .replace({
-        path: String(this.$route.path),
-        query: { ...this.$route.query, [key]: value },
+        path: String($route.path),
+        query: { ...$route.query, [key]: value },
       })
-      .catch(this.$consola.warn /*Navigation Duplicate err fix later */)
-  }
-
-  protected createTable(): void {
-    this.data = []
-    this.copyTableData = []
-
-    const chartData: ChartData = {
-      buy: [],
-      list: [],
-    }
-    const previousPriceMap = {}
-
-    for (const newEvent of this.events) {
-      const event: any = {}
-
-      const nftId = newEvent['nft'] ? newEvent['nft']['id'] : 'id'
-      // Type
-      switch (newEvent['interaction']) {
-        case Interaction.MINTNFT:
-          event['From'] = newEvent['caller']
-          event['To'] = ''
-          break
-        case Interaction.LIST:
-        case Interaction.UNLIST:
-          event['Type'] = parseInt(newEvent['meta'])
-            ? Interaction.LIST
-            : Interaction.UNLIST
-          event['From'] = newEvent['caller']
-          event['To'] = ''
-          event['Amount'] = newEvent['meta']
-          break
-        case Interaction.SEND:
-          event['From'] = newEvent['caller']
-          event['To'] = newEvent['meta']
-          break
-        case Interaction.CONSUME:
-          event['From'] = newEvent['caller']
-          event['To'] = ''
-          break
-        case Interaction.BUY:
-          event['From'] = newEvent['currentOwner']
-          event['To'] = newEvent['caller']
-          event['Amount'] = newEvent['meta']
-          if (previousPriceMap[nftId]) {
-            event['Percentage'] =
-              ((parseInt(newEvent['meta']) - previousPriceMap[nftId]) /
-                previousPriceMap[nftId]) *
-              100
-          } else {
-            event['Percentage'] = 100
-          }
-          previousPriceMap[nftId] = parseInt(newEvent['meta'])
-          break
-        case InteractionBsxOnly.ROYALTY:
-          event['From'] = newEvent['caller']
-          event['To'] = ''
-          event['Percentage'] = parseInt(newEvent['meta'])
-          break
-        case InteractionBsxOnly.PAY_ROYALTY:
-          event['From'] = newEvent['caller']
-          event['To'] = ''
-          event['Amount'] = newEvent['meta']
-          break
-        default:
-          // unsupported event
-          continue
-      }
-
-      event['Type'] = event['Type'] ?? newEvent['interaction']
-
-      // Item
-      if (this.displayItem) {
-        event['Item'] = newEvent['nft']
-      }
-
-      // Amount
-      event['Amount'] = event['Amount'] ?? '-'
-
-      // Date
-      const date = new Date(newEvent['timestamp'])
-      event['Date'] = parseDate(date)
-
-      // Time
-      event['Time'] = formatDistanceToNow(date, { addSuffix: true })
-
-      event['Block'] = String(newEvent['blockNumber'])
-
-      // ID for table: Use a unique key of your data Object for each row.
-      event['ID'] = newEvent['timestamp'] + newEvent['id']
-
-      // Push to chart data
-      if (newEvent['interaction'] === Interaction.LIST) {
-        this.pushChartData(chartData.list, date, event['Amount'])
-      } else if (newEvent['interaction'] === Interaction.BUY) {
-        this.pushChartData(chartData.buy, date, event['Amount'])
-      }
-
-      this.copyTableData.push(event)
-    }
-    this.copyTableData = this.copyTableData.reverse()
-    this.updateDataByEvent()
-
-    if (!this.data.length) {
-      this.event = HistoryEventType.ALL
-    }
-
-    this.$emit('setPriceChartData', [chartData.buy, chartData.list])
-  }
-
-  private pushChartData(array, date, amount) {
-    array.push([date, parseChartAmount(amount, this.decimals)])
-  }
-
-  @Watch('events', { immediate: true })
-  public watchEvent(): void {
-    if (this.events) {
-      this.createTable()
-    }
-  }
-
-  @Watch('event', { immediate: true })
-  public watchInteractionEvent(): void {
-    if (this.event) {
-      this.updateDataByEvent()
-    }
+      .catch($consola.warn /*Navigation Duplicate err fix later */)
   }
 }
+const updateDataByEvent = () => {
+  console.log('Im in')
+  console.log(copyTableData.value)
+  // const event = event.value
+  data.value =
+    event.value === HistoryEventType.ALL
+      ? copyTableData.value
+      : [...new Set(copyTableData.value.filter((v) => v.Type === event))]
+}
+
+const pushChartData = (array, date, amount) => {
+  array.push([date, parseChartAmount(amount, decimals.value)])
+}
+
+const createTable = (): void => {
+  data.value = []
+  copyTableData.value = []
+
+  const chartData: ChartData = {
+    buy: [],
+    list: [],
+  }
+  const previousPriceMap = {}
+
+  console.log(prop.events)
+
+  for (const newEvent of prop.events) {
+    console.log(newEvent)
+    const event: any = {}
+
+    const nftId = newEvent['nft'] ? newEvent['nft']['id'] : 'id'
+    // Type
+    switch (newEvent['interaction']) {
+      case Interaction.MINTNFT:
+        event['From'] = newEvent['caller']
+        event['To'] = ''
+        break
+      case Interaction.LIST:
+      case Interaction.UNLIST:
+        event['Type'] = parseInt(newEvent['meta'])
+          ? Interaction.LIST
+          : Interaction.UNLIST
+        event['From'] = newEvent['caller']
+        event['To'] = ''
+        event['Amount'] = newEvent['meta']
+        break
+      case Interaction.SEND:
+        event['From'] = newEvent['caller']
+        event['To'] = newEvent['meta']
+        break
+      case Interaction.CONSUME:
+        event['From'] = newEvent['caller']
+        event['To'] = ''
+        break
+      case Interaction.BUY:
+        event['From'] = newEvent['currentOwner']
+        event['To'] = newEvent['caller']
+        event['Amount'] = newEvent['meta']
+        if (previousPriceMap[nftId]) {
+          event['Percentage'] =
+            ((parseInt(newEvent['meta']) - previousPriceMap[nftId]) /
+              previousPriceMap[nftId]) *
+            100
+        } else {
+          event['Percentage'] = 100
+        }
+        previousPriceMap[nftId] = parseInt(newEvent['meta'])
+        break
+      case InteractionBsxOnly.ROYALTY:
+        event['From'] = newEvent['caller']
+        event['To'] = ''
+        event['Percentage'] = parseInt(newEvent['meta'])
+        break
+      case InteractionBsxOnly.PAY_ROYALTY:
+        event['From'] = newEvent['caller']
+        event['To'] = ''
+        event['Amount'] = newEvent['meta']
+        break
+      default:
+        // unsupported event
+        continue
+    }
+
+    event['Type'] = event['Type'] ?? newEvent['interaction']
+
+    // Item
+    if (prop.displayItem) {
+      event['Item'] = newEvent['nft']
+    }
+
+    // Amount
+    event['Amount'] = event['Amount'] ?? '-'
+
+    // Date
+    const date = new Date(newEvent['timestamp'])
+    event['Date'] = parseDate(date)
+
+    // Time
+    event['Time'] = formatDistanceToNow(date, { addSuffix: true })
+
+    event['Block'] = String(newEvent['blockNumber'])
+
+    // ID for table: Use a unique key of your data Object for each row.
+    event['ID'] = newEvent['timestamp'] + newEvent['id']
+
+    // Push to chart data
+    if (newEvent['interaction'] === Interaction.LIST) {
+      pushChartData(chartData.list, date, event['Amount'])
+    } else if (newEvent['interaction'] === Interaction.BUY) {
+      pushChartData(chartData.buy, date, event['Amount'])
+    }
+
+    copyTableData.value.push(event)
+  }
+
+  copyTableData.value = copyTableData.value.reverse()
+  updateDataByEvent()
+
+  if (!data.value.length) {
+    event.value = HistoryEventType.ALL
+  }
+
+  emit('setPriceChartData', [chartData.buy, chartData.list])
+}
+
+watch(
+  () => prop.events,
+  () => {
+    createTable()
+  }
+)
+
+watch(event, () => {
+  updateDataByEvent()
+})
 </script>
