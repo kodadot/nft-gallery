333f91b03c5f70680aaab98d1568146ff4bd26a6
Preschian Febryantara <preschian@users.noreply.github.com>
2023-08-14 17:40:55 +0700

    build: move unlockable

--- composables/useUnlockableIcon.ts
+++ composables/useUnlockableIcon.ts
@@ -1,10 +1,7 @@
-import unloackableDark from '@/assets/unlockable-dark.svg'
-import unloackable from '@/assets/unlockable.svg'
-
 export const useUnlockableIcon = () => {
   const { isDarkMode } = useTheme()
   const unlockableIcon = computed(() =>
-    isDarkMode.value ? unloackableDark : unloackable
+    isDarkMode.value ? '/unlockable-dark.svg' : '/unlockable.svg'
   )
 
   return { unlockableIcon }
