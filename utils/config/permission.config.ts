bbd52baee17d7e0d4657ca76b5372620d90eb845
Stephen Jason Wang <stephenjasonwang@gmail.com>
2023-08-18 18:40:45 +0800

    refactor: types to make it less redundant

--- utils/config/permission.config.ts
+++ utils/config/permission.config.ts
@@ -1,79 +1,50 @@
-import { Config, Prefix } from './types'
+import { PartialConfig, Prefix } from './types'
 
-const hasCreate: Config<boolean> = {
-  rmrk: true,
-  ksm: true,
-  bsx: true,
-  ahk: true,
+const hasCreate: PartialConfig<boolean> = {
   movr: false,
   glmr: false,
-  snek: true,
   dot: false,
-  ahp: true,
 }
 
-const hasInsight: Config<boolean> = {
-  rmrk: true,
-  ksm: true,
-  bsx: true,
+const hasInsight: PartialConfig<boolean> = {
   ahk: false,
   movr: false,
   glmr: false,
-  snek: true,
-  dot: true,
   ahp: false,
 }
 
-const hasSales: Config<boolean> = {
-  rmrk: true,
-  ksm: true,
-  bsx: true,
+const hasSales: PartialConfig<boolean> = {
   ahk: false,
   movr: false,
   glmr: false,
-  snek: true,
   dot: false,
   ahp: false,
 }
 
-const hasMassmintCreate: Config<boolean> = {
-  rmrk: true,
-  ksm: true,
-  bsx: true,
-  ahk: true,
+const hasMassmintCreate: PartialConfig<boolean> = {
   movr: false,
   glmr: false,
-  snek: true,
   dot: false,
-  ahp: true,
 }
 
-const hasExplorer: Config<boolean> = {
-  rmrk: true,
-  ksm: true,
-  bsx: true,
-  ahk: true,
-  movr: true,
-  glmr: true,
-  snek: true,
+const hasExplorer: PartialConfig<boolean> = {
   dot: false,
-  ahp: true,
 }
 
 export const createVisible = (prefix: Prefix | string): boolean => {
-  return hasCreate[prefix]
+  return hasCreate[prefix] ?? true
 }
 
 export const seriesInsightVisible = (prefix: Prefix | string) => {
-  return hasInsight[prefix]
+  return hasInsight[prefix] ?? true
 }
 
 export const massmintCreateVisible = (prefix: Prefix | string) => {
-  return hasMassmintCreate[prefix]
+  return hasMassmintCreate[prefix] ?? true
 }
 
 export const salesVisible = (prefix: Prefix | string) => {
-  return hasSales[prefix]
+  return hasSales[prefix] ?? true
 }
 
 export const dropsVisible = (prefix: Prefix | string) => {
