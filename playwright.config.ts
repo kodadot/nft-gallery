191082616e432194e16aa10fe88b68086a947756
prury <yco.arq@gmail.com>
2023-08-23 17:07:34 -0300

    webserver config

--- playwright.config.ts
+++ playwright.config.ts
@@ -69,9 +69,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'pnpm run dev',
+    url: 'http://localhost:9090',
+    reuseExistingServer: !process.env.CI,
+  },
 })
