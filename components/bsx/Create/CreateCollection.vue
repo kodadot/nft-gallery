92aa9128ad072c2b65c8364d302b54f106518780
Leo Anderson <128157824+leo-anderson-x@users.noreply.github.com>
2023-08-24 16:00:05 +0900

    bsx/snek CreateCollection to composition API

--- components/bsx/Create/CreateCollection.vue
+++ components/bsx/Create/CreateCollection.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Loader v-model="isLoading" :status="status" />
+    <Loader v-model="isTransactionLoading" :status="transactionStatus" />
     <BaseCollectionForm ref="collectionForm" v-bind.sync="base">
       <template #main>
         <NeoField class="mb-5" />
@@ -29,7 +29,7 @@
           <SubmitButton
             expanded
             label="create collection"
-            :loading="isLoading"
+            :loading="isTransactionLoading"
             @click="submit" />
         </NeoField>
       </template>
@@ -37,7 +37,7 @@
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { Attribute } from '@/components/rmrk/types'
 import {
   getMetadataDeposit,
@@ -45,15 +45,9 @@ import {
 } from '@/components/unique/apiConstants'
 import { hasEnoughToken } from '@/components/unique/utils'
 import formatBalance from '@/utils/format/balance'
-import AuthMixin from '@/utils/mixins/authMixin'
-import ChainMixin from '@/utils/mixins/chainMixin'
-import MetaTransactionMixin from '@/utils/mixins/metaMixin'
-import PrefixMixin from '@/utils/mixins/prefixMixin'
-import ApiUrlMixin from '@/utils/mixins/apiUrlMixin'
 import { notificationTypes, showNotification } from '@/utils/notification'
 import { estimate } from '@/utils/transactionExecutor'
 import { Interaction } from '@kodadot1/minimark/v1'
-import { Component, Ref, Watch, mixins } from 'nuxt-property-decorator'
 import { ApiFactory, onApiConnect } from '@kodadot1/sub-api'
 import { dummyIpfsCid } from '@/utils/ipfs'
 import { createArgs } from '@/composables/transaction/mintCollection/utils'
@@ -62,143 +56,141 @@ import shouldUpdate from '@/utils/shouldUpdate'
 import { Token, getBalance, getDeposit, getFeesToken } from './utils'
 import { NeoField } from '@kodadot1/brick'
 
-const components = {
-  Loader: () => import('@/components/shared/Loader.vue'),
-  BasicInput: () => import('@/components/shared/form/BasicInput.vue'),
-  BaseCollectionForm: () => import('@/components/base/BaseCollectionForm.vue'),
-  BasicSwitch: () => import('@/components/shared/form/BasicSwitch.vue'),
-  SubmitButton: () => import('@/components/base/SubmitButton.vue'),
-  Money: () => import('@/components/bsx/format/TokenMoney.vue'),
-  AccountBalance: () => import('@/components/shared/AccountBalance.vue'),
-  MultiPaymentFeeButton: () =>
-    import('@/components/bsx/specific/MultiPaymentFeeButton.vue'),
-  NeoField,
+const emit = defineEmits(['created'])
+
+const { $i18n, $consola } = useNuxtApp()
+const { accountId, balance } = useAuth()
+const { apiUrl } = useApi()
+const { decimals, unit } = useChain()
+const { urlPrefix, tokenId } = usePrefix()
+const { status: transactionStatus, isLoading: isTransactionLoading } =
+  useTransactionStatus()
+
+const Loader = () => import('@/components/shared/Loader.vue')
+const BaseCollectionForm = () =>
+  import('@/components/base/BaseCollectionForm.vue')
+const SubmitButton = () => import('@/components/base/SubmitButton.vue')
+const Money = () => import('@/components/bsx/format/TokenMoney.vue')
+const AccountBalance = () => import('@/components/shared/AccountBalance.vue')
+const MultiPaymentFeeButton = () =>
+  import('@/components/bsx/specific/MultiPaymentFeeButton.vue')
+
+const base = ref<BaseCollectionType>({
+  name: '',
+  file: null,
+  description: '',
+})
+const collectionDeposit = ref('')
+const attributes = ref<Attribute[]>([])
+const balanceNotEnough = ref(false)
+const feesToken = ref<Token>('BSX')
+const collectionForm = ref()
+
+onApiConnect(apiUrl.value, (api) => {
+  const classDeposit = getclassDeposit(api)
+  const metadataDeposit = getMetadataDeposit(api)
+  collectionDeposit.value = (classDeposit + metadataDeposit).toString()
+})
+
+function checkValidity() {
+  return collectionForm.value?.checkValidity()
 }
 
-@Component({ components })
-export default class CreateCollection extends mixins(
-  MetaTransactionMixin,
-  ChainMixin,
-  AuthMixin,
-  PrefixMixin,
-  ApiUrlMixin
-) {
-  public base: BaseCollectionType = {
-    name: '',
-    file: null,
-    description: '',
+const balanceNotEnoughMessage = computed(() => {
+  if (balanceNotEnough.value) {
+    return $i18n.t('tooltip.notEnoughBalance')
   }
-  public collectionDeposit = ''
-  protected id = '0'
-  protected attributes: Attribute[] = []
-  protected balanceNotEnough = false
-  public feesToken: Token = 'BSX'
-  @Ref('collectionForm') readonly collectionForm
+  return ''
+})
 
-  public checkValidity() {
-    return this.collectionForm.checkValidity()
+const balanceOfToken = computed(() => getBalance(feesToken.value))
+const depositOfToken = computed(() =>
+  getDeposit(feesToken.value, parseFloat(collectionDeposit.value))
+)
+
+async function tryToEstimateTx(): Promise<string> {
+  const api = await ApiFactory.useApiInstance(apiUrl.value)
+  const cb = api.tx.utility.batchAll
+  const metadata = dummyIpfsCid()
+  const randomId = 0
+  const args = [createArgs(randomId, metadata)]
+  return estimate(accountId.value, cb, args)
+}
+
+async function checkBalanceBeforeTx(): Promise<void> {
+  const estimated = await tryToEstimateTx()
+  const deposit = collectionDeposit.value
+  const hasTokens = hasEnoughToken(balance.value, estimated, deposit)
+  $consola.log('hasTokens', hasTokens)
+  if (!hasTokens) {
+    throw new Error(
+      `Not enough tokens: Currently have ${formatBalance(
+        balance.value,
+        decimals.value,
+        unit.value
+      )} tokens`
+    )
   }
+}
 
-  get balanceNotEnoughMessage() {
-    if (this.balanceNotEnough) {
-      return this.$t('tooltip.notEnoughBalance')
+async function submit(): Promise<void> {
+  // check fields
+  if (!checkValidity()) {
+    return
+  }
+  // check balance
+  if (
+    !!collectionDeposit.value &&
+    balanceOfToken.value < depositOfToken.value
+  ) {
+    balanceNotEnough.value = true
+    return
+  }
+  isTransactionLoading.value = true
+  transactionStatus.value = 'loader.checkBalance'
+
+  const { transaction, status, isLoading, blockNumber } = useTransaction()
+  watch([isLoading, status], () => {
+    isTransactionLoading.value = isLoading.value
+    if (Boolean(status.value)) {
+      transactionStatus.value = status.value
     }
-    return ''
-  }
+  })
+  watch(blockNumber, (block) => {
+    if (block) {
+      emit('created')
+    }
+  })
 
-  @Watch('accountId', { immediate: true })
-  async onAccountIdChange(val: string, oldVal: string) {
+  try {
+    // await this.checkBalanceBeforeTx()
+    showNotification(
+      $i18n.t('mint.creatingCollection', { name: base.value.name }),
+      notificationTypes.info
+    )
+    transactionStatus.value = 'loader.ipfs'
+    transaction({
+      interaction: Interaction.MINT,
+      urlPrefix: usePrefix().urlPrefix.value,
+      collection: {
+        ...base.value,
+        tags: attributes.value,
+      },
+    })
+  } catch (e) {
+    showNotification(`[ERR] ${e}`, notificationTypes.warn)
+    $consola.error(e)
+    isTransactionLoading.value = false
+  }
+}
+
+watch(
+  accountId,
+  async (val, oldVal) => {
     if (shouldUpdate(val, oldVal)) {
-      this.feesToken = await getFeesToken()
+      feesToken.value = await getFeesToken()
     }
-  }
-
-  public async created() {
-    onApiConnect(this.apiUrl, (api) => {
-      const classDeposit = getclassDeposit(api)
-      const metadataDeposit = getMetadataDeposit(api)
-      this.collectionDeposit = (classDeposit + metadataDeposit).toString()
-    })
-  }
-  get balanceOfToken() {
-    return getBalance(this.feesToken)
-  }
-
-  get depositOfToken() {
-    return getDeposit(this.feesToken, parseFloat(this.collectionDeposit))
-  }
-
-  protected async tryToEstimateTx(): Promise<string> {
-    const api = await ApiFactory.useApiInstance(this.apiUrl)
-    const cb = api.tx.utility.batchAll
-    const metadata = dummyIpfsCid()
-    const randomId = 0
-    const args = [createArgs(randomId, metadata)]
-    return estimate(this.accountId, cb, args)
-  }
-
-  protected async checkBalanceBeforeTx(): Promise<void> {
-    const estimated = await this.tryToEstimateTx()
-    const deposit = this.collectionDeposit
-    const hasTokens = hasEnoughToken(this.balance, estimated, deposit)
-    this.$consola.log('hasTokens', hasTokens)
-    if (!hasTokens) {
-      throw new Error(
-        `Not enough tokens: Currently have ${formatBalance(
-          this.balance,
-          this.decimals,
-          this.unit
-        )} tokens`
-      )
-    }
-  }
-
-  public async submit(): Promise<void> {
-    // check fields
-    if (!this.checkValidity()) {
-      return
-    }
-    // check balance
-    if (!!this.collectionDeposit && this.balanceOfToken < this.depositOfToken) {
-      this.balanceNotEnough = true
-      return
-    }
-    this.isLoading = true
-    this.status = 'loader.checkBalance'
-
-    const { transaction, status, isLoading, blockNumber } = useTransaction()
-    watch([isLoading, status], () => {
-      this.isLoading = isLoading.value
-      if (Boolean(status.value)) {
-        this.status = status.value
-      }
-    })
-    watch(blockNumber, (block) => {
-      if (block) {
-        this.$emit('created')
-      }
-    })
-
-    try {
-      // await this.checkBalanceBeforeTx()
-      showNotification(
-        this.$t('mint.creatingCollection', { name: this.base.name }),
-        notificationTypes.info
-      )
-      this.status = 'loader.ipfs'
-      transaction({
-        interaction: Interaction.MINT,
-        urlPrefix: usePrefix().urlPrefix.value,
-        collection: {
-          ...this.base,
-          tags: this.attributes,
-        },
-      })
-    } catch (e: any) {
-      showNotification(`[ERR] ${e}`, notificationTypes.warn)
-      this.$consola.error(e)
-      this.isLoading = false
-    }
-  }
-}
+  },
+  { immediate: true }
+)
 </script>
