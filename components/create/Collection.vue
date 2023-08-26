a8b88ac6f142063a9d1362b03ac42562be81fc53
Preschian Febryantara <preschian@users.noreply.github.com>
2023-08-26 22:24:12 +0700

    refactor: remove unused divs

--- components/create/Collection.vue
+++ components/create/Collection.vue
@@ -1,146 +1,142 @@
 <template>
-  <section>
-    <div class="container">
-      <div class="columns is-centered">
-        <Loader v-model="isLoading" :status="status" />
-        <form class="column is-half" @submit.prevent="createCollection">
-          <h2 class="title is-size-3">
-            {{ $t('mint.collection.create') }}
-          </h2>
+  <div class="columns is-centered">
+    <Loader v-model="isLoading" :status="status" />
+    <form class="column is-half" @submit.prevent="createCollection">
+      <h2 class="title is-size-3">
+        {{ $t('mint.collection.create') }}
+      </h2>
 
-          <!-- select blockchain -->
-          <NeoField :label="`${$t('mint.blockchain.label')} *`">
-            <div>
-              <p>{{ $t('mint.blockchain.message') }}</p>
-              <NeoDropdown v-model="selectBlockchain" class="mt-3" expanded>
-                <template #trigger>
-                  <NeoButton variant="primary" type="button" expanded>
-                    {{ selectBlockchain.text }}
-                    <NeoIcon icon="caret-down"></NeoIcon>
-                  </NeoButton>
-                </template>
+      <!-- select blockchain -->
+      <NeoField :label="`${$t('mint.blockchain.label')} *`">
+        <div>
+          <p>{{ $t('mint.blockchain.message') }}</p>
+          <NeoDropdown v-model="selectBlockchain" class="mt-3" expanded>
+            <template #trigger>
+              <NeoButton variant="primary" type="button" expanded>
+                {{ selectBlockchain.text }}
+                <NeoIcon icon="caret-down"></NeoIcon>
+              </NeoButton>
+            </template>
 
-                <NeoDropdownItem
-                  v-for="(menu, index) in menus"
-                  :key="index"
-                  :value="menu"
-                  aria-role="listitem">
-                  <div class="is-flex is-align-items-center">
-                    <img
-                      :src="menu.icon"
-                      :alt="menu.text"
-                      width="32px"
-                      class="is-inline-block mr-2" />
-                    <div>
-                      <span>{{ menu.text }}</span>
-                    </div>
-                  </div>
-                </NeoDropdownItem>
-              </NeoDropdown>
-            </div>
-          </NeoField>
+            <NeoDropdownItem
+              v-for="(menu, index) in menus"
+              :key="index"
+              :value="menu"
+              aria-role="listitem">
+              <div class="is-flex is-align-items-center">
+                <img
+                  :src="menu.icon"
+                  :alt="menu.text"
+                  width="32px"
+                  class="is-inline-block mr-2" />
+                <div>
+                  <span>{{ menu.text }}</span>
+                </div>
+              </div>
+            </NeoDropdownItem>
+          </NeoDropdown>
+        </div>
+      </NeoField>
 
-          <!-- collection logo -->
-          <NeoField :label="`${$t('mint.collection.logo.image')} *`">
-            <div>
-              <p>{{ $t('mint.collection.logo.message') }}</p>
-              <DropUpload
-                v-model="logo"
-                required
-                expanded
-                preview
-                :label="$t('mint.collection.drop')" />
-            </div>
-          </NeoField>
-
-          <!-- collection name -->
-          <NeoField
-            :label="`${$t('mint.collection.name.label')} *`"
+      <!-- collection logo -->
+      <NeoField :label="`${$t('mint.collection.logo.image')} *`">
+        <div>
+          <p>{{ $t('mint.collection.logo.message') }}</p>
+          <DropUpload
+            v-model="logo"
             required
-            :error="!name">
-            <NeoInput v-model="name" required />
-          </NeoField>
+            expanded
+            preview
+            :label="$t('mint.collection.drop')" />
+        </div>
+      </NeoField>
 
-          <!-- collection description -->
-          <NeoField
-            :label="`${$t('mint.collection.description.label')} (optional)`">
-            <NeoInput
-              v-model="description"
-              type="textarea"
-              has-counter
-              maxlength="1000"
-              height="10rem" />
-          </NeoField>
+      <!-- collection name -->
+      <NeoField
+        :label="`${$t('mint.collection.name.label')} *`"
+        required
+        :error="!name">
+        <NeoInput v-model="name" required />
+      </NeoField>
 
-          <!-- collection max nfts -->
-          <NeoField
-            v-if="!isBasilisk"
-            :label="$t('Maximum NFTs in collection')"
-            required>
-            <div class="w-full">
-              <div class="is-flex is-justify-content-space-between">
-                <p>{{ $t('mint.unlimited') }}</p>
-                <NeoSwitch v-model="unlimited" />
-              </div>
-              <NeoInput
-                v-if="!unlimited"
-                v-model="max"
-                class="mt-3"
-                type="number"
-                placeholder="1 is the minimum"
-                :min="1" />
-            </div>
-          </NeoField>
+      <!-- collection description -->
+      <NeoField
+        :label="`${$t('mint.collection.description.label')} (optional)`">
+        <NeoInput
+          v-model="description"
+          type="textarea"
+          has-counter
+          maxlength="1000"
+          height="10rem" />
+      </NeoField>
 
-          <!-- collection symbol -->
-          <NeoField
-            v-if="isKusama"
-            :label="`${$t('mint.collection.symbol.label')} *`">
-            <div>
-              <p>{{ $t('mint.collection.symbol.message') }}</p>
-              <NeoInput
-                ref="symbolInput"
-                v-model="symbol"
-                :placeholder="$t('mint.collection.symbol.placeholder')"
-                maxlength="10"
-                required
-                expanded />
-            </div>
-          </NeoField>
-
-          <div v-if="isBasilisk || isAssetHub">
-            <hr class="my-6" />
-            <NeoField>
-              <div class="monospace">
-                <p class="has-text-weight-medium is-size-6 has-text-info">
-                  <span>{{ $t('mint.deposit') }}:</span>
-                  <span>{{ collectionDeposit }} {{ balanceSymbol }}</span>
-                </p>
-                <p>
-                  <span>{{ $t('general.balance') }}: </span>
-                  <span>{{ balance }} {{ balanceSymbol }}</span>
-                </p>
-              </div>
-            </NeoField>
+      <!-- collection max nfts -->
+      <NeoField
+        v-if="!isBasilisk"
+        :label="$t('Maximum NFTs in collection')"
+        required>
+        <div class="w-full">
+          <div class="is-flex is-justify-content-space-between">
+            <p>{{ $t('mint.unlimited') }}</p>
+            <NeoSwitch v-model="unlimited" />
           </div>
+          <NeoInput
+            v-if="!unlimited"
+            v-model="max"
+            class="mt-3"
+            type="number"
+            placeholder="1 is the minimum"
+            :min="1" />
+        </div>
+      </NeoField>
 
-          <hr class="my-6" />
+      <!-- collection symbol -->
+      <NeoField
+        v-if="isKusama"
+        :label="`${$t('mint.collection.symbol.label')} *`">
+        <div>
+          <p>{{ $t('mint.collection.symbol.message') }}</p>
+          <NeoInput
+            ref="symbolInput"
+            v-model="symbol"
+            :placeholder="$t('mint.collection.symbol.placeholder')"
+            maxlength="10"
+            required
+            expanded />
+        </div>
+      </NeoField>
 
-          <!-- create collection button -->
-          <NeoField
-            variant="danger"
-            :message="`${canDeposit ? '' : $t('tooltip.notEnoughBalance')}`">
-            <SubmitButton
-              expanded
-              label="create collection"
-              type="submit"
-              :loading="isLoading"
-              :disabled="!canDeposit" />
-          </NeoField>
-        </form>
+      <div v-if="isBasilisk || isAssetHub">
+        <hr class="my-6" />
+        <NeoField>
+          <div class="monospace">
+            <p class="has-text-weight-medium is-size-6 has-text-info">
+              <span>{{ $t('mint.deposit') }}:</span>
+              <span>{{ collectionDeposit }} {{ balanceSymbol }}</span>
+            </p>
+            <p>
+              <span>{{ $t('general.balance') }}: </span>
+              <span>{{ balance }} {{ balanceSymbol }}</span>
+            </p>
+          </div>
+        </NeoField>
       </div>
-    </div>
-  </section>
+
+      <hr class="my-6" />
+
+      <!-- create collection button -->
+      <NeoField
+        variant="danger"
+        :message="`${canDeposit ? '' : $t('tooltip.notEnoughBalance')}`">
+        <SubmitButton
+          expanded
+          label="create collection"
+          type="submit"
+          :loading="isLoading"
+          :disabled="!canDeposit" />
+      </NeoField>
+    </form>
+  </div>
 </template>
 
 <script setup lang="ts">
