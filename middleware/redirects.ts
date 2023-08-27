62850191f5da1e8e7d3c176fd521d2671237de05
Peter Kompasz <kompaszbence@gmail.com>
2023-08-27 11:50:49 +0300

    refactor: middleware/redirects.ts

--- middleware/redirects.ts
+++ middleware/redirects.ts
@@ -1,27 +1,49 @@
 export default function ({ redirect, route }): void {
   const { urlPrefix } = usePrefix()
 
-  if (route.path === '/drops') {
-    return redirect('/ahk/drops')
-  }
+  let redirectValue
 
-  if (route.path.startsWith(`/${urlPrefix.value}`)) {
-    if (route.path.endsWith('collections')) {
-      return redirect(`/${urlPrefix.value}/explore/collectibles`)
+  console.log(route.path)
+
+  const paths = [
+    {
+      cond: (val) => val === '/drops',
+      replaceValue: () => '/ahk/drops',
+    },
+    {
+      replaceValue: () => `/${urlPrefix.value}/explore/collectibles`,
+      cond: (val) =>
+        new RegExp(`^\\/${urlPrefix.value}\\/.*\\/collections$`).test(val),
+    },
+    {
+      cond: (val) =>
+        new RegExp(`^\\/${urlPrefix.value}\\/.*\\/gallery$`).test(val),
+      replaceValue: () => `/${urlPrefix.value}/explore/items`,
+    },
+    {
+      cond: (val) => val.includes('/stmn/'),
+      replaceValue: () => window.location.href.replace('/stmn/', '/ahk/'),
+    },
+    {
+      cond: (val) => val.includes('/rmrk2/'),
+      replaceValue: () => window.location.href.replace('/rmrk2/', '/ksm/'),
+    },
+    {
+      cond: (val) => val.includes('/transfer'),
+      replaceValue: () =>
+        window.location.href.replace('/transfer/', '/ksm/transfer'),
+    },
+  ]
+
+  for (const path of paths) {
+    if (path.cond(route.path)) {
+      redirectValue = path.replaceValue()
+      console.log(route.path, redirectValue)
+      break
     }
-    if (route.path.endsWith('gallery')) {
-      return redirect(`/${urlPrefix.value}/explore/items`)
-    }
   }
 
-  if (route.path.includes('/stmn/')) {
-    return redirect(window.location.href.replace('/stmn/', '/ahk/'))
-  }
-
-  if (route.path.includes('/rmrk2/')) {
-    return redirect(window.location.href.replace('/rmrk2/', '/ksm/'))
-  }
-  if (route.path.startsWith('/transfer')) {
-    return redirect(window.location.href.replace('/transfer', '/ksm/transfer'))
+  if (redirect) {
+    return redirect(redirectValue)
   }
 }
