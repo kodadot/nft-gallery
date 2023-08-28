24c5b8005c67aa1b5c0d56ed8a349db9b763a2e8
Peter Kompasz <kompaszbence@gmail.com>
2023-08-28 11:50:37 +0300

    assign is-close-btn class

--- libs/ui/src/components/NeoButton/NeoButton.vue
+++ libs/ui/src/components/NeoButton/NeoButton.vue
@@ -5,6 +5,7 @@
       'is-fixed-width': fixedWidth,
       'no-shadow': noShadow,
       'loading-with-label': loadingWithLabel,
+      'is-close-btn': icon === 'xmark' || icon === 'close',
     }"
     :size="size"
     :icon-right="icon"
@@ -25,21 +26,21 @@
 import { OButton } from '@oruga-ui/oruga'
 import { NeoButtonVariant } from '@kodadot1/brick'
 
-  defineProps<{
-    size?: 'small' | 'medium' | 'large'
-    disabled?: boolean
-    expanded?: boolean
-    icon?: string
-    iconPack?: string
-    label?: string
-    active?: boolean
-    fixedWidth?: boolean
-    noShadow?: boolean
-    variant?: NeoButtonVariant
-    rounded?: boolean
-    tag?: string
-    loadingWithLabel?: boolean
-  }>()
+defineProps<{
+  size?: 'small' | 'medium' | 'large'
+  disabled?: boolean
+  expanded?: boolean
+  icon?: string
+  iconPack?: string
+  label?: string
+  active?: boolean
+  fixedWidth?: boolean
+  noShadow?: boolean
+  variant?: NeoButtonVariant
+  rounded?: boolean
+  tag?: string
+  loadingWithLabel?: boolean
+}>()
 </script>
 
 <style lang="scss">
