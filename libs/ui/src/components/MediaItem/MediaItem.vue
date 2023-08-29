c2e0e21e2da6ea1b834e31057ae7cf906f1d687f
lungern <xiii.at.cn@gmail.com>
2023-08-29 18:00:05 +0800

    feat: add button to toggle blurred layer over nsfw item

--- libs/ui/src/components/MediaItem/MediaItem.vue
+++ libs/ui/src/components/MediaItem/MediaItem.vue
@@ -15,110 +15,84 @@
       <NeoIcon icon="eye-slash" class="mb-3" />
       <span class="heading-nsfw"> {{ $t('lewd.explicit') }} </span>
       <span class="nsfw-desc">{{ $t('lewd.explicitDesc') }}</span>
-      <span class="nsfw-content" @click="showContent()">{{
-        $t('lewd.showContent')
-      }}</span>
     </div>
+    <button
+      v-if="isLewd"
+      class="nsfw-action px-4 py-1"
+      :class="[isLewdBlurredLayer ? '' : 'hide']"
+      @click="toggleContent">
+      {{ isLewdBlurredLayer ? $t('lewd.showContent') : $t('lewd.hideContent') }}
+    </button>
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { defineAsyncComponent } from 'vue'
-
 import { getMimeType, resolveMedia } from '@/utils/gallery/media'
-import NeoIcon from './../NeoIcon/NeoIcon.vue'
+import { NeoIcon } from '@kodadot1/brick'
 
 const SUFFIX = 'Media'
+const props = withDefaults(
+  defineProps<{
+    src: string
+    animationSrc: string
+    mimeType: string
+    title: string
+    original?: boolean
+    isLewd?: boolean
+    isDetail?: boolean
+    placeholder: string
+  }>(),
+  {
+    src: '',
+    animationSrc: '',
+    mimeType: '',
+    title: 'KodaDot NFT',
+    original: false,
+    isLewd: false,
+    isDetail: false,
+    placeholder: '',
+  }
+)
 
-export default {
-  components: {
-    NeoIcon,
-  },
-  props: {
-    src: {
-      // for mimeType image please use this props
-      type: String,
-      default: '',
-    },
-    animationSrc: {
-      // other than image please use this props instead
-      type: String,
-      default: '',
-    },
-    mimeType: {
-      type: String,
-      default: '',
-    },
-    title: {
-      type: String,
-      default: 'KodaDot NFT',
-    },
-    original: {
-      // original size of the image
-      type: Boolean,
-      default: false,
-    },
-    isLewd: {
-      type: Boolean,
-      default: false,
-    },
-    isDetail: {
-      type: Boolean,
-      default: false,
-    },
-    placeholder: {
-      type: String,
-      default: '',
-    },
-  },
-  data() {
-    return {
-      defaultMimeType: 'image',
-      components: {
-        ImageMedia: defineAsyncComponent(() => import('./type/ImageMedia.vue')),
-        VideoMedia: defineAsyncComponent(() => import('./type/VideoMedia.vue')),
-        AudioMedia: defineAsyncComponent(() => import('./type/AudioMedia.vue')),
-        ModelMedia: defineAsyncComponent(() => import('./type/ModelMedia.vue')),
-        JsonMedia: defineAsyncComponent(() => import('./type/JsonMedia.vue')),
-        IFrameMedia: defineAsyncComponent(
-          () => import('./type/IFrameMedia.vue')
-        ),
-        ObjectMedia: defineAsyncComponent(
-          () => import('./type/ObjectMedia.vue')
-        ),
-        Media: defineAsyncComponent(() => import('./type/UnknownMedia.vue')),
-      },
-      isLewdBlurredLayer: this.isLewd,
-    }
-  },
-  computed: {
-    resolveComponent() {
-      const type = this.mimeType || this.defaultMimeType
-      return this.components[resolveMedia(type) + SUFFIX]
-    },
-    properSrc() {
-      return this.src || this.placeholder
-    },
-  },
-  watch: {
-    animationSrc() {
-      this.updateComponent()
-    },
-  },
-  mounted() {
-    this.updateComponent()
-  },
-  methods: {
-    async updateComponent() {
-      if (this.animationSrc && !this.mimeType) {
-        this.defaultMimeType = await getMimeType(this.animationSrc)
-      }
-    },
-    showContent() {
-      this.isLewdBlurredLayer = false
-    },
-  },
+let defaultMimeType = ref('image')
+let isLewdBlurredLayer = ref<boolean>(props.isLewd)
+const components = {
+  ImageMedia: defineAsyncComponent(() => import('./type/ImageMedia.vue')),
+  VideoMedia: defineAsyncComponent(() => import('./type/VideoMedia.vue')),
+  AudioMedia: defineAsyncComponent(() => import('./type/AudioMedia.vue')),
+  ModelMedia: defineAsyncComponent(() => import('./type/ModelMedia.vue')),
+  JsonMedia: defineAsyncComponent(() => import('./type/JsonMedia.vue')),
+  IFrameMedia: defineAsyncComponent(() => import('./type/IFrameMedia.vue')),
+  ObjectMedia: defineAsyncComponent(() => import('./type/ObjectMedia.vue')),
+  Media: defineAsyncComponent(() => import('./type/UnknownMedia.vue')),
 }
+
+const resolveComponent = computed(() => {
+  const type = props.mimeType || defaultMimeType.value
+  return components[resolveMedia(type) + SUFFIX]
+})
+const properSrc = computed(() => {
+  return props.src || props.placeholder
+})
+
+watch(
+  () => props.animationSrc,
+  () => updateComponent()
+)
+onMounted(() => {
+  updateComponent()
+})
+const updateComponent = async () => {
+  if (props.animationSrc && !props.mimeType) {
+    defaultMimeType.value = await getMimeType(props.animationSrc)
+  }
+}
+const toggleContent = () => {
+  isLewdBlurredLayer.value = !isLewdBlurredLayer.value
+}
+
+defineExpose({ isLewdBlurredLayer })
 </script>
 
 <style lang="scss" scoped>
@@ -138,11 +112,21 @@ export default {
     max-width: 18.75rem;
     text-align: center;
   }
-
-  .nsfw-content {
-    cursor: pointer;
-    bottom: 3.125rem;
-    position: absolute;
+}
+.nsfw-action {
+  cursor: pointer;
+  bottom: 3.125rem;
+  left: 50%;
+  transform: translate(-50%);
+  font-size: 1rem;
+  position: absolute;
+  border-radius: 1.5rem;
+  border: none;
+  background: #fff;
+  color: #000;
+  &.hide {
+    background: #000;
+    color: #fff;
   }
 }
 </style>
