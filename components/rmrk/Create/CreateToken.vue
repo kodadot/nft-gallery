0c6478ffd833acfedf1eba2b86f425dba114fdb3
Peter Kompasz <kompaszbence@gmail.com>
2023-08-29 16:52:26 +0300

    refactor CreateToken

--- components/rmrk/Create/CreateToken.vue
+++ components/rmrk/Create/CreateToken.vue
@@ -77,16 +77,11 @@
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { BaseTokenType } from '@/components/base/types'
 import collectionForMint from '@/queries/subsquid/rmrk/collectionForMint.graphql'
 import { Location } from 'vue-router/types/router'
 import { DETAIL_TIMEOUT } from '@/utils/constants'
-import AuthMixin from '@/utils/mixins/authMixin'
-import ChainMixin from '@/utils/mixins/chainMixin'
-import MetaTransactionMixin from '@/utils/mixins/metaMixin'
-import PrefixMixin from '@/utils/mixins/prefixMixin'
-import UseApiMixin from '@/utils/mixins/useApiMixin'
 import {
   notificationTypes,
   showNotification,
@@ -97,7 +92,6 @@ import { CreatedNFT, Interaction } from '@kodadot1/minimark/v1'
 import { CreatedNFT as CreatedNFTV2 } from '@kodadot1/minimark/v2'
 import { Attribute } from '@kodadot1/minimark/common'
 import { formatBalance } from '@polkadot/util'
-import { Component, Prop, Ref, Watch, mixins } from 'nuxt-property-decorator'
 import { unwrapSafe } from '@/utils/uniquery'
 import { toNFTId } from '../service/scheme'
 import { usePreferencesStore } from '@/stores/preferences'
@@ -109,284 +103,297 @@ import {
 } from '@/composables/transaction/types'
 import { NeoField, NeoMessage } from '@kodadot1/brick'
 
-const components = {
-  AttributeTagInput: () =>
-    import('@/components/rmrk/Create/AttributeTagInput.vue'),
-  CustomAttributeInput: () =>
-    import('@/components/rmrk/Create/CustomAttributeInput.vue'),
-  CollapseWrapper: () =>
-    import('@/components/shared/collapse/CollapseWrapper.vue'),
-  Loader: () => import('@/components/shared/Loader.vue'),
-  BalanceInput: () => import('@/components/shared/BalanceInput.vue'),
-  BaseTokenForm: () => import('@/components/base/BaseTokenForm.vue'),
-  BasicSwitch: () => import('@/components/shared/form/BasicSwitch.vue'),
-  Money: () => import('@/components/shared/format/Money.vue'),
-  SubmitButton: () => import('@/components/base/SubmitButton.vue'),
-  RoyaltyForm: () => import('@/components/bsx/Create/RoyaltyForm.vue'),
-  NeoMessage,
-  NeoField,
+import CustomAttributeInput from '@/components/rmrk/Create/CustomAttributeInput.vue'
+import CollapseWrapper from '@/components/shared/collapse/CollapseWrapper.vue'
+import Loader from '@/components/shared/Loader.vue'
+import BalanceInput from '@/components/shared/BalanceInput.vue'
+import BaseTokenForm from '@/components/base/BaseTokenForm.vue'
+import BasicSwitch from '@/components/shared/form/BasicSwitch.vue'
+import SubmitButton from '@/components/base/SubmitButton.vue'
+import RoyaltyForm from '@/components/bsx/Create/RoyaltyForm.vue'
+
+const base: BaseTokenType<MintedCollectionKusama> = {
+  name: '',
+  file: null,
+  description: '',
+  selectedCollection: null,
+  copies: 1,
+  secondFile: null,
 }
 
-@Component({ components })
-export default class CreateToken extends mixins(
-  MetaTransactionMixin,
-  ChainMixin,
-  PrefixMixin,
-  AuthMixin,
-  UseApiMixin
-) {
-  public base: BaseTokenType<MintedCollectionKusama> = {
-    name: '',
-    file: null,
-    description: '',
-    selectedCollection: null,
-    copies: 1,
-    secondFile: null,
-  }
+const collections = ref([] as MintedCollectionKusama[])
+const tags: Attribute[] = []
+const price = ref(0 as string | number)
+const nsfw = false
+const listed = true
+const postfix = true
+const balanceNotEnough = ref(false)
 
-  public collections: MintedCollectionKusama[] = []
-  public tags: Attribute[] = []
-  public price: string | number = 0
-  public nsfw = false
-  public listed = true
-  public postfix = true
-  public balanceNotEnough = false
+const hasRoyalty = true
+const royalty: Royalty = {
+  amount: 0.15,
+  address: '',
+}
 
-  public hasRoyalty = true
-  public royalty: Royalty = {
-    amount: 0.15,
-    address: '',
-  }
+const isLoading = ref(false)
+const status = ref()
 
-  private preferencesStore = usePreferencesStore()
+const preferencesStore = usePreferencesStore()
 
-  @Ref('balanceInput') readonly balanceInput
-  @Ref('baseTokenForm') readonly baseTokenForm
-  @Prop({ type: Boolean, default: false }) showExplainerText!: boolean
+const balanceInput = ref()
+const baseTokenForm = ref()
+defineProps({
+  showExplainerText: {
+    type: Boolean,
+    default: false,
+  },
+})
 
-  public updatePrice(value: string) {
-    this.price = value
-  }
+const updatePrice = (value: string) => {
+  price.value = value
+}
 
-  get version() {
-    return useRmrkVersion().version.value
-  }
+const version = computed(() => {
+  return useRmrkVersion().version.value
+})
 
-  get hasPrice() {
-    return Number(this.price)
-  }
+const hasPrice = computed(() => {
+  return Number(price.value)
+})
 
-  get balanceNotEnoughMessage() {
-    return this.balanceNotEnough ? this.$t('tooltip.notEnoughBalance') : ''
-  }
+const { $i18n, $apollo } = useNuxtApp()
 
-  @Watch('accountId', { immediate: true })
-  hasAccount(value: string, oldVal: string) {
+const balanceNotEnoughMessage = computed(() => {
+  return balanceNotEnough ? $i18n.t('tooltip.notEnoughBalance') : ''
+})
+
+const { accountId, isLogIn, balance } = useAuth()
+
+watch(
+  accountId,
+  (value, oldVal) => {
     if (shouldUpdate(value, oldVal)) {
-      this.fetchCollections()
+      fetchCollections()
     }
+  },
+  {
+    immediate: true,
   }
+)
 
-  public async fetchCollections() {
-    const collections = await this.$apollo.query({
-      query: collectionForMint,
-      client: this.client,
-      variables: {
-        account: this.accountId,
-      },
-      fetchPolicy: 'network-only',
-    })
+const { client } = usePrefix()
 
-    const {
-      data: { collectionEntities },
-    } = collections
+const fetchCollections = async () => {
+  const _collections = await $apollo.query({
+    query: collectionForMint,
+    client: client.value,
+    variables: {
+      account: accountId.value,
+    },
+    fetchPolicy: 'network-only',
+  })
 
-    this.collections = unwrapSafe(collectionEntities)
-      ?.map((ce: any) => ({
-        ...ce,
-        alreadyMinted: ce.nfts?.length,
-        lastIndexUsed: Number(ce.nfts?.at(0)?.index || 0),
-        totalCount: ce.nfts?.filter((nft) => !nft.burned).length,
-      }))
-      .filter(
-        (ce: MintedCollectionKusama) =>
-          (ce.max || Infinity) - ce.alreadyMinted > 0
-      )
-  }
+  const {
+    data: { collectionEntities },
+  } = _collections
 
-  public checkValidity() {
-    const balanceInputValid = !this.listed || this.balanceInput.checkValidity()
-    const baseTokenFormValid = this.baseTokenForm.checkValidity()
-    return balanceInputValid && baseTokenFormValid
-  }
-
-  public async submit() {
-    if (!this.base.selectedCollection) {
-      throw ReferenceError('[MINT] Unable to mint without collection')
-    }
-
-    if (!this.checkValidity()) {
-      return
-    }
-
-    if (parseFloat(this.balance) === 0) {
-      this.balanceNotEnough = true
-      return
-    }
-
-    this.isLoading = true
-    this.status = 'loader.ipfs'
-    const { urlPrefix } = usePrefix()
-    const { transaction, status, isLoading, blockNumber } = useTransaction()
-
-    watch([isLoading, status], () => {
-      this.isLoading = isLoading.value
-      if (Boolean(status.value)) {
-        this.status = status.value
-      }
-    })
-
-    try {
-      const { createdNFTs } = (await transaction({
-        interaction: Interaction.MINTNFT,
-        urlPrefix: urlPrefix.value,
-        token: {
-          ...this.base,
-          copies: Number(this.base.copies),
-          tags: this.tags,
-          nsfw: this.nsfw,
-          postfix: this.postfix,
-          price: this.price.toString(),
-          royalty: this.royalty,
-          hasRoyalty: this.hasRoyalty,
-        },
-      })) as {
-        createdNFTs: RefType<CreatedNFT[]>
-      }
-
-      watch([blockNumber, createdNFTs], () => {
-        if (blockNumber.value && createdNFTs.value) {
-          const isJustNftMint = !this.listed
-          if (isJustNftMint) {
-            setTimeout(
-              () =>
-                this.handleCreatedNftsRedirect(
-                  createdNFTs.value,
-                  blockNumber.value as string
-                ),
-              300
-            )
-          } else if (this.hasPrice) {
-            setTimeout(
-              () =>
-                this.listForSale(
-                  createdNFTs.value,
-                  blockNumber.value as string
-                ),
-              300
-            )
-          }
-        }
-      })
-    } catch (e) {
-      if (e instanceof Error) {
-        warningMessage(e)
-      }
-    }
-  }
-
-  public async listForSale(
-    createdNFT: CreatedNFT[] | CreatedNFTV2[],
-    originalBlockNumber: string
-  ) {
-    try {
-      const { transaction, status, isLoading, blockNumber } = useTransaction()
-
-      watch([isLoading, status], () => {
-        this.isLoading = isLoading.value
-        if (Boolean(status.value)) {
-          this.status = status.value
-        }
-      })
-
-      const balance = formatBalance(this.price, {
-        decimals: this.decimals,
-        withUnit: this.unit,
-      })
-
-      if (!createdNFT) {
-        showNotification('Can not list empty NFTs', notificationTypes.warn)
-        return
-      }
-
-      showNotification(`[💰] Listing NFT to sale for ${balance}`)
-      const list: TokenToList[] = createdNFT.map((nft) => ({
-        price: this.price.toString(),
-        nftId: toNFTId(nft, originalBlockNumber),
-      }))
-
-      this.isLoading = true
-      transaction({
-        interaction: Interaction.LIST,
-        urlPrefix: this.urlPrefix,
-        token: list,
-
-        successMessage: (blockNumber) =>
-          `[💰] Listed ${this.base.name} for ${balance} in block ${blockNumber}`,
-      })
-
-      watch([isLoading, blockNumber], () => {
-        if (!isLoading.value && blockNumber.value) {
-          this.handleCreatedNftsRedirect(createdNFT, originalBlockNumber)
-        }
-      })
-    } catch (e) {
-      showNotification((e as Error).message, notificationTypes.warn)
-    }
-  }
-
-  protected handleCreatedNftsRedirect(
-    createdNFT: CreatedNFT[] | CreatedNFTV2[],
-    originalBlockNumber: string
-  ) {
-    const nfts = createdNFT.map((nft) => toNFTId(nft, originalBlockNumber))
-
-    const isSingle = nfts.length === 1
-
-    this.navigateToDetail({
-      pageId: isSingle ? nfts[0] : (this.base.selectedCollection?.id as string),
-      nftName: this.base.name,
-      toCollectionPage: !isSingle,
-    })
-  }
-
-  protected async estimateTx() {
-    // TODO: implement
-  }
-
-  protected navigateToDetail({
-    pageId,
-    nftName,
-    toCollectionPage,
-  }: {
-    pageId: string
-    nftName: string
-    toCollectionPage: boolean
-  }) {
-    showNotification(
-      `You will go to the detail in ${DETAIL_TIMEOUT / 1000} seconds`
+  collections.value = unwrapSafe(collectionEntities)
+    ?.map((ce: any) => ({
+      ...ce,
+      alreadyMinted: ce.nfts?.length,
+      lastIndexUsed: Number(ce.nfts?.at(0)?.index || 0),
+      totalCount: ce.nfts?.filter((nft) => !nft.burned).length,
+    }))
+    .filter(
+      (ce: MintedCollectionKusama) =>
+        (ce.max || Infinity) - ce.alreadyMinted > 0
     )
-    const subPath = toCollectionPage ? 'collection' : 'gallery'
-    const go = () => {
-      const navigateTo: Location = {
-        path: `/${this.urlPrefix}/${subPath}/${pageId}`,
-      }
-      if (!toCollectionPage) {
-        navigateTo.query = { congratsNft: nftName }
-      }
-      return this.$router.push(navigateTo)
-    }
-    setTimeout(go, DETAIL_TIMEOUT)
+}
+
+const checkValidity = () => {
+  const balanceInputValid = !listed || balanceInput.value.checkValidity()
+  const baseTokenFormValid = baseTokenForm.value.checkValidity()
+  return balanceInputValid && baseTokenFormValid
+}
+
+const submit = async () => {
+  if (!base.selectedCollection) {
+    throw ReferenceError('[MINT] Unable to mint without collection')
   }
+
+  if (!checkValidity()) {
+    return
+  }
+
+  if (parseFloat(balance.value) === 0) {
+    balanceNotEnough.value = true
+    return
+  }
+
+  isLoading.value = true
+  status.value = 'loader.ipfs'
+  const { urlPrefix } = usePrefix()
+  const {
+    transaction,
+    status: transactionStatus,
+    isLoading: transactionIsLoading,
+    blockNumber,
+  } = useTransaction()
+
+  watch([transactionIsLoading, transactionStatus], () => {
+    isLoading.value = transactionIsLoading.value
+    if (Boolean(transactionStatus.value)) {
+      status.value = transactionStatus.value
+    }
+  })
+
+  try {
+    const { createdNFTs } = (await transaction({
+      interaction: Interaction.MINTNFT,
+      urlPrefix: urlPrefix.value,
+      token: {
+        ...base,
+        copies: Number(base.copies),
+        tags: tags,
+        nsfw: nsfw,
+        postfix: postfix,
+        price: price.toString(),
+        royalty: royalty,
+        hasRoyalty: hasRoyalty,
+      },
+    })) as {
+      createdNFTs: RefType<CreatedNFT[]>
+    }
+
+    watch([blockNumber, createdNFTs], () => {
+      if (blockNumber.value && createdNFTs.value) {
+        const isJustNftMint = !listed
+        if (isJustNftMint) {
+          setTimeout(
+            () =>
+              handleCreatedNftsRedirect(
+                createdNFTs.value,
+                blockNumber.value as string
+              ),
+            300
+          )
+        } else if (hasPrice) {
+          setTimeout(
+            () => listForSale(createdNFTs.value, blockNumber.value as string),
+            300
+          )
+        }
+      }
+    })
+  } catch (e) {
+    if (e instanceof Error) {
+      warningMessage(e)
+    }
+  }
+}
+
+const { urlPrefix } = usePrefix()
+const { unit, decimals } = useChain()
+
+const listForSale = async (
+  createdNFT: CreatedNFT[] | CreatedNFTV2[],
+  originalBlockNumber: string
+) => {
+  try {
+    const {
+      transaction,
+      status: transactionStatus,
+      isLoading: transactionIsLoading,
+      blockNumber,
+    } = useTransaction()
+
+    watch([transactionIsLoading, transactionStatus], () => {
+      isLoading.value = transactionIsLoading.value
+      if (Boolean(transactionStatus.value)) {
+        status.value = transactionStatus.value
+      }
+    })
+
+    const balance = formatBalance(price.value, {
+      decimals: decimals.value,
+      withUnit: unit.value,
+    })
+
+    if (!createdNFT) {
+      showNotification('Can not list empty NFTs', notificationTypes.warn)
+      return
+    }
+
+    showNotification(`[💰] Listing NFT to sale for ${balance}`)
+    const list: TokenToList[] = createdNFT.map((nft) => ({
+      price: price.value.toString(),
+      nftId: toNFTId(nft, originalBlockNumber),
+    }))
+
+    isLoading.value = true
+    transaction({
+      interaction: Interaction.LIST,
+      urlPrefix: urlPrefix.value,
+      token: list,
+
+      successMessage: (blockNumber) =>
+        `[💰] Listed ${base.name} for ${balance} in block ${blockNumber}`,
+    })
+
+    watch([isLoading, blockNumber], () => {
+      if (!isLoading.value && blockNumber.value) {
+        handleCreatedNftsRedirect(createdNFT, originalBlockNumber)
+      }
+    })
+  } catch (e) {
+    showNotification((e as Error).message, notificationTypes.warn)
+  }
+}
+
+const handleCreatedNftsRedirect = (
+  createdNFT: CreatedNFT[] | CreatedNFTV2[],
+  originalBlockNumber: string
+) => {
+  const nfts = createdNFT.map((nft) => toNFTId(nft, originalBlockNumber))
+
+  const isSingle = nfts.length === 1
+
+  navigateToDetail({
+    pageId: isSingle ? nfts[0] : (base.selectedCollection?.id as string),
+    nftName: base.name,
+    toCollectionPage: !isSingle,
+  })
+}
+
+const estimateTx = async () => {
+  // TODO: implement
+  throw new Error('Not implemented')
+}
+
+const router = useRouter()
+
+const navigateToDetail = ({
+  pageId,
+  nftName,
+  toCollectionPage,
+}: {
+  pageId: string
+  nftName: string
+  toCollectionPage: boolean
+}) => {
+  showNotification(
+    `You will go to the detail in ${DETAIL_TIMEOUT / 1000} seconds`
+  )
+  const subPath = toCollectionPage ? 'collection' : 'gallery'
+  const go = () => {
+    const navigateTo: Location = {
+      path: `/${urlPrefix.value}/${subPath}/${pageId}`,
+    }
+    if (!toCollectionPage) {
+      navigateTo.query = { congratsNft: nftName }
+    }
+    return router.push(navigateTo)
+  }
+  setTimeout(go, DETAIL_TIMEOUT)
 }
 </script>
