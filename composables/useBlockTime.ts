7ad52611efe879ff937319206f8ac96cf84fe315
daiagi <daiagi@gmail.com>
2023-08-29 12:28:19 +0700

    implement viki code review

--- composables/useBlockTime.ts
+++ composables/useBlockTime.ts
@@ -1,4 +1,4 @@
-import { Prefix } from '@kodadot1/static'
+import { PartialConfig } from '@/utils/config/types'
 
 export default function () {
   const { urlPrefix } = usePrefix()
@@ -6,20 +6,15 @@ export default function () {
   const paraChainBlockTime = 12 //seconds
   const relayChainBlockTime = 6 //seconds
 
-  const chainBlockTimes: { [K in Prefix]: number } = {
+  const chainBlockTimes: PartialConfig<number> = {
     ksm: relayChainBlockTime,
     dot: relayChainBlockTime,
     rmrk: relayChainBlockTime,
-    ahk: paraChainBlockTime,
-    ahp: paraChainBlockTime,
-    bsx: paraChainBlockTime,
-    snek: paraChainBlockTime,
-    movr: paraChainBlockTime,
-    glmr: paraChainBlockTime,
   }
 
-  const blocktime = computed(() => chainBlockTimes[urlPrefix.value])
-
+  const blocktime = computed(
+    () => chainBlockTimes[urlPrefix.value] ?? paraChainBlockTime
+  )
   return {
     blocktime,
   }
