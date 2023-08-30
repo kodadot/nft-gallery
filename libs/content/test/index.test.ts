f87a0b73d35228beab72260b6411e3a87b26788e
Viki Val <viktorko99@gmail.com>
2023-08-30 17:18:27 +0200

    :test_tube: attributeFrom

--- libs/content/test/index.test.ts
+++ libs/content/test/index.test.ts
@@ -1,12 +1,15 @@
 import { describe, expect, it } from 'vitest'
-import { APOLLO_ENDPOINTS, INDEXERS } from '../src'
+import { OpenSeaAttribute, attributeFrom } from '../src'
 
-describe('Endpoints', () => {
-  for (const key of Object.keys(INDEXERS)) {
-    it(`should parse SQUID::${key} to APOLLO_INDEXER correctly`, () => {
-      expect(APOLLO_ENDPOINTS[key]).toStrictEqual({
-        httpEndpoint: INDEXERS[key],
-      })
+describe('attributeFrom', () => {
+  it(`should parse OpenSea Attribute correctly`, () => {
+    const attribute: OpenSeaAttribute = {
+      trait_type: 'NSFW',
+      value: 1,
+    }
+    expect(attributeFrom(attribute)).toStrictEqual({
+      trait: 'NSFW',
+      value: '1',
     })
-  }
+  })
 })
