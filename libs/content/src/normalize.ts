e401856cd7ed94a83d2970b4e3f5354b2734cec1
Viki Val <viktorko99@gmail.com>
2023-08-30 17:19:58 +0200

    :broom: use any, that is how docs said that

--- libs/content/src/normalize.ts
+++ libs/content/src/normalize.ts
@@ -13,6 +13,7 @@ import {
 export function attributeFrom(attr: OpenSeaAttribute): Attribute
 export function attributeFrom(attr: TezosAttribute): Attribute
 export function attributeFrom(attr: PluralAttribute): Attribute
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function attributeFrom(attr: any): Attribute {
   const display: string = attr.display_type || attr.type || ''
   const trait: string | undefined = attr.trait_type || attr.name || attr.label
@@ -28,6 +29,7 @@ export function attributeFrom(attr: any): Attribute {
 export function contentFrom(meta: OpenSeaMetadata): Content
 export function contentFrom(meta: FXHashMetadata): Content
 export function contentFrom(meta: PluralAssetMetadata): Content
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function contentFrom(meta: any): Content {
   const description = meta.description || ''
   const image = meta.image || meta.thumbnailUri || meta.mediaUri
