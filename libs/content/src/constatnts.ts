875c0422d6071cdbf548f818914f1a8ff7dfd549
Viki Val <viktorko99@gmail.com>
2023-09-01 13:31:58 +0200

    :lock: infinite regex match

--- libs/content/src/constatnts.ts
+++ libs/content/src/constatnts.ts
@@ -1,2 +1,4 @@
 export const LEWD = 'NSFW'
-export const MIME_TYPE = /\w+\/[-+.\w]+/
+// based on https://www.regextester.com/110183
+// list ipfs://bafkreiequ3mnfu2ytaixzdzolcpfwlc3vrj4544pzqg4tjvyth2zi5mhgq
+export const MIME_TYPE = /\w{4,12}\/[-+.\w]{2,62}/
