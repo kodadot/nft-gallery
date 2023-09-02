8226c030938c00a68d864e63eae6e9caa2bda014
hassnian <hassnianxd@gmail.com>
2023-09-02 11:33:21 +0500

    removed: unsued code

--- components/shared/AddressChecker.vue
+++ components/shared/AddressChecker.vue
@@ -66,7 +66,7 @@ import correctFormat from '@/utils/ss58Format'
 import { CHAINS } from '@/libs/static/src/chains'
 import InfoBox from '@/components/shared/view/InfoBox.vue'
 import { NeoButton } from '@kodadot1/brick'
-import { type Prefix, chainNames } from '@kodadot1/static'
+import { type Prefix } from '@kodadot1/static'
 
 enum AddressType {
   ETHEREUM = 'ethereum',
@@ -88,7 +88,6 @@ const props = defineProps<{
 }>()
 
 const { chainProperties, unit } = useChain()
-const { urlPrefix } = usePrefix()
 const ss58Format = computed(() => chainProperties.value?.ss58Format)
 const addressCheck = ref<AddressCheck | null>(null)
 const showAddressCheck = ref(false)
