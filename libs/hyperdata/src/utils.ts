141f6b0e78fc0b5893657d45ad3bc93c88820abd
Viki Val <viktorko99@gmail.com>
2023-09-03 14:27:02 +0200

    Revert ":tada: unify the metadata (#7079)"
    This reverts commit 7ed55f4a3e5adb1de70f29a8a75658eacca6bbba.

--- libs/hyperdata/src/utils.ts
+++
@@ -1,7 +0,0 @@
-import { LEWD } from './constatnts'
-import { Attribute } from './types'
-
-export function isLewdAttribute(attribute: Attribute | string): boolean {
-  const trait = typeof attribute === 'string' ? attribute : attribute.trait
-  return trait.toUpperCase() === LEWD
-}
