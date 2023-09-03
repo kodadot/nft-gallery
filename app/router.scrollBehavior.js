ac1765db558e674d05c9046265c6471e1382fe52
tnaylor <tnaylor@bonline.com>
2023-09-03 18:14:13 +0200

    fix: codeclimate issues

--- app/router.scrollBehavior.js
+++ app/router.scrollBehavior.js
@@ -1,3 +1,17 @@
+const findEl = async (hash, x = 0) => {
+  return (
+    document.querySelector(hash) ||
+    new Promise((resolve) => {
+      if (x > 50) {
+        return resolve(document.querySelector('#app'))
+      }
+      setTimeout(() => {
+        resolve(findEl(hash, ++x || 1))
+      }, 100)
+    })
+  )
+}
+
 // https://nuxtjs.org/docs/configuration-glossary/configuration-router/#scrollbehavior
 export default async function (to) {
   const toPath = to.name
@@ -15,20 +29,6 @@ export default async function (to) {
   }
 
   // https://github.com/nuxt/nuxt/issues/5359
-  const findEl = async (hash, x = 0) => {
-    return (
-      document.querySelector(hash) ||
-      new Promise((resolve) => {
-        if (x > 50) {
-          return resolve(document.querySelector('#app'))
-        }
-        setTimeout(() => {
-          resolve(findEl(hash, ++x || 1))
-        }, 100)
-      })
-    )
-  }
-
   if (to.hash) {
     let el = await findEl(to.hash)
     if ('scrollBehavior' in document.documentElement.style) {
