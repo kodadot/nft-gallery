8e0494ed4b8b734f32db814487b8daf2f53ee424
lungern <xiii.at.cn@gmail.com>
2023-09-06 13:17:29 +0800

    fix: use filter to avoid twice loop

--- libs/ui/src/components/NeoModalExtend/plugin.js
+++ libs/ui/src/components/NeoModalExtend/plugin.js
@@ -12,12 +12,7 @@ const ModalProgrammatic = {
     openListener.push(callback)
   },
   removeOpenListener(callback) {
-    for (const listener of openListener) {
-      if (listener === callback) {
-        openListener.splice(openListener.indexOf(listener), 1)
-        break
-      }
-    }
+    openListener = openListener.filter((e) => e !== callback)
   },
   open(params) {
     let parent
