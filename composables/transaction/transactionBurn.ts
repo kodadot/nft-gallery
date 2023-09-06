155e96e37a86bbc430325abbd907c40dad276fc4
Peter Kompasz <kompaszbence@gmail.com>
2023-09-06 18:55:40 +0300

    refactor: transactionBurn

--- composables/transaction/transactionBurn.ts
+++ composables/transaction/transactionBurn.ts
@@ -12,38 +12,86 @@ import {
 } from '@/utils/gallery/abstractCalls'
 import type { ActionConsume } from './types'
 
-export function execBurnTx(item: ActionConsume, api, executeTransaction) {
+const getTransactionOptsAhk = (item, api) => {
+  const legacy = isLegacy(item.nftId)
+  const paramResolver = assetHubParamResolver(legacy)
+
+  return {
+    cb: getApiCall(api, item.urlPrefix, Interaction.CONSUME),
+    arg: paramResolver(item.nftId, Interaction.CONSUME, ''),
+    successMessage: item.successMessage,
+    errorMessage: item.errorMessage,
+  }
+}
+
+const getTransactionOptsRmrk = (item, api) => {
+  return {
+    cb: api.tx.system.remark,
+    arg: [createInteraction(Interaction.CONSUME, item.nftId, '')],
+    successMessage: item.successMessage,
+    errorMessage: item.errorMessage,
+  }
+}
+
+const getTransactionOptsKsm = (item, api) => {
+  return {
+    cb: api.tx.system.remark,
+    arg: [
+      createNewInteraction({
+        action: NewInteraction.BURN,
+        payload: { id: item.nftId },
+      }),
+    ],
+    successMessage: item.successMessage,
+    errorMessage: item.errorMessage,
+  }
+}
+
+const getTransactionOptsSnek = (item, api, val) => {
+  const hasOffers = Boolean(val.offers.length)
+  const [collectionId, tokenId] = bsxParamResolver(
+    item.nftId,
+    Interaction.CONSUME,
+    ''
+  )
+  const offerWithdrawArgs = val.offers.map((offer: { caller: string }) => {
+    return api.tx.marketplace.withdrawOffer(collectionId, tokenId, offer.caller)
+  })
+  const cb = hasOffers
+    ? api.tx.utility.batchAll
+    : getApiCall(api, item.urlPrefix, Interaction.CONSUME)
+  const arg = hasOffers
+    ? [[...offerWithdrawArgs, api.tx.nft.burn(collectionId, tokenId)]]
+    : bsxParamResolver(item.nftId, Interaction.CONSUME, '')
+
+  return {
+    cb,
+    arg,
+    successMessage: item.successMessage,
+    errorMessage: item.errorMessage,
+  }
+}
+
+const getTransactionOpts = (item, api) => {
   if (item.urlPrefix === 'rmrk') {
-    executeTransaction({
-      cb: api.tx.system.remark,
-      arg: [createInteraction(Interaction.CONSUME, item.nftId, '')],
-      successMessage: item.successMessage,
-      errorMessage: item.errorMessage,
-    })
+    return getTransactionOptsRmrk(item, api)
   }
 
   if (item.urlPrefix === 'ksm') {
-    executeTransaction({
-      cb: api.tx.system.remark,
-      arg: [
-        createNewInteraction({
-          action: NewInteraction.BURN,
-          payload: { id: item.nftId },
-        }),
-      ],
-      successMessage: item.successMessage,
-      errorMessage: item.errorMessage,
-    })
+    return getTransactionOptsKsm(item, api)
   }
 
+  if (item.urlPrefix === 'ahk' || item.urlPrefix === 'ahp') {
+    return getTransactionOptsAhk(item, api)
+  }
+}
+
+export function execBurnTx(item: ActionConsume, api, executeTransaction) {
+  executeTransaction(getTransactionOpts(item, api))
+
   if (item.urlPrefix === 'snek' || item.urlPrefix === 'bsx') {
-    const [collectionId, tokenId] = bsxParamResolver(
-      item.nftId,
-      Interaction.CONSUME,
-      ''
-    )
-    const hasOffers = ref(false)
     const { data } = useGraphql({
+      clientName: '',
       queryName: 'acceptableOfferListByNftId',
       queryPrefix: 'chain-bsx',
       variables: {
@@ -51,41 +99,8 @@ export function execBurnTx(item: ActionConsume, api, executeTransaction) {
       },
     })
 
-    watch(data, () => {
-      hasOffers.value = Boolean(data.value.offers.length)
-      const offerWithdrawArgs = data.value.offers.map(
-        (offer: { caller: string }) => {
-          return api.tx.marketplace.withdrawOffer(
-            collectionId,
-            tokenId,
-            offer.caller
-          )
-        }
-      )
-      const cb = hasOffers.value
-        ? api.tx.utility.batchAll
-        : getApiCall(api, item.urlPrefix, Interaction.CONSUME)
-      const arg = hasOffers.value
-        ? [[...offerWithdrawArgs, api.tx.nft.burn(collectionId, tokenId)]]
-        : bsxParamResolver(item.nftId, Interaction.CONSUME, '')
-
-      executeTransaction({
-        cb,
-        arg,
-        successMessage: item.successMessage,
-        errorMessage: item.errorMessage,
-      })
-    })
-  }
-
-  if (item.urlPrefix === 'ahk' || item.urlPrefix === 'ahp') {
-    const legacy = isLegacy(item.nftId)
-    const paramResolver = assetHubParamResolver(legacy)
-    executeTransaction({
-      cb: getApiCall(api, item.urlPrefix, Interaction.CONSUME),
-      arg: paramResolver(item.nftId, Interaction.CONSUME, ''),
-      successMessage: item.successMessage,
-      errorMessage: item.errorMessage,
-    })
+    watch(data, (val) =>
+      executeTransaction(getTransactionOptsSnek(item, api, val))
+    )
   }
 }
