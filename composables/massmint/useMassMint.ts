3511ebafff3f9592c63c7559038e6769ec7e3d68
daiagi <daiagi@gmail.com>
2023-09-11 10:55:08 +0700

    fix: loader stay when transacrion cancelled

--- composables/massmint/useMassMint.ts
+++ composables/massmint/useMassMint.ts
@@ -123,26 +123,20 @@ export const useMassMint = (
   const willItList = tokens.some(
     (token) => token.price && Number(token.price) > 0
   )
-  const isBsx = computed(
-    () => urlPrefix.value === 'bsx' || urlPrefix.value === 'snek'
+  const isKsm = computed(
+    () => urlPrefix.value === 'rmrk' || urlPrefix.value === 'ksm'
   )
 
-  if (willItList) {
-    if (isBsx.value) {
-      simpleMint()
-    } else {
-      // kusama
-      const mintAndListResults = kusamaMintAndList(tokens)
-      watchEffect(() => {
-        collectionUpdated.value = mintAndListResults.collectionUpdated.value
-        isLoading.value = mintAndListResults.isLoading.value
-        status.value = mintAndListResults.status.value
-        blockNumber.value = mintAndListResults.blockNumber.value
-        isError.value = mintAndListResults.isError.value
-      })
-    }
+  if (willItList && isKsm.value) {
+    const mintAndListResults = kusamaMintAndList(tokens)
+    watchEffect(() => {
+      collectionUpdated.value = mintAndListResults.collectionUpdated.value
+      isLoading.value = mintAndListResults.isLoading.value
+      status.value = mintAndListResults.status.value
+      blockNumber.value = mintAndListResults.blockNumber.value
+      isError.value = mintAndListResults.isError.value
+    })
   } else {
-    //nothing to list, just mint
     simpleMint()
   }
   return {
