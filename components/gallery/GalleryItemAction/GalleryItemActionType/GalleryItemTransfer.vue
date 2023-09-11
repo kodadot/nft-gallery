ac4fdfe9eadd754cc0fd35ab21467783e39b0676
hassnian <hassnianxd@gmail.com>
2023-09-11 17:10:26 +0500

    remove: slide action code

--- components/gallery/GalleryItemAction/GalleryItemActionType/GalleryItemTransfer.vue
+++ components/gallery/GalleryItemAction/GalleryItemActionType/GalleryItemTransfer.vue
@@ -15,7 +15,6 @@
 </template>
 
 <script setup lang="ts">
-import { onClickOutside } from '@vueuse/core'
 import { NeoButton } from '@kodadot1/brick'
 
 import ItemTransferModal from '@/components/common/itemTransfer/ItemTransferModal.vue'
@@ -30,9 +29,6 @@ const isModalActive = ref(false)
 function sendItem() {
   isModalActive.value = true
 }
-
-const actionRef = ref(null)
-onClickOutside(actionRef, () => (isModalActive.value = false))
 </script>
 
 <style lang="scss" scoped>
