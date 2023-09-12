6ae515f4620d87f7547d2275ba2ecd42e9462ba8
Preschian Febryantara <preschian@users.noreply.github.com>
2023-09-12 16:42:18 +0700

    styles: mobile version

--- components/create/CreateLanding.vue
+++ components/create/CreateLanding.vue
@@ -1,20 +1,25 @@
 <template>
-  <div class="container">
+  <div
+    class="container is-flex is-align-items-center is-justify-content-center is-flex-direction-column">
     <h1 class="title is-size-2">{{ $t('mint.landing.heading') }}</h1>
 
     <div v-if="select === 'landing'" class="pack">
       <nuxt-link class="pack-box" to="/create/collection">
         <div class="pack-content">
           <NeoIcon icon="image-polaroid" size="large" />
-          <p class="is-size-4 has-text-weight-bold mt-4">Collection</p>
-          <p>{{ $t('mint.landing.collection') }}</p>
+          <div class="pack-content-text">
+            <p class="is-size-4 has-text-weight-bold">Collection</p>
+            <p>{{ $t('mint.landing.collection') }}</p>
+          </div>
         </div>
       </nuxt-link>
       <a class="pack-box" @click="select = 'nft'">
         <div class="pack-content">
           <NeoIcon icon="gem" size="large" />
-          <p class="is-size-4 has-text-weight-bold mt-4">NFT</p>
-          <p>{{ $t('mint.landing.nft') }}</p>
+          <div class="pack-content-text">
+            <p class="is-size-4 has-text-weight-bold">NFT</p>
+            <p>{{ $t('mint.landing.nft') }}</p>
+          </div>
         </div>
       </a>
     </div>
@@ -23,19 +28,23 @@
       <nuxt-link class="pack-box" to="/create/nft">
         <div class="pack-content">
           <NeoIcon icon="image" size="large" />
-          <p class="is-size-4 has-text-weight-bold mt-4">
-            {{ $t('mint.landing.singleNft') }}
-          </p>
-          <p>{{ $t('mint.landing.singleNftDesc') }}</p>
+          <div class="pack-content-text">
+            <p class="is-size-4 has-text-weight-bold">
+              {{ $t('mint.landing.singleNft') }}
+            </p>
+            <p>{{ $t('mint.landing.singleNftDesc') }}</p>
+          </div>
         </div>
       </nuxt-link>
       <nuxt-link class="pack-box" :to="`/${urlPrefix}/massmint`">
         <div class="pack-content">
           <NeoIcon icon="photo-film" size="large" />
-          <p class="is-size-4 has-text-weight-bold mt-4">
-            {{ $t('mint.landing.massNft') }}
-          </p>
-          <p>{{ $t('mint.landing.massNftDesc') }}</p>
+          <div class="pack-content-text">
+            <p class="is-size-4 has-text-weight-bold">
+              {{ $t('mint.landing.massNft') }}
+            </p>
+            <p>{{ $t('mint.landing.massNftDesc') }}</p>
+          </div>
         </div>
       </nuxt-link>
     </div>
@@ -54,11 +63,7 @@ const select = ref<'landing' | 'nft'>('landing')
 @import '@/styles/abstracts/variables';
 
 .container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  height: calc(100vh - 5.25rem);
+  height: calc(100vh - 5.25rem); // 5.25rem = header height
   margin: 0;
   max-width: 100% !important;
 
@@ -68,6 +73,14 @@ const select = ref<'landing' | 'nft'>('landing')
 
   @include touch() {
     height: 100vh;
+  }
+
+  @include mobile() {
+    padding: 0 2rem;
+
+    .title {
+      font-size: 2rem !important;
+    }
   }
 }
 
@@ -87,6 +100,30 @@ const select = ref<'landing' | 'nft'>('landing')
   &-content {
     text-align: center;
     width: 14rem;
+
+    &-text {
+      margin-top: 2rem;
+    }
+  }
+
+  @include mobile() {
+    flex-direction: column;
+    width: 100%;
+
+    &-box {
+      padding: 2rem;
+    }
+
+    &-content {
+      display: flex;
+      gap: 2rem;
+      text-align: left;
+      width: 100%;
+
+      &-text {
+        margin-top: 0;
+      }
+    }
   }
 }
 </style>
