4299176c4812e959381dcb9db6a715d0bea0b4f9
prury <yco.arq@gmail.com>
2023-09-15 08:40:30 -0300

    better redirect syntax

--- tests/e2e/redirect.spec.ts
+++ tests/e2e/redirect.spec.ts
@@ -6,13 +6,16 @@ test('Should redirect from rmrk2 prefix to ksm', async ({ page }) => {
   )
   await page.waitForLoadState()
   // expects redirection to occur
-  expect(await page.url()).toContain(
+  await expect(page.url()).toContain(
     '/ksm/gallery/17842583-22708b368d163c8007-CITY-LOWER_ART_DISTRICT-00000006'
   )
 
   await page.goto('/rmrk2/explore/items?listed=true&sort=updatedAt_DESC')
   await page.waitForLoadState()
-  expect(await page.url()).toContain(
+  await expect(page.url()).toContain(
+    '/ksm/explore/items?listed=true&sort=updatedAt_DESC'
+  )
+  await expect(page).toHaveURL(
     '/ksm/explore/items?listed=true&sort=updatedAt_DESC'
   )
 })
