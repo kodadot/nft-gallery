7d2559c72a8da9a7ae6a2e13e655a0c2ca2192fc
prury <yco.arq@gmail.com>
2023-09-16 14:11:44 -0300

    reduced global timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  timeout: 3 * 60 * 1000,
+  timeout: 1 * 60 * 1000,
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://localhost:9090',
