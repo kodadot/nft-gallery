7e9f06d5217ab8804dde59235615c0fcaef402e3
Jarsen <31397967+Jarsen136@users.noreply.github.com>
2023-09-26 02:24:35 +0800

    fix: clear button

--- components/common/IdentityForm.vue
+++ components/common/IdentityForm.vue
@@ -24,7 +24,7 @@
           <NeoButton
             :label="$i18n.t('identity.clear')"
             variant="text"
-            class="border border-k-shade border-radius mb-1 px-2"
+            class="border border-k-shade border-radius is-size-7 mb-1 px-3"
             no-shadow
             @click.native="submit(true)" />
         </div>
