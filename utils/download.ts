58e90552802a286d391ad7dbaf3f510a0bf87f6e
Nick Dev <nick@dev.com>
2023-09-27 22:33:33 +0300

    fix: download

--- utils/download.ts
+++ utils/download.ts
@@ -2,9 +2,22 @@ export const downloadImage = async (imageSrc: string, name = 'unnamed') => {
   if (!imageSrc) {
     return
   }
-  const image = await fetch(imageSrc)
-  const imageBlog = await image.blob()
-  const imageURL = URL.createObjectURL(imageBlog)
+
+  const { $consola } = useNuxtApp()
+
+  let resp
+  try {
+    resp = await useFetch(imageSrc)
+  } catch (error) {
+    $consola.warn('[ERR] unable to fetch image')
+    return
+  }
+
+  const image = resp.data.value
+  if (!(image instanceof Blob)) {
+    return
+  }
+  const imageURL = URL.createObjectURL(image)
 
   const link = document.createElement('a')
   link.href = imageURL
