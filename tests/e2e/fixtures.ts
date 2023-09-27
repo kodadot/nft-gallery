46b6a2af37e49b004a00952146b1c635579311af
prury <yco.arq@gmail.com>
2023-09-27 17:48:49 -0300

    removing some comments

--- tests/e2e/fixtures.ts
+++ tests/e2e/fixtures.ts
@@ -1,13 +1,11 @@
 import { test as base } from '@playwright/test'
 import { Commands } from './custom'
 
-// Declare the types of your fixtures.
 type MyFixtures = {
   Commands: Commands
 }
 
-// Extend base test by providing "todoPage" and "settingsPage".
-// This new "test" can be used in multiple test files, and each of them will get the fixtures.
+// Extend base
 export const test = base.extend<MyFixtures>({
   Commands: async ({ page }, use) => {
     await use(new Commands(page))
