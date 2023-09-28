91f302629f06e74ea88ce9c4257883a49d2d8dca
Nick Dev <nick@dev.com>
2023-09-28 23:49:14 +0300

    refactor

--- utils/download.ts
+++ utils/download.ts
@@ -5,25 +5,23 @@ export const downloadImage = async (imageSrc: string, name = 'unnamed') => {
 
   const { $consola } = useNuxtApp()
 
-  let resp
   try {
-    resp = await useFetch(imageSrc)
+    const { data } = await useFetch(imageSrc)
+    const image = data.value
+
+    if (!(image instanceof Blob)) {
+      return
+    }
+
+    const link = document.createElement('a')
+    link.href = URL.createObjectURL(image)
+    link.download = name
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+    return link
   } catch (error) {
     $consola.warn('[ERR] unable to fetch image')
     return
   }
-
-  const image = resp.data.value
-  if (!(image instanceof Blob)) {
-    return
-  }
-  const imageURL = URL.createObjectURL(image)
-
-  const link = document.createElement('a')
-  link.href = imageURL
-  link.download = name
-  document.body.appendChild(link)
-  link.click()
-  document.body.removeChild(link)
-  return link
 }
