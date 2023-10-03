d52aad915f44d1e666f4cddc3ca3498f478f8763
Preschian Febryantara <preschian@users.noreply.github.com>
2023-10-03 17:14:53 +0700

    revert: readd onbeforemount

--- pages/[prefix]/index.vue
+++ pages/[prefix]/index.vue
@@ -8,4 +8,10 @@ import LandingPage from '@/components/landing/LandingPage.vue'
 definePageMeta({
   layout: 'full-width-layout',
 })
+
+const router = useRouter()
+
+onBeforeMount(() => {
+  router.replace('/')
+})
 </script>
