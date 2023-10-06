c55c92fa66cee8f3517a33328879e739984b87ce
prury <yco.arq@gmail.com>
2023-10-06 15:41:50 -0300

    adding locator

--- tests/e2e/simple.spec.ts
+++ tests/e2e/simple.spec.ts
@@ -3,5 +3,6 @@ import { expect, test } from '@playwright/test'
 test('if test passes', async ({ page }) => {
   await page.goto('')
   await expect(page).toHaveURL('http://localhost:9090/')
+  await expect(page.locator('[class="navbar-brand"]')).toBeVisible()
   await expect(page.getByTestId('drops')).toBeVisible()
 })
