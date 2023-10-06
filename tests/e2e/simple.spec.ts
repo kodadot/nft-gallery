f37b373de1dff47c9d54beaee8d12c1dd979ce02
prury <yco.arq@gmail.com>
2023-10-06 16:03:27 -0300

    see if tests run outside

--- tests/e2e/simple.spec.ts
+++ tests/e2e/simple.spec.ts
@@ -1,5 +1,12 @@
 import { expect, test } from '@playwright/test'
 
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/')
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/)
+})
+
 test('if test passes', async ({ page }) => {
   await page.goto('')
   await expect(page).toHaveURL('http://localhost:9090/')
