fbd07681d06981d74db38493790e149b6eb76be8
prury <yco.arq@gmail.com>
2023-10-06 15:35:21 -0300

    localhost search

--- tests/e2e/simple.spec.ts
+++ tests/e2e/simple.spec.ts
@@ -2,6 +2,6 @@ import { expect, test } from '@playwright/test'
 
 test('if test passes', async ({ page }) => {
   await page.goto('')
-  await expect(page.getByTestId('drops')).toBeVisible()
   await expect(page).toHaveURL('http://localhost:9090/')
+  await expect(page.getByTestId('drops')).toBeVisible()
 })
