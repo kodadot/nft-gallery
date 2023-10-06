9ff84624b84f5d37afb4f9a7452b828c9e46ca0e
prury <yco.arq@gmail.com>
2023-10-06 16:14:50 -0300

    raising normal action timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   timeout: 1 * 60 * 1000,
-  expect: { timeout: 5000 },
+  expect: { timeout: 30000 },
   use: {
     headless: true,
     /* Base URL to use in actions like `await page.goto('/')`. */
