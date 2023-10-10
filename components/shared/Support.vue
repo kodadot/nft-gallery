5a9461902434f75da04a69a0708a94e0dd124209
Floyd Li <floyd.li@outlook.com>
2023-10-10 09:34:58 +0800

    feat: rounded switch

--- components/shared/Support.vue
+++ components/shared/Support.vue
@@ -1,5 +1,5 @@
 <template>
-  <NeoSwitch v-model="model" :type="type" :rounded="false">
+  <NeoSwitch v-model="model" :type="type">
     <div class="is-flex is-align-items-center">
       <span class="mr-2">
         {{ value ? `${$t(activeMessage)}${priceString}` : $t(passiveMessage) }}
