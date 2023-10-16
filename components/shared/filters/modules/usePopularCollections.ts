e0b00a560e2d097f558739670bf5e174f7a9dd05
Jarsen <31397967+Jarsen136@users.noreply.github.com>
2023-10-16 23:51:45 +0800

    fix: Seems popular collections are gone

--- components/shared/filters/modules/usePopularCollections.ts
+++ components/shared/filters/modules/usePopularCollections.ts
@@ -1,33 +1,24 @@
 import { POPULAR_COLLECTIONS as ALL_POPULAR_COLLECTIONS } from './constants'
 import { CollectionEntityMinimal } from '@/components/collection/utils/types'
 
-type QueryResult = {
+type CollectionData = {
   value: {
     collectionEntities: Collection[]
   }
 }
 
-function handleResult(
-  collections: Collection[],
-  result: QueryResult,
-  chain: string,
-): Collection[] {
+function handleResult(collections: Collection[], chain: string): Collection[] {
   const newCollections =
-    result.value?.collectionEntities?.map((item) => ({
+    collections?.map((item) => ({
       ...item,
       owners: new Set(item.nfts.map((nft) => nft.currentOwner)).size,
       chain,
     })) || []
-  return collections
-    .concat(newCollections)
-    .sort((a, b) => a.meta.name?.localeCompare(b.meta.name))
+  return newCollections.sort((a, b) => a.meta.name?.localeCompare(b.meta.name))
 }
 
-function getCollections(chain: string): { [chain: string]: string[] } {
-  if (ALL_POPULAR_COLLECTIONS[chain]) {
-    return { [chain]: ALL_POPULAR_COLLECTIONS[chain] }
-  }
-  return {}
+function getCollections(chain: string): string[] {
+  return ALL_POPULAR_COLLECTIONS[chain] || []
 }
 
 export const collectionArray = ref<Collection[]>([])
@@ -45,34 +36,20 @@ export type Collection = CollectionEntityMinimal & {
 
 export const usePopularCollections = (chain: string) => {
   const collections = ref<Collection[]>([])
-  const chainData = reactive<{
-    [clientName: string]: {
-      loading: { value: boolean }
-      queryResult: QueryResult | null
-    }
-  }>({})
-  const POPULAR_COLLECTIONS = getCollections(chain)
-  for (const [clientName, ids] of Object.entries(POPULAR_COLLECTIONS)) {
-    const { data: queryResult, loading } = useGraphql({
-      queryName: 'collectionByIds',
-      clientName,
-      variables: { ids },
-    })
-    chainData[clientName] = { loading, queryResult }
-  }
+  const collectionIds = getCollections(chain)
+  const { data: queryResult, loading } = useGraphql({
+    queryName: 'collectionByIds',
+    variables: { ids: collectionIds },
+  })
 
-  // Aoid using Array as root value for reactive() as it cannot be tracked in watch() or watchEffect(). Use ref() instead. This is a Vue-2-only limitation.
-  watch(chainData, (val) => {
-    Object.keys(val).forEach((chain) => {
-      const { loading, queryResult } = chainData[chain]
-      // not best but better than add an extra flag...
-      if (!loading.value && queryResult !== null) {
-        collections.value = handleResult(collections.value, queryResult, chain)
-        collectionArray.value = collections.value
-        // clear queryResult, prevent to be processed again
-        chainData[chain].queryResult = null
-      }
-    })
+  watch([queryResult, loading], () => {
+    if (!loading.value && queryResult.value) {
+      collections.value = handleResult(
+        (queryResult as CollectionData).value.collectionEntities,
+        chain,
+      )
+      collectionArray.value = collections.value
+    }
   })
 
   return { collections }
