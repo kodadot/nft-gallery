ce8c16d3cbb5069f0deea364233647b984c23479
Matej yangwao <5887929+yangwao@users.noreply.github.com>
2023-10-26 15:00:25 +0200

    Update components/collection/drop/Generative.vue
    Co-authored-by: roiLeo <medina.leo42@gmail.com>

--- components/collection/drop/Generative.vue
+++ components/collection/drop/Generative.vue
@@ -95,7 +95,7 @@
             target="_blank"
             variant="secondary"
             class="mt-2">
-            Learn More
+            {{ $t('helper.learnMore') }}
           </NeoButton>
         </div>
         <div class="column">
