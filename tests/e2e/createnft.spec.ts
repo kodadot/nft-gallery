223a4abf7ac1f49c8a1bf6f969359b188a2cf5a7
prury <yco.arq@gmail.com>
2023-11-03 15:56:44 -0300

    create nft test

---
+++ tests/e2e/createnft.spec.ts
@@ -0,0 +1,95 @@
+import { expect, test } from './fixtures'
+
+const prefix = ['ahk', 'ahp', 'ksm']
+
+test('Create NFT', async ({ page, Commands }) => {
+  await Commands.e2elogin()
+  //await page.goto('/ahk/create')
+  await page.goto(`/${prefix[2]}/create`)
+  await page.getByTestId('create-landing-nft-button').click()
+  await expect(page).toHaveURL(`/${prefix[2]}/create?select=nft`)
+  //Single NFT
+  await test.step('Single NFT', async () => {
+    await Commands.acceptCookies()
+    //select single nft creation
+    await page.getByTestId('create-landing-single-nft-button').click()
+    //upload NFT image
+    await page.setInputFiles(
+      'input[type="file"]',
+      'tests/e2e/unsplash-image.jpg',
+    )
+    //set name
+    await page
+      .getByTestId('create-nft-input-name')
+      .fill('Parametric NFT divergencies')
+    //set description
+    await page
+      .getByTestId('create-nft-input-description')
+      .fill('Testing purpose NFT description using Playwright')
+    //select AHK chain on dropdown
+    await page
+      .getByTestId('create-nft-dropdown-select')
+      .selectOption('KusamaHub [Beta]')
+    await expect(page.getByTestId('chain')).toHaveText('ahk')
+    //list for sale switch
+    await page.getByTestId('create-nft-sale-switch').click()
+    //set price
+    await page.getByTestId('create-nft-input-list-value').fill('5.5')
+    //set number of copies
+    await page.getByTestId('create-nft-input-copies').fill('5')
+    await expect(
+      page.getByTestId('create-nft-input-copies-switch'),
+    ).toBeVisible()
+    //activate hastag switch
+    await page.getByTestId('create-nft-input-copies-switch').click()
+    // - - NFT attributes interactions - -
+    //open NFT attribute section
+    await page.getByTestId('create-nft-properties').click()
+    //add new attribute
+    await page.getByTestId('button-add-attribute').click()
+    //fill section
+    await page.getByTestId('attr-0').getByPlaceholder('Section').fill('BG')
+    //fill value
+    await page.getByTestId('attr-0').getByPlaceholder('Value').fill('Red')
+    //add new attribute
+    await page.getByTestId('button-add-attribute').click()
+    await page.getByTestId('attr-1').getByPlaceholder('Section').fill('Dress')
+    await page.getByTestId('attr-1').getByPlaceholder('Value').fill('Blue')
+    //remove last attribute
+    await page.getByTestId('attribute-button-remove').last().click()
+    await expect(page.getByTestId('attr-1')).not.toBeVisible()
+    //royalty
+    const royaltyFields = page
+      .getByTestId('royalty-form-input-percentage')
+      .locator('input')
+    await royaltyFields.fill('5')
+    //royalty switch
+    await page.getByTestId('royalty-form-switch').click()
+    //get user address
+    const royaltyCustom = page
+      .getByTestId('royalty-form-custom-address')
+      .locator('input')
+    await expect(royaltyCustom).toBeVisible()
+    //NSFW switch
+    await page.getByTestId('create-nft-nsfw-switch').click()
+    //deposit check
+    await expect(page.getByTestId('create-nft-deposit-amount')).toHaveText(
+      '0.0077 KSM',
+      { timeout: 30000 },
+    )
+    //preview box
+    await expect(page.getByTestId('create-nft-preview-box')).toBeVisible()
+    expect(
+      (await page.getByTestId('nft-preview-name').innerText()).valueOf(),
+    ).toBe('Parametric NFT divergencies')
+    expect(
+      (await page.getByTestId('nft-preview-price').innerText()).valueOf(),
+    ).toContain('5.5')
+    //learn more link
+    await expect(page.getByTestId('create-nft-learn-more-link')).toBeVisible()
+    await Commands.checkNewTab(
+      'https://hello.kodadot.xyz/multi-chain/fees',
+      page.getByTestId('create-nft-learn-more-link').click(),
+    )
+  })
+})
