e94568e82406c74bec5d8fd0939479cb980a87c7
Jarsen <31397967+Jarsen136@users.noreply.github.com>
2023-11-19 23:25:38 +0800

    fix: nft card

--- components/items/ItemsGrid/ItemsGridImage.vue
+++ components/items/ItemsGrid/ItemsGridImage.vue
@@ -171,8 +171,7 @@ const onClickListingCart = () => {
 .in-cart-border {
   @include ktheme() {
     outline: 2px solid theme('k-blue') !important;
-    outline-offset: -2px;
-    border-width: 2px !important;
+    outline-offset: -1px;
     border-color: transparent !important;
     background-color: theme('blue-light-cards');
   }
