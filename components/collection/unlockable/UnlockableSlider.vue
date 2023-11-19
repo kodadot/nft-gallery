41568a99c9a5ff0dec31b650876b6528fcf81811
Shashkov Danil <shashkovdanil@gmail.com>
2023-11-19 12:30:39 +0000

    fixed slider borders

--- components/collection/unlockable/UnlockableSlider.vue
+++ components/collection/unlockable/UnlockableSlider.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="unlockable-slider border is-flex is-flex-1 slider-bar">
+  <div class="unlockable-slider border slider-bar">
     <div
       v-if="value > 0"
-      class="unlockable-slider-inner border slider-bar"
-      :style="widthStyle"></div>
+      class="unlockable-slider-inner slider-bar"
+      :style="style"></div>
   </div>
 </template>
 
@@ -17,26 +17,33 @@ const props = withDefaults(
   },
 )
 
-const widthStyle = computed(() => ({ width: `${props.value * 100}%` }))
+const style = computed(() => {
+  const progress = props.value * 100
+
+  return {
+    width: `${progress}%`,
+    'border-right': progress === 100 ? 'none' : '',
+  }
+})
 </script>
 
 <style lang="scss" scoped>
 @import '@/assets/styles/abstracts/variables.scss';
 .unlockable-slider {
   position: relative;
+  height: 14px;
+  overflow: hidden;
   @include ktheme() {
     background: theme('background-color');
   }
   &-inner {
-    position: absolute;
-    left: -1px;
-    top: -1px;
+    height: 100%;
     background: $primary;
+    border-right: inherit;
   }
 }
 
 .slider-bar {
   border-radius: 2.5rem;
-  height: 14px;
 }
 </style>
