86492da8601f88da48941412360a1af8005e25b5
roiLeo <medina.leo42@gmail.com>
2023-11-23 13:52:34 +0100

    🔧 drops view collection button

--- components/collection/unlockable/UnlockableCollectionInfo.vue
+++ components/collection/unlockable/UnlockableCollectionInfo.vue
@@ -18,27 +18,15 @@
           class="no-shadow is-text has-text-left p-0 is-underlined"
           :label="seeAllDescription ? $t('showLess') : $t('showMore')"
           @click="toggleSeeAllDescription" />
-        <div v-else />
-
-        <nuxt-link :to="`/${urlPrefix}/collection/${collectionId}`">
-          <NeoButton
-            variant="secondary"
-            tag="span"
-            target="_blank"
-            class="has-text-left is-flex is-align-items-center">
-            <span>View Collection</span>
-            <svg
-              width="12"
-              height="12"
-              viewBox="0 0 12 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M1.41708 6.95833L0.95875 6.95833L0.95875 6.04167L1.41708 6.04167L1.41708 6.95833ZM10.9078 6.1759C11.0868 6.35489 11.0868 6.64509 10.9078 6.82408L7.99103 9.7409C7.81204 9.91989 7.52183 9.91989 7.34284 9.7409C7.16385 9.56191 7.16385 9.27171 7.34284 9.09272L9.93557 6.49999L7.34284 3.90727C7.16385 3.72828 7.16385 3.43808 7.34284 3.25909C7.52183 3.0801 7.81204 3.0801 7.99103 3.25909L10.9078 6.1759ZM1.41708 6.04167L10.5838 6.04166L10.5838 6.95833L1.41708 6.95833L1.41708 6.04167Z"
-                fill="currentColor" />
-            </svg>
-          </NeoButton>
-        </nuxt-link>
+        <NeoButton
+          variant="secondary"
+          rounded
+          :tag="NuxtLink"
+          :to="`/${urlPrefix}/collection/${collectionId}`"
+          target="_blank"
+          icon="arrow-right">
+          <span>View Collection</span>
+        </NeoButton>
       </div>
     </div>
     <div>
