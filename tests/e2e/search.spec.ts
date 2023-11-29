555ccccd81ca47f5b5f7f5372036ca533a50af4b
prury <yco.arq@gmail.com>
2023-11-29 15:21:30 -0300

    search readability and improvements

--- tests/e2e/search.spec.ts
+++ tests/e2e/search.spec.ts
@@ -1,31 +1,40 @@
 import { expect, test } from './fixtures'
 
-function imagesHaveLoaded() {
-  return Array.from(document.images).every((i) => i.complete)
-}
-
 test('Check if search provide results', async ({ page }) => {
-  //page.route('**', route => route.continue())
-  await page.goto('/ksm')
-  const searchBar = page.getByTestId('search-bar')
-  await searchBar.locator('input').fill('waifu')
-  await page.locator('.search-suggestion-container').hover()
-  //check contents of Collection Tab
-  const tabCollection = page.getByTestId('collection-tab')
-  await expect(tabCollection.locator('.neo-skeleton-item').first()).toBeVisible(
-    { timeout: 30000 },
-  )
-  await expect(
-    tabCollection.locator('.search-result-item').first(),
-  ).toBeVisible({ timeout: 30000 })
-  await expect(tabCollection.locator('.link-item').first()).toBeVisible({
-    timeout: 30000,
+  await test.step('Go to landing within Kusama chain', async () => {
+    await page.goto('/ksm')
   })
-  await page.waitForFunction(imagesHaveLoaded, { timeout: 30000 })
-  //switchs and check NFT results
-  await page.locator('div[aria-controls="NFTs-content"]').click()
-  const tabNft = page.getByTestId('nft-tab')
-  await expect(tabNft.locator('.search-result-item').first()).toBeVisible({
-    timeout: 30000,
+
+  //Search term
+  await test.step('Search for the term Waifu and hover over results', async () => {
+    const searchBar = page.getByTestId('search-bar')
+    await searchBar.locator('input').fill('waifu')
+    await page.locator('.search-suggestion-container').hover()
+  })
+
+  //check contents of Collection Tab
+  await test.step('Switches to Collection tab and wait for results to load', async () => {
+    const tabCollection = page.getByTestId('collection-tab')
+    await expect(
+      tabCollection.locator('.neo-skeleton-item').first(),
+    ).not.toBeVisible({ timeout: 30000 })
+    await expect(
+      tabCollection.locator('.search-result-item').first(),
+    ).toBeVisible({ timeout: 30000 })
+    await expect(tabCollection.locator('.link-item').first()).toBeVisible({
+      timeout: 30000,
+    })
+  })
+
+  //Switchs to NFT tab
+  await test.step('Switches to NFT tab and wait for results to load', async () => {
+    await page.locator('div[aria-controls="NFTs-content"]').click()
+    const tabNft = page.getByTestId('nft-tab')
+    await expect(tabNft.locator('.neo-skeleton-item').first()).not.toBeVisible({
+      timeout: 30000,
+    })
+    await expect(tabNft.locator('.search-result-item').first()).toBeVisible({
+      timeout: 30000,
+    })
   })
 })
