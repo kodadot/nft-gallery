7dddc6c68b680ea28cc476f945f5ea28a64d50f4
hassnian <hassnianxd@gmail.com>
2023-12-04 17:54:32 +0500

    ref: getTokenIconBySymbol useIcon

--- composables/useIcon.ts
+++ composables/useIcon.ts
@@ -1,18 +1,18 @@
+const getTokenIconBySymbol = (token: string) => {
+  switch (token.toLowerCase()) {
+    case 'bsx':
+      return '/token/bsx.svg'
+    case 'dot':
+      return '/token/dot.svg'
+    case 'ksm':
+      return '/token/ksm.svg'
+    default:
+      return '/token/ksm.svg'
+  }
+}
+
 export const useIcon = () => {
   const { isDarkMode } = useTheme()
-
-  const getTokenIconBySymbol = (token: string) => {
-    switch (token.toLowerCase()) {
-      case 'bsx':
-        return '/token/bsx.svg'
-      case 'dot':
-        return '/token/dot.svg'
-      case 'ksm':
-        return '/token/ksm.svg'
-      default:
-        return '/token/ksm.svg'
-    }
-  }
 
   const signUpVoucherIcon = computed(() =>
     isDarkMode.value ? '/signup-voucher-dark.svg' : '/signup-voucher.svg',
