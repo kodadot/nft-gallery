7faf9f9b929b7621c2d08e958da79b236fd0fb7e
newraina <newraina@users.noreply.github.com>
2023-12-06 20:43:01 +0900

    Add neutral color palette to tailwind config

--- libs/ui/tailwind.config.js
+++ libs/ui/tailwind.config.js
@@ -50,6 +50,22 @@ module.exports = {
         'separator-line-color': 'var(--separator-line-color)',
         'toggle-primary': 'var(--toggle-primary)',
         'toggle-active-switch': 'var(--toggle-active-switch)',
+
+        neutral: {
+          1: '#ffffff',
+          2: '#fcfcfc',
+          3: '#f5f5f5',
+          4: '#f0f0f0',
+          5: '#d9d9d9',
+          6: '#bfbfbf',
+          7: '#8c8c8c',
+          8: '#595959',
+          9: '#454545',
+          10: '#262626',
+          11: '#1f1f1f',
+          12: '#141414',
+          13: '#000000',
+        },
       },
       boxShadow: {
         primary: 'var(--primary-shadow)',
