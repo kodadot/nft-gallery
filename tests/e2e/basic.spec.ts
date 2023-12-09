41c2a62b3e6c25ba2456a322639c149d7c549ea5
Danil Shashkov <shashkovdanil@gmail.com>
2023-12-09 14:28:12 +0000

    Fixed basic test

--- tests/e2e/basic.spec.ts
+++ tests/e2e/basic.spec.ts
@@ -15,7 +15,7 @@ test('Landing Elements', async ({ page }) => {
     await page.getByTestId(data).click()
     await expect(page.getByTestId('drops')).toBeVisible()
     await expect(page.getByTestId('explore').last()).toBeVisible()
-    await expect(page.getByTestId('search-bar')).toBeVisible()
+    await expect(page.getByTestId('search-bar').last()).toBeVisible()
     await expect(page.getByTestId('footer-container')).toBeVisible()
     await expect(page.getByTestId('navbar-profile-dropdown')).toBeVisible()
   }
