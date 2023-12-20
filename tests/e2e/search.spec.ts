84dc37a635202ea7982670bf59489418f5ef6d0d
Jarsen <31397967+Jarsen136@users.noreply.github.com>
2023-12-20 14:08:18 +0800

    Revert "fix: landing test"
    This reverts commit 80e8724084f478b3bad849d0eb0788d31c6fc2ef.

--- tests/e2e/search.spec.ts
+++ tests/e2e/search.spec.ts
@@ -5,6 +5,13 @@ const LANDING_PATH = '/ksm'
 test('Check if search provide results', async ({ page }) => {
   await page.goto(LANDING_PATH)
 
+  //Search term
+  await test.step('Search for the term Waifu and hover over results', async () => {
+    const searchBar = page.getByTestId('search-bar')
+    await searchBar.locator('input').fill('waifu')
+    await page.locator('.search-suggestion-container').hover()
+  })
+
   //check contents of Collection Tab
   await test.step('Switches to Collection tab and wait for results to load', async () => {
     const tabCollection = page.getByTestId('collection-tab')
