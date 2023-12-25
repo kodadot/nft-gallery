e6cb7c8121ebf96d271aceee8629414b433b39de
Viki Val <viktorko99@gmail.com>
2023-12-25 20:00:57 +0100

    :wastebasket: :snake: constructing attributes  not possible

--- composables/transaction/mintCollection/constructMeta.ts
+++ composables/transaction/mintCollection/constructMeta.ts
@@ -1,20 +1,10 @@
-import { ActionMintCollection, CollectionToMintBasilisk } from '../types'
+import { ActionMintCollection } from '../types'
 import { createMetadata, unSanitizeIpfsUrl } from '@kodadot1/minimark/utils'
 import { pinJson } from '@/services/nftStorage'
 import { uploadDirect } from '@/utils/directUpload'
 import { getImageTypeSafe, pinImageSafe } from '@/utils/safePin'
 
-const createAttributes = (item: ActionMintCollection) => {
-  if (item.urlPrefix === 'bsx') {
-    return (item.collection as CollectionToMintBasilisk).tags.map((val) => ({
-      ...val,
-      display_type: null,
-    }))
-  }
-  if (item.urlPrefix === 'rmrk' || item.urlPrefix === 'ksm') {
-    return []
-  }
-
+const createAttributes = (_item: ActionMintCollection) => {
   return []
 }
 
