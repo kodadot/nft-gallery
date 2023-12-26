cfaf2245810774f8ebb3bf1bbd0313b97ab8e877
hassnian <hassnianxd@gmail.com>
2023-12-26 15:57:39 +0500

    ref: use useSearchNfts

--- composables/useSearchNfts.ts
+++ composables/useSearchNfts.ts
@@ -1,18 +1,20 @@
-export const queryPrefix = {
-  ksm: 'chain-ksm',
-  rmrk: 'chain-rmrk',
-}
-
 export default function useSearchNfts({
   search,
   first = 10,
+  offset = 0,
   prefix = '',
   orderBy = 'blockNumber_DESC',
+  denyList = [] as string[],
+  immediate = true,
 }) {
   const { client } = usePrefix()
   const chainPrefix = prefix || client.value
+  const queryPrefix = {
+    ksm: 'chain-ksm',
+    rmrk: 'chain-rmrk',
+  }
 
-  const { data } = useGraphql({
+  const { data, refetch, loading } = useGraphql({
     queryPrefix: queryPrefix[chainPrefix] || client.value,
     clientName: chainPrefix,
     queryName: 'nftListWithSearch',
@@ -20,13 +22,18 @@ export default function useSearchNfts({
       first,
       search,
       orderBy,
+      denyList,
+      offset,
     },
     options: {
       fetchPolicy: 'cache-first',
     },
+    disabled: computed(() => !immediate),
   })
 
   return {
     data,
+    refetch,
+    loading,
   }
 }
