d45adb2594136e57e83c3f3916bcd7be06320f94
Viki Val <viktorko99@gmail.com>
2023-12-27 19:13:58 +0100

    :coffin: dead functions

--- components/bsx/Create/utils.ts
+++ components/bsx/Create/utils.ts
@@ -1,20 +1,7 @@
-import { useFiatStore } from '@/stores/fiat'
-
-export type Token = 'BSX' | 'KSM'
-
-export const ksmToBsx = (value: number): number => {
-  const fiatStore = useFiatStore()
-  const KSMToUsd = fiatStore.getCurrentKSMValue
-  const BSXToUsd = fiatStore.getCurrentBSXValue
-  return value * (Number(KSMToUsd) / Number(BSXToUsd))
-}
+export type Token = 'DOT' | 'KSM'
 
 export const getBalance = (_token?: Token): number => {
   const { balance } = useAuth()
 
   return Number(balance.value)
-}
-
-export const getDeposit = (token: Token, depositInKSM: number): number => {
-  return token === 'KSM' ? depositInKSM : ksmToBsx(depositInKSM)
 }
