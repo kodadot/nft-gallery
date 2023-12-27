4275370f8ef93e576d2ad03ff3d6cea8a1221b52
Viki Val <viktorko99@gmail.com>
2023-12-27 18:20:08 +0100

    :wastebasket: removed bsx in Gallery item  tabs

--- components/gallery/GalleryItemTabsPanel/GalleryItemTabsPanel.vue
+++ components/gallery/GalleryItemTabsPanel/GalleryItemTabsPanel.vue
@@ -27,12 +27,12 @@
         </div>
       </template>
 
-      <GalleryItemOffers
-        v-if="isBasilisk && nft?.collection.id && nft?.id && nft.currentOwner"
+      <!-- <GalleryItemOffers
+        v-if="isAssetHub && nft?.collection.id && nft?.id && nft.currentOwner"
         :collection-id="nft?.collection.id"
         :nft-id="nft?.id"
         :account="nft?.currentOwner" />
-      <div v-else></div>
+      <div v-else></div> -->
     </NeoTabItem>
 
     <!-- activity -->
@@ -56,7 +56,7 @@ import { NeoTabItem, NeoTabs, NeoTooltip } from '@kodadot1/brick'
 import { GalleryItem } from '../useGalleryItem'
 import GalleryItemActivity from './GalleryItemActivity.vue'
 import GalleryItemChart from './GalleryItemChart.vue'
-import GalleryItemOffers from './GalleryItemOffers.vue'
+// import GalleryItemOffers from './GalleryItemOffers.vue'
 
 const props = withDefaults(
   defineProps<{
@@ -68,14 +68,15 @@ const props = withDefaults(
   },
 )
 
-const { urlPrefix } = usePrefix()
+// TODO: enable once offers are implemented
+// const { urlPrefix } = usePrefix()
 const nft = computed(() => props.galleryItem.nft.value)
 const { offersDisabled } = useChain()
 
 const activeTab = ref('0')
 const collectionId = ref('')
 
-const { isBasilisk } = useIsChain(urlPrefix)
+// const { isAssetHub } = useIsChain(urlPrefix)
 
 watchEffect(() => {
   if (props.activeTab) {
