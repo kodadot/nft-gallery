04f97e9ea0b850e041a5c4c922a6210841e09eb4
Viki Val <viktorko99@gmail.com>
2023-12-27 18:21:49 +0100

    :wastebasket: removed bsx in useDeposit

--- composables/useDeposit.ts
+++ composables/useDeposit.ts
@@ -1,20 +1,15 @@
 import type { ComputedRef } from 'vue'
-import type { PalletBalancesAccountData } from '@polkadot/types/lookup'
 
 import { decodeAddress, encodeAddress } from '@polkadot/util-crypto'
 import { CHAINS, type Prefix } from '@kodadot1/static'
 import { balanceOf } from '@kodadot1/sub-api'
-import {
-  getAssetMetadataByAccount,
-  getKusamaAssetId,
-} from '@/utils/api/bsx/query'
 import format from '@/utils/format/balance'
 import { chainPropListOf } from '@/utils/config/chain.config'
 
 export default function (prefix: ComputedRef<Prefix>) {
   const { apiInstanceByPrefix } = useApi()
   const { accountId } = useAuth()
-  const { isBasilisk, isAssetHub } = useIsChain(prefix)
+  const { isAssetHub } = useIsChain(prefix)
 
   const balance = ref()
 
@@ -47,14 +42,6 @@ export default function (prefix: ComputedRef<Prefix>) {
         metadataDeposit.value = api.consts.nfts.metadataDepositBase.toNumber()
       }
 
-      if (isBasilisk.value) {
-        collectionDeposit.value =
-          api.consts.uniques.collectionDeposit.toNumber()
-        itemDeposit.value = api.consts.uniques.collectionDeposit.toNumber()
-        metadataDeposit.value =
-          api.consts.uniques.metadataDepositBase.toNumber()
-      }
-
       totalCollectionDeposit.value = format(
         metadataDeposit.value +
           collectionDeposit.value +
@@ -78,15 +65,6 @@ export default function (prefix: ComputedRef<Prefix>) {
       const chainInfo = await api.registry.getChainProperties()
       chainSymbol.value = chainInfo?.tokenSymbol.toHuman()?.[0]
 
-      if (isBasilisk.value && accountId.value) {
-        const assetMetadata = await getAssetMetadataByAccount(
-          api,
-          accountId.value,
-        )
-
-        chainSymbol.value = assetMetadata.symbol
-      }
-
       // set balance
       if (accountId.value) {
         const chain = CHAINS[prefix.value]
@@ -94,15 +72,6 @@ export default function (prefix: ComputedRef<Prefix>) {
         const prefixAddress = encodeAddress(publicKey, chain.ss58Format)
 
         balance.value = await balanceOf(api, prefixAddress)
-
-        if (isBasilisk.value && chainSymbol.value === 'KSM') {
-          balance.value = (
-            (await api.query.tokens.accounts(
-              prefixAddress,
-              getKusamaAssetId(prefix.value),
-            )) as PalletBalancesAccountData
-          ).free.toString()
-        }
 
         balance.value = format(balance.value, chain.tokenDecimals, false)
       }
