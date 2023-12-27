13e36dceb735bfe8f823b1811c07d91e9225dc9a
Viki Val <viktorko99@gmail.com>
2023-12-27 19:08:48 +0100

    :coffin: basilisk extrinsics

--- utils/api/bsx/extrinsics.ts
+++
@@ -1,9 +0,0 @@
-import { ExtrinsicCall } from './types'
-import type { ApiPromise } from '@polkadot/api'
-
-export function setDefaultFeeToken(api: ApiPromise, id: string): ExtrinsicCall {
-  return {
-    call: api.tx.multiTransactionPayment.setCurrency,
-    args: [id],
-  }
-}
