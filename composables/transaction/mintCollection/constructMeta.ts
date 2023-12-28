8a975c5c81172742e50050f11355dd45455fcecd
Viki Val <viktorko99@gmail.com>
2023-12-28 10:31:13 +0100

    :wastebasket: do not costruct metadata attributes

--- composables/transaction/mintCollection/constructMeta.ts
+++ composables/transaction/mintCollection/constructMeta.ts
@@ -4,17 +4,13 @@ import { pinJson } from '@/services/nftStorage'
 import { uploadDirect } from '@/utils/directUpload'
 import { getImageTypeSafe, pinImageSafe } from '@/utils/safePin'
 
-const createAttributes = (_item: ActionMintCollection) => {
-  return []
-}
-
 export async function constructMeta(item: ActionMintCollection) {
   const { file, name, description } = item.collection
 
   const { $consola } = useNuxtApp()
   const imageHash = await pinImageSafe(file)
   const type = getImageTypeSafe(file)
-  const attributes = createAttributes(item)
+  const attributes = [] // createAttributes(item)
 
   const meta = createMetadata(
     name,
