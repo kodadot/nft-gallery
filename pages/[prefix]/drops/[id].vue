70ec56b2899cc17fbc5a2e9d462dd4831bc7e0ea
Jarsen <31397967+Jarsen136@users.noreply.github.com>
2024-01-04 19:56:32 +0800

    fix: disabled drop details page on ahk

--- pages/[prefix]/drops/[id].vue
+++ pages/[prefix]/drops/[id].vue
@@ -14,6 +14,7 @@
 <script lang="ts" setup>
 import { useDrop } from '@/components/drops/useDrops'
 import UnlockableCollectionBanner from '@/components/collection/unlockable/UnlockableCollectionBanner.vue'
+import { isProduction } from '@/utils/chain'
 
 definePageMeta({
   layout: 'unlockable-mint-layout',
@@ -26,6 +27,10 @@ const drop = await useDrop(params.id.toString())
 const dropType = computed(() => drop?.type)
 
 onMounted(() => {
+  if (drop?.chain === 'ahk' && isProduction) {
+    useRouter().push('/')
+    return
+  }
   if (drop?.chain && urlPrefix.value !== drop?.chain) {
     setUrlPrefix(drop?.chain)
     redirectAfterChainChange(drop?.chain)
