5dd1ce6d2f83b935efa0e0b0b6ac2b29e7979946
roiLeo <medina.leo42@gmail.com>
2024-01-10 08:42:37 +0100

    🚀 release new squid version

--- libs/static/src/indexers.ts
+++ libs/static/src/indexers.ts
@@ -8,9 +8,11 @@ type SquidEndpoint =
 export const INDEXERS: Config<SquidEndpoint> = {
   rmrk: 'https://squid.subsquid.io/rubick/graphql',
   ksm: 'https://squid.subsquid.io/marck/v/v2/graphql',
-  ahk: 'https://query-stick.stellate.sh',
+  // ahk: 'https://query-stick.stellate.sh',
+  ahk: 'https://squid.subsquid.io/stick/v/v9/graphql',
   dot: 'https://squid.subsquid.io/rubick/graphql', // TODO: change to dot indexer when available
-  ahp: 'https://squid.subsquid.io/speck/graphql',
+  // ahp: 'https://squid.subsquid.io/speck/graphql',
+  ahp: 'https://squid.subsquid.io/speck/v/v10/graphql',
   // ahr: 'https://squid.subsquid.io/snack/graphql',
   // movr: 'https://squid.subsquid.io/antick/v/001-rc0/graphql',
   // glmr: 'https://squid.subsquid.io/click/v/002/graphql',
