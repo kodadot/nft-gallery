17f5d5d349b75da6a25dd4aa92642149274cbe4c
hassnian <hassnianxd@gmail.com>
2024-01-15 11:08:57 +0500

    add: burn nft add new sign modal

--- components/gallery/GalleryItemButton/GalleryItemMoreActionBtn.vue
+++ components/gallery/GalleryItemButton/GalleryItemMoreActionBtn.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <Loader v-model="isLoading" :status="status" />
+    <SigningModal
+      v-model="isLoading"
+      :status="status"
+      :title="$t('mint.nft.burning')" />
+
     <NeoDropdown position="bottom-left" :mobile-modal="false">
       <template #trigger="{ active }">
         <NeoButton
@@ -32,7 +36,6 @@ import { NeoButton, NeoDropdown, NeoDropdownItem } from '@kodadot1/brick'
 import { Interaction } from '@kodadot1/minimark/v1'
 import { downloadImage } from '@/utils/download'
 import { toOriginalContentUrl } from '@/utils/ipfs'
-import Loader from '@/components/shared/Loader.vue'
 import { isMobileDevice } from '@/utils/extension'
 
 const { $i18n, $consola } = useNuxtApp()
