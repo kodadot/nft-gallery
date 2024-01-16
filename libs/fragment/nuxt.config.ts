cf4c834f1ad6827b347b673e28e5535a4c2acfad
Preschian Febryantara <preschian@users.noreply.github.com>
2024-01-16 19:41:13 +0700

    custom colour

--- libs/fragment/nuxt.config.ts
+++ libs/fragment/nuxt.config.ts
@@ -1,4 +1,10 @@
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: [join(currentDir, './assets/css/main.scss')],
 })
