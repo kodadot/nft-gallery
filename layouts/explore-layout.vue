7988e1cecfc8bc7baa93ffc4e26cc2c9b5b7eaf6
Preschian Febryantara <preschian@users.noreply.github.com>
2024-01-18 10:07:17 +0700

    fix: lockfile and linter

--- layouts/explore-layout.vue
+++ layouts/explore-layout.vue
@@ -66,8 +66,8 @@ useHead({
 })
 
 const isExplore = computed(() => route.path.includes('/explore'))
-const isCollection = computed(
-  () => route.name?.toString().includes('prefix-collection-id'),
+const isCollection = computed(() =>
+  route.name?.toString().includes('prefix-collection-id'),
 )
 
 const getExploreTitle = computed(() => {
