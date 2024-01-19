28b557fffafdffae5442fd3284b2f685bae8b1a9
hassnian <hassnianxd@gmail.com>
2024-01-19 14:04:03 +0500

    ref: use with tailwind classes

--- components/shared/SigningModal/SigningModalBody.vue
+++ components/shared/SigningModal/SigningModalBody.vue
@@ -5,10 +5,8 @@
 
   <slot>
     <div class="py-5 flex items-start">
-      <NeoIcon icon="lightbulb" size="small" class="mr-2 is-block" />
-      <p
-        v-dompurify-html="$t('autoTeleport.tip')"
-        class="is-size-7 capitalize" />
+      <NeoIcon icon="lightbulb" size="small" class="mr-2 !block" />
+      <p v-dompurify-html="$t('autoTeleport.tip')" class="text-xs capitalize" />
     </div>
   </slot>
 
