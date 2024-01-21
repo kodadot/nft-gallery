c9c98e0df98baaba94fad2d23d53fbe108c9f23f
hassnian <hassnianxd@gmail.com>
2024-01-21 10:09:34 +0500

    add: try again signing modal burn all nfts

--- components/collection/HeroButtonDeleteNfts.vue
+++ components/collection/HeroButtonDeleteNfts.vue
@@ -1,8 +1,9 @@
 <template>
   <SigningModal
-    v-model="isLoading"
+    :title="$t('moreActions.deletingNfts')"
+    :is-loading="isLoading"
     :status="status"
-    :title="$t('moreActions.deletingNfts')" />
+    @try-again="deleteNfts" />
 
   <NeoDropdownItem @click="deleteNfts()">
     {{ $i18n.t('moreActions.deleteNfts') }}
@@ -13,20 +14,27 @@
 import { NFTs } from '@/composables/transaction/types'
 import { NeoDropdownItem } from '@kodadot1/brick'
 
-const route = useRoute()
-const { $i18n } = useNuxtApp()
-const { transaction, status } = useTransaction()
-const { urlPrefix } = usePrefix()
-const { accountId } = useAuth()
-
-const isLoading = ref(false)
-const id = route.params.id.toString()
-
 type NftIds = {
   nfts?: {
     id: string
   }[]
 }
+
+const route = useRoute()
+const { $i18n } = useNuxtApp()
+const { urlPrefix } = usePrefix()
+const { accountId } = useAuth()
+
+const {
+  transaction,
+  status,
+  isLoading: isTransactionLoading,
+  blockNumber,
+} = useTransaction()
+
+const id = route.params.id.toString()
+const isLoading = ref(false)
+const unsubscribeSubscription = ref(() => {})
 
 const { data } = useGraphql({
   queryName: 'nftIdListByCollection',
@@ -37,18 +45,20 @@ const { data } = useGraphql({
 })
 
 const deleteNfts = async () => {
-  isLoading.value = true
   const nfts = (data.value as NftIds).nfts
   const ids = nfts?.map((nft) => nft.id)
 
   if (ids?.length) {
+    isLoading.value = true
+
     await transaction({
       interaction: NFTs.BURN_MULTIPLE,
       nftIds: ids,
       urlPrefix: urlPrefix.value,
     })
 
-    useSubscriptionGraphql({
+    unsubscribeSubscription.value()
+    unsubscribeSubscription.value = useSubscriptionGraphql({
       query: `
       nftEntities(where: {collection: {id_eq: "${id}"}, burned_eq: false}) {
         id
@@ -66,4 +76,10 @@ const deleteNfts = async () => {
     })
   }
 }
+
+watch([isTransactionLoading, blockNumber], ([loading, block]) => {
+  if (!loading && !block) {
+    isLoading.value = false
+  }
+})
 </script>
