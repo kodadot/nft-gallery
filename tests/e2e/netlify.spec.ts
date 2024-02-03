d4144c5d56c0c980a1db84cbcb295f88d4ff77ec
prury <yco.arq@gmail.com>
2024-02-03 16:53:51 -0300

    check netlify preview inside PR

--- tests/e2e/netlify.spec.ts
+++ tests/e2e/netlify.spec.ts
@@ -1,13 +1,13 @@
 import { expect, test } from './fixtures'
 
 test('Check if netlify deploy preview was built', async ({ page, baseURL }) => {
+  const pr_number = process.env.PR_NUMBER
+  await page.goto(`https://github.com/kodadot/nft-gallery/pull/${pr_number}`)
+  await expect(
+    page.getByText(
+      `https://deploy-preview-${pr_number}--koda-canary.netlify.app`,
+    ),
+  ).toBeVisible({ timeout: 180000 })
   await page.goto(`${baseURL}`)
   await expect(page.getByText('Polkadot NFT')).toBeVisible()
-  /*
-  const responsePromise = page.waitForResponse(
-    (response) => response.url() === `${baseURL}` && response.status() === 200,
-  )
-  const response = await responsePromise
-  await response
-  */
 })
