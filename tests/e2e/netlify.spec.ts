35509008a9691b842a916922bd4bfce927a5baa5
prury <yco.arq@gmail.com>
2024-02-03 13:45:15 -0300

    simplify netlify test

--- tests/e2e/netlify.spec.ts
+++ tests/e2e/netlify.spec.ts
@@ -1,10 +1,13 @@
-import { test } from './fixtures'
+import { expect, test } from './fixtures'
 
 test('Check if netlify deploy preview was built', async ({ page, baseURL }) => {
   await page.goto(`${baseURL}`)
+  await expect(page.getByText('Polkadot NFT')).toBeVisible()
+  /*
   const responsePromise = page.waitForResponse(
     (response) => response.url() === `${baseURL}` && response.status() === 200,
   )
   const response = await responsePromise
   await response
+  */
 })
