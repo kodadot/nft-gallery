38d0172c3d8339ab54f56ebc567eccb78a0a80a5
prury <yco.arq@gmail.com>
2024-02-03 13:16:24 -0300

    netlify test

---
+++ tests/e2e/netlify.spec.ts
@@ -0,0 +1,10 @@
+import { test } from './fixtures'
+
+test('Check if netlify deploy preview was built', async ({ page, baseURL }) => {
+  await page.goto(`${baseURL}`)
+  const responsePromise = page.waitForResponse(
+    (response) => response.url() === `${baseURL}` && response.status() === 200,
+  )
+  const response = await responsePromise
+  await response
+})
