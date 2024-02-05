6e3891c894508d06153ad9732518ee968453cb82
prury <yco.arq@gmail.com>
2024-02-05 12:57:18 -0300

    test refactor to search only for visible

--- tests/e2e/search.spec.ts
+++ tests/e2e/search.spec.ts
@@ -7,14 +7,20 @@ test('Check if search provide results', async ({ page }) => {
 
   //Search term
   await test.step('Search for the term Waifu and hover over results', async () => {
-    await expect(page.getByTestId('search-bar').last()).toBeVisible()
-    await page.getByTestId('search-bar-input').fill('waifu')
-    await page.locator('[class="search-suggestion-container"]').hover()
+    await expect(
+      page.locator('[data-testid="search-bar"] >> visible = true'),
+    ).toBeVisible()
+    await page
+      .locator('[data-testid="search-bar-input"] >> visible = true')
+      .fill('waifu')
+    await page
+      .locator('[class="search-suggestion-container"] >> visible = true')
+      .hover()
   })
 
   //check contents of Collection Tab
   await test.step('Switches to Collection tab and wait for results to load', async () => {
-    const tabCollection = page.getByTestId('collection-tab')
+    const tabCollection = page.getByTestId('collection-tab').last()
     await expect(
       tabCollection.locator('.neo-skeleton-item').first(),
     ).not.toBeVisible({ timeout: 30000 })
@@ -28,12 +34,14 @@ test('Check if search provide results', async ({ page }) => {
 
   //Switchs to NFT tab
   await test.step('Switches to NFT tab and wait for results to load', async () => {
-    await page.locator('div[aria-controls="NFTs-content"]').click()
+    await page.locator('div[aria-controls="NFTs-content"]').last().click()
     const tabNft = page.getByTestId('nft-tab')
     await expect(tabNft.locator('.neo-skeleton-item').first()).not.toBeVisible({
       timeout: 30000,
     })
-    await expect(tabNft.locator('.search-result-item').first()).toBeVisible({
+    await expect(
+      tabNft.locator('.search-result-item >> visible = true').first(),
+    ).toBeVisible({
       timeout: 30000,
     })
   })
