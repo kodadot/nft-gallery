21c775dd49d2b7e8791bbc6d06e3bcc2edb15099
prury <yco.arq@gmail.com>
2024-02-05 19:17:35 -0300

    test timeout 2

--- tests/e2e/netlify.spec.ts
+++ tests/e2e/netlify.spec.ts
@@ -1,6 +1,7 @@
 import { expect, test } from './fixtures'
 
 test('Check if netlify deploy preview was built', async ({ page, baseURL }) => {
+  test.setTimeout(600000)
   const pr_number = process.env.PR_NUMBER
   await page.goto(`https://github.com/kodadot/nft-gallery/pull/${pr_number}`)
   await expect(
