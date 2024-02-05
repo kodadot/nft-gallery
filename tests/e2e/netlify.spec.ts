990a430f8ee27d878d13feb499c26636bd3baa82
prury <yco.arq@gmail.com>
2024-02-05 15:31:10 -0300

    system matrix

--- tests/e2e/netlify.spec.ts
+++ tests/e2e/netlify.spec.ts
@@ -1,14 +1,14 @@
 import { expect, test } from './fixtures'
 
 test('Check if netlify deploy preview was built', async ({ page, baseURL }) => {
-  test.setTimeout(180000)
+  //test.setTimeout(180000)
   const pr_number = process.env.PR_NUMBER
   await page.goto(`https://github.com/kodadot/nft-gallery/pull/${pr_number}`)
   await expect(
     page.getByText(
       `https://deploy-preview-${pr_number}--koda-canary.netlify.app`,
     ),
-  ).toBeVisible({ timeout: 180000 })
+  ).toBeVisible()
   await page.goto(`${baseURL}`)
   await expect(page.getByText('Polkadot NFT')).toBeVisible()
 })
