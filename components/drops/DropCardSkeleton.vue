be6fb4782136d285b959879dbc8e51cb1621868d
hassnian <hassnianxd@gmail.com>
2024-02-07 10:27:28 +0500

    fix: drop card skeleton same as drop card

--- components/drops/DropCardSkeleton.vue
+++ components/drops/DropCardSkeleton.vue
@@ -4,17 +4,23 @@
 
     <div class="py-5 px-5 flex flex-col items-start gap-4">
       <div class="w-52">
-        <NeoSkeleton no-margin class="w-full" rounded height="24" />
+        <NeoSkeleton no-margin class="w-full" :rounded="false" height="28" />
       </div>
 
-      <div class="flex items-center justify-between w-full">
-        <div>
-          <NeoSkeleton rounded no-margin height="34" class="!w-32" />
+      <div
+        class="flex items-start md:items-center flex-col md:flex-row justify-between gap-4 md:gap-0 w-full">
+        <div class="w-32">
+          <NeoSkeleton
+            height="34"
+            width="100%"
+            rounded
+            no-margin
+            border-radius="3rem" />
         </div>
 
         <div class="flex flex-row gap-4 w-24">
-          <NeoSkeleton :rounded="false" height="12" no-margin width="100%" />
-          <NeoSkeleton :rounded="false" no-margin height="12" width="100%" />
+          <NeoSkeleton height="24" width="100%" :rounded="false" no-margin />
+          <NeoSkeleton height="24" width="100%" :rounded="false" no-margin />
         </div>
       </div>
     </div>
