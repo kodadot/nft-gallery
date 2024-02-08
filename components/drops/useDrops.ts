3600cf7dd929b4913c2f99811e783cc28d904359
hassnian <hassnianxd@gmail.com>
2024-02-08 16:07:12 +0500

    add: drop status comments

--- components/drops/useDrops.ts
+++ components/drops/useDrops.ts
@@ -30,9 +30,9 @@ export interface Drop {
 export enum DropStatus {
   MINTING_ENDED = 'minting_ended',
   MINTING_LIVE = 'minting_live',
-  COMING_SOON = 'coming_soon',
-  SCHEDULED_SOON = 'scheduled_soon',
-  SCHEDULED = 'scheduled',
+  COMING_SOON = 'coming_soon', // live but disabled by backend
+  SCHEDULED_SOON = 'scheduled_soon', // live in < 24h
+  SCHEDULED = 'scheduled', // live in > 24
   UNSCHEDULED = 'unscheduled',
 }
 
