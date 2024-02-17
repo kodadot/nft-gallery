28e5f8f40c7f924d549fb9837563dc5c8cb4e288
hassnian <hassnianxd@gmail.com>
2024-02-17 10:59:46 +0500

    fix: total spent from user profile

---
+++ composables/useUserStats.ts
@@ -0,0 +1,33 @@
+import resolveQueryPath from '@/utils/queryPathResolver'
+
+export default () => {
+  const { client, urlPrefix } = usePrefix()
+  const { accountId } = useAuth()
+
+  const totalSpent = ref(0)
+
+  const getUserStats = async () => {
+    const query = await resolveQueryPath(client.value, 'profileStatsById')
+    const { data } = await useAsyncQuery<{ invested: Event[] }>({
+      query: query.default,
+      clientId: client.value,
+      variables: {
+        id: accountId.value,
+        denyList: getDenyList(urlPrefix.value),
+      },
+    })
+
+    const holdingsEvents = data.value?.invested.filter(
+      (event) => event.nft.currentOwner === accountId.value,
+    )
+
+    totalSpent.value = Number(getSumOfObjectField(holdingsEvents, 'meta'))
+  }
+
+  onBeforeMount(getUserStats)
+
+  return {
+    totalSpent,
+    getUserStats,
+  }
+}
