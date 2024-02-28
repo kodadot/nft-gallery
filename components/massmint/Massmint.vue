334a72765443974ee8c78e832436a82a1917f446
prury <yco.arq@gmail.com>
2024-02-28 14:05:22 -0300

    props in the right place

--- components/massmint/Massmint.vue
+++ components/massmint/Massmint.vue
@@ -11,7 +11,6 @@
             <p class="mb-4">{{ $t('massmint.chooseCollection') }}</p>
             <ChooseCollectionDropdown
               :preselected="preselectedCollectionId"
-              :disabled="isRemark"
               @selectedCollection="onCollectionSelected" />
           </div>
         </div>
@@ -128,8 +127,6 @@ const smallerThenDesktop = computed(() => width.value < 1024)
 
 const isMinting = ref(false)
 const mintStatus = ref('')
-
-const { isRemark } = useIsChain(urlPrefix)
 
 const numberOfMissingNames = computed(
   () => Object.values(NFTS.value).filter((nft) => !nft.name).length,
