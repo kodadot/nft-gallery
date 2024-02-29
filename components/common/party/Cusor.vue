c1f519814948691bc43ffa30a2933ece93725dbf
iMac7 <macharian77@gmail.com>
2024-02-29 02:47:56 +0000

    different partykit cursor

--- components/common/party/Cusor.vue
+++ components/common/party/Cusor.vue
@@ -6,11 +6,22 @@
       top: `${connection.cursor?.y}px`,
       left: `${connection.cursor?.x}px`,
     }">
-    <NeoIcon
+    <svg
       :id="`cursor-${connection.id}`"
-      icon="arrow-pointer"
-      pack="fas"
-      :class="cursorDetails.color" />
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      height="32"
+      viewBox="0 0 32 32"
+      width="32"
+      xmlns="http://www.w3.org/2000/svg">
+      <g fill="none" fill-rule="evenodd" transform="translate(10 7)">
+        <path
+          d="m6.148 18.473 1.863-1.003 1.615-.839-2.568-4.816h4.332l-11.379-11.408v16.015l3.316-3.221z"
+          fill="#FFFFFF"></path>
+        <path
+          d="m6.431 17 1.765-.941-2.775-5.202h3.604l-8.025-8.043v11.188l2.53-2.442z"
+          fill="#0066FF"></path>
+      </g>
+    </svg>
 
     <div
       v-if="cursorLabel?.label"
