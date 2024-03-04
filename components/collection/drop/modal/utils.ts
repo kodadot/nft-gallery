a822c418ea0f542d8b118d9b0b2735460b082ac2
hassnian <hassnianxd@gmail.com>
2024-03-04 13:09:49 +0500

    ref(usePreloadImages.ts): moved promise retry logic into a new composable useAsyncRetry

--- components/collection/drop/modal/utils.ts
+++ components/collection/drop/modal/utils.ts
@@ -1,56 +1,21 @@
-const RETRY_COUNT = 3
-
 type PreloadableImage = {
   id: string
   image: string
 }
 
 export const usePreloadImages = (mintedNFTs: Ref<PreloadableImage[]>) => {
-  const states = ref(new Map<string, { tries: number; loaded: boolean }>())
-
-  const stateValues = computed(() =>
-    Object.values(Object.fromEntries(states.value.entries())),
-  )
-  const triedAll = computed(
-    () =>
-      stateValues.value.length &&
-      Number(getSumOfObjectField(stateValues.value, 'tries')) === 0,
-  )
-  const loadedAll = computed(() => {
-    const loaded = stateValues.value.map((item) => item.loaded)
-    return loaded.length && loaded.every(Boolean)
-  })
-
-  const tryPreload = async (preloadableImage: PreloadableImage) => {
-    const id = preloadableImage.id
-    const currentState = states.value.get(id) || {
-      tries: RETRY_COUNT,
-      loaded: false,
-    }
-
-    if (currentState.tries === 0 || currentState.loaded) {
-      return
-    }
-
-    try {
-      await preloadImage(sanitizeIpfsUrl(preloadableImage.image))
-
-      states.value.set(id, {
-        tries: currentState.tries,
-        loaded: true,
-      })
-    } catch (error) {
-      states.value.set(id, { tries: currentState.tries - 1, loaded: false })
-      tryPreload(preloadableImage)
-    }
-  }
+  const { completedAll, tryItem, triedAll } = useAsyncRetry()
 
   watch(mintedNFTs, (items) => {
-    if (items && items.length) {
-      states.value = new Map()
-      items.forEach(tryPreload)
+    if (items?.length) {
+      items.forEach((item) =>
+        tryItem({
+          id: item.id,
+          promise: () => preloadImage(sanitizeIpfsUrl(item.image)),
+        }),
+      )
     }
   })
 
-  return { triedAll, loadedAll }
+  return { triedAll, loadedAll: completedAll }
 }
