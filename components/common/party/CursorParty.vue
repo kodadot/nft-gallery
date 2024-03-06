d0a5556eee3e4f7e9c48d8c9fada7da03498a3ba
iMac7 <macharian77@gmail.com>
2024-03-06 13:50:52 +0000

    randomize cursor colors

--- components/common/party/CursorParty.vue
+++ components/common/party/CursorParty.vue
@@ -23,6 +23,46 @@ export type CursorLabel =
 export type CursorDetails = {
   label: CursorLabel
   ghost?: boolean
+  color: () => string
+}
+
+const colors = [
+  'text-red-500',
+  'text-orange-500',
+  'text-amber-500',
+  'text-yellow-500',
+  'text-lime-500',
+  'text-green-500',
+  'text-emerald-500',
+  'text-teal-500',
+  'text-cyan-500',
+  'text-sky-500',
+  'text-blue-500',
+  'text-indigo-500',
+  'text-violet-500',
+  'text-purple-500',
+  'text-fuchsia-500',
+  'text-pink-500',
+  'text-rose-500',
+  'text-stone-500',
+  'text-neutral-500',
+  'text-zinc-500',
+  'text-gray-500',
+  'text-slate-500',
+]
+
+const availableColors = ref([...colors])
+
+const getRandomColor = () => {
+  let newColors = [...availableColors.value]
+  if (!newColors.length) {
+    newColors = [...colors]
+  }
+  const randomIndex = Math.floor(Math.random() * newColors.length)
+  const randomColor = newColors[randomIndex]
+  newColors.splice(randomIndex, 1)
+  availableColors.value = newColors
+  return randomColor
 }
 
 const props = defineProps<{
@@ -94,6 +134,7 @@ watch(
         cursorConnections.value.set(connection.id, {
           ghost: Boolean(cursorConnection?.ghost),
           label: props.labelFormatter(connection),
+          color: getRandomColor,
         })
       }
     })
