95af051adc3884a105a8d9d152f53ca7de28a805
Jarsen <31397967+Jarsen136@users.noreply.github.com>
2024-03-10 14:00:29 +0100

    fix: landing heading

--- components/landing/FarcasterBanner.vue
+++ components/landing/FarcasterBanner.vue
@@ -3,7 +3,7 @@
     v-safe-href="'https://warpcast.com/~/channel/koda'"
     target="_blank"
     class="hover:text-text-color flex p-2 bg-background-color border border-text-color rounded-[1.5rem]">
-    <div class="bg-purple-light-color flex gap-3 px-3 py-2 rounded-[0.75rem]">
+    <div class="bg-purple-light-color flex gap-3 px-3 py-2 rounded-[1rem]">
       <img :src="farcasterIcon" />
       <div class="text-purple-dark-color">
         {{ $t('landing.connectFarcaster') }}
--- components/landing/HeroBanner.vue
+++ components/landing/HeroBanner.vue
@@ -7,14 +7,14 @@
         class="-z-10 absolute -translate-x-2/4 left-2/4 top-[-9rem] scale-120 md:scale-125 md:top-[-14rem] pointer-events-none" />
 
       <h1
-        class="break-all text-3xl md:text-5xl font-bold text-center flex flex-col flex-wrap justify-center items-center mb-0">
+        class="break-all text-3xl md:text-5xl font-bold text-center flex flex-col md:flex-row md:gap-5 flex-wrap justify-center items-center mb-0">
         <div>
           {{ $t('search.landingTitle1') }}
-          <span
-            class="inverse-text text-3xl md:text-5xl font-bold text-center capitalize block md:inline">
-            {{ $t('search.landingTitle2') }}
-          </span>
         </div>
+        <span
+          class="inverse-text text-3xl md:text-5xl font-bold text-center capitalize md:inline">
+          {{ $t('search.landingTitle2') }}
+        </span>
         <div>
           {{ $t('search.landingTitle3') }}
         </div>
