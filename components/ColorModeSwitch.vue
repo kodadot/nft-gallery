8b436e26a5986ceaf7743e90ef43ec4985a7b6bb
Jarsen <31397967+Jarsen136@users.noreply.github.com>
2024-03-20 11:11:43 +0100

    feat: mobile switch

--- components/ColorModeSwitch.vue
+++ components/ColorModeSwitch.vue
@@ -1,10 +1,16 @@
 <template>
   <ColorScheme>
+    <NeoIcon
+      v-if="isMobile"
+      class="cursor-pointer"
+      :icon="currentMode.icon"
+      size="medium"
+      @click="switchToNextMode" />
     <NeoDropdown
+      v-else
       v-model="preference"
       position="top-left"
       :close-on-click="true"
-      mobile-modal
       aria-role="list"
       @change="onChange">
       <template #trigger>
@@ -29,38 +35,50 @@
 
 <script lang="ts" setup>
 import { NeoDropdown, NeoDropdownItem, NeoIcon } from '@kodadot1/brick'
+
+type COLOR_MODE = 'system' | 'light' | 'dark'
+
 const { isDarkMode, preference, setColorMode, isSystemMode } = useTheme()
+const { isMobile } = useViewport()
+
 const { $i18n } = useNuxtApp()
 
-const options = computed(() => [
-  {
-    value: 'system',
-    icon: 'computer-classic',
-    label: isSystemMode.value
-      ? $i18n.t('profileMenu.systemMode', [
-          isDarkMode.value
-            ? $i18n.t('profileMenu.dark')
-            : $i18n.t('profileMenu.light'),
-        ])
-      : $i18n.t('profileMenu.system'),
-  },
-  {
-    value: 'light',
-    icon: 'sun',
-    label: $i18n.t('profileMenu.lightMode'),
-  },
-  {
-    value: 'dark',
-    icon: 'moon',
-    label: $i18n.t('profileMenu.darkMode'),
-  },
-])
-
-const currentMode = computed(
-  () =>
-    options.value.find((option) => option.value === preference.value) ||
-    options.value[0],
+const options = computed<{ value: COLOR_MODE; icon: string; label: string }[]>(
+  () => [
+    {
+      value: 'system',
+      icon: 'computer-classic',
+      label: isSystemMode.value
+        ? $i18n.t('profileMenu.systemMode', [
+            isDarkMode.value
+              ? $i18n.t('profileMenu.dark')
+              : $i18n.t('profileMenu.light'),
+          ])
+        : $i18n.t('profileMenu.system'),
+    },
+    {
+      value: 'light',
+      icon: 'sun',
+      label: $i18n.t('profileMenu.lightMode'),
+    },
+    {
+      value: 'dark',
+      icon: 'moon',
+      label: $i18n.t('profileMenu.darkMode'),
+    },
+  ],
 )
+
+const currentModeIndex = computed(() =>
+  options.value.findIndex((option) => option.value === preference.value),
+)
+
+const currentMode = computed(() => options.value[currentModeIndex.value || 0])
+
+const switchToNextMode = () => {
+  const nextIndex = (currentModeIndex.value + 1) % options.value.length
+  setColorMode(options.value[nextIndex].value)
+}
 
 const onChange = (option) => {
   setColorMode(option.value)
