763c468790341cce0b2dcbbdd724c8a47a4e8b25
Floyd Li <floyd.li@outlook.com>
2024-03-20 11:07:51 +0800

    fix: remove margin in lg

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -181,7 +181,7 @@
                 {{ $t('profile.page') }}
                 <NeoIcon
                   icon="user-circle"
-                  class="w-4 h-4 ml-2"
+                  class="w-4 h-4 ml-2 lg:!ml-0"
                   size="medium" />
               </span>
               <NeoIcon class="icon--right" icon="chevron-right" />
