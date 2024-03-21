345ba1bd63622c414686486d0c21b829d39c85af
hassnian <hassnianxd@gmail.com>
2024-03-21 12:53:35 +0500

    ref(DropsGrid.vue): fix use of any type

--- components/drops/DropsGrid.vue
+++ components/drops/DropsGrid.vue
@@ -8,29 +8,34 @@
     <div
       v-for="(drop, index) in drops"
       v-else
-      :key="`${drop.collection?.id}=${index}`"
+      :key="`${isDrop(drop) ? drop.collection?.id : drop.id}=${index}`"
       class="w-full h-full"
       :data-testid="index">
-      <slot name="card" :item="drop">
-        <DropCard :drop="drop" />
+      <slot name="card" :item="drop as DropCalendar">
+        <DropCard :drop="drop as Drop" />
       </slot>
     </div>
   </DynamicGrid>
 </template>
 <script lang="ts" setup>
 import DropCard from '@/components/drops/DropCard.vue'
-
-defineProps<{
-  drops: any[]
-  loaded: boolean
-  defaultSkeletonCount: number
-  skeletonKey: string
-  clickable?: boolean
-}>()
+import { DropCalendar } from '@/services/fxart'
+import { Drop } from './useDrops'
 
 const GRID_DEFAULT_WIDTH = {
   small: 0,
   medium: DROP_CARD_MIN_WIDTH,
   large: 0,
 }
+
+defineProps<{
+  drops: Drop[] | DropCalendar[]
+  loaded: boolean
+  defaultSkeletonCount: number
+  skeletonKey: string
+  clickable?: boolean
+}>()
+
+const isDrop = (item: Drop | DropCalendar): item is Drop =>
+  (item as Drop).collection !== undefined
 </script>
