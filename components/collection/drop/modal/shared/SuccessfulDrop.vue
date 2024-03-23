0bcfed30773d8512ec76f33008d282588d40bd3d
hassnian <hassnianxd@gmail.com>
2024-03-23 11:57:42 +0500

    ref(SuccessfulDrop.vue): reuse <SuccessfulItemsMedia /> component

--- components/collection/drop/modal/shared/SuccessfulDrop.vue
+++ components/collection/drop/modal/shared/SuccessfulDrop.vue
@@ -3,24 +3,18 @@
     :tx-hash="txHash"
     :share="share"
     :action-buttons="actionButtons">
-    <!-- ?? '' below is to appease TS, in reality it's always defined because of the v-if -->
-    <SingleItemMedia
-      v-if="singleMint"
-      :header="$t('drops.youSuccessfullyClaimedNft', [1])"
-      :src="sanitizeIpfsUrl(mintedNft?.image)"
-      :nft-name="mintedNft?.name ?? ''"
-      :collection-id="mintedNft?.collection ?? ''"
-      :collection-name="mintedNft?.collectionName ?? ''"
-      media-mime-type="text/html" />
-    <MultiItemMedia
-      v-else
-      :header="$t('drops.amountMintedSuccessfully', [items.length])"
-      :items="sanatizedItems"
+    <SuccessfulItemsMedia
+      :header="{
+        single: $t('drops.youSuccessfullyClaimedNft', [1]),
+        multiple: $t('drops.amountMintedSuccessfully', [items.length]),
+      }"
+      :items="items"
       media-mime-type="text/html" />
   </SuccessfulModalBody>
 </template>
 
 <script setup lang="ts">
+import type { ItemMedia } from '@/components/common/successfulModal/SuccessfulItemsMedia.vue'
 import { MintingSession } from '../../types'
 
 const emit = defineEmits(['list'])
@@ -29,37 +23,43 @@ const props = defineProps<{
   canListNfts: boolean
 }>()
 
-const mintedNft = computed(() => props.mintingSession.items[0])
-const sanatizedItems = computed(() =>
-  items.value.map((item) => ({
-    id: item.id,
-    name: item.name,
-    image: sanitizeIpfsUrl(item.image),
-  })),
-)
-
 const { $i18n } = useNuxtApp()
 const { toast } = useToast()
 const { urlPrefix } = usePrefix()
 const { accountId } = useAuth()
 
-const sharingTxt = computed(() =>
-  $i18n.t('sharing.dropNft', [mintedNft.value?.sn, mintedNft.value?.max]),
+const cantList = computed(() => !props.canListNfts)
+const txHash = computed(() => props.mintingSession.txHash ?? '')
+const singleMint = computed(() => props.mintingSession.items.length === 1)
+const mintedNft = computed(() => props.mintingSession.items[0])
+
+const items = computed<ItemMedia[]>(() =>
+  props.mintingSession.items.map((item) => ({
+    id: item.id,
+    name: item.name,
+    image: item.image,
+    collection: item.collection,
+    collectionName: item.collectionName,
+  })),
 )
 
-const txHash = computed(() => props.mintingSession.txHash ?? '')
+const nftPath = computed(
+  () => `/${mintedNft.value.chain}/gallery/${mintedNft.value.id}`,
+)
+const nftFullUrl = computed(() => `${window.location.origin}${nftPath.value}`)
+const userProfilePath = computed(
+  () => `/${urlPrefix.value}/u/${accountId.value}`,
+)
+
+const sharingTxt = computed(() =>
+  $i18n.t('sharing.dropNft', [mintedNft.value.id, mintedNft.value?.max]),
+)
+
 const share = computed(() => ({
   text: sharingTxt.value,
   url: nftFullUrl.value,
   withCopy: singleMint.value,
 }))
-const items = computed(() =>
-  props.mintingSession.items.map((item) => ({
-    id: item.id,
-    name: item.name,
-    image: sanitizeIpfsUrl(item.image),
-  })),
-)
 
 const actionButtons = computed(() => ({
   secondary: {
@@ -72,16 +72,6 @@ const actionButtons = computed(() => ({
     disabled: cantList.value,
   },
 }))
-
-const nftPath = computed(
-  () => `/${mintedNft.value.chain}/gallery/${mintedNft.value.id}`,
-)
-const nftFullUrl = computed(() => `${window.location.origin}${nftPath.value}`)
-const userProfilePath = computed(
-  () => `/${urlPrefix.value}/u/${accountId.value}`,
-)
-const singleMint = computed(() => props.mintingSession.items.length === 1)
-const cantList = computed(() => !props.canListNfts)
 
 const handleViewNft = () => {
   window.open(
