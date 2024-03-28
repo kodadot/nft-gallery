d503bd3c35795767460f26aefbbecd4628e4f007
Mohammed Zeeshan <work.zeeshan@gmail.com>
2024-03-28 09:47:01 +0530

    revert adding seo tags

--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,4 @@
 definePageMeta({
   layout: 'full-width-layout',
 })
-
-useSeoMeta({
-  title: 'KodaDot - Your Generative Art Marketplace',
-  ogTitle: 'KodaDot - Your Generative Art Marketplace',
-  description: 'One Stop NFT Shop on Polkadot',
-  ogDescription: 'One Stop NFT Shop on Polkadot',
-  ogImage: '/k_card.png',
-  twitterCard: 'summary_large_image',
-  ogUrl: process.env.BASE_URL || 'http://localhost:9090',
-})
 </script>
