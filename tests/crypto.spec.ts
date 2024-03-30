37b7fb7687ea0d560c275b8cafbdf6b4eceaf3ad
Preschian Febryantara <preschian@users.noreply.github.com>
2024-03-30 21:39:54 +0700

    refactor(crypto.spec.ts): rename 'attemps' to 'users' and remove unused 'length' set feat(crypto.spec.ts): add comment about uidCrypto() or uidMath() for more than 1k concurrent users

--- tests/crypto.spec.ts
+++ tests/crypto.spec.ts
@@ -1,21 +1,19 @@
 import { uidCrypto, uidMath, uidMathDate } from '../utils/randomize'
 
-const attemps = 100
-// const attemps = 10_000
+const users = 100 // concurrent users
 
 // uidMathDate() has less randomness in its format because it has a date prefix (yyMM). However, it has a slightly higher chance than the others of generating a non-unique ID because of the date prefix.
 
+// for more than 1k concurrent users, prefer uidCrypto() or uidMath() to ensure uniqueness
+
 it.each([uidCrypto, uidMath, uidMathDate])('generate unique id', (random) => {
   const ids = new Set<string>()
-  const length = new Set<number>()
 
   // eslint-disable-next-line no-restricted-syntax
-  for (let index = 0; index < attemps; index++) {
+  for (let index = 0; index < users; index++) {
     const id = random()
     ids.add(id)
-    length.add(id.length)
   }
 
-  expect(ids.size).toBe(attemps)
-  expect(length.has(9)).toEqual(true)
+  expect(ids.size).toBe(users)
 })
