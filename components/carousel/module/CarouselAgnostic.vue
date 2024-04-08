fd76219b0a7a2d212406b361f6e7f110054467c0
hassnian <hassnianxd@gmail.com>
2024-04-08 12:30:32 +0500

    fix(CarouselAgnostic): glitch  on windows resize

--- components/carousel/module/CarouselAgnostic.vue
+++ components/carousel/module/CarouselAgnostic.vue
@@ -45,6 +45,7 @@ export type CarouseBreakpointsConfig = Record<
 >
 
 const SYNC_SLIDER_ON_EVENTS = ['slideChanged', 'optionsChanged']
+const SYNC_CONFIG_DEBOUNCE_AMOUNT = 1 // anything bellow this makes the carousel to glitch
 const DEFAULT_SLIDES_CONFIG = { perView: 3, spacing: 16 }
 
 const props = defineProps<{
@@ -128,6 +129,12 @@ const syncSlider = (s) => {
   updateSliderArrows(s)
 }
 
+const updateConfig = () => {
+  if (slider.value) {
+    slider.value.update(props.config)
+  }
+}
+
 if (props.config) {
   watch(
     slider,
@@ -141,15 +148,11 @@ if (props.config) {
     { immediate: true },
   )
 
-  watch(
-    [() => props.config, () => props.items.length],
-    () => {
-      if (slider.value) {
-        slider.value.update(props.config)
-      }
-    },
-    { immediate: true },
-  )
+  onMounted(updateConfig)
+
+  watchDebounced([() => props.config, () => props.items.length], updateConfig, {
+    debounce: SYNC_CONFIG_DEBOUNCE_AMOUNT,
+  })
 }
 </script>
 
