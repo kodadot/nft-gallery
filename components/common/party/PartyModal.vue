02150cd1b345eee61065594ec83ddc50f0cf7a52
iMac7 <macharian77@gmail.com>
2024-04-08 07:47:51 +0000

    transition on component

--- components/common/party/PartyModal.vue
+++ components/common/party/PartyModal.vue
@@ -1,33 +1,36 @@
 <template>
-  <div
-    class="party shadow-primary h-fit fixed !top-20 md:!top-24 sm:left-4 border-2 bg-background-color p-2 sm:!p-6 flex justify-between gap-2 sm:gap-6 max-w-[498px]">
-    <div class="flex-1 min-w-[91px] h-[116px] flex justify-center items-center">
-      <img :src="'/drop/party.svg'" />
-    </div>
-    <div class="max-w-[335px] px-1 sm:px-4 flex flex-col gap-2">
-      <div class="flex justify-between font-bold">
-        <p>{{ $i18n.t('partyModal.title') }}</p>
-        <NeoIcon icon="close" size="large" @click="closeModal" />
+  <transition name="slide">
+    <div
+      class="party shadow-primary h-fit fixed !top-20 md:!top-24 sm:left-4 border-2 bg-background-color p-2 sm:!p-6 flex justify-between gap-2 sm:gap-6 max-w-[498px]">
+      <div
+        class="flex-1 min-w-[91px] h-[116px] flex justify-center items-center">
+        <img :src="'/drop/party.svg'" />
       </div>
-      <p class="text-sm">
-        {{ $i18n.t('partyModal.text') }}
-        <a
-          href="https://hello.kodadot.xyz/tutorial/generative-art/generative-drops/drop-party"
-          class="text-k-blue"
-          >{{ $i18n.t('helper.learnMore') }}
-        </a>
-      </p>
+      <div class="max-w-[335px] px-1 sm:px-4 flex flex-col gap-2">
+        <div class="flex justify-between font-bold">
+          <p>{{ $i18n.t('partyModal.title') }}</p>
+          <NeoIcon icon="close" size="large" @click="closeModal" />
+        </div>
+        <p class="text-sm">
+          {{ $i18n.t('partyModal.text') }}
+          <a
+            href="https://hello.kodadot.xyz/tutorial/generative-art/generative-drops/drop-party"
+            class="text-k-blue"
+            >{{ $i18n.t('helper.learnMore') }}
+          </a>
+        </p>
 
-      <div class="mt-4 flex justify-between items-center">
-        <NeoButton variant="text" no-shadow @click="stopParty">
-          <span class="text-k-grey">{{ $i18n.t('partyModal.no') }}</span>
-        </NeoButton>
-        <NeoButton variant="pill" no-shadow @click="closeModal">
-          <span>{{ $i18n.t('partyModal.yes') }}</span>
-        </NeoButton>
+        <div class="mt-4 flex justify-between items-center">
+          <NeoButton variant="text" no-shadow @click="stopParty">
+            <span class="text-k-grey">{{ $i18n.t('partyModal.no') }}</span>
+          </NeoButton>
+          <NeoButton variant="pill" no-shadow @click="closeModal">
+            <span>{{ $i18n.t('partyModal.yes') }}</span>
+          </NeoButton>
+        </div>
       </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script setup lang="ts">
