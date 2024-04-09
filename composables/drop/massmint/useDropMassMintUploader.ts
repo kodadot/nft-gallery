e4673980768ed1a6fbc5ba28a89b53a4118d8911
daiagi <daiagi@gmail.com>
2024-04-09 14:59:43 +0700

    refactor prepareMetadatas

--- composables/drop/massmint/useDropMassMintUploader.ts
+++ composables/drop/massmint/useDropMassMintUploader.ts
@@ -6,40 +6,49 @@ import { makeUnlockableMetadata } from '@/components/collection/unlockable/utils
 import { useCollectionEntity } from '@/composables/drop/useGenerativeDropMint'
 import useDropMassMintState from './useDropMassMintState'
 
-export default () => {
+const getFiles = () => {
+  const { toMintNFTs } = storeToRefs(useDropStore())
+  return Promise.all(
+    toMintNFTs.value.map((nft) =>
+      getCaptureImageFile({
+        image: nft.image,
+        data: nft.imageDataPayload as GenerativePreviewItem,
+      }),
+    ),
+  )
+}
+
+const generateMetadatas = (files) => {
   const { toMintNFTs, drop } = storeToRefs(useDropStore())
   const { collectionName, description } = useCollectionEntity()
+
+  return uploadMediaAndMetadataDirectories(files, (imageHashes) =>
+    toMintNFTs.value.map((nft, index) =>
+      makeJsonFile(
+        makeUnlockableMetadata(
+          collectionName.value || drop.value.name,
+          description.value || '',
+          imageHashes[index],
+          nft.image,
+          'text/html',
+        ),
+        String(index),
+      ),
+    ),
+  )
+}
+
+export default () => {
+  const { toMintNFTs } = storeToRefs(useDropStore())
   const { canPin } = useDropMassMintState()
 
   const prepareMetadatas = async () => {
     try {
       console.log('[MASSMINT::UPLOADER] Starting to upload metadata')
 
-      const files = await Promise.all(
-        toMintNFTs.value.map((nft) =>
-          getCaptureImageFile({
-            image: nft.image,
-            data: nft.imageDataPayload as GenerativePreviewItem,
-          }),
-        ),
-      )
+      const files = await getFiles()
 
-      const metadatas = await uploadMediaAndMetadataDirectories(
-        files,
-        (imageHashes) =>
-          toMintNFTs.value.map((nft, index) =>
-            makeJsonFile(
-              makeUnlockableMetadata(
-                collectionName.value || drop.value.name,
-                description.value || '',
-                imageHashes[index],
-                nft.image,
-                'text/html',
-              ),
-              String(index),
-            ),
-          ),
-      )
+      const metadatas = await generateMetadatas(files)
 
       toMintNFTs.value = toMintNFTs.value.map((nft, index) => ({
         ...nft,
