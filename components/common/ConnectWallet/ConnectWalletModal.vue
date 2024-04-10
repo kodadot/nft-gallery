d3be4503089b309199769494b3cc2074fba2da46
Jarsen <31397967+Jarsen136@users.noreply.github.com>
2024-04-10 22:43:43 +0200

    feat: New to koda? Connect wallet sidebar improvements

--- components/common/ConnectWallet/ConnectWalletModal.vue
+++ components/common/ConnectWallet/ConnectWalletModal.vue
@@ -21,22 +21,65 @@
           @set-account="setAccount" />
       </div>
 
+      <div v-if="showUninstalledWallet" class="!px-8 !pt-6">
+        <div class="font-bold capitalize mb-3">
+          {{ $i18n.t('walletConnect.newToKoda') }}
+        </div>
+        <div class="text-xs !mb-6">
+          {{ $i18n.t('walletConnect.startConnect') }}
+        </div>
+        <div class="border py-4 !px-6 !mb-6 bg-k-grey-light border-k-grey">
+          <div class="mb-3">{{ $i18n.t('walletConnect.quickSteps') }}</div>
+          <ol class="text-xs font-bold pl-4">
+            <li class="mb-3">
+              {{ $i18n.t('walletConnect.chooseWallet')
+              }}<span class="font-normal">{{
+                $i18n.t('walletConnect.chooseBelow')
+              }}</span>
+            </li>
+            <li>
+              {{ $i18n.t('walletConnect.downloadAndConnect')
+              }}<span class="font-normal">{{
+                $i18n.t('walletConnect.setUpConnect')
+              }}</span>
+            </li>
+          </ol>
+        </div>
+        <div class="font-bold mb-3 capitalize">
+          {{ $i18n.t('walletConnect.selectWallet') }}
+        </div>
+      </div>
+
       <a
+        v-else
         class="flex items-center pt-4 pb-3 text-xs text-k-grey mx-6 my-0 hover:text-k-hover-grey border-t border-k-shade2"
         @click="toggleShowUninstalledWallet">
         {{ $i18n.t('walletConnect.moreOption') }}
         <NeoIcon
-          v-if="showUninstalledWallet"
+          v-if="isUninstalledWalletExpand"
           class="ml-1"
           icon="chevron-down" />
 
         <NeoIcon v-else class="ml-1" icon="chevron-right" />
       </a>
-      <div v-if="showUninstalledWallet" class="buttons">
+      <div
+        v-if="showUninstalledWallet || isUninstalledWalletExpand"
+        class="buttons">
         <WalletMenuItem
           v-for="wallet in uninstalledWallet"
           :key="wallet.name"
           :wallet="wallet" />
+      </div>
+
+      <div v-if="showUninstalledWallet" class="px-6">
+        <hr class="!my-0" />
+        <NeoButton
+          class="!w-full !my-6"
+          variant="outlined-rounded"
+          @click="checkWallet">
+          {{ $t('walletConnect.walletHeading') }}
+          <NeoIcon icon="arrow-right" />
+        </NeoButton>
       </div>
     </section>
 
@@ -62,7 +105,7 @@
 <script setup lang="ts">
 import { SupportedWallets } from '@/utils/config/wallets'
 import { BaseDotsamaWallet } from '@/utils/config/wallets/BaseDotsamaWallet'
-import { NeoIcon, NeoModalHead } from '@kodadot1/brick'
+import { NeoButton, NeoIcon, NeoModalHead } from '@kodadot1/brick'
 import { Auth, useIdentityStore } from '@/stores/identity'
 import WalletMenuItem from '@/components/common/ConnectWallet/WalletMenuItem.vue'
 import WalletAsset from '@/components/common/ConnectWallet/WalletAsset.vue'
@@ -73,11 +116,27 @@ const forceWalletSelect = ref(false)
 const identityStore = useIdentityStore()
 const { urlPrefix } = usePrefix()
 const emit = defineEmits(['close', 'connect'])
-
+const { toast } = useToast()
 const account = computed(() => identityStore.auth.address)
 const showAccount = computed(() => account.value)
 
-const wallets = SupportedWallets()
+const wallets = ref<BaseDotsamaWallet[]>([])
+
+const refreshWallets = () => {
+  wallets.value = SupportedWallets()
+}
+
+onMounted(() => {
+  refreshWallets()
+})
+
+const checkWallet = () => {
+  refreshWallets()
+  if (showUninstalledWallet.value) {
+    toast('Please download a wallet first')
+  }
+}
+
 const setAccount = (account: Auth) => {
   forceWalletSelect.value = false
   identityStore.setAuth(account)
@@ -89,15 +148,16 @@ const setAccount = (account: Auth) => {
 }
 
 const installedWallet = computed(() => {
-  return wallets.filter((wallet) => wallet.installed)
+  return wallets.value.filter((wallet) => wallet.installed)
 })
 const uninstalledWallet = computed(() => {
-  return wallets.filter((wallet) => !wallet.installed)
+  return wallets.value.filter((wallet) => !wallet.installed)
 })
-const showUninstalledWallet = ref(!installedWallet.value.length)
+const showUninstalledWallet = computed(() => !installedWallet.value.length)
 
+const isUninstalledWalletExpand = ref(!showUninstalledWallet.value)
 const toggleShowUninstalledWallet = () => {
-  showUninstalledWallet.value = !showUninstalledWallet.value
+  isUninstalledWalletExpand.value = !isUninstalledWalletExpand.value
 }
 
 watch(account, (account) => {
