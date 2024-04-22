2d936e602672cf2a3c87f2b981e1f6a77533f940
Jarsen <31397967+Jarsen136@users.noreply.github.com>
2024-04-22 09:53:47 +0200

    chore: naming

--- components/landing/FarcasterMintNotificationBanner.vue
+++ components/landing/FarcasterMintNotificationBanner.vue
@@ -1,16 +1,16 @@
 <template>
   <a
-    v-safe-href="FarcasterLiveMintConfig.url"
+    v-safe-href="FARCASTER_LIVE_MINT_CONFIG.url"
     target="_blank"
     class="hover:text-text-color flex items-center p-2 gap-[10px] bg-background-color border border-text-color rounded-[1.5rem]">
     <img
-      :src="FarcasterLiveMintConfig.image"
+      :src="FARCASTER_LIVE_MINT_CONFIG.image"
       class="w-[38px] h-[38px] rounded-[14px]" />
 
     <div
       class="bg-k-red-accent-2 flex flex-wrap gap-2 px-3 py-2 rounded-[14px]">
-      <div class="font-bold">{{ FarcasterLiveMintConfig.name }}</div>
-      <div class="capitalize">{{ $t('landing.FarcasterMintLive') }}</div>
+      <div class="font-bold">{{ FARCASTER_LIVE_MINT_CONFIG.name }}</div>
+      <div class="capitalize">{{ $t('landing.farcasterMintLive') }}</div>
     </div>
     <NeoIcon icon="arrow-right" />
   </a>
@@ -18,5 +18,4 @@
 
 <script setup lang="ts">
 import { NeoIcon } from '@kodadot1/brick'
-import { FarcasterLiveMintConfig } from '@/utils/drop'
 </script>
--- utils/drop.ts
+++ utils/drop.ts
@@ -51,7 +51,7 @@ export const FALLBACK_DROP_COLLECTION_MAX = 256
 
 export const DROP_CARD_MIN_WIDTH = 334 // px
 
-export const FarcasterLiveMintConfig = {
+export const FARCASTER_LIVE_MINT_CONFIG = {
   name: 'Unfoldings',
   image:
     'https://imagedelivery.net/jk5b6spi_m_-9qC4VTnjpg/bafybeicdw6phpf3xyng5icxenzmxpken3izmk5gwyew3jahxrno7ql3s3e/public',
