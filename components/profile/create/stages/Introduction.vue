486e117e8637a5b51d2357099fc03fe75332eb9b
daiagi <daiagi@gmail.com>
2024-04-28 16:31:27 +0300

    style fix

--- components/profile/create/stages/Introduction.vue
+++ components/profile/create/stages/Introduction.vue
@@ -7,9 +7,9 @@
           class="px-4 py-2 rounded-full bg-k-accent-light-2 text-k-accent dark:text-k-accent-light text-xl font-bold">
           New
         </div>
-        <span class="ml-4 weigh-bold text-xl"> profile customization </span>
+        <span class="ml-4 font-bold text-xl"> Profile Customization </span>
       </div>
-      <p class="mt-4 text-sm">
+      <p class="mt-4 text-sm text-center">
         Make Your Profile Truly Yours In
         <span class="font-bold">Just 1 Minute</span>. Add Your Banner And
         Profile Image To Stand Out.
