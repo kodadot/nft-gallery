3bfe476ffc653ed285533ae7359f972afa4bf02b
daiagi <daiagi@gmail.com>
2024-05-01 19:05:35 +0300

    title center

--- components/profile/create/stages/Introduction.vue
+++ components/profile/create/stages/Introduction.vue
@@ -7,7 +7,9 @@
           class="px-4 py-2 rounded-full bg-k-accent-light-2 text-k-accent dark:text-k-accent-light text-xl font-bold">
           New
         </div>
-        <span class="ml-4 font-bold text-xl"> Profile Customization </span>
+        <span class="ml-4 font-bold text-xl text-center">
+          Profile Customization
+        </span>
       </div>
       <p class="mt-4 text-sm text-center">
         Make Your Profile Truly Yours In
