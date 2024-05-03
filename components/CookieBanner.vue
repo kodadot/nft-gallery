7a76ca12b815d742f590293d8534d8ee09d075f8
roiLeo <medina.leo42@gmail.com>
2024-05-03 08:41:18 +0200

    🔧 replace font awesome icon

--- components/CookieBanner.vue
+++ components/CookieBanner.vue
@@ -61,12 +61,11 @@ onMounted(() => {
   @apply text-text-color;
 }
 button.cookieControl__ModalClose {
-  font-family: 'Font Awesome 6 Sharp';
   @apply invisible absolute top-3 right-0 h-0 w-0;
 }
 button.cookieControl__ModalClose:after {
   @apply absolute top-1 right-5 text-xs text-text-color font-medium bg-background-color hover:bg-gray-100 hover:text-gray-800 rounded-lg px-4 py-2.5 duration-300 transition-colors focus:outline-none visible;
-  content: '\f00d';
+  content: url('data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2216%22%20height%3D%2216%22%20fill%3D%22currentColor%22%20class%3D%22bi%20bi-x-lg%22%20viewBox%3D%220%200%2016%2016%22%3E%0A%20%20%3Cpath%20d%3D%22M2.146%202.854a.5.5%200%201%201%20.708-.708L8%207.293l5.146-5.147a.5.5%200%200%201%20.708.708L8.707%208l5.147%205.146a.5.5%200%200%201-.708.708L8%208.707l-5.146%205.147a.5.5%200%200%201-.708-.708L7.293%208z%22%2F%3E%0A%3C%2Fsvg%3E');
 }
 .cookieControl__ModalButtons {
   @apply grid grid-cols-3 gap-4 mt-4 shrink-0 w-full;
