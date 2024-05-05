e409f03dcbd3798c352ca0241f60ad44a456fded
Jarsen <31397967+Jarsen136@users.noreply.github.com>
2024-05-05 17:17:47 +0200

    fix: Loaders and skeletons for new profile

--- components/profile/follow/Tab.vue
+++ components/profile/follow/Tab.vue
@@ -1,5 +1,8 @@
 <template>
-  <div v-if="totalCount > 0" ref="el" class="flex flex-col gap-5">
+  <div v-if="isLoading">
+    <UserRowSkeleton v-for="(_, index) in loadingSkeletonItems" :key="index" />
+  </div>
+  <div v-else-if="totalCount > 0" ref="el" class="flex flex-col gap-5">
     <UserRow v-for="user in vList" :key="user.address" :user="user" />
   </div>
   <div v-else class="text-center text-k-grey">
@@ -10,6 +13,7 @@
 <script setup lang="ts">
 import { useInfiniteScroll } from '@vueuse/core'
 import UserRow from '@/components/profile/follow/UserRow.vue'
+import UserRowSkeleton from '@/components/profile/follow/UserRowSkeleton.vue'
 
 import { Follower, fetchFollowersOf, fetchFollowing } from '@/services/profile'
 import { Tab } from '../types'
@@ -26,12 +30,17 @@ const vList = useVModel(props, 'userList')
 const offset = computed(() => vList.value.length)
 const limit = 10
 const el = ref<HTMLElement | null>(null)
+const loadingSkeletonItems = computed(() => Math.min(4, props.totalCount))
+const isLoading = ref(false)
 
 const fetchNextPage = async () => {
   if (offset.value >= props.totalCount) {
     return
   }
 
+  if (offset.value === 0) {
+    isLoading.value = true
+  }
   if (props.type === 'following') {
     const { following } = await fetchFollowing(
       route.params.id as string,
@@ -47,6 +56,8 @@ const fetchNextPage = async () => {
     )
     vList.value.push(...followers)
   }
+
+  isLoading.value = false
 }
 
 onMounted(() => {
