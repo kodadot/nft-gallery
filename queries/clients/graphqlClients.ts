5e1a7c6913a1d28515c920d107ea421f7ae0fc2c
daiagi <daiagi@gmail.com>
2024-05-06 14:46:30 +0300

    datetime scalar type

--- queries/clients/graphqlClients.ts
+++ queries/clients/graphqlClients.ts
@@ -6,18 +6,32 @@ import type { introspection as ahkSchema } from './types/ahk'
 
 export const graphqlRmrk = initGraphQLTada<{
   introspection: rmrkSchema
+  scalars: {
+    DateTime: string
+  }
 }>()
 
 export const graphqlKsm = initGraphQLTada<{
   introspection: ksmScehma
+  scalars: {
+    DateTime: string
+  }
 }>()
 
 export const graphqlAhp = initGraphQLTada<{
   introspection: ahpSchema
+  scalars: {
+    DateTime: string
+  }
 }>()
 
 export const graphqlAhk = initGraphQLTada<{
   introspection: ahkSchema
+  scalars: {
+    DateTime: string
+  }
 }>()
+
+export const graphql = graphqlAhp // default client  for general queries
 export type { FragmentOf, ResultOf, VariablesOf } from 'gql.tada'
 export { readFragment } from 'gql.tada'
