afde25ebe86d48f5c61a748b26a1ffdeaba8fe70
Jarsen <31397967+Jarsen136@users.noreply.github.com>
2024-05-09 14:30:33 +0200

    Update composables/useFetchProfile.ts
    Co-authored-by: Hassnian <44554284+hassnian@users.noreply.github.com>

--- composables/useFetchProfile.ts
+++ composables/useFetchProfile.ts
@@ -4,12 +4,7 @@ import type { Profile } from '@/services/profile'
 export default function useFetchProfile(address: Ref<string | undefined>) {
   const { data: profile } = useAsyncData<Profile | null>(
     `userProfile-${address.value}`,
-    () => {
-      if (address.value) {
-        return fetchProfileByAddress(address.value)
-      }
-      return Promise.resolve(null)
-    },
+    () =>  address.value ? fetchProfileByAddress(address.value) : Promise.resolve(null),
     {
       watch: [address],
     },
