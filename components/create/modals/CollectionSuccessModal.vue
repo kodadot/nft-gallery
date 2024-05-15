44e89a2d2616233043dd7e18ec66313e8d9001d4
hassnian <hassnianxd@gmail.com>
2024-05-15 13:52:52 +0500

    add(CreateCollection.vue): show success modal in block

--- components/create/modals/CollectionSuccessModal.vue
+++ components/create/modals/CollectionSuccessModal.vue
@@ -4,17 +4,18 @@
       v-model="isModalActive"
       :tx-hash="txHash"
       :share="share"
+      :status="status"
       :action-buttons="actionButtons">
       <div>
         <div
           class="relative bg-cover bg-center w-full h-28 border border-k-grey-fix"
           :style="{
-            backgroundImage: `url(${avatar})`,
+            backgroundImage: `url(${collection?.image})`,
           }">
           <div class="flex justify-center items-center h-full">
             <div class="bg-background-color p-2 border border-k-grey-fix">
               <BaseMediaItem
-                :src="avatar"
+                :src="collection?.image"
                 :image-component="NuxtImg"
                 :title="collection?.name"
                 class="w-16 h-16 border border-k-grey-fix" />
@@ -37,12 +38,14 @@
 
 <script setup lang="ts">
 import { MintedCollectionInfo } from '../CreateCollection.vue'
+import { TransactionStatus } from '@/composables/useTransactionStatus'
 
 const NuxtImg = resolveComponent('NuxtImg')
 defineEmits(['modelValue'])
 const props = defineProps<{
   modelValue: boolean
   txHash: string
+  status: TransactionStatus
   collection?: MintedCollectionInfo
 }>()
 
@@ -50,7 +53,6 @@ const isModalActive = useVModel(props, 'modelValue')
 
 const { $i18n } = useNuxtApp()
 const { urlPrefix } = usePrefix()
-const avatar = computed(() => sanitizeIpfsUrl(props.collection?.meta?.image))
 
 const collectionPath = computed(
   () => `/${urlPrefix.value}/collection/${props.collection?.id}`,
@@ -64,14 +66,18 @@ const share = computed(() => ({
   url: `${window.location.origin}${collectionPath.value}`,
 }))
 
+const disabled = computed(() => !props.collection?.id)
+
 const actionButtons = computed(() => ({
   secondary: {
     label: 'View Collection',
     onClick: handleViewCollection,
+    disabled: disabled.value,
   },
   primary: {
     label: 'Add NFTs',
     onClick: handleAddNfts,
+    disabled: disabled.value,
   },
 }))
 
