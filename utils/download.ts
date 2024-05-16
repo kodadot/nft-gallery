257a993db44fc75673e4d33cf7036984ae7e8e4b
panda6151025 <liamhu0914@gmail.com>
2024-05-15 19:11:34 -0700

    improve the download skill

--- utils/download.ts
+++ utils/download.ts
@@ -1,30 +1,33 @@
-const FILE_SUFFIX = '.png'
 export const downloadImage = async (imageSrc: string, name = 'unnamed') => {
   if (!imageSrc) {
     return
   }
+  const image = new Image()
+  image.src = imageSrc
+  image.crossOrigin = 'Anonymous'
+  image.onload = async function () {
+    const canvas = document.createElement('canvas')
+    canvas.width = image.width
+    canvas.height = image.height
+    const ctx = canvas.getContext('2d')
+    ctx?.drawImage(image, 0, 0)
+    ctx?.save()
+    const blob = await new Promise((resolve) =>
+      canvas.toBlob(resolve, 'image/webp'),
+    )
+    // const dataUrl = canvas.toDataURL('image/png')
+    const downloadLink = document.createElement('a')
+    downloadLink.href = URL.createObjectURL(blob)
+    downloadLink.download = name
 
-  const { data } = await useFetch(imageSrc)
-  const image = data.value
-
-  if (!(image instanceof Blob)) {
-    return
+    // Trigger the download
+    downloadLink.click()
   }
-
-  const link = document.createElement('a')
-  link.href = URL.createObjectURL(image)
-  name += FILE_SUFFIX
-  link.download = name
-  document.body.appendChild(link)
-  link.click()
-  document.body.removeChild(link)
-  return link
 }
 
 export const downloadBase64Image = (base64: string, name = 'unnamed') => {
   const a = document.createElement('a')
   a.href = base64
-  name += FILE_SUFFIX
   a.download = name
   document.body.appendChild(a)
   a.click()
