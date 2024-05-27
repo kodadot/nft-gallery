83526c3528cdae26559b0fdb4916e7ecf4a0b9bf
Preschian Febryantara <preschian@users.noreply.github.com>
2024-05-27 18:11:02 +0700

    * fix(GalleryItem.vue): modify mime-type attribute to check for nftAnimation existence * feat(GalleryItemToolBar.vue): add conditional rendering for newTab button based on disableNewTab computed property * refactor(useGalleryItem.ts): rearrange assignment of nftImage and nftMimeType values after nftAnimation and nftAnimationMimeType

--- components/gallery/useGalleryItem.ts
+++ components/gallery/useGalleryItem.ts
@@ -100,17 +100,17 @@ export const useGalleryItem = (nftId?: string): GalleryItem => {
     nftMetadata.value = metadata
     nftResources.value = resources
 
-    nftImage.value = sanitizeIpfsUrl(metadata.image) || ''
-    nftMimeType.value =
-      metadata.imageMimeType ||
-      metadata.type ||
-      (await getMimeType(nftImage.value)) ||
-      ''
     nftAnimation.value =
       sanitizeIpfsUrl(metadata.animationUrl || metadata.animation_url) || ''
     nftAnimationMimeType.value =
       metadata.animationUrlMimeType ||
       (await getMimeType(nftAnimation.value)) ||
+      ''
+    nftImage.value = sanitizeIpfsUrl(metadata.image) || ''
+    nftMimeType.value =
+      metadata.imageMimeType ||
+      metadata.type ||
+      (await getMimeType(nftImage.value)) ||
       ''
 
     // use cf-video & replace the video thumbnail
