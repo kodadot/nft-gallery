322e6b689fb85f6182dbd4238bc25a4f1222dbb4
Preschian Febryantara <preschian@users.noreply.github.com>
2024-05-28 09:06:11 +0700

    * refactor(randomize.spec.ts): comment out uidMathDate and use methods array for it.each test function

--- tests/randomize.spec.ts
+++ tests/randomize.spec.ts
@@ -1,12 +1,18 @@
-import { uidCrypto, uidMathDate } from '../utils/randomize'
+import {
+  uidCrypto,
+  // uidMathDate,
+} from '../utils/randomize'
 
 const users = 100 // concurrent users
 
 // uidMathDate() has less randomness in its format because it has a date prefix (yyMM). However, it has a slightly higher chance than the others of generating a non-unique ID because of the date prefix.
-
 // for more than 1k concurrent users, prefer uidCrypto() or uidMath() to ensure uniqueness
+const methods = [
+  uidCrypto,
+  // uidMathDate
+]
 
-it.each([uidCrypto, uidMathDate])('generate unique id', (random) => {
+it.each(methods)('generate unique id', (random) => {
   const ids = new Set<string>()
 
   // eslint-disable-next-line no-restricted-syntax
