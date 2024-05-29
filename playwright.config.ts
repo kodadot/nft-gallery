17595dfefa6100b0e331cc70d42e52a24399b591
Preschian Febryantara <preschian@users.noreply.github.com>
2024-05-29 20:20:05 +0700

    * fix(playwright.config.ts): reduce TIMEOUT from 2 minutes to 1 minute and retries from 3 to 1

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, devices } from '@playwright/test'
  */
 // require('dotenv').config();
 
-const TIMEOUT = 2 * 60 * 1000
+const TIMEOUT = 1 * 60 * 1000
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 3 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
