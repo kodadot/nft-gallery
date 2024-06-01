8f83093f8012e8e924a87838e0ddd6064275478f
hassnian <hassnianxd@gmail.com>
2024-06-01 14:01:12 +0500

    add(cookies): configure cookies using settings

---
+++ components/settings/UserData.vue
@@ -0,0 +1,11 @@
+<template>
+  <NeoButton @click="openModal">{{
+    $t('profileMenu.configureCookies')
+  }}</NeoButton>
+</template>
+
+<script lang="ts" setup>
+import { NeoButton } from '@kodadot1/brick'
+
+const { openModal } = useCookies()
+</script>
