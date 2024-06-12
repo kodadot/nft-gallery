4178e917047f223bdf6ce7f316e4d7e69e60cee5
prury <prury@fakemail.com>
2024-06-12 08:21:54 -0300

    remove wrong data-testid

--- components/common/ConnectWallet/ConnectWalletModal.vue
+++ components/common/ConnectWallet/ConnectWalletModal.vue
@@ -3,7 +3,6 @@
     class="wallet-modal-container flex flex-col"
     data-testid="wallet-connect-sidebar-modal">
     <NeoModalHead
-      data-testid="kkkkkkkkkkkk"
       :title="
         showAccount
           ? $i18n.t('profile.page')
