91832a4cb8be5a3c8098d1da984d77cd297d8b0f
hassnian <hassnianxd@gmail.com>
2024-06-17 14:24:39 +0500

    add(IdentityPopoverFooter.vue): add nft skeleton loader

--- components/identity/module/IdentityPopoverFooter.vue
+++ components/identity/module/IdentityPopoverFooter.vue
@@ -22,27 +22,40 @@
       </div>
     </div>
 
-    <div v-if="nftEntities.length" class="grid grid-cols-2 gap-5">
-      <div v-for="nft in nftEntities" :key="nft.id">
-        <GalleryCard
-          :id="nft.id"
-          hide-name
-          :metadata="nft.metadata"
-          :current-owner="nft.currentOwner"
-          :route="`/${urlPrefix}/gallery`"
-          :data-testid="nftEntities.indexOf(nft)" />
-      </div>
+    <div class="grid grid-cols-2 gap-5">
+      <template v-if="!nftEntities.length">
+        <div
+          v-for="skeleton in NFT_AMOUNT"
+          :key="skeleton"
+          class="aspect-square relative border border-border-color">
+          <NeoSkeleton :rounded="false" no-margin full-size />
+        </div>
+      </template>
+      <template v-else>
+        <div v-for="nft in nftEntities" :key="nft.id">
+          <GalleryCard
+            :id="nft.id"
+            hide-name
+            :metadata="nft.metadata"
+            :current-owner="nft.currentOwner"
+            :route="`/${urlPrefix}/gallery`"
+            :data-testid="nftEntities.indexOf(nft)" />
+        </div>
+      </template>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { NeoSkeleton } from '@kodadot1/brick'
 import { fetchFollowersOf } from '@/services/profile'
 import useIdentityStats from '../utils/useIdentityStats'
 import type { NFT } from '@/components/rmrk/service/scheme'
 const GalleryCard = defineAsyncComponent(
   () => import('../../rmrk/Gallery/GalleryCard.vue'),
 )
+
+const NFT_AMOUNT = 2
 
 const address = inject('address') as string
 
@@ -69,7 +82,7 @@ const { data } = useSearchNfts({
       ],
     },
   ],
-  first: 2,
+  first: NFT_AMOUNT,
   orderBy: 'updatedAt_DESC',
 })
 
