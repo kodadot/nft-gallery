0117dc145f13188a0447a45299bf27cfebe71a0d
Viki Val <viktorko99@gmail.com>
2024-06-19 10:48:47 +0200

    Update components/codeChecker/issueHint/NotUsingExternalResources.vue

--- components/codeChecker/issueHint/NotUsingExternalResources.vue
+++ components/codeChecker/issueHint/NotUsingExternalResources.vue
@@ -9,7 +9,7 @@
 <script setup lang="ts">
 const code = `
   <!-- DON'T ❌ -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.2/p5.js" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.2/p5.min.js" />
 
   <!-- DO ✅-->
   <script src="./p5.min.js" />
