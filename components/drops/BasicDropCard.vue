b60365f9f757a828bf91dcc4d6d4251e8200bd0c
hassnian <hassnianxd@gmail.com>
2024-06-26 08:08:44 +0500

    add(drops): user fiendly prefix name map

--- components/drops/BasicDropCard.vue
+++ components/drops/BasicDropCard.vue
@@ -23,13 +23,16 @@
                 >{{ name }}</span
               >
               <div
+                v-if="dropPrefix"
                 class="flex items-center rounded-full bg-neutral-3 dark:bg-neutral-11 px-[6px] py-[3px] h-[22px] gap-[0.375rem]">
                 <img
                   v-if="dropIcon"
                   :src="dropIcon"
                   class="w-[12.5px] aspect-square flex-shrink-0"
                   alt="chain icon" />
-                <span class="text-sm uppercase">{{ dropPrefix }}</span>
+                <span class="text-sm uppercase">{{
+                  UserFriendlyPrefixNameMap[dropPrefix] ?? dropPrefix
+                }}</span>
               </div>
             </div>
 
@@ -68,6 +71,12 @@
 <script setup lang="ts">
 import type { Prefix } from '@kodadot1/static'
 import { DropStatus } from '@/components/drops/useDrops'
+
+const UserFriendlyPrefixNameMap: Partial<Record<Prefix, string>> = {
+  ahp: 'dot',
+  base: 'base',
+  ahk: 'ahk',
+}
 
 const emit = defineEmits(['click'])
 const props = withDefaults(
