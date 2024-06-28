21908862e92191f2e4951f2d6b25581623bf8d75
mikechao2022 <mike.chao2022@gmail.com>
2024-06-28 10:17:22 -0400

    fix: update the Non-genart icon z-index

--- components/base/MediaItem.vue
+++ components/base/MediaItem.vue
@@ -31,7 +31,7 @@
     </div>
     <div
       v-if="hasNormalTag"
-      class="bg-k-shade border-k-grey text-text-color flex items-center justify-center border rounded-md absolute right-3 top-3 image is-24x24">
+      class="bg-k-shade border-k-grey text-text-color flex items-center justify-center border rounded-md absolute right-3 top-3 image is-24x24 z-[18]">
       <NeoIcon icon="image" pack="far" class="text-sm font-medium" />
     </div>
     <NeoButton
