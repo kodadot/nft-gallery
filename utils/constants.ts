c1a784b135d2871aae5607072aa52fd27abb3e7c
Viki Val <viktorko99@gmail.com>
2024-07-19 09:33:08 +0200

    :wrench: return market manipulator to deny list

--- utils/constants.ts
+++ utils/constants.ts
@@ -19,6 +19,7 @@ export const ksmHubDenyList = [
 export const dotHubDenyList = [
   '128qRiVjxU3TuT37tg7AX99zwqfPtj2t4nDKUv9Dvi5wzxuF', // RMRK test
   '155HWw3J9jyYphMm5is4vp9Bzj7ZRRd6HEzCPdWd8cq97KfT', // Very old unreacheable IPFS
+  '16aWyT5Xa2wogTARQxk8LnQqi5nquy3Xrc4YcgJrmjEWrduq', // Market Manipulation
 ]
 
 export const IPFS_KODADOT_IMAGE_PLACEHOLDER =
