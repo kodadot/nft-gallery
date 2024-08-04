66e079201ae47bfdc3d6efb98ab344d31b2972d7
Jarsen <31397967+Jarsen136@users.noreply.github.com>
2024-08-04 23:04:37 +0200

    fix: profile text

--- components/profile/ProfileDetail.vue
+++ components/profile/ProfileDetail.vue
@@ -236,9 +236,9 @@
           />
         </div>
         <!-- Followers -->
-        <div>
+        <div v-if="!isOwner">
           <span
-            v-if="isOwner || !hasProfile || followersCount == 0"
+            v-if="!hasProfile || followersCount == 0"
             class="text-sm text-k-grey"
           >
             {{ $t('profile.notFollowed') }}
