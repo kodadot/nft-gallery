ff2ec26e05084b6780234f81c8e82c96ac5aed60
Preschian Febryantara <preschian@users.noreply.github.com>
2024-08-07 20:28:34 +0700

    feat(ogi): add service to fetch EVM collection data refactor(evm): integrate ogi service for fetching EVM collection details

--- utils/onchain/evm.ts
+++ utils/onchain/evm.ts
@@ -1,6 +1,8 @@
 import { http, createConfig, readContract } from '@wagmi/core'
 import { base, immutableZkEvm } from '@wagmi/core/chains'
-import { GENSOL_ABI } from '~/composables/transaction/evm/utils'
+import type { Prefix } from '@kodadot1/static'
+import { GENSOL_ABI } from '@/composables/transaction/evm/utils'
+import { getEvmCollection } from '@/services/ogi'
 
 export const wagmiConfig = createConfig({
   chains: [base, immutableZkEvm],
@@ -10,7 +12,7 @@ export const wagmiConfig = createConfig({
   },
 })
 
-export const evmCollection = async (address: `0x${string}`, chain: string) => {
+export const evmCollection = async (address: `0x${string}`, chain: Prefix) => {
   const [collectionSupply, collectionMetadata, getCollection] = await Promise.all([
     readContract(wagmiConfig, {
       address,
@@ -22,13 +24,15 @@ export const evmCollection = async (address: `0x${string}`, chain: string) => {
       abi: GENSOL_ABI,
       functionName: 'contractURI',
     }),
+
     // usually erc721 contract have minted function to get the minted count
     // 1. drops on opensea: https://etherscan.io/token/0xc5d914aea6f463f8e3f2797b4e258904c82cedad
     // use totalSupply to get the minted count. maxSupply also exists
     // 2. drops on fxhash: https://basescan.org/token/0xAC2D45854EAFF6eF61c2d5c8c7224Ea255937a9C
     // also use totalSupply to get the minted count. maxSupply = totalSupply + remainingSupply
+
     // at the moment use api from ogi is because our contract doesn't have the minted function
-    $fetch(`https://ogi-feat--evm-api-for-item-d.workers-ogi.pages.dev/api/evm/${chain}/collection/${address}`),
+    getEvmCollection(chain, address),
   ]) as [bigint, string, { claimed?: string }]
 
   let metadata = { description: '', name: '', image: '' }
