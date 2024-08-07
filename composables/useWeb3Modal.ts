c0e84d91853854e3b1c133ec4d8139b45abc6254
hassnian <hassnianxd@gmail.com>
2024-08-07 16:53:26 +0500

    ref(useWeb3Modal.ts): use `modal` from `createWeb3Modal`

--- composables/useWeb3Modal.ts
+++ composables/useWeb3Modal.ts
@@ -1,19 +1,18 @@
-import { createWeb3Modal, useWeb3Modal } from '@web3modal/wagmi/vue'
+import { createWeb3Modal } from '@web3modal/wagmi/vue'
+import type { Web3Modal } from '@web3modal/wagmi'
 
-const modal = ref()
+const modal = ref<Web3Modal>()
 
 export default () => {
   const { urlPrefix } = usePrefix()
   const { $wagmiConfig } = useNuxtApp()
 
-  createWeb3Modal({
-    wagmiConfig: $wagmiConfig as any,
-    projectId: useRuntimeConfig().public.walletConnectProjectId,
-    defaultChain: PREFIX_TO_CHAIN[urlPrefix.value],
-  })
-
   if (!modal.value) {
-    modal.value = useWeb3Modal()
+    modal.value = createWeb3Modal({
+      wagmiConfig: $wagmiConfig as any,
+      projectId: useRuntimeConfig().public.walletConnectProjectId,
+      defaultChain: PREFIX_TO_CHAIN[urlPrefix.value],
+    })
   }
 
   return {
