79cd2bbad53ce7b0ef51701e748648fb14947da4
Jarsen <31397967+Jarsen136@users.noreply.github.com>
2024-08-20 16:38:51 +0200

    chore: computed

--- components/common/ConnectWallet/WalletTabs.vue
+++ components/common/ConnectWallet/WalletTabs.vue
@@ -21,16 +21,14 @@ const props = defineProps<{ modelValue: ChainVM, preselected?: ChainVM }>()
 
 const selectedTab = useVModel(props, 'modelValue')
 
-const walletTabs = computed<{ label: string, value: ChainVM }[]>(() => {
-  return [
-    {
-      label: 'Polkadot',
-      value: 'SUB',
-    },
-    {
-      label: 'Evm',
-      value: 'EVM',
-    },
-  ]
-})
+const walletTabs: { label: string, value: ChainVM }[] = [
+  {
+    label: 'Polkadot',
+    value: 'SUB',
+  },
+  {
+    label: 'Evm',
+    value: 'EVM',
+  },
+]
 </script>
