fbd27d52d80edacf3224bc200ee8361828d2e9aa
Preschian Febryantara <preschian@users.noreply.github.com>
2024-09-05 12:53:07 +0700

    feat(gallery): integrate NFT store with GalleryItem component

--- pages/[prefix]/gallery/[id].vue
+++ pages/[prefix]/gallery/[id].vue
@@ -2,7 +2,30 @@
   <GalleryItem />
 </template>
 
-<script>
+<script setup lang="ts">
+import { useGalleryItem } from '@/components/gallery/useGalleryItem'
+import { useNftStore } from '@/stores/nft'
+
+const nftStore = useNftStore()
+
+const {
+  nft,
+  nftMetadata,
+  nftImage,
+  nftAnimation,
+  nftAnimationMimeType,
+  nftMimeType,
+} = useGalleryItem()
+
+watchEffect(() => {
+  nftStore.nft = nft.value
+  nftStore.nftMetadata = nftMetadata.value
+  nftStore.nftImage = nftImage.value
+  nftStore.nftAnimation = nftAnimation.value
+  nftStore.nftAnimationMimeType = nftAnimationMimeType.value
+  nftStore.nftMimeType = nftMimeType.value
+})
+
 definePageMeta({
   layout: 'gallery-item-layout',
 })
