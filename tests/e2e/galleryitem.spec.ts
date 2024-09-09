eb9c4402bd3c3064ce9eb61584865a54b3e575d5
hassnian <hassnianxd@gmail.com>
2024-09-09 13:36:31 +0500

    fix(galleryitem.spec.ts): chart tab index

--- tests/e2e/galleryitem.spec.ts
+++ tests/e2e/galleryitem.spec.ts
@@ -45,7 +45,7 @@ test('Gallery item Interactions', async ({ page }) => {
   await test.step('Verify chart visibility', async () => {
     await page
       .getByTestId('gallery-item-tabs')
-      .locator('[aria-controls="2-content"]')
+      .locator('[aria-controls="3-content"]')
       .click()
     await expect(page.getByTestId('gallery-item-chart')).toBeVisible()
   })
