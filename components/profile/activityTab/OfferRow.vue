a16701d723b37337d1e33f4f34f34a3b35e05fdd
hassnian <hassnianxd@gmail.com>
2024-09-10 16:22:08 +0500

    fix(Offers.vue): offers `outgoing` `incoming` should be tabs

--- components/profile/activityTab/OfferRow.vue
+++ components/profile/activityTab/OfferRow.vue
@@ -58,15 +58,15 @@
       <div class="h-[50px] flex items-center gap-2">
         <ProfileAvatar
           :size="24"
-          :address="offer.desired.currentOwner"
+          :address="targetAddress"
         />
 
         <nuxt-link
-          :to="`/${urlPrefix}/u/${offer.desired.currentOwner}`"
+          :to="`/${urlPrefix}/u/${targetAddress}`"
           class="text-k-blue hover:text-k-blue-hover"
         >
           <IdentityIndex
-            :address="offer.desired.currentOwner"
+            :address="targetAddress"
           />
         </nuxt-link>
       </div>
@@ -155,14 +155,14 @@
           <div class="flex items-center gap-2">
             <ProfileAvatar
               :size="24"
-              :address="offer.desired.currentOwner"
+              :address="targetAddress"
             />
             <nuxt-link
-              :to="`/${urlPrefix}/u/${offer.desired.currentOwner}`"
+              :to="`/${urlPrefix}/u/${targetAddress}`"
               class="text-k-blue hover:text-k-blue-hover"
             >
               <IdentityIndex
-                :address="offer.desired.currentOwner"
+                :address="targetAddress"
               />
             </nuxt-link>
           </div>
@@ -195,6 +195,7 @@ defineEmits(['select'])
 const props = defineProps<{
   offer: NFTOfferItem
   variant: 'Desktop' | 'Touch'
+  tab: 'outgoing' | 'incoming'
 }>()
 
 const { urlPrefix } = usePrefix()
@@ -205,6 +206,7 @@ const image = ref()
 const animationUrl = ref()
 const isDesktop = computed(() => props.variant === 'Desktop')
 const isExpired = computed(() => props.offer.status === 'EXPIRED')
+const targetAddress = computed(() => props.tab === 'outgoing' ? props.offer.desired.currentOwner : props.offer.caller)
 
 const interactionName = computed(
   () =>
