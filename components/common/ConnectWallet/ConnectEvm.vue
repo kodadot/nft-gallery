e6fd128bd014bf872b513ccd89e6e4db1f475064
hassnian <hassnianxd@gmail.com>
2024-09-12 11:44:04 +0500

    add(ConnectEvm.vue): close active connections before openning web3modal

--- components/common/ConnectWallet/ConnectEvm.vue
+++ components/common/ConnectWallet/ConnectEvm.vue
@@ -3,9 +3,9 @@
     <NeoButton
       variant="primary"
       no-shadow
-      :loading="isConnecting"
-      :disabled="isConnecting"
-      loading-with-label
+      :loading="loading"
+      :disabled="loading"
+      :loading-with-label="isConnecting"
       size="large"
       class="w-full"
       @click="modal?.open()"
@@ -15,7 +15,7 @@
           {{ isConnecting ? 'Connecting' : 'Connect with Web3Modal' }}
         </span>
         <NeoIcon
-          v-if="!isConnecting"
+          v-if="!loading"
           icon="arrow-right"
         />
       </div>
@@ -25,13 +25,23 @@
 
 <script lang="ts" setup>
 import { NeoButton, NeoIcon } from '@kodadot1/brick'
-import { useAccount } from '@wagmi/vue'
+import { useAccount, useDisconnect, useConnections } from '@wagmi/vue'
 
 const emits = defineEmits(['select'])
 
 const { address, isConnected, isConnecting, chainId } = useAccount()
 const { urlPrefix, setUrlPrefix } = usePrefix()
 const { modal } = useWeb3Modal()
+const { disconnectAsync: disconnect } = useDisconnect()
+const connections = useConnections()
+
+const disconnecting = ref(false)
+const loading = computed(() => isConnecting.value || disconnecting.value)
+
+if (connections.value.length) {
+  disconnecting.value = true
+  disconnect().then(() => disconnecting.value = false)
+}
 
 watch([address, isConnected, chainId], ([address, isConnected, chainId]) => {
   const chainPrefix = CHAIN_ID_TO_PREFIX?.[chainId ?? '']
