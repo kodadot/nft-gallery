f825b0e53e14cffbd0ba500c7807bd40bec3e0cc
Jarsen <31397967+Jarsen136@users.noreply.github.com>
2024-09-29 21:14:01 +0200

    fix: remove clarity

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -192,16 +192,6 @@ export default defineNuxtConfig({
           crossorigin: 'anonymous',
           async: true,
         },
-        process.env.NODE_ENV !== 'development'
-          ? {
-              // https://learn.microsoft.com/en-us/clarity/setup-and-installation/clarity-setup
-              innerHTML: `(function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "kksold2jal");`,
-              type: 'text/javascript',
-            }
-          : undefined,
       ],
     },
   },
