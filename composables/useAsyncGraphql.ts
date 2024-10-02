8a0cc9d654a0453fdaeabef4382ca5ee19b3187a
hassnian <hassnianxd@gmail.com>
2024-10-02 12:27:15 +0500

    add(collection activity.): loading skeleton

--- composables/useAsyncGraphql.ts
+++ composables/useAsyncGraphql.ts
@@ -4,12 +4,15 @@ type AyncQueryParams = {
   query: string
   variables: Record<string, unknown>
   clientId?: string
+  prefix?: string
 }
 
-export default async function ({ query, clientId, variables }: AyncQueryParams) {
+export default async function ({ query, variables, clientId, prefix }: AyncQueryParams) {
+  const { urlPrefix, client } = usePrefix()
+
   return useAsyncQuery({
-    query: (await resolveQueryPath(clientId || usePrefix().client.value, query)).default,
+    query: (await resolveQueryPath(prefix || urlPrefix.value, query)).default,
     variables: variables,
-    clientId: clientId,
+    clientId: clientId || client.value,
   })
 }
