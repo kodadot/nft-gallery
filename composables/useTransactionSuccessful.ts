df333617de8d0085754fcb0afcfaee081ba2cc6a
hassnian <hassnianxd@gmail.com>
2024-10-04 11:08:19 +0500

    ref(useTransactionSuccessful.ts): rollback to prev simpler version

--- composables/useTransactionSuccessful.ts
+++ composables/useTransactionSuccessful.ts
@@ -11,33 +11,24 @@ export default ({
   status,
   isLoading,
 }: TransactionSuccessfulParams) => {
-  const isSuccessful = ref<boolean>(false)
-
-  const isTransactionSuccessful = (status: TransactionStatus, isError: boolean) => {
-    if ([
-      TransactionStatus.Unknown,
-      TransactionStatus.Casting,
-      TransactionStatus.Broadcast,
-    ].includes(status)) {
-      return false
-    }
-
-    return TransactionStatus.Block === status ? !isError : TransactionStatus.Finalized === status
-  }
+  const isTransactionSuccessful = ref<boolean>(false)
 
   watch(isLoading, (loading) => {
     if (loading) {
-      isSuccessful.value = false
+      isTransactionSuccessful.value = false
     }
   })
 
   watchDebounced(
     [status, isError],
-    ([status, isError]) => {
-      isSuccessful.value = isTransactionSuccessful(status, isError)
+    () => {
+      isTransactionSuccessful.value
+        = TransactionStatus.Block === status.value
+          ? !isError.value
+          : TransactionStatus.Finalized === status.value
     },
     { debounce: 500 }, // tx eroror case: status gets updated before errorCb is called and isError is updated
   )
 
-  return { isTransactionSuccessful: isSuccessful }
+  return { isTransactionSuccessful }
 }
