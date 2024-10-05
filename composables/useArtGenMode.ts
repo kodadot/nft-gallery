c2e0672ec3ef2bb83f0a39fedbd1c328260236ad
Jarsen <31397967+Jarsen136@users.noreply.github.com>
2024-10-05 17:22:49 +0200

    fix: init art mode

--- composables/useArtGenMode.ts
+++ composables/useArtGenMode.ts
@@ -1,5 +1,3 @@
-import { useExploreFiltersStore } from '@/stores/exploreFilters'
-
 const enabledOnPages = [
   'prefix-explore-items', // explore items page
   'prefix-explore-collectibles', // explore collections page
@@ -10,11 +8,10 @@ const enabledPrefix = ['ahp', 'base', 'mnt']
 export default function () {
   const route = useRoute()
   const { urlPrefix } = usePrefix()
-  const exploreFiltersStore = useExploreFiltersStore()
   const artGenModeFeatureEnabled = computed(() => isArtGenDomain && enabledPrefix.includes(urlPrefix.value) && enabledOnPages.includes(route.name as string ?? ''))
   return {
     artGenModeEnabled: computed(() =>
-      artGenModeFeatureEnabled.value && exploreFiltersStore.artGen,
+      artGenModeFeatureEnabled.value && route.query.gen_art?.toString() === 'true',
     ),
     artGenModeFeatureEnabled,
   }
