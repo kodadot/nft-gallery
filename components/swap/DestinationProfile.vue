0fe2f3d7516ed5e0b3664abed39a46fb5ad5ddc3
roiLeo <medina.leo42@gmail.com>
2024-10-24 11:22:18 +0200

    ✨ wip: swap page Step 2 list

--- components/swap/DestinationProfile.vue
+++ components/swap/DestinationProfile.vue
@@ -1,7 +1,7 @@
 <template>
-  <section class="pt-5 container is-fluid">
+  <section class="pt-5 container is-fluid flex flex-col space-y-10">
     <div class="flex justify-between">
-      <div class="flex flex-col">
+      <div class="flex flex-col justify-center">
         <h1 class="title">
           {{ $t('swap.selectNft') }}
         </h1>
@@ -35,10 +35,19 @@
 
     <div class="columns">
       <div class="column">
-        <hr>
+        <hr class="mb-6 mt-0">
+        // display list here
+        <ItemsGrid
+          :search="itemsGridSearch"
+          grid-size="medium"
+          collection-popover-hide
+          hide-listing
+          show-timestamp
+          :reset-search-query-params="['sort']"
+        />
       </div>
-      <div class="column">
-        <div class="border bg-background-color shadow-primary p-5 pb-6 w-full h-min md:w-[444px] lg:w-[490px] relative">
+      <div class="column is-narrow">
+        <div class="border bg-background-color shadow-primary pb-6 w-full h-min md:w-[444px] lg:w-[490px] relative">
           <header class="px-6 py-4 flex justify-between border-b items-center">
             <div class="text-base font-bold line-height">
               Your Swap List
@@ -52,7 +61,17 @@
 
 <script setup lang="ts">
 import { NeoIcon } from '@kodadot1/brick'
+import ItemsGrid from '@/components/items/ItemsGrid/ItemsGrid.vue'
 
 const { accountId } = useAuth()
 const route = useRoute()
+
+const itemsGridSearch = computed(() => {
+  const query: Record<string, any> = {
+    issuer_eq: route.params.id,
+    burned_eq: false,
+  }
+
+  return query
+})
 </script>
