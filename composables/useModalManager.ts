5822c6cd9a282ed1810ecdcf636647f748334b49
hassnian <44554284+hassnian@users.noreply.github.com>
2024-10-25 11:21:19 +0500

    add: add funds modal without overlap

---
+++ composables/useModalManager.ts
@@ -0,0 +1,39 @@
+const modals = ref(new Map<symbol, Ref>())
+
+type ModalRef = Ref<{ isActive: boolean }>
+
+export default function (modalRef: Ref) {
+  const targetModals = ref<ModalRef[]>([])
+  const symbol = Symbol()
+
+  modals.value.set(symbol, modalRef)
+
+  const otherModals = computed(() => {
+    return Array.from(modals.value.keys())
+      .filter(modal => modal !== symbol)
+      .map(modal => modals.value.get(modal)) as ModalRef[]
+  })
+
+  const closeOthers = () => {
+    for (const modal of otherModals.value) {
+      if (modal.value?.isActive) {
+        modal.value.isActive = false
+        targetModals.value?.push(modal)
+      }
+    }
+  }
+
+  const openClosed = () => {
+    for (const modal of targetModals.value) {
+      modal.value.isActive = true
+    }
+    targetModals.value = []
+  }
+
+  onUnmounted(() => modals.value.delete(symbol))
+
+  return {
+    closeOthers,
+    openClosed,
+  }
+}
