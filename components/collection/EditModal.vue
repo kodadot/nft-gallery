c27e4c95a6cf834292a6dcee67e49081bbe4f12f
Viki Val <viktorko99@gmail.com>
2024-12-04 12:08:43 +0200

    Update components/collection/EditModal.vue

--- components/collection/EditModal.vue
+++ components/collection/EditModal.vue
@@ -24,7 +24,7 @@
           />
         </NeoField>
 
-        <!-- nft description -->
+        <!-- collection description -->
         <NeoField :label="$t('mint.collection.description.label')">
           <NeoInput
             v-model="description"
