7371259ff36ac67e62c3825ce576adb4a5d22d0a
Viki Val <viktorko99@gmail.com>
2024-12-10 23:24:17 +0200

    :broom: no more minimark in composables/transaction/transactionList.ts

--- composables/transaction/transactionList.ts
+++ composables/transaction/transactionList.ts
@@ -1,4 +1,4 @@
-import { Interaction } from '@kodadot1/minimark/v1'
+import { Interaction } from '@kodadot1/static'
 import type { ActionList, TokenToList } from './types'
 import {
   assetHubParamResolver,
