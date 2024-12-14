0e45c02da1bafa82b5fce995c35ab951b302c3d5
hassnian <44554284+hassnian@users.noreply.github.com>
2024-12-14 10:26:07 +0500

    add: `ExploreLayoutWithSidebar.vue` component
    - remove duplication on explore pages with sidebar

---
+++ components/ExploreLayoutWithSidebar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="container is-fluid"
+    :class="{ 'sidebar-padding-left': isSidebarOpen }"
+  >
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+// convert to nested layout:  explore-layout-with-sidebar -> explore-layout
+const { getsidebarFilterCollapse: isSidebarOpen } = storeToRefs(usePreferencesStore())
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/abstracts/variables';
+
+.sidebar-padding-left {
+  padding-left: 0;
+}
+
+// this cover the edge case where sidebar is open and then screen size changes to mobile
+// for exmpale on rotation of tablet device
+// in that case the padding need to match that of the fluid container
+@include mobile {
+  .sidebar-padding-left {
+    padding-left: $fluid-container-padding-mobile;
+  }
+}
+</style>
