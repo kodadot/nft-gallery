5bd2763ecf318c194b7c14c637522d721dc155b0
hassnian <44554284+hassnian@users.noreply.github.com>
2024-12-17 12:19:06 +0500

    ref(ExploreLayoutWithSidebar.vue): remove edge case and use tailwind css class for padding

--- components/ExploreLayoutWithSidebar.vue
+++ components/ExploreLayoutWithSidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="container is-fluid"
-    :class="{ 'sidebar-padding-left': isSidebarOpen }"
+    :class="{ '!pl-0': isSidebarOpen }"
   >
     <slot />
   </div>
@@ -11,20 +11,3 @@
 // convert to nested layout:  explore-layout-with-sidebar -> explore-layout
 const { getsidebarFilterCollapse: isSidebarOpen } = storeToRefs(usePreferencesStore())
 </script>
-
-<style lang="scss" scoped>
-@import '@/assets/styles/abstracts/variables';
-
-.sidebar-padding-left {
-  padding-left: 0;
-}
-
-// this cover the edge case where sidebar is open and then screen size changes to mobile
-// for exmpale on rotation of tablet device
-// in that case the padding need to match that of the fluid container
-@include mobile {
-  .sidebar-padding-left {
-    padding-left: $fluid-container-padding-mobile;
-  }
-}
-</style>
