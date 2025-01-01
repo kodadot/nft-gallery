fa5a22b27183783d5c04193ed88c92d0242a897a
Jarsen <31397967+Jarsen136@users.noreply.github.com>
2025-01-01 16:30:19 +0100

    fix: stop user from inputting their own address

--- components/swap/landing.vue
+++ components/swap/landing.vue
@@ -61,7 +61,7 @@
 <script lang="ts" setup>
 import { NeoButton } from '@kodadot1/brick'
 
-const { isCurrentOwner } = useAuth()
+const { isCurrentAccount } = useAuth()
 const { $i18n } = useNuxtApp()
 
 const traderAddress = ref('')
@@ -89,7 +89,7 @@ const label = computed(() => {
 
 const handleAddressCheck = (isValid: boolean) => {
   isTraderAddressValid.value = isValid
-  isYourAddress.value = isTraderAddressValid.value ? isCurrentOwner(traderAddress.value) : false
+  isYourAddress.value = isTraderAddressValid.value ? isCurrentAccount(traderAddress.value) : false
 }
 
 const handleSubmit = async () => {
