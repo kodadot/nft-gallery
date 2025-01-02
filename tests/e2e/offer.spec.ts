fad39ce4241e7ed311e2179bb8b6a87c2dd68318
Preschian Febryantara <preschian@users.noreply.github.com>
2025-01-02 13:13:10 +0700

    test: rename e2e test for offer modal interaction

--- tests/e2e/offer.spec.ts
+++ tests/e2e/offer.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from './fixtures'
 
-test('Landing Elements', async ({ page, Commands }) => {
+test('show offer modal', async ({ page, Commands }) => {
   await Commands.e2elogin()
   await page.goto('/ahp/gallery/256-4235681345')
   await page.getByTestId('gallery-item-trade-make-offer').click()
