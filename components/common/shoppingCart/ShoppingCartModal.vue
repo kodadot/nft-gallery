b0507bd793a01cb54f420406d8b7bece030a7f1c
Preschian Febryantara <preschian@users.noreply.github.com>
2025-01-09 17:54:11 +0700

    refactor: update ShoppingCartModal styles for improved responsiveness

--- components/common/shoppingCart/ShoppingCartModal.vue
+++ components/common/shoppingCart/ShoppingCartModal.vue
@@ -190,18 +190,18 @@ const onCompletePurchase = () => {
 </script>
 
 <style scoped lang="scss">
-@import '@/assets/styles/abstracts/variables';
 .shopping-cart-modal {
   .shopping-cart-modal-container {
     position: fixed;
     top: 0;
     right: 0;
     height: 100%;
-    padding-top: $navbar-desktop-min-height;
+    padding-top: 80px;
     max-width: 360px;
     width: 100%;
-    @include mobile {
-      padding-top: $navbar-mobile-min-height;
+
+    @media (max-width: 640px) {
+      padding-top: 64px;
       max-width: 100vw;
     }
   }
