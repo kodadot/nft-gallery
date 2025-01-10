735baf5c4ffff0b2caf6ca566aecdafccf4c581c
hassnian <44554284+hassnian@users.noreply.github.com>
2025-01-10 14:09:47 +0500

    ref(collectionEntities): simplify `useOwnedCollections`
    -remove: getOwnedCollections in utils/query.ts
    -add: collectionIdList query

--- composables/useOwnedCollections.ts
+++ composables/useOwnedCollections.ts
@@ -1,8 +1,23 @@
 import { useQuery } from '@tanstack/vue-query'
+import collectionIdList from '@/queries/subsquid/general/collectionIdList.graphql'
 
-export function useOwnedCollections(id: Ref<string>) {
+export function useOwnedCollections(id: Ref<string>, { client = usePrefix().client } = {}) {
   return useQuery({
     queryKey: ['ownedCollections', id],
-    queryFn: () => getOwnedCollections(id.value),
+    queryFn: async () => {
+      const { data } = await useAsyncQuery<{ collectionEntities: { id: string }[] }>({
+        query: collectionIdList,
+        variables: {
+          search: {
+            nfts_some: {
+              currentOwner_eq: id.value,
+            },
+          },
+        },
+        clientId: client.value,
+      })
+
+      return data.value.collectionEntities
+    },
   })
 }
