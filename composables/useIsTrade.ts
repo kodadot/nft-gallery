f68946b65b3405600108129c51b4e9c8690912da
hassnian <44554284+hassnian@users.noreply.github.com>
2025-01-11 07:46:34 +0500

    fix: add missing trade type imports

--- composables/useIsTrade.ts
+++ composables/useIsTrade.ts
@@ -1,3 +1,5 @@
+import type { TradeNftItem } from '@/components/trade/types'
+
 export default function (trade: ComputedRef<TradeNftItem | undefined>, target: MaybeRef<string>) {
   const isCreatorOfTrade = computed(() => trade.value?.caller === unref(target))
   const isTargetOfTrade = computed(() => trade.value?.targets.includes(unref(target)))
