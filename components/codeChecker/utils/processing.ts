8f89c913b4c499f2aa3453e7ff96670e4e088d27
hassnian <44554284+hassnian@users.noreply.github.com>
2025-01-14 17:28:23 +0500

    add(codeChecker): support for `loadTable` function

---
+++ components/codeChecker/utils/processing.ts
@@ -0,0 +1,91 @@
+import type { ZipEntry } from 'unzipit'
+import config from '../codechecker.config'
+import { getObjectUrl, getUpload, uploadFile } from '@/services/playground'
+
+const getProxiedUrl = (url: string): string => {
+  // deploy own worker
+  return `https://api.codetabs.com/v1/proxy?quest=${encodeURIComponent(url)}`
+}
+
+type FileProcessingHandler = {
+  processInstance: (params: { content: string, entry: ZipEntry, entries: Record<string, ZipEntry> }) => Promise<string>
+  findAll: (content: string) => RegExpMatchArray | null
+}
+
+const loadTableHandler: FileProcessingHandler = {
+  findAll: (content: string) => content.match(/loadTable\([^)]+\)/g),
+  processInstance: async ({ content, entries }) => {
+    const csvFileName = content.match(/loadTable\(['"]([^'"]*)['"]/)?.[1] || ''
+    const csvFile = entries[csvFileName] as ZipEntry
+
+    if (!csvFile) {
+      return content
+    }
+
+    const { key } = await uploadFile({
+      file: await csvFile.blob(),
+      fileName: csvFileName,
+      prefix: 'codeChecker',
+    })
+
+    await exponentialBackoff(() => getUpload(key)).catch(console.log)
+
+    // proxy fixes issue with cors
+    const url = getProxiedUrl(getObjectUrl(key))
+
+    return `loadTable("${url}", "csv", "header")`
+  },
+}
+
+const fileHandlers: Record<string, FileProcessingHandler[]> = {
+  [config.sketchFile]: [
+    loadTableHandler,
+  ],
+}
+
+const processFileHandler = async ({ content, entry, handler, entries }: { content: string, entry: ZipEntry, entries: Record<string, ZipEntry>, handler: FileProcessingHandler }): Promise<string> => {
+  const matches = handler.findAll(content)
+
+  if (!matches) return content
+
+  let updatedContent = content
+
+  for (const match of matches) {
+    const replacement = await handler.processInstance({
+      entry,
+      content: updatedContent,
+      entries,
+    })
+
+    updatedContent = updatedContent.replace(match, replacement)
+  }
+
+  return updatedContent
+}
+
+export const processFile = async ({
+  entry,
+  mimeType,
+  entries,
+  fileName,
+}: {
+  entry: ZipEntry
+  mimeType: string
+  entries: Record<string, ZipEntry>
+  fileName: string
+},
+): Promise<string> => {
+  let content = await entry.text()
+
+  for (const handler of fileHandlers[fileName]) {
+    if (handler.findAll(content)) {
+      content = await processFileHandler({ handler, content, entry, entries })
+    }
+  }
+
+  return URL.createObjectURL(new Blob([content], { type: mimeType }))
+}
+
+export const hasFileProcessing = (fileName: string): boolean => {
+  return fileHandlers[fileName] !== undefined
+}
