329826c3d56c283e83cef448161dab36550a1710
Preschian Febryantara <preschian@users.noreply.github.com>
2025-03-04 16:00:57 +0700

    feat(icons): add icon comparison page for NeoIcon and Nuxt Icon

---
+++ pages/how-to-icon.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="min-h-screen max-w-2xl">
+    <h1 class="text-2xl font-bold">
+      Icon comparison
+    </h1>
+
+    <div class="flex flex-col gap-2">
+      <div
+        v-for="size in sizes"
+        :key="size"
+        class="flex items-center gap-4"
+      >
+        <p>{{ size }} size:</p>
+        <NeoIcon
+          icon="circle-info"
+          :size="size"
+        />
+        <Icon
+          name="i-mdi:information-slab-circle-outline"
+          :class="sizeMap[size]"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { NeoIcon } from '@kodadot1/brick'
+
+const sizes = [
+  'default',
+  'small',
+  'medium',
+  'large',
+]
+
+const sizeMap = {
+  default: 'size-5',
+  small: 'size-4',
+  medium: 'size-6',
+  large: 'size-8',
+}
+</script>
