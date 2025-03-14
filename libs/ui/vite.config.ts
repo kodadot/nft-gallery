44dc4a6733ed3f61ad449cf466fcf0884a351071
Preschian Febryantara <preschian@users.noreply.github.com>
2025-03-14 15:23:43 +0700

    feat(ui): restructure build config and update dependencies
    This commit restructures the build configuration for the UI library by switching the build to Vite, updating script commands, and specifying module exports. It also updates multiple package dependencies to enhance compatibility and performance.

---
+++ libs/ui/vite.config.ts
@@ -0,0 +1,20 @@
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'KodaBrick',
+      fileName: 'koda-brick',
+    },
+    rollupOptions: {
+      external: ['vue', '@oruga-ui/oruga-next', 'bulma', '@vueuse/core', '@fortawesome/vue-fontawesome', 'three'],
+    },
+  },
+})
