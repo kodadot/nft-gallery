f49cb9b8bbea8fe51e740541efbe45576b036333
Preschian Febryantara <preschian@users.noreply.github.com>
2025-03-29 07:02:14 +0700

    refactor(icons): replace NeoIcon with KIcon and update icon names

--- components/items/ItemsGrid/ItemsGridImage.vue
+++ components/items/ItemsGrid/ItemsGridImage.vue
@@ -41,14 +41,13 @@
           class="p-1 !border-l-0"
           @click.prevent="onClickShoppingCart"
         >
-          <NeoIcon
+          <KIcon
             class="w-4 h-4"
-            :icon="
+            :name="
               shoppingCartStore.isItemInCart(nft.id)
-                ? 'fa-striked-out-cart-shopping'
-                : 'fa-shopping-cart-outline-sharp'
+                ? 'i-mdi:cart-off'
+                : 'i-mdi:cart'
             "
-            pack="fa-kit"
           />
         </NeoButton>
       </div>
@@ -76,7 +75,7 @@
 <script setup lang="ts">
 // PLEASE FIX bind-key href => to
 import { resolveComponent } from 'vue'
-import { NeoButton, NeoIcon } from '@kodadot1/brick'
+import { NeoButton } from '@kodadot1/brick'
 import useAtomicSwapAction from './useAtomicSwapAction'
 import type { NftCardVariant } from '@/components/shared/nftCard/types'
 import type { NFTWithMetadata } from '@/composables/useNft'
