3e944f2350be757b2e38f982b4bac00afae027b8
Jarsen <31397967+Jarsen136@users.noreply.github.com>
2025-04-22 16:23:20 +0200

    chore: remove session storage

--- stores/airdrop.ts
+++ stores/airdrop.ts
@@ -2,19 +2,12 @@ import { defineStore } from 'pinia'
 
 import type { ListCartItem } from '@/stores/listingCart'
 
-const sessionStorage = useStorage<ListCartItem[]>('airdrop', [], 'sessionStorage')
-
 export const useAirdropStore = defineStore('airdrop', () => {
   const {
     itemsInChain,
     setItem,
-    clear: clearItems,
-  } = useCart<ListCartItem>({ items: sessionStorage.value })
-
-  function clear() {
-    clearItems()
-    sessionStorage.value = []
-  }
+    clear,
+  } = useCart<ListCartItem>({ items: [] })
 
   return {
     itemsInChain,
