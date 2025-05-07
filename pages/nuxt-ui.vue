77258332d93f2aab90ebd6b24b3bb12a15160370
Preschian Febryantara <preschian@users.noreply.github.com>
2025-05-07 20:38:28 +0700

    refactor: remove NeoModalHead

---
+++ pages/nuxt-ui.vue
@@ -0,0 +1,15 @@
+<template>
+  <USlideover title="Slideover with title">
+    <UButton
+      label="Open"
+      color="neutral"
+      variant="subtle"
+    />
+
+    <template #body>
+      <div class="h-full">
+        asflsakdmfaslfkm
+      </div>
+    </template>
+  </USlideover>
+</template>
