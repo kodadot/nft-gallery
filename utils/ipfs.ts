6456bcd15a6783efef11209eaff576a874c8f52a
Tetiana Kukhelna <43496412+busikus@users.noreply.github.com>
2025-07-17 22:54:57 +0200

    add waifu generator

--- utils/ipfs.ts
+++ utils/ipfs.ts
@@ -114,7 +114,7 @@ export const sanitizeIpfsUrl = (
     return ''
   }
 
-  if (ipfsUrl.includes(kodaImage)) {
+  if (ipfsUrl.includes(kodaImage) || ipfsUrl.includes('kusama-waifus.vercel.app')) {
     return ipfsUrl
   }
 
